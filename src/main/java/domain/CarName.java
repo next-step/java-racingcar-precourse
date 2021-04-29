5855795daa97f0227b017dc03a5922c9d8e4e392
ohgillwhan <soora_genius@naver.com>
2021-04-29 22:34:55 +0900

    refact : CarName에 있는 매직넘버 제거

--- src/main/java/domain/CarName.java
+++ src/main/java/domain/CarName.java
@@ -1,11 +1,12 @@
 package domain;
 
 public class CarName {
+    private static final int CAR_NAME_MAX_LENGTH = 5;
     private String name;
 
     public CarName(String name) {
-        if(name.length() > 5) {
-            throw new IllegalArgumentException("자동차의 이름은 5글자를 넘어서는 안됩니다.");
+        if(name.length() > CAR_NAME_MAX_LENGTH) {
+            throw new IllegalArgumentException(String.format("자동차의 이름은 %d글자를 넘어서는 안됩니다.", CAR_NAME_MAX_LENGTH));
         }
 
         this.name = name;
