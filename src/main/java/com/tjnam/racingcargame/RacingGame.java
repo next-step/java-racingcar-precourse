de5e9bb636fa7254fe66a8fbe6d267276f05f4e2
TJ NAM <dynamicnam@gmail.com>
2021-04-30 20:49:19 +0900

    feat : 레이싱게임 스타트 함수 추가
    우선 더미 테스트 케이스 기반 테스트 성공,
    추가 로직 추가에 따라서 테스트 케이스도 변경
    TDD 적용

--- src/main/java/com/tjnam/racingcargame/RacingGame.java
+++ src/main/java/com/tjnam/racingcargame/RacingGame.java
@@ -1,5 +1,7 @@
 package com.tjnam.racingcargame;
 
 public class RacingGame {
-
+    public void start(){
+        System.out.println("Racing Game is started");
+    }
 }
--- src/test/java/com/tjnam/racingcargame/RacingGameTest.java
+++ src/test/java/com/tjnam/racingcargame/RacingGameTest.java
@@ -1,4 +1,21 @@
 package com.tjnam.racingcargame;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class RacingGameTest {
+
+    private static RacingGame racingGame;
+
+    @BeforeAll
+    public static void setUp(){
+        racingGame = new RacingGame();
+    }
+
+    @Test
+    public void startTest(){
+        racingGame.start();
+        return;
+    }
+
 }
