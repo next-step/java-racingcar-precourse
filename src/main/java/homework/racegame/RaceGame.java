08eebc8cc63aa8d9b5e984da980bee14dfc82f40
Junhyunny <kang3966@naver.com>
2021-05-01 00:17:35 +0900

    Feat: RaceGame 클래스 기능 추가
    다음과 같은 내용들이 추가되었습니다.
      - getCarNames - 사용자가 입력한 차량 이름을 배열로 만들어 반환
      - getTryCount - 사용자가 입력한 숫자를 반환
      - printWinners - 승리한 차량을 이용해 마지막 문자열 반환
      - run - 게임의 전체 흐름을 제어하는 메소드

---
+++ src/main/java/homework/racegame/RaceGame.java
@@ -0,0 +1,56 @@
+package homework.racegame;
+
+import homework.racegame.domain.Car;
+import homework.racegame.domain.CarGroup;
+import homework.racegame.util.ValidationChecker;
+import java.util.List;
+import java.util.Scanner;
+
+public class RaceGame {
+
+    String[] getCarNames(Scanner scanner) {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] splitedCarNames;
+        do {
+            String carNames = scanner.next();
+            splitedCarNames = carNames.split(",");
+        } while (!ValidationChecker.isValidCarName(splitedCarNames));
+        return splitedCarNames;
+    }
+
+    int getTryCount(Scanner scanner) {
+        System.out.println("시도할 횟수는 몇회인가요?");
+        String strTryCount;
+        do {
+            strTryCount = scanner.next();
+        } while (!ValidationChecker.isValidTryCount(strTryCount));
+        return Integer.valueOf(strTryCount);
+    }
+
+    void printWinners(List<Car> winners) {
+        if (winners == null || winners.isEmpty()) {
+            System.out.println("우승자가 없습니다.");
+            return;
+        }
+        StringBuffer stringBuffer = new StringBuffer();
+        for (Car winner : winners) {
+            stringBuffer.append(winner.getName()).append(", ");
+        }
+        stringBuffer.delete(stringBuffer.length() - 2, stringBuffer.length());
+        stringBuffer.append("가 최종 우승했습니다.");
+        System.out.println(stringBuffer);
+    }
+
+    public void run() {
+        Scanner scanner = new Scanner(System.in);
+        String[] carNames = getCarNames(scanner);
+        int tryCount = getTryCount(scanner);
+        CarGroup carGroup = new CarGroup(carNames);
+        for (int index = 0; index < tryCount; index++) {
+            carGroup.moveOrStopCars();
+            carGroup.printCarsProgress();
+        }
+        printWinners(carGroup.getWinners());
+        scanner.close();
+    }
+}
