e6bf117054ed4c74771c13ed75f54f76f7505a46
Junhyunny <kang3966@naver.com>
2021-04-30 17:21:11 +0900

    Feat: CarGroup 클래스 기능 추가
    다음과 같은 기능이 추가되었습니다.
      - CarGroup  생성자 - 차량 리스트를 받아 setting 할 수 있는 생성자
      - getMaximumMoveProgressValueInGroup 메소드 - CarGroup 객체가 관리하는 차량 중 가장 먼 거리를 이동한 차량의 이동거리
      - setCarIntoListCheckWithPrediate 메소드 - Predicate 조건에 만족하는 차량만 매개변수로 전달한 리스트에 담는 메소드
      - getWinners 메소드 - 승리한 차량들을 반환하는 메소드

--- src/main/java/homework/racegame/domain/CarGroup.java
+++ src/main/java/homework/racegame/domain/CarGroup.java
@@ -1,7 +1,9 @@
 package homework.racegame.domain;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Predicate;
 
 public class CarGroup {
 
@@ -11,6 +13,24 @@ public class CarGroup {
         cars = new ArrayList<>();
         for (String carName : carNames) {
             cars.add(new Car(carName));
+        }
+    }
+
+    public CarGroup(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    int getMaximumMoveProgressValueInGroup() {
+        List<Car> temp = new ArrayList<>(cars);
+        Collections.sort(temp,
+            (car1, car2) -> (car1.getMoveProgressValue() - car2.getMoveProgressValue()) * -1
+        );
+        return temp.get(0).getMoveProgressValue();
+    }
+
+    void setCarIntoListCheckWithPredicate(Car car, List<Car> carList, Predicate<Car> predicate) {
+        if (predicate.test(car)) {
+            carList.add(car);
         }
     }
 
@@ -24,6 +44,18 @@ public class CarGroup {
         for (Car car : cars) {
             car.printMoveProgress();
         }
+        System.out.println();
+    }
+
+    public List<Car> getWinners() {
+        int size = cars.size();
+        int maximumValue = getMaximumMoveProgressValueInGroup();
+        List<Car> result = new ArrayList<>();
+        Predicate<Car> predicate = car -> car.getMoveProgressValue() >= maximumValue;
+        for (int index = 0; index < size; index++) {
+            setCarIntoListCheckWithPredicate(cars.get(index), result, predicate);
+        }
+        return result;
     }
 
     @Override
