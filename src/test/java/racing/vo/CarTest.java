407a2c651ebeddc1c7e222c2e1204244e137ac43
leemingyu09 <hikakaka13@naver.com>
2021-05-01 01:10:14 +0900

    test: import 부분 수정

--- src/test/java/racing/vo/CarTest.java
+++ src/test/java/racing/vo/CarTest.java
@@ -8,8 +8,7 @@ import org.junit.jupiter.api.DisplayName;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CarTest {
 
