30ce456431c2afcbe1616899f7d3aad549c20370
TJ NAM <dynamicnam@gmail.com>
2021-04-30 23:52:57 +0900

    feat : 사용자 입력의 split 구현

--- src/main/java/com/tjnam/racingcargame/CarNameInputGetter.java
+++ src/main/java/com/tjnam/racingcargame/CarNameInputGetter.java
@@ -1,5 +1,34 @@
 package com.tjnam.racingcargame;
 
-public class CarNameInputGetter {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class CarNameInputGetter {
+    private static Scanner carNameScanner = new Scanner(System.in);
+    private String userInput;
+
+    public List<CarName> getCarNameInput(){
+        String userInput = inputCarNames();
+        String[] carNamesStr = splitUserInput(userInput);
+        List<CarName> carNames = convertStringToCarName(carNamesStr);
+        return carNames;
+    }
+
+    private String inputCarNames(){
+        String userInput = carNameScanner.nextLine();
+        return userInput;
+    }
+
+    private Boolean validateUserInput(String userInput){
+        return true;
+    }
+
+    private String[] splitUserInput(String userInput){
+        return userInput.split(",");
+    }
+
+    private List<CarName> convertStringToCarName(String[] carNameInputs){
+
+    }
 }
