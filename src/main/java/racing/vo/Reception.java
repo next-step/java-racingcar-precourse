c55db94c9efbc1f0396e9c0f38e49fbe06eb468a
leemingyu09 <hikakaka13@naver.com>
2021-05-01 01:11:21 +0900

    feat : 자동차 이름을 매개변수로 받아서 리스트에 저장       - (,)를 구분자로 리스트에 저장

---
+++ src/main/java/racing/vo/Reception.java
@@ -0,0 +1,25 @@
+package racing.vo;
+import java.util.List;
+
+public class Reception {
+
+    private final List<Car> participants;
+
+    public Reception(List<Car> participants){
+        this.participants = participants;
+    }
+
+    public List<Car> getReceptionList(){
+        return participants;
+    }
+
+    public void register(String names) {
+        String[] namesArray;
+        namesArray = names.split(",");
+        for(String name:namesArray){
+            Car car = new Car(name);
+            participants.add(car);
+        }
+    }
+
+}
