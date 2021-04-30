8deebc0b02a418646bf45229691bb20da1fb5e19
TJ NAM <dynamicnam@gmail.com>
2021-04-30 20:37:09 +0900

    feat : main 함수의 초기동작 정의
    레이싱 게임 스타트 동작 정의

--- src/main/java/com/tjnam/racingcargame/Main.java
+++ src/main/java/com/tjnam/racingcargame/Main.java
@@ -2,6 +2,7 @@ package com.tjnam.racingcargame;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("RacingGame Start");
+        RacingGame racingGame = new RacingGame();
+        racingGame.start();
     }
 }
