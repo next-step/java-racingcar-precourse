84132c6e0441206d1ebb26b1734604a6fd8d7298
TJ NAM <dynamicnam@gmail.com>
2021-04-30 23:31:47 +0900

    test : 사용자 입력의 split을 수행
    테스트 케이스 정의

--- src/test/java/com/tjnam/racingcargame/CarNameInputGetterTest.java
+++ src/test/java/com/tjnam/racingcargame/CarNameInputGetterTest.java
@@ -1,5 +1,41 @@
 package com.tjnam.racingcargame;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CarNameInputGetterTest {
+
+    private static CarNameInputGetter carNameInputGetter;
+    private String[] splitedUserInputCarName;
+
+    @BeforeAll
+    public static void setUp(){
+        carNameInputGetter = new CarNameInputGetter();
+    }
+
+    @Test
+    public void splitUserInputTest(){
+        try {
+            Method splitUserInputTestMethod = carNameInputGetter.getClass().getDeclaredMethod("splitUserInput", String.class);
+            splitUserInputTestMethod.setAccessible(true);
+            String userInputData = "abc,abcd,abcde,abcdef,abcdefg";
+
+            splitedUserInputCarName = (String[]) splitUserInputTestMethod.invoke(carNameInputGetter, userInputData);
+            assertThat(splitedUserInputCarName[0]).isEqualTo("abc");
+            assertThat(splitedUserInputCarName[1]).isEqualTo("abcd");
+            assertThat(splitedUserInputCarName[2]).isEqualTo("abcde");
+            assertThat(splitedUserInputCarName[3]).isEqualTo("abcdef");
+            assertThat(splitedUserInputCarName[4]).isEqualTo("abcdefg");
+            assertThat(splitedUserInputCarName.length).isEqualTo(5);
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
