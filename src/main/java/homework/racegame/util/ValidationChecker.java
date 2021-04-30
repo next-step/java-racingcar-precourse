1b88c2b320a0feedc71b16349184629d5b93033f
Junhyunny <kang3966@naver.com>
2021-05-01 00:14:54 +0900

    Fix: ValidationChecker 클래스 사용자 입력 관련 버그 수정
    다음과 같은 내용이 수정되었습니다.
      - isValidTryCount - 사용자가 빈 문자열을 입력하는 경우 에러 체크
      - isValidCarName - 잘못된 입력이 있는 경우 에러 로그 출력

--- src/main/java/homework/racegame/util/ValidationChecker.java
+++ src/main/java/homework/racegame/util/ValidationChecker.java
@@ -5,7 +5,7 @@ public class ValidationChecker {
     public static boolean isValidTryCount(String times) {
         int length = times.length();
         String replace = times.replaceAll("[^0-9]", "");
-        if (length == replace.length()) {
+        if (length != 0 && length == replace.length()) {
             return true;
         }
         System.out.println("숫자만 입력하세요.");
@@ -16,6 +16,9 @@ public class ValidationChecker {
         boolean result = true;
         for (int index = 0; index < carNames.length; index++) {
             result = result & carNames[index].length() <= 5;
+        }
+        if (!result) {
+            System.out.println("이름이 5자리가 넘어가는 차 이름이 존재합니다. 다시 입력해주세요.");
         }
         return result;
     }
