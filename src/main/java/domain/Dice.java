0609c37c37804e662f2aeaf49d00377e9e9a4dbd
sejinpark <3jin.pk@gmail.com>
2021-04-30 07:27:46 -0700

    feat : 1 ~ 9 사이 랜덤한 수를 생성하는 클래스 생성

---
+++ src/main/java/domain/Dice.java
@@ -0,0 +1,22 @@
+package domain;
+
+import java.util.Random;
+
+public class Dice {
+    public static final int MAX_VALUE = 9;
+    public static final int MIN_VALUE = 1;
+    private int ticks;
+
+    public static Dice roll() {
+        return new Dice();
+    }
+
+    private Dice() {
+        Random random = new Random();
+        this.ticks = random.nextInt(MAX_VALUE)+ MIN_VALUE;
+    }
+
+    public int getTicks() {
+        return ticks;
+    }
+}
---
+++ src/test/java/domain/DiceTest.java
@@ -0,0 +1,16 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DiceTest {
+
+    @RepeatedTest(20)
+    @DisplayName("랜덤 수 생성 주사위가 지정한 최솟값과 최댓값 사이의 값만을 반환하는지 테스트")
+    public void dice_ShouldReturn_LessThanMin_GreaterThanMax() {
+        Dice dice = Dice.roll();
+        assertThat(dice.getTicks()).isLessThanOrEqualTo(Dice.MAX_VALUE).isGreaterThanOrEqualTo(Dice.MIN_VALUE);
+    }
+}
