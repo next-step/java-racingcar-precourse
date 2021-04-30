f0a26b122c8f31033c777cb52fb0ac7ae4e3c833
jeeyongpark <jeeyong.park@homeplus.co.kr>
2021-04-30 23:43:13 +0900

    feat : 메인 메서드 구현
    Application이 실행될때 처음 시작하는 메인 메서드 구현

---
+++ src/main/java/racing/Application.java
@@ -0,0 +1,10 @@
+package racing;
+
+public class Application {
+    
+    public static void main(String[] args) {
+        RacingGame racingGame = new RacingGame();
+        racingGame.gameStart();
+    }
+    
+}
