1aeae91f370edf2cb2cb19ccb869f2424b4ff1ed
TJ NAM <dynamicnam@gmail.com>
2021-04-30 20:35:29 +0900

    feat : 추가 정의한 클래스 추가
    - 메인클래스
    - 사용자 이름 Inputer 클래스 추가

---
+++ src/main/java/com/tjnam/racingcargame/CarNameInputer.java
@@ -0,0 +1,4 @@
+package com.tjnam.racingcargame;
+
+public class CarNameInputer {
+}
---
+++ src/main/java/com/tjnam/racingcargame/Main.java
@@ -0,0 +1,7 @@
+package com.tjnam.racingcargame;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("RacingGame Start");
+    }
+}
