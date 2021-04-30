f26436e2e28502db8db794a03e8ef58172aa13fd
jeeyongpark <jeeyong.park@homeplus.co.kr>
2021-04-30 23:42:36 +0900

    feat : 자동차 관련 객체 구현 (원시값 포장+일급컬렉션)
    원시값 포장 / 일급 컬렉션 구현

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -1,9 +1,19 @@
 package racing;
 
 public class Car {
-    
+
+    public static final String CAR_NAME_LENGTH_LIMIT_MESSAGE = "자동차 이름은 5글자까지만 가능합니다.";
+    public static final int NAME_LENGTH_LIMIT = 5;
+
     private String name;
     private int position = 0;
+    
+    public Car(String name) {
+        if (isNameLengthExcessLimit(name)) {
+            throw new IllegalArgumentException(CAR_NAME_LENGTH_LIMIT_MESSAGE);
+        }
+        this.name = name;
+    }
     
     public Car(String name, int position) {
         this.name = name;
@@ -14,7 +24,15 @@ public class Car {
         return position; 
     }
     
-    public void move(Racing racing) {
+    public void move() {
         position++;
+    }
+    
+    public String getCarName() {
+        return name;
+    }
+    
+    private boolean isNameLengthExcessLimit(String name) {
+        return name.length() > NAME_LENGTH_LIMIT;
     }
 }
---
+++ src/main/java/racing/Cars.java
@@ -0,0 +1,24 @@
+package racing;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cars {
+    
+    private static List<Car> cars;
+    
+    public Cars(String userInput) {
+        this.cars = carList(userInput);
+    }
+    
+    public List<Car> carList(String userInput) {
+        List<Car> cars = new ArrayList<>();
+        String[] carNameArray = userInput.split(",");
+        
+        for (int i = 0; i < carNameArray.length; i++) {
+            cars.add(new Car(carNameArray[i]));
+        }
+        
+        return cars;
+    }
+}
