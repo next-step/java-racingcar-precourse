d55a05831d19339e93afdb3e24431323bbaca159
최홍희 <vvshinevv@nhntoast.com>
2021-05-01 17:33:30 +0900

    fix : 멤버변수 선언 위치 수정
    Car 클래스
     - static final 변수 상위로 이동

--- src/main/java/com/racingcar/domain/Car.java
+++ src/main/java/com/racingcar/domain/Car.java
@@ -2,14 +2,14 @@ package com.racingcar.domain;
 
 public class Car {
 
+    public static final Integer MIN_CONDITION_OF_CAR_MOVING = 0;
+    public static final Integer MAX_CONDITION_OF_CAR_MOVING = 3;
+
     private final CarName carName;
 
     public Car(CarName carName) {
         this.carName = carName;
     }
-
-    public static final Integer MIN_CONDITION_OF_CAR_MOVING = 0;
-    public static final Integer MAX_CONDITION_OF_CAR_MOVING = 3;
 
     public Boolean isCarMoving(Integer condition) {
         return condition >= MIN_CONDITION_OF_CAR_MOVING && condition <= MAX_CONDITION_OF_CAR_MOVING ?
