fbaec0a5946fa1c752e97cc348501b17b13ca091
ParkJiwoon <bcp0109@gmail.com>
2021-05-01 16:31:18 +0900

    test: CarNameTest 코드 리팩토링

--- src/test/java/model/CarNameTest.java
+++ src/test/java/model/CarNameTest.java
@@ -1,24 +1,22 @@
 package model;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 public class CarNameTest {
 
-    @Test
     @DisplayName("5자 이하의 자동차 이름 정상적으로 생성")
+    @Test
     void testConstructor() {
         String name = "abcde";
         CarName carName = new CarName(name);
-        Assertions.assertThat(carName.getName()).isEqualTo(name);
+        assertThat(carName.getName()).isEqualTo(name);
     }
 
-    @Test
     @DisplayName("자동차 이름은 최대 5자")
+    @Test
     void testValidateCarNameSize() {
         assertThatThrownBy(() -> new CarName("abcdef"))
                 .isInstanceOf(IllegalArgumentException.class)
