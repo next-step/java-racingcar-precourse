c8a74f6b776d05f3053f3f1c7630a94107e2f243
songseongbin <ppsosqq@gmail.com>
2021-05-01 16:49:59 +0900

    refactor(Name): 정적 생성 함수 추가
    테스트 수정

--- src/main/java/com/racingcar/Name.java
+++ src/main/java/com/racingcar/Name.java
@@ -5,10 +5,14 @@ public class Name {
 
 	private String value;
 
-	public Name(String value) {
+	private Name(String value) {
 		if (value.length() > MAX_LENGTH) {
 			throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
 		}
 		this.value = value;
+	}
+
+	public static Name of(String name) {
+		return new Name(name);
 	}
 }
--- src/test/java/com/racingcar/NameTest.java
+++ src/test/java/com/racingcar/NameTest.java
@@ -10,13 +10,13 @@ public class NameTest {
 	@Test
 	void 이름_5자_넘는_경우_예외_발생() {
 		assertThatIllegalArgumentException()
-			.isThrownBy(() -> new Name("123456"))
+			.isThrownBy(() -> Name.of("123456"))
 			.withMessage("이름은 5자 이하만 가능합니다.");
 	}
 
 	@ParameterizedTest
 	@ValueSource(strings = {"", "1", "12", "123", "1234", "12345"})
 	void 이름_길이가_0_5_사이인_경우_생성_가능(String input) {
-		assertThatNoException().isThrownBy(() -> new Name(input));
+		assertThatNoException().isThrownBy(() -> Name.of(input));
 	}
 }
