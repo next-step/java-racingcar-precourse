3c3ce60a2a32e664a588f55a3b070f50972cdc40
Kim Dae Hee <anyozcall@gmail.com>
2021-05-01 20:08:12 +0900

    refactor : Car 초기화 시 객체 할당 부분 refactor
    - Car 객체 작업시 전진 횟수 초기화 배열값 할당
    - 전체 Car 갯수 할당 부분 확인

--- src/main/java/util/Car.java
+++ src/main/java/util/Car.java
@@ -5,19 +5,25 @@ public class Car {
     private int[] carProcessList;
 
     public void initCarList(int indexLength){
+        carList = new String[indexLength];
         for(int i=0;i<indexLength;i++){
             carList[i] = "";
         }
     }
 
     public void initCarProcessList(int indexLength){
+        carProcessList = new int[indexLength];
         for(int i=0;i<indexLength;i++){
             carProcessList[i] = 0;
         }
     }
 
     public void setCarList(String carString, int index){
-        carList[index] = carString;
+       if(carString.length() < 6) {
+           carList[index] = carString;
+       } else{
+         throw new AssertionError("문자 자릿수 초과로 인한 Exception");
+       }
     }
 
     public String getCarList(int index){
