7d02674958261d62f4ecb01bea7c94ff3f3188ef
ohgillwhan <soora_genius@naver.com>
2021-05-01 20:51:27 +0900

    refact : RandomMoveStrategy에 있는 매직넘버 제거

--- src/main/java/domain/strategy/RandomMoveStrategy.java
+++ src/main/java/domain/strategy/RandomMoveStrategy.java
@@ -3,10 +3,13 @@ package domain.strategy;
 import java.util.Random;
 
 public class RandomMoveStrategy implements MoveStrategy{
+    private static final int MAX_RANDOM_NUMBER = 10;
+    private static final int RANDOM_BIGGER_THAN = 4;
+
     @Override
     public boolean movable() {
         Random random = new Random();
 
-        return random.nextInt(10) >= 4;
+        return random.nextInt(MAX_RANDOM_NUMBER) >= RANDOM_BIGGER_THAN;
     }
 }
