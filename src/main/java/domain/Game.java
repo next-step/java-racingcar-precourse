16137bdf53551594205946f8d3a3fd8521cb2d7a
ohgillwhan <soora_genius@naver.com>
2021-05-01 15:46:53 +0900

    feat : 게임은 움직임을 할 때 마다 현재 위치 계속 갖고 있는다.

--- src/main/java/domain/Game.java
+++ src/main/java/domain/Game.java
@@ -3,8 +3,12 @@ package domain;
 import domain.strategy.MoveStrategy;
 import domain.strategy.RandomMoveStrategy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     private Cars cars;
+    private List<Positions> moveHistories = new ArrayList<>();
 
     public Game(CarNames carNames) {
         this(carNames, new RandomMoveStrategy());
@@ -12,5 +16,16 @@ public class Game {
 
     public Game(CarNames carNames, MoveStrategy moveStrategy) {
         this.cars = new Cars(carNames, moveStrategy);
+    }
+
+    public void move(int times) {
+        for(int i = 0; i<times; i++) {
+            cars.move();
+            moveHistories.add(cars.getPositions());
+        }
+    }
+
+    public List<Positions> getMoveHistories() {
+        return new ArrayList<>(moveHistories);
     }
 }
---
+++ src/test/java/domain/GameTest.java
@@ -0,0 +1,30 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameTest {
+    @Test
+    @DisplayName("움직임을 할 때 마다 현재 위치 계속 갖고 있는다.")
+    public void 움직임을_할_때_마다_현재_위치_계속_갖고_있는다() {
+        int times = 5;
+        CarNames carNames = new CarNames("a,b,c");
+        Game game = new Game(carNames, () -> true);
+
+        game.move(times);
+
+        List<Positions> moveHistories = game.getMoveHistories();
+
+        for(int i = 1; i<=times; i++) {
+            Positions positions = moveHistories.get(i - 1);
+            for(Position position : positions.getPositions()) {
+                assertThat(position.getPosition())
+                        .isEqualTo(i);
+            }
+        }
+    }
+}
