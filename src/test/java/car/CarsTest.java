195d94595eb90c67b0612a392fd5eb3df9a9cb84
ChangSubKwak <paradise1732@gmail.com>
2021-05-01 21:14:24 +0900

    fix: 기능 테스트 수정(Cars를 생성할 때 유효성 테스트 함에 따름)

--- src/test/java/car/CarsTest.java
+++ src/test/java/car/CarsTest.java
@@ -2,9 +2,6 @@ package car;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
-
 import org.junit.jupiter.api.Test;
 
 import util.GameUtil;
@@ -12,20 +9,9 @@ import util.GameUtil;
 public class CarsTest {
 	@Test
 	public void validateCarNameLengthTest() throws Exception {
-		// given
-		Cars cars = new Cars(GameUtil.createCars("pobi123,crong,honux"));
-
-		// when
-		Method method = cars.getClass().getDeclaredMethod("validateCarNameLength");
-		method.setAccessible(true);
-
-		// then
-		try {
-			method.invoke(cars);
-		} catch (InvocationTargetException e) {
-			assertThat(e.getCause()).isInstanceOf(IllegalArgumentException.class)
-				.hasMessageContaining("자동차의 이름은 5자 이내로 가능합니다.");
-		}
+		assertThatThrownBy(() -> {
+			Cars cars = new Cars(GameUtil.createCars("pobi123,crong,honux"));        // given, when
+		}).isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("자동차의 이름은 5자 이내로 가능합니다.");                    // then
 	}
-
 }
