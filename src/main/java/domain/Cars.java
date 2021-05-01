578dc2cbc424d033ff0d2f5357dc39de81e8693d
ohgillwhan <soora_genius@naver.com>
2021-05-01 15:11:04 +0900

    feat : Cars생성시 이름으로도 생성이 가능하도록 생성자 추가

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -1,10 +1,20 @@
 package domain;
 
+import domain.strategy.MoveStrategy;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
     private List<Car> cars;
+
+    public Cars(CarNames carNames, MoveStrategy moveStrategy) {
+        this.cars = new ArrayList<>();
+
+        for(CarName carName : carNames.getNames()) {
+            this.cars.add(new Car(carName, moveStrategy));
+        }
+    }
 
     public Cars(List<Car> cars) {
         this.cars = cars;
