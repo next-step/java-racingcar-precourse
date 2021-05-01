9037eacda473e90331d0e305b59dc0ae5bb14ea0
leemingyu09 <hikakaka13@naver.com>
2021-05-01 16:20:44 +0900

    feat : 자동차 이동 거리 랜덤 숫자 생성 Car 객체안에 랜덤 함수로 설정하게 구현

--- src/main/java/racing/vo/Car.java
+++ src/main/java/racing/vo/Car.java
@@ -1,10 +1,9 @@
 package racing.vo;
 
-import java.math.BigDecimal;
-
 public class Car {
     private CarName carName;
-    private int position = 0;
+    private int carPosition;
+    private static final int maxCount = 9;
 
     public Car(String name) {
         this.carName = new CarName(name);
@@ -14,4 +13,15 @@ public class Car {
         return this.carName.getValue();
     }
 
+    public int randomSetPosition(){
+        int randomNo =  (int)(Math.random()*maxCount);
+        if(randomNo>=4){
+            carPosition++;
+        }
+        return randomNo;
+    }
+
+    public int getPosition() {
+        return carPosition;
+    }
 }
