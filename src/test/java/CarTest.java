266f417a1ddb7bee4566dd05b613bb67466ea3b7
DESKTOP-HFHRDO6\heewon <ccc4804@gmail.com>
2021-05-01 21:13:19 +0900

    test: car 전진 확인 테스트 추가

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,3 +1,17 @@
-public class CarTest {
+import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+    @Test
+    @DisplayName("Car_전진_테스트")
+    void carMoveTest() {
+        Car car = new Car("test");
+        car.setCarStatus(CarStatus.MOVE);
+        assertThat(car.getMoveCount().getCount()).isEqualTo(1);
+
+        car.setCarStatus(CarStatus.STOP);
+        assertThat(car.getMoveCount().getCount()).isEqualTo(1);
+    }
 }
