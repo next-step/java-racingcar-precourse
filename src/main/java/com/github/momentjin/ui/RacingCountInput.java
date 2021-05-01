83eb47ab5a3bb28704d1353f36be45a6e3579d5c
momentjin <momentjin@gmail.com>
2021-05-01 23:32:25 +0900

    chore: 객체 이름 수정 (RacingNameInput -> RacingCountInput)
    횟수 count로 일관성있게 사용하고자 변경합니다!

--- src/main/java/com/github/momentjin/ui/RacingCountInput.java
+++ src/main/java/com/github/momentjin/ui/RacingCountInput.java
@@ -2,7 +2,7 @@ package com.github.momentjin.ui;
 
 import java.util.Scanner;
 
-public class RacingNumberInput {
+public class RacingCountInput {
 
     public int execute(Scanner sc) {
         System.out.println("시도할 회수는 몇 회인가요?");
