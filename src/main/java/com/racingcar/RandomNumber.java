6619329dfecdab30aadec3b470c32e034a4305c3
songseongbin <ppsosqq@gmail.com>
2021-05-01 15:33:35 +0900

    fix(RandomNumber): 0-9사이의 숫자인지 유효성 검증 추가
    테스트 케이스 추가

--- src/main/java/com/racingcar/RandomNumber.java
+++ src/main/java/com/racingcar/RandomNumber.java
@@ -1,9 +1,15 @@
 package com.racingcar;
 
 public class RandomNumber {
+	private static final int MIN = 0;
+	private static final int MAX = 9;
+
 	private int value;
 
-	private RandomNumber(int value) {
+	RandomNumber(int value) {
+		if (value < MIN || value > MAX) {
+			throw new IllegalArgumentException("랜덤 숫자는 0에서 9사이의 숫자여야 한다.");
+		}
 		this.value = value;
 	}
 
--- src/test/java/com/racingcar/RandomNumberTest.java
+++ src/test/java/com/racingcar/RandomNumberTest.java
@@ -2,7 +2,10 @@ package com.racingcar;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class RandomNumberTest {
 
@@ -13,5 +16,20 @@ public class RandomNumberTest {
 		RandomNumber randomNumber = RandomNumber.create();
 
 		assertThat(randomNumber.getValue()).isBetween(0, 9);
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {-1, 10})
+	@DisplayName("생성된 숫자가 0-9사이의 숫자가 아닌 경우 예외 발생")
+	void 랜덤_숫자_0보다_작은_수_생성시_예외_테스트(Integer input) {
+		assertThatExceptionOfType(IllegalArgumentException.class)
+			.isThrownBy(() -> new RandomNumber(input));
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {0, 9})
+	@DisplayName("랜덤 숫자 경계값 테스트")
+	void 랜덤_숫자_경계값_테스트(Integer input) {
+		assertThatNoException().isThrownBy(() -> new RandomNumber(input));
 	}
 }
