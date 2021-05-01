8dac92687e7255658a3cda225fe04a5dc06e644d
전계원 <tonyjev93@gmail.com>
2021-05-02 00:50:56 +0900

    feat: add Util - InputUtil

---
+++ src/main/java/com/game/utils/InputUtil.java
@@ -0,0 +1,11 @@
+package com.game.utils;
+
+import java.util.Scanner;
+
+public class InputUtil {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static String input() {
+        return scanner.next();
+    }
+}
