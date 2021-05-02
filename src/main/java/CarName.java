e584c6d147a2f127a41470dfa0de3ec0068ee875
praivesi <praivesi@gmail.com>
2021-05-02 18:35:11 +0900

    refactor: move printing validation result message into CarName class

--- src/main/java/CarName.java
+++ src/main/java/CarName.java
@@ -1,5 +1,6 @@
 public class CarName {
     private static final String CAR_NAME_CONDITION_EXCEPTION_MSG = "자동차 이름은 5글자 이하이고 비어있지 않아야 합니다.";
+    private static final String CAR_NAME_CONDITION_CONSOLE_MSG = CAR_NAME_CONDITION_EXCEPTION_MSG + "(자동차 이름: %s)";
     private static final int NAME_MIN_LENGTH = 1;
     private static final int NAME_MAX_LENGTH = 5;
 
@@ -9,6 +10,9 @@ public class CarName {
         String trimmedName = name.trim();
 
         if (trimmedName.length() < NAME_MIN_LENGTH || trimmedName.length() > NAME_MAX_LENGTH) {
+            this.name = "";
+
+            System.out.println(String.format(CAR_NAME_CONDITION_CONSOLE_MSG, name));
             throw new IllegalArgumentException(CAR_NAME_CONDITION_EXCEPTION_MSG);
         }
 
