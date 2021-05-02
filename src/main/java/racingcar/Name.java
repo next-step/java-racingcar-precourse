35b8053006cba683a2f0f941e2132bb07e98b478
Min Kim <=>
2021-05-02 15:32:49 +0900

    refactor: 자동차 이름 값 객체로 활용하기 위한 erquals(), hashCode() 오버라이딩

--- src/main/java/racingcar/Name.java
+++ src/main/java/racingcar/Name.java
@@ -13,4 +13,19 @@ public class Name {
         if (name.length() > 5)
             throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다.");
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Name name1 = (Name) o;
+
+        return name.equals(name1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
 }
