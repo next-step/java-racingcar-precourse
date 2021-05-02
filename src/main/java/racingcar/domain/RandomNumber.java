8b0b81edd59e09c07b65abf1793062254826a015
kslee <meant0415@gmail.com>
2021-05-02 15:19:19 +0900

    test: 0~9 사이 랜덤코드 생성 Wrapper 테스트

--- src/main/java/racingcar/domain/RandomNumber.java
+++ src/main/java/racingcar/domain/RandomNumber.java
@@ -1,2 +1,17 @@
-package racingcar.domain;public class RandomNumber {
+package racingcar.domain;
+
+import java.util.Random;
+
+public class RandomNumber {
+    private static final int LESS_THAN_NUMBER = 10;
+
+    private final int number;
+
+    public RandomNumber() {
+        this.number = new Random().nextInt(LESS_THAN_NUMBER);
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
 }
--- src/test/java/racingcar/domain/RandomNumberTest.java
+++ src/test/java/racingcar/domain/RandomNumberTest.java
@@ -1,2 +1,18 @@
-package racingcar.domain;public class RandomNumberTest {
+package racingcar.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomNumberTest {
+
+    @Test
+    void 랜덤_0_9_사이_숫자_생성() {
+        RandomNumber randomNumber = new RandomNumber();
+
+        for (int i = 0; i < 100000; i++) {
+            assertThat(randomNumber.getNumber()).isLessThan(10).isGreaterThan(-1);
+        }
+    }
+
 }
