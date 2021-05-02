435cb19a2ec43bf96fe339a17909851a4f4d13e6
ParkJiwoon <bcp0109@gmail.com>
2021-05-03 02:44:42 +0900

    feat: Application 에서 RacingCarGame 시작 추가

--- src/main/java/RacingCarApplication.java
+++ src/main/java/RacingCarApplication.java
@@ -1,5 +1,6 @@
 public class RacingCarApplication {
     public static void main(String[] args) {
-
+        RacingCarGame racingCarGame = new RacingCarGame();
+        racingCarGame.start();
     }
 }
