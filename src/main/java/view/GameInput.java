8b96987a0162514503452826daef7bf81b527071
Min Kim <=>
2021-05-02 18:18:44 +0900

    feat: 사용자에게 시도 횟수를 입력받는 기능 추가

--- src/main/java/view/GameInput.java
+++ src/main/java/view/GameInput.java
@@ -8,15 +8,37 @@ public class GameInput {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String NAME_SPLIT_REGEX = ",";
 
-    public static Set<Name> inputCarNames() {
+    public static Set<Name> enterCarNames() {
         String[] carNames = scanner.next().split(NAME_SPLIT_REGEX);
         try {
             validCountCars(carNames);
             return convertNames(carNames);
         } catch (IllegalArgumentException e) {
             GameOutput.printEnterAgain(e);
-            return inputCarNames();
+            return enterCarNames();
         }
+    }
+
+    public static int enterAttemptCount() {
+        try {
+            int attemptCount = scanner.nextInt();
+            validAttemptCount(attemptCount);
+            return attemptCount;
+        } catch (InputMismatchException | IllegalArgumentException e) {
+            emptyScanner(e);
+            GameOutput.printReEnterAttemptCount();
+            return enterAttemptCount();
+        }
+    }
+
+    private static void validAttemptCount(int attemptCount) {
+        if (attemptCount < 1)
+            throw new IllegalArgumentException();
+    }
+
+    private static void emptyScanner(Exception e) {
+        if (e instanceof InputMismatchException)
+            scanner.next();
     }
 
     private static Set<Name> convertNames(String[] carNames) {
@@ -30,5 +52,4 @@ public class GameInput {
         if (carNames.length < 2)
             throw new IllegalArgumentException("경주 자동차는 2대 이상이어야 합니다.");
     }
-
 }
--- src/main/java/view/GameOutput.java
+++ src/main/java/view/GameOutput.java
@@ -2,13 +2,14 @@ package view;
 
 public class GameOutput {
 
-    private static final String INSERT_CAR_NAME = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    private static final String ENTER_CAR_NAME = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
     private static final String ATTEMPT_COUNT = "시도할 횟수는 몇 회인가요?";
     private static final String GAME_RESULT_COMMENT = "실행 결과";
     private static final String ENTER_AGAIN = " 다시 입력해 주세요.";
+    private static final String ATTEMPT_COUNT_ENTER_POSITIVE = "시도 횟수는 1 이상의 숫자로 입력해 주세요.";
 
-    public static void printInsertCarName() {
-        System.out.println(INSERT_CAR_NAME);
+    public static void printEnterCarNames() {
+        System.out.println(ENTER_CAR_NAME);
     }
 
     public static void printEnterAgain(Exception e) {
@@ -17,6 +18,10 @@ public class GameOutput {
 
     public static void printAttemptCount() {
         System.out.println(ATTEMPT_COUNT);
+    }
+
+    public static void printReEnterAttemptCount() {
+        System.out.println(ATTEMPT_COUNT_ENTER_POSITIVE);
     }
 
 }
