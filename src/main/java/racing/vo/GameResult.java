91580f50f9276d0600638e8db9823bc14f9b86c4
leemingyu09 <hikakaka13@naver.com>
2021-05-02 17:42:48 +0900

    feat : 우승자 출력기능        가장 멀리 이동한 자동차객체를 기준으로 비교        가장 멀리 이동한 거리와 같으면 우승자 목록에 추가        그외에는 패스

---
+++ src/main/java/racing/vo/GameResult.java
@@ -0,0 +1,29 @@
+package racing.vo;
+
+
+import java.util.Collections;
+import java.util.StringJoiner;
+
+public class GameResult {
+
+    public GameResult(Reception reception) {
+        gameWinner(reception);
+    }
+
+    public void gameWinner(Reception reception){
+        Collections.sort(reception.getParticipants());
+        int winnerPosition = reception.getParticipants().get(0).getPosition();
+        StringJoiner sj  = new StringJoiner(",");
+        for(Car car:reception.getParticipants()){
+            sj.add(findWinner(winnerPosition,car));
+        }
+        System.out.println(sj +"가 최종 우승했습니다.");
+    }
+
+    private String findWinner(int winnerPosition,Car currentCar){
+        if(winnerPosition==currentCar.getPosition()){
+            return currentCar.getCarName();
+        }
+        return "";
+    }
+}
---
+++ src/test/java/racing/service/GameResultTest.java
@@ -0,0 +1,33 @@
+package racing.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racing.vo.*;
+
+import java.util.*;
+
+public class GameResultTest {
+
+    private List<Car> cars;
+    private Reception reception;
+    private Rule rule;
+    @BeforeEach
+    void setUp() {
+        List<String> list = Arrays.asList("lucas","son","kane","mane");
+        cars = new ArrayList();
+        for(String str:list){
+            Car car = new Car(str);
+            cars.add(car);
+        }
+        reception = new Reception(cars);
+        rule= new Rule("5");
+    }
+
+    @Test
+    @DisplayName("자동차경주 라운드별 이동 출력")
+    public void movingCar(){
+        GameResult gameResult = new GameResult(reception);
+    }
+
+}
