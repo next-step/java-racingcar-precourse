da9bccbf876c69f53175992e1ee134597f7b0f4b
홍창의 <redcrow4@gmail.com>
2021-05-02 16:50:24 +0900

    test: 1 이상인지 검증

--- src/test/java/GamePlayServiceTest.java
+++ src/test/java/GamePlayServiceTest.java
@@ -8,22 +8,30 @@ class GamePlayServiceTest {
 	// 숫자인지 검증
 	boolean isNumber(String randomValue) {
 		try {
-			Integer.parseInt(randomValue);
-			return true;
+			return isAboveMinimum(Integer.parseInt(randomValue));
 		} catch (NumberFormatException e) {
 			System.out.println("숫자를 입력해 주세요.");
 			return false;
 		}
 	}
 
+	// 1 이상인지 검증
+	boolean isAboveMinimum(int playCount) {
+		if (1 > playCount) {
+			System.out.println("1이상의 숫자를 입력해 주세요.");
+			return false;
+		}
+		return true;
+	}
+
 	@ParameterizedTest
-	// @ValueSource(strings = {"a", "ㅁ"}) // 실패
-	@ValueSource(strings = {"0", "1", "12351"}) // 성공
+	@ValueSource(strings = {"-14234", "0", "a", "ㅁ"}) // 실패
+	// @ValueSource(strings = {"1", "12351"}) // 성공
 	void getRaceCount(String randomValue) {
 		assertThat(isNumber(randomValue))
 			.withFailMessage("숫자 검증 실패= " + randomValue)
-			// .isFalse();  // 실패
-			.isTrue(); // 성공
+			.isFalse();  // 실패
+			// .isTrue(); // 성공
 	}
 
 }
