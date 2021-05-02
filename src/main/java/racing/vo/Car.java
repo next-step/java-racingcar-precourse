9860907b8fb1435512de5122f8534eb27fdf60ec
leemingyu09 <hikakaka13@naver.com>
2021-05-02 11:58:50 +0900

    feat : 자동차 이동 기능(출력)

--- src/main/java/racing/vo/Car.java
+++ src/main/java/racing/vo/Car.java
@@ -3,10 +3,12 @@ package racing.vo;
 public class Car {
     private CarName carName;
     private int carPosition;
+    private StringBuilder positionText;
     private static final int maxCount = 9;
 
     public Car(String name) {
         this.carName = new CarName(name);
+        this.positionText = new StringBuilder();
     }
 
     public String getCarName(){
@@ -17,11 +19,15 @@ public class Car {
         int randomNo =  (int)(Math.random()*maxCount);
         if(randomNo>=4){
             carPosition++;
+            positionText.append("-");
         }
         return randomNo;
     }
 
     public int getPosition() {
         return carPosition;
+    }
+    public StringBuilder getPositionText() {
+        return positionText;
     }
 }
