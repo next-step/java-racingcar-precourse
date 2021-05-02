6eabcae2a1fee1ed2155fce6fd1e7525e9c12426
jaewon.lim <jewon945@gmail.com>
2021-05-02 13:54:00 +0900

    feat(RacingCarTest) : playOneRound 발생한 난수가 4 이상일 경우 전진, 3 이하일 경우 정지하는지 테스트

---
+++ src/test/java/RacingCarTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RacingCarTest {
+
+    /**
+     * 발생한 난수가 4 이상일 경우 전진, 3 이하일 경우 정지하는지 테스트
+     */
+    @Test
+    void playOneRound() {
+        RacingCar car = new RacingCar("TEST");
+        int res = car.playOneRound();
+        assertTrue(((res>=0 && res<=3)==(car.getCount()==0)));
+        assertTrue(((res>=4)==(car.getCount()==1)));
+    }
+}
