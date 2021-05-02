e17a6f75689a97841ac400bb07b1ad4b472a3c43
홍창의 <redcrow4@gmail.com>
2021-05-02 15:21:40 +0900

    test: 자동차 이름 목록 검증
    -자동차 이름 검증 테스트 병합

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,20 +1,15 @@
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTest {
 
-	@ParameterizedTest
-	@DisplayName("자동차 이름 검증")
-	// @ValueSource(strings = {"", "123456"}) // 실패
-	@ValueSource(strings = {"1", "12345"}) // 성공
-	void isValidCarName(String carName) {
-		assertThat(5 >= carName.length() && 1 <= carName.length())
-			.withFailMessage("자동차 이름 검증실패= [" + carName + "]")
-			// .isFalse(); // 실패
-			.isTrue(); // 성공
+	boolean isValidCarName(String carName) {
+		return 5 >= carName.length() && 1 <= carName.length();
 	}
 
 	@ParameterizedTest
@@ -24,6 +19,25 @@ class CarTest {
 	void isValidCarCount(String carNameArr) {
 		assertThat(2 <= carNameArr.split(",").length)
 			.withFailMessage("자동차 대수 검증실패= [" + carNameArr + "]")
+			// .isFalse(); // 실패
+			.isTrue(); // 성공
+	}
+
+	@ParameterizedTest
+	@DisplayName("자동차 이름 목록 검증")
+	// @ValueSource(strings = {"1,123456"}) // 실패
+	@ValueSource(strings = {"1", "1,12345"}) // 성공
+	void isValidCarNames(String carNames) {
+		String[] carNameArr = carNames.split(",");
+		int loopCount = 0;
+		boolean validCarNameResult = false;
+		while (loopCount < carNameArr.length && !validCarNameResult) {
+			validCarNameResult = isValidCarName(carNameArr[loopCount]);
+			loopCount++;
+		}
+		validCarNameResult = loopCount == carNameArr.length && validCarNameResult;
+		assertThat(validCarNameResult)
+			.withFailMessage("자동차 이름 목록 검증실패=" + Arrays.toString(carNameArr))
 			// .isFalse(); // 실패
 			.isTrue(); // 성공
 	}
