695132637bccfdfafc66ec2134c8ff2aea4abf4b
Min Kim <=>
2021-05-02 14:40:51 +0900

    test: 자동차 전진 기능 테스트 구현 및 테스트 인수 추가

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,24 +1,46 @@
 package racingcar;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class RacingCarTest {
 
-    @Test
-    @DisplayName("난수 3이 입력되었을 경우 자동차는 이동하지 않는다.")
-    void stop() {
-        // given
-        RacingCar racingCar = new RacingCar(new Name("tester"));
-        TravelDistance zero = TravelDistance.zero();
+    RacingCar racingCar;
 
-        int random = 3;
+    @BeforeEach
+    void setUp() {
+        racingCar = new RacingCar(new Name("hello"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @DisplayName("난수 4가 입력되었을 경우 자동차는 한칸 전진한다.")
+    void move(int random) {
+        // given
+        TravelDistance one = TravelDistance.create(1);
 
         // when
         racingCar.moveForward(random);
-        TravelDistance travelDistance = racingCar.getForwardDistance();
+        TravelDistance travelDistance = racingCar.getTravelDistance();
+
+        // then
+        assertThat(travelDistance).isEqualTo(one);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("난수 3이 입력되었을 경우 자동차는 이동하지 않는다.")
+    void stop(int random) {
+        // given
+        TravelDistance zero = TravelDistance.ZERO;
+
+        // when
+        racingCar.moveForward(random);
+        TravelDistance travelDistance = racingCar.getTravelDistance();
 
         // then
         assertThat(travelDistance).isEqualTo(zero);
