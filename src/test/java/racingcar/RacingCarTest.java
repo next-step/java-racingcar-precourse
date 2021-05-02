1e08b7621a4938c4d399d8768eebb51271d76836
junseok <ixtears23@icloud.com>
2021-05-02 23:02:28 +0900

    test(car) : RacingCar 전진횟수 필드 Test
    RacingCar 생성시 전진횟수를 가지고 있는지 테스트

--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -16,11 +16,20 @@ class RacingCarTest {
     }
 
     @Test
-    @DisplayName("자동")
+    @DisplayName("자동차 생성시 이름을 가지고 있는지 테스트")
     void createCarNameTest() {
 
         final String name = car.getName();
 
         assertThat(name).isNotNull();
+    }
+
+    @Test
+    @DisplayName("자동차 생성시 전진횟수를 가지고 있는지 테스트")
+    void createCarNumberOfAdvancesTest() {
+
+        final int numberOfAdvances = car.getNumberOfAdvances();
+
+        assertThat(numberOfAdvances).isNotNull();
     }
 }
