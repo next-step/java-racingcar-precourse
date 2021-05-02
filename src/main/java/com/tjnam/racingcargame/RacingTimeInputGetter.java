0986a19aabe4c75e853825300e929dc5c6e822f3
TJ NAM <dynamicnam@gmail.com>
2021-05-02 21:06:07 +0900

    feat : Racing Time Input getter 구현

--- src/main/java/com/tjnam/racingcargame/RacingTimeInputGetter.java
+++ src/main/java/com/tjnam/racingcargame/RacingTimeInputGetter.java
@@ -1,4 +1,35 @@
 package com.tjnam.racingcargame;
 
+import java.util.Scanner;
+
 public class RacingTimeInputGetter {
+    private static final Scanner racingTimeScanner = new Scanner(System.in);
+
+    public int getUserInput(){
+        boolean inputResult = false;
+        int userInput;
+        this.printRacingInputGuideMessage();
+
+        do {
+            userInput = this.getUserRacingTimeInput();
+            inputResult = validateRacingTime(userInput);
+        } while (inputResult);
+
+        return userInput;
+    }
+
+    private int getUserRacingTimeInput(){
+        return racingTimeScanner.nextInt();
+    }
+
+    private void printRacingInputGuideMessage(){
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    private boolean validateRacingTime(int userInput){
+        return userInput > 0;
+    }
+
+
+
 }
