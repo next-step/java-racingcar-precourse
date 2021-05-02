abf1316ea11a3cb6b7e60021fcf74e5596841940
junlee <laonzenamoon@gmail.com>
2021-05-02 19:33:10 +0900

    feat: 자동차 경주 설정(RacingCarConfig) 객체

---
+++ src/main/java/com/game/global/config/RacingCarConfig.java
@@ -0,0 +1,9 @@
+package com.game.global.config;
+
+public class RacingCarConfig {
+
+    public static final int MAX_CAR_NAME_LENGTH = 5;
+    public static final int GO_FORWARD_STANDARD = 4;
+    public static final int RACING_START_LINE = 0;
+
+}
