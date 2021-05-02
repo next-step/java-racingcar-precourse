95e6ddd53662cd131d5f7c776c41ad560b772d28
TJ NAM <dynamicnam@gmail.com>
2021-05-02 21:09:40 +0900

    refactor : 안내 메시지에 대한 내용의 위치 변경
    안내 메시지의 위치또한 Input getter에서 실행하고, 해당 프린팅 함수를 분리 함

--- src/main/java/com/tjnam/racingcargame/CarNameInputGetter.java
+++ src/main/java/com/tjnam/racingcargame/CarNameInputGetter.java
@@ -5,11 +5,15 @@ import java.util.List;
 import java.util.Scanner;
 
 public class CarNameInputGetter {
+
     private static Scanner carNameScanner = new Scanner(System.in);
-    private String userInput;
 
     public List<CarName> getCarNameInput(){
-        String userInput = inputCarNames();
+        String userInput;
+        printGetCarNameGuideMessages();
+        do {
+            userInput = inputCarNames();
+        } while(userInput.length() == 0);
         String[] carNamesStr = splitUserInput(userInput);
         List<CarName> carNames = convertStringToCarName(carNamesStr);
         return carNames;
@@ -18,6 +22,7 @@ public class CarNameInputGetter {
     private String inputCarNames(){
         String userInput = carNameScanner.nextLine();
         if (userInput.length() == 0) {
+            printUserInputErrorMessage();
             throw new IllegalArgumentException("NO_USER_INPUT");
         }
         return userInput;
@@ -33,5 +38,13 @@ public class CarNameInputGetter {
             carNames.add(new CarName(carNameInput));
         }
         return carNames;
+    }
+
+    private void printGetCarNameGuideMessages(){
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분");
+    }
+
+    private void printUserInputErrorMessage(){
+        System.out.println("사용자 입력이 없었습니다. 다시 자동차 이름을 입력해주세요.");
     }
 }
