addb5c891398423baec238dc125df5a166a8236a
TJ NAM <dynamicnam@gmail.com>
2021-05-02 21:08:58 +0900

    feat : Racing Time 도메인 클래스 구현
    레이싱 횟수에 대한 값을 저장 및 관리

--- src/main/java/com/tjnam/racingcargame/RacingTime.java
+++ src/main/java/com/tjnam/racingcargame/RacingTime.java
@@ -1,5 +1,17 @@
 package com.tjnam.racingcargame;
 
 public class RacingTime {
+    private int remained;
 
+    public RacingTime(int time){
+        this.remained = time;
+    }
+
+    public void use(){
+        this.remained--;
+    }
+
+    public int getRemained(){
+        return this.remained;
+    }
 }
