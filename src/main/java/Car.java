451ebe2802cb187b4d03f20c927ee80eb31d097f
홍창의 <redcrow4@gmail.com>
2021-05-02 16:06:33 +0900

    feat: 자동차 이름 검증

---
+++ src/main/java/Car.java
@@ -0,0 +1,18 @@
+public class Car {
+
+	private final String carName;
+
+	public Car(String carName) {
+		this.carName = carName;
+	}
+
+	public String getCarName() {
+		return carName;
+	}
+
+	// 자동차 이름 검증
+	boolean isValidCarName(String carName) {
+		return 5 >= carName.length() && 1 <= carName.length();
+	}
+
+}
