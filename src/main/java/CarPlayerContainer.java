b2940e952786a2f01a7ea301b8a382ecd0b0247b
praivesi <praivesi@gmail.com>
2021-05-02 21:30:00 +0900

    feat: (5) add determining winner mechanism in CarPlayerContainer

--- src/main/java/CarPlayerContainer.java
+++ src/main/java/CarPlayerContainer.java
@@ -15,4 +15,31 @@ public class CarPlayerContainer {
     public List<CarPlayer> getPlayers() {
         return this.carPlayers;
     }
+
+    public List<CarPlayer> getWinners() {
+        int maxMoveCount = this.getMaxMoveCount();
+
+        List<CarPlayer> winners = new ArrayList<>();
+        for (CarPlayer carPlayer : this.carPlayers) {
+            this.tryWinner(carPlayer, maxMoveCount, winners);
+        }
+
+        return winners;
+    }
+
+    private int getMaxMoveCount() {
+        int maxMoveCount = 0;
+
+        for (CarPlayer carPlayer : this.carPlayers) {
+            maxMoveCount = Math.max(maxMoveCount, carPlayer.getMoveCount());
+        }
+
+        return maxMoveCount;
+    }
+
+    private void tryWinner(CarPlayer carPlayer, int maxMoveCount, List<CarPlayer> winners) {
+        if (carPlayer.getMoveCount() == maxMoveCount) {
+            winners.add(carPlayer);
+        }
+    }
 }
---
+++ src/test/java/CarPlayerContainerTest.java
@@ -0,0 +1,58 @@
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarPlayerContainerTest {
+    private CarPlayerContainer carPlayerContainer;
+
+    @BeforeEach
+    void setup() {
+        this.carPlayerContainer = new CarPlayerContainer();
+    }
+
+    @Test
+    @DisplayName("순차적으로 전진 개수가 증가하는 player리스트에서 winner를 찾아낸다.")
+    void get_winners_in_ordered_players() {
+        for (int i = 0; i < 10; i++) {
+            CarPlayer carPlayer = mock(CarPlayer.class);
+            when(carPlayer.getMoveCount()).thenReturn(i);
+
+            this.carPlayerContainer.add(carPlayer);
+        }
+
+        Assertions.assertThat(this.carPlayerContainer.getWinners().size()).isGreaterThan(0);
+        Assertions.assertThat(this.carPlayerContainer.getWinners().get(0).getMoveCount()).isEqualTo(9);
+    }
+
+    @Test
+    @DisplayName("순차적으로 전진 개수가 감소하는 player리스트에서 winner를 찾아낸다.")
+    void get_winners_in_reverse_ordered_players() {
+        for (int i = 9; i >= 0; i--) {
+            CarPlayer carPlayer = mock(CarPlayer.class);
+            when(carPlayer.getMoveCount()).thenReturn(i);
+
+            this.carPlayerContainer.add(carPlayer);
+        }
+
+        Assertions.assertThat(this.carPlayerContainer.getWinners().size()).isGreaterThan(0);
+        Assertions.assertThat(this.carPlayerContainer.getWinners().get(0).getMoveCount()).isEqualTo(9);
+    }
+
+    @Test
+    @DisplayName("Winner가 여러 명일 경우 모두 찾아낸다.")
+    void find_all_winners() {
+        for (int i = 0; i < 10; i++) {
+            CarPlayer carPlayer = mock(CarPlayer.class);
+            when(carPlayer.getMoveCount()).thenReturn(i % 3);
+
+            this.carPlayerContainer.add(carPlayer);
+        }
+
+        Assertions.assertThat(this.carPlayerContainer.getWinners().size()).isEqualTo(3);
+        Assertions.assertThat(this.carPlayerContainer.getWinners().get(0).getMoveCount()).isEqualTo(2);
+    }
+}
