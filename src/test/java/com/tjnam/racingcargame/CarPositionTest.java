149a6add974d099deec4d9b692a365a0e8ad54c7
TJ NAM <dynamicnam@gmail.com>
2021-05-02 17:20:52 +0900

    Test : CarPosition Test
    초기 값 설정에 대한 테스트 추가

--- src/test/java/com/tjnam/racingcargame/CarPositionTest.java
+++ src/test/java/com/tjnam/racingcargame/CarPositionTest.java
@@ -1,4 +1,21 @@
 package com.tjnam.racingcargame;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CarPositionTest {
+
+    private static CarPosition position;
+
+    @BeforeAll
+    public static void setUp(){
+        position = new CarPosition();
+    }
+
+    @Test
+    public void initialPositionTest() {
+        assertThat(position.getPosition()).isEqualTo(0);
+    }
 }
