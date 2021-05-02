0918da4b9c73a5ddd03c403d437f44dcac20dc1c
Junhong-Kim <jhk1005jhk@gmail.com>
2021-05-02 17:02:40 +0900

    feat(RacingGame): 사용자로부터 횟수 입력받기
    - 횟수가 1이상의 숫자인가?

--- src/main/java/camp/nextstep/edu/constant/Message.java
+++ src/main/java/camp/nextstep/edu/constant/Message.java
@@ -6,5 +6,7 @@ public class Message {
     }
 
     public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public static final String INPUT_NUMBER_OF_ATTEMPTS = "시도할 회수는 몇회인가요?";
     public static final String INVALID_CAR_NAME = "자동차 이름은 5자 이하만 가능합니다.";
+    public static final String INVALID_NUMBER_OF_ATTEMPTS = "1이상의 숫자를 입력해주세요.";
 }
--- src/main/java/camp/nextstep/edu/game/RacingGame.java
+++ src/main/java/camp/nextstep/edu/game/RacingGame.java
@@ -10,14 +10,32 @@ public class RacingGame implements Game {
 
     private static final Scanner scanner = new Scanner(System.in);
     private final List<RacingCar> racingCarList = new ArrayList<>();
+    private int attempts;
 
     @Override
     public void start() {
         System.out.println(Message.INPUT_CAR_NAME);
         String[] names = scanner.nextLine().split(",");
+        setRacingCarList(names);
+        System.out.println(Message.INPUT_NUMBER_OF_ATTEMPTS);
+        setAttempts(scanner.nextLine());
+    }
 
+    private void setRacingCarList(String[] names) {
         for (String name : names) {
             racingCarList.add(new RacingCar(name));
+        }
+    }
+
+    private void setAttempts(String str) {
+        try {
+            int number = Integer.parseInt(str);
+            if (number <= 0) {
+                throw new IllegalArgumentException(Message.INVALID_NUMBER_OF_ATTEMPTS);
+            }
+            this.attempts = number;
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
         }
     }
 }
