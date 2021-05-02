8efc4b3d12388dc098bfe62c4b4cf84e82873760
ShinHan <tlsgks8668@naver.com>
2021-05-03 01:59:56 +0900

    feat: 자동자의 전진, 정지 기능

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,2 +1,18 @@
-package racingcar;public class Car {
+package racingcar;
+
+public class Car {
+
+    private int position;
+
+    public Car(int position) {
+        this.position = position;
+    }
+
+    public int move(int randomnumber) {
+        if(randomnumber >= 4) {
+            this.position = position + 1;
+        }
+
+        return position;
+    }
 }
