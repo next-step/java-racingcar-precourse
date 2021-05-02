67ab4c381304b149e4829c1ddac6e5d28a16647d
TJ NAM <dynamicnam@gmail.com>
2021-05-02 21:05:42 +0900

    Test : RacingTime Input Getter 테스트 케이스 작성

--- src/test/java/com/tjnam/racingcargame/RacingTimeInputGetterTest.java
+++ src/test/java/com/tjnam/racingcargame/RacingTimeInputGetterTest.java
@@ -1,4 +1,51 @@
 package com.tjnam.racingcargame;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RacingTimeInputGetterTest {
+
+    private static RacingTimeInputGetter racingTimeInputGetter;
+
+    @BeforeAll
+    public static void setUp(){
+        racingTimeInputGetter = new RacingTimeInputGetter();
+    }
+
+    @Test
+    public void validateRacingTimeTest(){
+
+        try {
+            Method validateRacingTimeMethod = racingTimeInputGetter.getClass()
+                    .getDeclaredMethod("validateRacingTime", int.class);
+            validateRacingTimeMethod.setAccessible(true);
+
+            int racingTimeInput = 0;
+            boolean isValid = (boolean) validateRacingTimeMethod.invoke(racingTimeInputGetter, racingTimeInput);
+            assertThat(isValid).isFalse();
+
+            racingTimeInput = 1;
+            isValid = (boolean) validateRacingTimeMethod.invoke(racingTimeInputGetter, racingTimeInput);
+            assertThat(isValid).isTrue();
+
+            racingTimeInput = 5000;
+            isValid = (boolean) validateRacingTimeMethod.invoke(racingTimeInputGetter, racingTimeInput);
+            assertThat(isValid).isTrue();
+
+            racingTimeInput = -200;
+            isValid = (boolean) validateRacingTimeMethod.invoke(racingTimeInputGetter, racingTimeInput);
+            assertThat(isValid).isFalse();
+
+        } catch (NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
 }
