2fe79ef5b82801979fa1afa5633b218c8ad384d9
홍창의 <redcrow4@gmail.com>
2021-05-02 16:07:21 +0900

    feat: 자동차 대수 검증

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -15,4 +15,9 @@ public class Car {
 		return 5 >= carName.length() && 1 <= carName.length();
 	}
 
+	// 자동차 대수 검증
+	boolean isValidCarCount(String[] carNameArr) {
+		return 2 <= carNameArr.length;
+	}
+
 }
