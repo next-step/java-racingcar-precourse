501d4c63ebffba1aead064bc6e96e24013bcb65f
홍창의 <redcrow4@gmail.com>
2021-05-02 15:16:37 +0900

    test: 자동차 대수 검증

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -17,4 +17,15 @@ class CarTest {
 			.isTrue(); // 성공
 	}
 
+	@ParameterizedTest
+	@DisplayName("자동차 대수 검증")
+	// @ValueSource(strings = {"1", "1,", ",1", ",", ""}) // 실패
+	@ValueSource(strings = {"1,2", "1,2,3,4,5"}) // 성공
+	void isValidCarCount(String carNameArr) {
+		assertThat(2 <= carNameArr.split(",").length)
+			.withFailMessage("자동차 대수 검증실패= [" + carNameArr + "]")
+			// .isFalse(); // 실패
+			.isTrue(); // 성공
+	}
+
 }
