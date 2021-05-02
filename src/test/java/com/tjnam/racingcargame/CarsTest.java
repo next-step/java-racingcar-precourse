3fb329b29d1f11a67b5c9637e7e06ff7c04a4380
TJ NAM <dynamicnam@gmail.com>
2021-05-02 18:11:33 +0900

    Test : Cars 생성 테스트 추가
    초기 CarName을 받아서 차 생성

--- src/test/java/com/tjnam/racingcargame/CarsTest.java
+++ src/test/java/com/tjnam/racingcargame/CarsTest.java
@@ -1,4 +1,21 @@
 package com.tjnam.racingcargame;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CarsTest {
+
+    @Test
+    public void initializeTest(){
+        List<CarName> carNames = new ArrayList<CarName>();
+        carNames.add(new CarName("Car1"));
+        carNames.add(new CarName("Car2"));
+        carNames.add(new CarName("Car3"));
+        assertThat(carNames.size()).isEqualTo(3);
+    }
+
 }
