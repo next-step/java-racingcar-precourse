5728afba70596e80bc49f9d376f37cb277275d07
전계원 <tonyjev93@gmail.com>
2021-05-02 12:24:43 +0900

    test: fix RandomUtilTest - startNumber 1 -> 0

--- src/test/java/com/game/utils/RandomUtilTest.java
+++ src/test/java/com/game/utils/RandomUtilTest.java
@@ -7,10 +7,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class RandomUtilTest {
     @Test
-    @DisplayName("랜덤 숫자(1~9) 생성 테스트")
+    @DisplayName("랜덤 숫자(0~9) 생성 테스트")
     public void getRandomNumberTest() {
         // given
-        int startNumber = 1;
+        int startNumber = 0;
         int endNumber = 9;
 
         // when
