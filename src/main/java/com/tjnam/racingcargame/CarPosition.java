4d2c9effb6fecb7e32ef81ee822bc6bfa1100161
TJ NAM <dynamicnam@gmail.com>
2021-05-02 17:22:49 +0900

    feat : 자동차 위치에 대한 Car Position 클래스 구현

--- src/main/java/com/tjnam/racingcargame/CarPosition.java
+++ src/main/java/com/tjnam/racingcargame/CarPosition.java
@@ -1,4 +1,19 @@
 package com.tjnam.racingcargame;
 
 public class CarPosition {
+
+    private int position;
+
+    public CarPosition(){
+        this.position = 0;
+    }
+
+    public void move(){
+        this.position++;
+    }
+
+    public int getPosition(){
+        return this.position;
+    }
+
 }
