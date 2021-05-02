742760154d00394235a20f2a73d49415c7373360
leemingyu09 <hikakaka13@naver.com>
2021-05-02 12:12:51 +0900

    test : 클래스명 변경

---
+++ src/main/java/racing/service/gameMain.java
@@ -0,0 +1,5 @@
+package racing.service;
+
+public class gameMain {
+
+}
--- src/test/java/racing/service/RacingGameTest.java
+++ src/test/java/racing/service/RacingGameTest.java
@@ -12,9 +12,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-public class RacingServiceTest {
+public class RacingGameTest {
 
     private List<Car> cars;
     private Reception reception;
