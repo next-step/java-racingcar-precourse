60a09afe59d2e7f054583854bcb110c7a6b3282d
홍창의 <redcrow4@gmail.com>
2021-05-02 16:19:45 +0900

    refactor: Cars 생성으로 인한 리팩토링
    -자동차 이름 목록 검증 -> 클래스 변경 Cars
    -사용자 입력 자동차 이름들 검증 -> 클래스 변경 Cars
    -자동차 이름 얻기 -> 클래스 변경 Cars

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -13,43 +13,13 @@ public class Car {
 	}
 
 	// 자동차 이름 검증
-	public boolean isValidCarName(String carName) {
+	public static boolean isValidCarName(String carName) {
 		return 5 >= carName.length() && 1 <= carName.length();
 	}
 
 	// 자동차 대수 검증
-	public boolean isValidCarCount(String[] carNameArr) {
+	public static boolean isValidCarCount(String[] carNameArr) {
 		return 2 <= carNameArr.length;
-	}
-
-	// 자동차 이름 목록 검증
-	public boolean isValidCarNames(String[] carNameArr) {
-		int loopCount = 0;
-		boolean validCarNameResult = true;
-		while (loopCount < carNameArr.length && validCarNameResult) {
-			validCarNameResult = isValidCarName(carNameArr[loopCount]);
-			loopCount++;
-		}
-		return loopCount == carNameArr.length && validCarNameResult;
-	}
-
-	// 사용자 입력 자동차 이름들 검증
-	public boolean isValidCar(String[] carNameArr) {
-		if (!isValidCarCount(carNameArr)) {
-			return false;
-		}
-		return isValidCarNames(carNameArr);
-	}
-
-	// 자동차 이름 얻기
-	public String[] getCarNames() {
-		String[] carNameArr;
-		do {
-			System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-			carNameArr = new Scanner(System.in).nextLine().split(",");
-		} while (!isValidCar(carNameArr));
-
-		return carNameArr;
 	}
 
 }
---
+++ src/main/java/Cars.java
@@ -0,0 +1,47 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Cars {
+
+	private final List<Car> carList;
+
+	public Cars(List<Car> carList) {
+		this.carList = carList;
+	}
+
+	public List<Car> getCarList() {
+		return carList;
+	}
+
+	// 자동차 이름 목록 검증
+	public boolean isValidCarNames(String[] carNameArr) {
+		int loopCount = 0;
+		boolean validCarNameResult = true;
+		while (loopCount < carNameArr.length && validCarNameResult) {
+			validCarNameResult = Car.isValidCarName(carNameArr[loopCount]);
+			loopCount++;
+		}
+		return loopCount == carNameArr.length && validCarNameResult;
+	}
+
+	// 사용자 입력 자동차 이름들 검증
+	public boolean isValidCar(String[] carNameArr) {
+		if (!Car.isValidCarCount(carNameArr)) {
+			return false;
+		}
+		return isValidCarNames(carNameArr);
+	}
+
+	// 자동차 이름 얻기
+	public String[] getCarNames() {
+		String[] carNameArr;
+		do {
+			System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+			carNameArr = new Scanner(System.in).nextLine().split(",");
+		} while (!isValidCar(carNameArr));
+
+		return carNameArr;
+	}
+
+
+}
