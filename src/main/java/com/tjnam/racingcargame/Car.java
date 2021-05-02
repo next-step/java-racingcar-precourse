3d2a9ab21a0c19f61ec88e99b18ed2ed45a45443
TJ NAM <dynamicnam@gmail.com>
2021-05-02 17:43:57 +0900

    Feat : Car Class 작성

--- src/main/java/com/tjnam/racingcargame/Car.java
+++ src/main/java/com/tjnam/racingcargame/Car.java
@@ -1,4 +1,24 @@
 package com.tjnam.racingcargame;
 
 public class Car {
+    private CarName name;
+    private CarPosition position;
+    
+    public Car (CarName name) {
+        this.name = name;
+        this.position = new CarPosition();
+    }
+
+    public void moveCar(){
+        position.move();
+    }
+
+    public String getCarName(){
+        return this.name.getName();
+    }
+
+    public int getPosition(){
+        return this.position.getPosition();
+    }
+
 }
