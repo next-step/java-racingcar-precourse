e2524185119129f01783e3de52662b2eff4fb389
TJ NAM <dynamicnam@gmail.com>
2021-05-02 17:43:26 +0900

    Test : CarTest 테스트 케이스 작성

--- src/test/java/com/tjnam/racingcargame/CarTest.java
+++ src/test/java/com/tjnam/racingcargame/CarTest.java
@@ -1,4 +1,41 @@
 package com.tjnam.racingcargame;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CarTest {
+
+    private static String testCarName = "Testing car";
+    private static Car car;
+
+    @BeforeAll
+    public static void setUp(){
+        car = new Car(new CarName(testCarName));
+    }
+
+    @Test
+    public void constructCarPositionTest(){
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+
+    @Test
+    public void moveCarTest(){
+        Car newTestCar = new Car(new CarName("New Test Car"));
+        newTestCar.moveCar();
+        assertThat(newTestCar.getPosition()).isEqualTo(1);
+
+        int movingTime = 10;
+        for (int i=0 ; i<movingTime ; i++) {
+            newTestCar.moveCar();
+        }
+        assertThat(newTestCar.getPosition()).isEqualTo(11);
+    }
+
+    @Test
+    public void getCarName(){
+        assertThat(car.getCarName()).isEqualTo(testCarName);
+    }
+
 }
