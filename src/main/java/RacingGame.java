c4b988e4df0c61c574cd8f3a2490db1d2691fec6
jaewon.lim <jewon945@gmail.com>
2021-05-02 16:23:59 +0900

    feat(RacingGame) : printResult 최종 우승자 리스트를 받아서 결과를 출력

--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -1,15 +1,29 @@
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class RacingGame {
+    /**
+     * 최종 우승자 리스트를 받아서 결과를 출력
+     * @param winners 최종 우승자 리스트
+     * @return 결과 문자열
+     */
+    public String printResult(List<RacingCar> winners) {
+        String res = "";
+        res = winners.get(0).getName();
+        for (int i=1;i<winners.size();i++) {
+            res += ", " + winners.get(i).getName();
+        }
+        res += "가 최종 우승했습니다.";
+        System.out.println(res);
+        return res;
+    }
+
     /**
      * 사용자에게 입력받은 자동차 이름 문자열을 RacingCar 리스트로 반환
      * 5자를 초과하는 이름이 존재할 경우 null을 리턴한다.
      * @param str 입력받은 자동차 이름 문자열 (구분자 ",")
      * @return RacingCar 리스트, 5자 초과하는 이름 있을 경우 null
      */
-    List<RacingCar> splitString(String str){
+    public List<RacingCar> splitString(String str){
         String[] name_array = str.split(",");
         int len = name_array.length;
         List<RacingCar> res = new ArrayList<>();
