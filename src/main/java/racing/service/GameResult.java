0af2f86647c38c443978fdcb7378df832fa5f0b9
leemingyu09 <hikakaka13@naver.com>
2021-05-02 18:01:52 +0900

    bug : ,가 뒤에 붙는 경우가 발생       StringJoiner 를       findWinner 의 if 절에 부합 하는 경우에만 사용

--- src/main/java/racing/service/GameResult.java
+++ src/main/java/racing/service/GameResult.java
@@ -9,6 +9,7 @@ import java.util.StringJoiner;
 
 public class GameResult {
 
+    private StringJoiner sj  = new StringJoiner(",");
     public GameResult(Reception reception) {
         gameWinner(reception);
     }
@@ -16,17 +17,15 @@ public class GameResult {
     public void gameWinner(Reception reception){
         Collections.sort(reception.getParticipants());
         int winnerPosition = reception.getParticipants().get(0).getPosition();
-        StringJoiner sj  = new StringJoiner(",");
         for(Car car:reception.getParticipants()){
-            sj.add(findWinner(winnerPosition,car));
+            findWinner(winnerPosition,car);
         }
         System.out.println(sj +"가 최종 우승했습니다.");
     }
 
-    private String findWinner(int winnerPosition,Car currentCar){
+    private void findWinner(int winnerPosition,Car currentCar){
         if(winnerPosition==currentCar.getPosition()){
-            return currentCar.getCarName();
+            sj.add(currentCar.getCarName());
         }
-        return "";
     }
 }
