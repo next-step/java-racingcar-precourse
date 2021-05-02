bc7216f5a9d36a111c605b93b9030c0b55df1de6
홍창의 <redcrow4@gmail.com>
2021-05-02 17:12:26 +0900

    feat: 레이싱 횟수 얻기

--- src/main/java/GamePlayService.java
+++ src/main/java/GamePlayService.java
@@ -21,4 +21,14 @@ public class GamePlayService {
 		}
 	}
 
+	// 레이싱 횟수 얻기
+	int getPlayCount() {
+		String playCount;
+		do {
+			System.out.println("시도할 회수는 몇 회인가요?");
+			playCount = new Scanner(System.in).nextLine();
+		} while (!isNumber(playCount));
+		return Integer.parseInt(playCount);
+	}
+
 }
