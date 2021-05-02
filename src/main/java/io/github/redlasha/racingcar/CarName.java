2e85dd2cee4db4564f6b6c225ff6a5cc980b8b3c
heonseob <redlasha@gmail.com>
2021-05-02 22:08:17 +0900

    feat: add carName class
    - add default setting
    - add carName class and test

---
+++ src/main/java/io/github/redlasha/racingcar/CarName.java
@@ -0,0 +1,18 @@
+package io.github.redlasha.racingcar;
+
+public class CarName {
+
+	private String name;
+
+	public CarName(String name) {
+		if (name == null || name.isEmpty() || name.length() > 5) {
+			throw new IllegalArgumentException("차 이름은 5글자 이하의 문자열입니다. ");
+		}
+		this.name = name;
+	}
+
+	public Object getName() {
+		return this.name;
+	}
+
+}
---
+++ src/test/java/io/github/redlasha/racingcar/CarNameTest.java
@@ -0,0 +1,43 @@
+package io.github.redlasha.racingcar;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import io.github.redlasha.racingcar.CarName;
+
+class CarNameTest {
+
+	@Test
+	void 자동차_이름은_getName으로_조회되어야_한다() {
+		String name = "abcde";
+		CarName carName = new CarName(name);
+		assertThat(carName.getName()).isEqualTo(name);
+	}
+
+	@Test
+	void 자동차_이름은_null이_아니어야_한다() {
+		String nullName = null;
+		assertThrows(IllegalArgumentException.class, () -> {
+			new CarName(nullName);
+		});
+	}
+
+	@Test
+	void 자동차_이름은_1자_이상이어야_한다() {
+		String shortName = "";
+		assertThrows(IllegalArgumentException.class, () -> {
+			new CarName(shortName);
+		});
+	}
+
+	@Test
+	void 자동차_이름은_5자_이하여야_한다() {
+		String longName = "abcdef";
+		assertThrows(IllegalArgumentException.class, () -> {
+			new CarName(longName);
+		});
+	}
+
+}
