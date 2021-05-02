cf7e81e9a59e4ce09753a6feb383b43095df4cc1
lewis <bcm.lewisbaek@gmail.com>
2021-05-03 01:02:13 +0900

    refactor: VaildationUtils 메소드를 Car, CarGroup 클래스에서 일급콜렉션으로 활용해 구현

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,29 @@
 package racingcar;
 
+import java.util.Objects;
+
 public class Car {
+	private final int MAX_NAME_LENGTH = 5;
 	private String name;
 	private int position;
 
 	public Car(String name) {
-		this.name = name;
+		this.name = isValidCarName(name);
 		this.position = 0;
+	}
+
+	private String isValidCarName(String name) {
+		if (checkLength(name)) {
+			throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+		}
+		if (name.isEmpty()) {
+			throw new IllegalArgumentException("입력된 이름이 없습니다.");
+		}
+		return name;
+	}
+
+	private boolean checkLength(String name) {
+		return name.length() > MAX_NAME_LENGTH;
 	}
 
 	public CarStatus paly(int moveValue) {
@@ -20,4 +37,15 @@ public class Car {
 	private boolean isMove(int moveValue) {
 		return moveValue >= 4;
 	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Car car = (Car)o;
+		return Objects.equals(name, car.name);
+	}
+
 }
--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,27 +1,37 @@
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import racingcar.Car;
 import racingcar.CarStatus;
 
 public class CarTest {
-	private Car benz;
+	@Test
+	void 자동차_이름_검증_빈값() {
+		String carName = "";
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			new Car(carName);
+		}).withMessageContaining("입력된 이름이 없습니다.");
+	}
 
-	@BeforeEach
-	void init() {
-		benz = new Car("benz");
+	@Test
+	void 자동차_이름_검증_5자초과() {
+		String carName = "abcdefg";
+		assertThatIllegalArgumentException().isThrownBy(() -> {
+			new Car(carName);
+		}).withMessageContaining("이름은 5자 이하만 가능합니다.");
 	}
 
 	@Test
 	void 자동차_이동() {
+		Car benz = new Car("benz");
 		CarStatus carStatus = benz.paly(4);
 		assertThat(carStatus).isEqualTo(CarStatus.GO);
 	}
 
 	@Test
 	void 자동차_정지() {
+		Car benz = new Car("benz");
 		CarStatus carStatus = benz.paly(3);
 		assertThat(carStatus).isEqualTo(CarStatus.STOP);
 	}
