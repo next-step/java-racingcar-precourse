4f9605dec6fe2543e82c81b30ab59caec0adb4ec
jaewon.lim <jewon945@gmail.com>
2021-05-02 12:33:11 +0900

    feat(RacingCar) : playOneRound 0~9 사이 난수를 생성하여 4이상일 경우 전진

--- src/main/java/RacingCar.java
+++ src/main/java/RacingCar.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class RacingCar {
     private String name;
     private int count = 0;
@@ -15,5 +17,17 @@ public class RacingCar {
     }
     public void setCount(int num){
         this.count = num;
+    }
+
+    /**
+     * 0~9 사이의 난수를 추출하여, 4 이상일 경우 전진한다.
+     * @return 해당 회차의 난수를 반환 (확인용)
+     */
+    public int playOneRound() {
+        int rand_number = (int)(Math.random()*10);
+        if(rand_number>=4){
+            this.count++;
+        }
+        return rand_number;
     }
 }
