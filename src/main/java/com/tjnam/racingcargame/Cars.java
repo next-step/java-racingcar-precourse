6a8e1bc566cc045180f90b16afff736a7b6741c5
TJ NAM <dynamicnam@gmail.com>
2021-05-02 18:11:52 +0900

    feat : Cars 클래스 구현

--- src/main/java/com/tjnam/racingcargame/Cars.java
+++ src/main/java/com/tjnam/racingcargame/Cars.java
@@ -1,4 +1,15 @@
 package com.tjnam.racingcargame;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cars {
+
+    private List<Car> cars = new ArrayList<Car>;
+
+    public Cars(List<CarName> carNames){
+        for(CarName name : carNames) {
+            cars.add(new Car(name));
+        }
+    }
 }
