813ceaaad5fc739370d5abb685fe7a5080f289d8
TJ NAM <dynamicnam@gmail.com>
2021-05-02 16:59:04 +0900

    Test : CarName 클래스의 getName 테스트 작성
    생성된 객체에 대한 이름이 맞는지.

--- src/test/java/com/tjnam/racingcargame/CarNameTest.java
+++ src/test/java/com/tjnam/racingcargame/CarNameTest.java
@@ -1,4 +1,17 @@
 package com.tjnam.racingcargame;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class CarNameTest {
+    private static CarName carName;
+
+    @Test
+    public void getCarNameTest(){
+        String testedCarName = "TestedCarName";
+        carName = new CarName(testedCarName);
+        assertThat(carName.getName()).isEqualTo(testedCarName);
+    }
 }
