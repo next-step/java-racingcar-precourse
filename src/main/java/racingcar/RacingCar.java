d40d415e9d3bebd078103037678a9fb3b7917be0
ShinHan <tlsgks8668@naver.com>
2021-05-03 22:51:26 +0900

    refactor: InputOutputTest.java 삭제 및 RacingCarTest.java로 이동

--- src/main/java/racingcar/RacingCar.java
+++ src/main/java/racingcar/RacingCar.java
@@ -1,2 +1,38 @@
-package racingcar;public class RacingCar {
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RacingCar {
+    private static InputOutput io = new InputOutput();
+    private static List<String> carNameList;
+    private static List<Car> carList = new ArrayList<>();
+    private static int attemptsCount;
+
+    public static void run() {
+        carNameList = separateName(io.input());
+        attemptsCount = Integer.parseInt(io.input());
+
+        initCar();
+    }
+
+    private static void initCar() {
+        for (int i = 0; i < carNameList.size(); i++) {
+            carList.add(new Car("", carNameList.get(i)));
+        }
+    }
+
+    public static List<String> separateName(String input) {
+        List<String> carNameList = Arrays.asList(input.split(","));
+
+        return carNameList;
+    }
+
+    public static boolean checkLen(String carName) {
+        if(carName.length() <= 5) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/racingcar/RacingCarTest.java
+++ src/test/java/racingcar/RacingCarTest.java
@@ -1,2 +1,28 @@
-package racingcar;public class RacingCarTest {
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingCarTest {
+    @Test
+    void play() {
+
+    }
+
+    @Test
+    @DisplayName("자동차의 이름 구분")
+    void separateCarName() {
+        assertThat(RacingCar.separateName("pobi,crong,honux")).isEqualTo(Arrays.asList("pobi", "crong", "honux"));
+    }
+
+    @Test
+    @DisplayName("이름이 5자 이하인지 검증")
+    void name_5_less_than() {
+        assertThat(RacingCar.checkLen("pobi")).isTrue();
+        assertThat(RacingCar.checkLen("pobiaaa")).isFalse();
+    }
 }
