ab9da28ef9ef26f1ef2e165f4a02a9f17f975a69
ssj5638 <ssj5638@gmail.com>
2021-05-04 01:08:10 +0900

    feat : 입력된 자동차 이름 문자열 배열로 반환

--- src/test/java/racingcar/ValidationUtilsTest.java
+++ src/test/java/racingcar/ValidationUtilsTest.java
@@ -3,6 +3,8 @@ package racingcar;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ValidationUtilsTest {
@@ -20,4 +22,22 @@ public class ValidationUtilsTest {
         assertThat(result).isFalse();
     }
 
+    @Test
+    @DisplayName("입력된_자동차_이름_배열_반환_4개")
+    void string_car_names_to_list(){
+        String carNames = "hello,jeju,kirk,land";
+        List<String> listCar = ValidationUtils.stringToList(carNames);
+        assertThat(listCar).contains("hello");
+        assertThat(listCar).contains("jeju");
+        assertThat(listCar).contains("kirk");
+        assertThat(listCar).contains("land");
+    }
+
+    @Test
+    @DisplayName("입력된_자동차_이름_배열_반환_1개")
+    void string_car_names_to_list2(){
+        String carNames = "hello";
+        List<String> listCar = ValidationUtils.stringToList(carNames);
+        assertThat(listCar).contains("hello");
+    }
 }
