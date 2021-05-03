3f8ceef7cff3c0502b9fb92e112ab84ea347775c
JUNYOUN SHIN <jyshine3@gmail.com>
2021-05-03 14:45:34 +0900

    carNameValidTest

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -2,10 +2,14 @@ package racingcar;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
+
 public class CarTest {
 
     @Test
-    void carTest() {
-
+    void carNameValidTest() {
+        assertThatIllegalArgumentException().isThrownBy(() -> {
+            Car car = new Car("aaaaaa");
+        });
     }
 }
