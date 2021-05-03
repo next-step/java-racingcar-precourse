fee56f937dca1e1b2de35ee1819ac232eab03a6a
TJ NAM <dynamicnam@gmail.com>
2021-05-03 22:17:44 +0900

    test : RandomValueGenerator 테스트케이스 작성

--- src/test/java/com/tjnam/racingcargame/RandomValueGeneratorTest.java
+++ src/test/java/com/tjnam/racingcargame/RandomValueGeneratorTest.java
@@ -1,4 +1,23 @@
 package com.tjnam.racingcargame;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class RandomValueGeneratorTest {
+
+    private static RandomValueGenerator randomValueGenerator;
+
+    @BeforeAll
+    public static void setUp(){
+        randomValueGenerator = new RandomValueGenerator();
+    }
+
+    @Test
+    public void getValueTest(){
+        for (int i=0 ; i < 1000 ; i++) {
+            assertThat(randomValueGenerator.getValue()).isGreaterThan(0).isLessThan(10);
+        }
+    }
 }
