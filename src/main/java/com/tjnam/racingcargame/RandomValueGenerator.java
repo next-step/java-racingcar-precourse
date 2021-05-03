50907c6f43cebaf19d615b38f5c4a8843c892e78
TJ NAM <dynamicnam@gmail.com>
2021-05-03 22:18:04 +0900

    feat : RandomValueGenerator 구현

--- src/main/java/com/tjnam/racingcargame/RandomValueGenerator.java
+++ src/main/java/com/tjnam/racingcargame/RandomValueGenerator.java
@@ -1,4 +1,8 @@
 package com.tjnam.racingcargame;
 
 public class RandomValueGenerator {
+    public int getValue(){
+        int pick = (((int) (Math.random() * 10)) % 9) + 1;
+        return pick;
+    }
 }
