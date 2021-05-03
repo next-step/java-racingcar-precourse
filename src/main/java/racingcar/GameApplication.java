ed17f9e7955d7338cb3dd7306974445a8648c809
junseok <ixtears23@icloud.com>
2021-05-03 16:49:13 +0900

    feat(app) : 전진 시도횟수 입력 텍스트 표시
    자동차가 몇번 이동할 수 있는지
    전진 시도횟수를 사용자에게
    입력받을 수 있도록
    텍스트 표시

--- src/main/java/racingcar/GameApplication.java
+++ src/main/java/racingcar/GameApplication.java
@@ -10,6 +10,8 @@ public class GameApplication {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         Scanner scanner = new Scanner(System.in);
         String carsName = scanner.nextLine();
-
+        List<RacingCar> racingCars = RacingCarFactory.create(carsName);
+        System.out.println("시도할 회수는 몇회인가요?");
+        int gameCount = scanner.nextInt();
     }
 }
