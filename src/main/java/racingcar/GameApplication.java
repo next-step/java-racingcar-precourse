f5b19ba5ddb9eef731caa6de35000fe7e56e554d
junseok <ixtears23@icloud.com>
2021-05-03 16:47:56 +0900

    feat(app) : 게임 시작시 자동차 이름 입력 텍스트 표시
    게임 시작 시 자동차 이름을
    사용자에게 입력 받을 수 있도록
    텍스트 표시

---
+++ src/main/java/racingcar/GameApplication.java
@@ -0,0 +1,15 @@
+package racingcar;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class GameApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        Scanner scanner = new Scanner(System.in);
+        String carsName = scanner.nextLine();
+
+    }
+}
