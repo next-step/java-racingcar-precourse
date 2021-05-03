80474b3b8dcc7aec1943367ca20369b75ecca45a
cptbong <cptbbong@gmail.com>
2021-05-03 22:59:58 +0900

    test - 자동차이름테스트.

--- src/test/java/domain/CarsTest.java
+++ src/test/java/domain/CarsTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarsTest {
     @Test
@@ -12,5 +12,13 @@ class CarsTest {
     void name() {
         Cars cars = new Cars("a,b,c");
         assertThat(cars.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("자동차이름은 5자 이하만 가능")
+    void exception() {
+        assertThatThrownBy(() -> {
+            new Cars("a,b,blabla");
+        }).isExactlyInstanceOf(IllegalArgumentException.class);
     }
 }
