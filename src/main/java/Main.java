d557a33e090fa503806a28cfa5dcc28b6a7ae1a8
ChangSubKwak <paradise1732@gmail.com>
2021-05-04 07:04:05 +0900

    refactor: Main 클래스 리팩토링(MVC패턴으로 변경)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,22 @@
-import game.GameMain;
+import java.util.Scanner;
+
+import controller.Controller;
+import model.Cars;
+import util.GameUtil;
+import view.View;
 
 public class Main {
+	static final Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		GameMain gameMain = new GameMain();
-		gameMain.proceedGame();
+		View view = new View();
+		view.outputInputCarName();
+		String carName = GameUtil.removeAllSpaceInCarName(sc.nextLine());
+		Cars cars = new Cars(GameUtil.createCars(carName));
+
+		view.outputInputTryCount();
+		int tryCount = sc.nextInt();
+		Controller controller = new Controller(cars, view);
+		controller.proceedEachTurn(tryCount);
 	}
 }
