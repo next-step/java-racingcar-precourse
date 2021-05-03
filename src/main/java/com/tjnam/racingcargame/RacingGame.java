1bf942ca5bec56ab1938699e220e50f898dc10f8
TJ NAM <dynamicnam@gmail.com>
2021-05-04 00:22:15 +0900

    feat : Racing game 코드 구현
    게임 전체 진행 로직 작성.
    다른 클래스의 로직들 활용 및 일부 print문

--- src/main/java/com/tjnam/racingcargame/RacingGame.java
+++ src/main/java/com/tjnam/racingcargame/RacingGame.java
@@ -1,7 +1,47 @@
 package com.tjnam.racingcargame;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RacingGame {
+
+    private Cars cars;
+    private RacingTime racingTime;
+    private List<Car> winners;
+
+    private static CarNameInputGetter carNameInput = new CarNameInputGetter();
+    private static RacingTimeInputGetter racingTimeInput= new RacingTimeInputGetter();
+
     public void start(){
-        System.out.println("Racing Game is started");
+        List<CarName> carNameList = carNameInput.getCarNameInput();
+        List<Car> carsList = new ArrayList<Car>();
+        for (CarName carName : carNameList) {
+            carsList.add(new Car(carName));
+        }
+        cars = new Cars(carsList);
+        racingTime = new RacingTime(racingTimeInput.getUserInput());
+        race();
+    }
+
+    private void race(){
+        do {
+            cars.raceCars();
+            racingTime.use();
+        } while(racingTime.getRemained() > 0);
+        getWinner();
+        printWinner();
+    }
+
+    private void getWinner(){
+        winners = cars.getWinner();
+    }
+
+    private void printWinner(){
+        List<String> carNames = new ArrayList<String>();
+        for (Car car : winners){
+            carNames.add(car.getCarName());
+        }
+        System.out.print(String.join(", ", carNames));
+        System.out.println("가 최종 우승했습니다");
     }
 }
