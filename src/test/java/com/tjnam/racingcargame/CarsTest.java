421939477c6be7e3e7f39f46b05d3a3646a2a431
TJ NAM <dynamicnam@gmail.com>
2021-05-03 20:29:09 +0900

    Test : getWinner에 대한 함수 테스트 케이스 작성

--- src/test/java/com/tjnam/racingcargame/CarsTest.java
+++ src/test/java/com/tjnam/racingcargame/CarsTest.java
@@ -18,4 +18,39 @@ public class CarsTest {
         assertThat(carNames.size()).isEqualTo(3);
     }
 
+    @Test
+    public void getWinnerTest(){
+        List<Car> carList = new ArrayList<Car>();
+        Car car1 = new Car(new CarName("car1"));
+        int moveCnt = 10;
+        for (int i=0 ; i<moveCnt * 3 ; i++) {
+            car1.moveCar();
+        }
+
+        Car car2 = new Car(new CarName("car2"));
+        for (int i=0 ; i<moveCnt * 2 ; i++) {
+            car2.moveCar();
+        }
+
+        Car car3 = new Car(new CarName("car3"));
+        for (int i=0 ; i<moveCnt * 1 ; i++) {
+            car3.moveCar();
+        }
+        carList.add(car1);
+        carList.add(car2);
+        carList.add(car3);
+        Cars cars = new Cars(carList);
+
+        List<Car> winners = cars.getWinner();
+        assertThat(winners.get(0).getCarName()).isEqualTo("car3");
+        assertThat(winners.get(0).getPosition()).isEqualTo(moveCnt * 3);
+
+        for (int i=0 ; i<moveCnt * 1 ; i++) {
+            car2.moveCar();
+        }
+
+        winners = cars.getWinner();
+        assertThat(winners.size()).isEqualTo(2);
+    }
+
 }
