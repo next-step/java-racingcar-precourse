f695509b8482c600451032d2a39adec04c5babda
TJ NAM <dynamicnam@gmail.com>
2021-05-03 20:31:14 +0900

    feat : winner를 가져오는 방법 및 print 구현

--- src/main/java/com/tjnam/racingcargame/Cars.java
+++ src/main/java/com/tjnam/racingcargame/Cars.java
@@ -1,15 +1,40 @@
 package com.tjnam.racingcargame;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Cars {
 
-    private List<Car> cars = new ArrayList<Car>();
+    private List<Car> cars = new ArrayList<Car>();;
+    private List<Car> winners;
 
-    public Cars(List<CarName> carNames){
-        for(CarName name : carNames) {
-            cars.add(new Car(name));
+    public Cars(List<Car> carList){
+        for(Car car : carList) {
+            cars.add(car);
         }
     }
+
+    public void raceCars(){
+
+    }
+
+    public List<Car> getWinner(){
+        List<Car> winners = new ArrayList<Car>();
+        Collections.sort(cars);
+        int index = 0;
+        Car winner = cars.get(index);
+        int winnerPosition = winner.getPosition();
+        while(winnerPosition == cars.get(index).getPosition()) {
+            winners.add(cars.get(index));
+            index++;
+        }
+        return winners;
+    }
+
+    private void printCarsPositionStatus(){
+        cars.stream().forEach(car->car.printStatus());
+    }
+
+
 }
