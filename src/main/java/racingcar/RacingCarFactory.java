f163cd1d27a1eded8e3f055f9730b33add58d473
junseok <ixtears23@icloud.com>
2021-05-03 17:53:31 +0900

    refactor(factory) : 글자 수 validation 추가
    문자열을 split 한 후
    자동차 이름의 길이가 한글자보다 작거나
    다섯글자 보다 큰경우는 자동차가 생성되지 않도록
    글자 수 validation을 성공한 경우만 자동차 생성

--- src/main/java/racingcar/RacingCarFactory.java
+++ src/main/java/racingcar/RacingCarFactory.java
@@ -5,16 +5,25 @@ import java.util.List;
 
 public class RacingCarFactory {
 
+    private RacingCarFactory() {}
+
+    private static List<RacingCar> racingCars;
+
     public static List<RacingCar> create(String carsName) {
+        racingCars = new ArrayList<>();
+        String[] carNames = carsName.split(",");
 
-        String[] carName = carsName.split(",");
-        List<RacingCar> racingCars = new ArrayList<>();
-
-        for (String s : carName) {
-            RacingCar racingCar = new RacingCar(s);
-            racingCars.add(racingCar);
+        for (String carName : carNames) {
+            generateIfNameCountBetweenOneAndFive(carName);
         }
 
         return racingCars;
-    };
+    }
+
+    private static void generateIfNameCountBetweenOneAndFive(String carName) {
+        if (Validation.checkNameLength(carName)) {
+            RacingCar racingCar = new RacingCar(carName);
+            racingCars.add(racingCar);
+        }
+    }
 }
