0f957f166afe039d3e8460bdb3ec3e49060320c8
right42 <chjm452@naver.com>
2021-05-04 18:32:39 +0900

    feat: 랜덤 이동여부 전략 내용 구현

--- src/main/java/domain/car/strategy/RandomMovableStrategy.java
+++ src/main/java/domain/car/strategy/RandomMovableStrategy.java
@@ -1,8 +1,28 @@
 package domain.car.strategy;
 
+import java.util.Random;
+
 public class RandomMovableStrategy implements MovableStrategy {
+
+	public static final int BOUND = 10;
+
+	public static final int MIN_RANGE = 4;
+
+	public static final int MAX_RANGE = 9;
+
+	private final Random random = new Random();
+
 	@Override
 	public boolean movable() {
-		return false;
+		return isMovable();
+	}
+
+	private boolean isMovable() {
+		int randomValue = generateRandomValue();
+		return randomValue >= MIN_RANGE && randomValue <= MAX_RANGE;
+	}
+
+	private int generateRandomValue(){
+		return random.nextInt(BOUND);
 	}
 }
