ac8542b06c7cf6ec4364d027c9fd1f3c517d36ff
sangho lee <sangholee.dev@gmail.com>
2021-05-04 22:06:03 +0900

    feat: RaceCount 횟수 조회 추가

--- src/main/java/com/github/sangholeedev/javaracingcarprecourse/racing/RaceCount.java
+++ src/main/java/com/github/sangholeedev/javaracingcarprecourse/racing/RaceCount.java
@@ -1,5 +1,13 @@
 package com.github.sangholeedev.javaracingcarprecourse.racing;
 
 public class RaceCount {
+	private final int count;
 
+	public RaceCount(int count) {
+		this.count = count;
+	}
+
+	public int getCount() {
+		return count;
+	}
 }
--- src/test/java/com/github/sangholeedev/javaracingcarprecourse/racing/RaceCountTest.java
+++ src/test/java/com/github/sangholeedev/javaracingcarprecourse/racing/RaceCountTest.java
@@ -2,14 +2,27 @@ package com.github.sangholeedev.javaracingcarprecourse.racing;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RaceCountTest {
+	RaceCount raceCount;
+
+	@BeforeEach
+	void 초기레이트카운트_세팅() {
+		raceCount = new RaceCount(5);
+	}
+
 	@Test
 	@DisplayName("레이스카운트_생성_테스트")
 	void 레이스카운트_생성_테스트() {
-		RaceCount raceCount = new RaceCount();
 		assertThat(raceCount).isNotNull();
+	}
+
+	@Test
+	@DisplayName("레이스카운트_횟수_조회_테스트")
+	void 레이스카운트_횟수_조회_테스트() {
+		assertThat(raceCount.getCount()).isEqualTo(5);
 	}
 }
