e9a4c4568f8622904025b98e8a70cf0f9490c183
홍창의 <redcrow4@gmail.com>
2021-05-04 21:40:39 +0900

    refactor: 자동차 현재 주행거리 표시
    -자동차 이름 획득 방법 변경

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -1,20 +1,18 @@
 import static org.assertj.core.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTest {
 
 	private static Car classCar = new Car("테스트 자동차");
 
-	@BeforeEach
+	@BeforeAll
 	static void createCar() {
 		do {
 			classCar.applyForwardOrNot();
-		} while (1 != classCar.getCarMileage());
+		} while (10 > classCar.getCarMileage());
 	}
 
 	@Test
@@ -26,6 +24,19 @@ class CarTest {
 		} while (0 == car.getCarMileage());
 		assertThat(car.getCarMileage())
 			.isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("자동차 현재 주행거리 표시")
+	public void printCurrentCarMileage() {
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append(classCar.getCarName()).append(" : ");
+		for (int i = 0; i < classCar.getCarMileage(); i++) {
+			stringBuilder.append("-");
+		}
+		System.out.println(stringBuilder.toString());
+		assertThat(stringBuilder.toString())
+			.isEqualTo("테스트 자동차 : ----------");
 	}
 
 }
