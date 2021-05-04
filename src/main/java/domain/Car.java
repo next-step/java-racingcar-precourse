06117f7e6cdc90a2dd8e948fb6ab10a332e27b02
HeeSeok Noh <nhs0912@nate.com>
2021-05-04 14:57:55 +0900

    feat: Car Class and Test 구현 완료

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -2,6 +2,34 @@ package domain;
 
 public class Car {
 
+    private int distance;
+    private Name carName;
+    private static final int MOVABLE_NUMBER = 4;
+
+    public Car(Name carName) {
+        this(0, carName);
+    }
+
+    public Car(int distance, Name carName) {
+        this.distance = distance;
+        this.carName = carName;
+    }
+
+    public int distance() {
+        return this.distance;
+    }
+
+    public String carName() {
+        return this.carName.name();
+    }
+
+    public Car move() {
+        return new Car(this.distance + 1, this.carName);
+    }
+
+    public boolean isMovable(int number) {
+        return number > MOVABLE_NUMBER;
+    }
 
 
 }
---
+++ src/test/java/domain/CarTest.java
@@ -0,0 +1,53 @@
+package domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+class CarTest {
+
+    @Test
+    @DisplayName("Car의 위치와 이름을 입력받아서 생성 TEST")
+    void makeCar() {
+        Car car = new Car(1, new Name("test"));
+        assertAll(
+                () -> assertThat(car.carName()).isEqualTo("test"),
+                () -> assertThat(car.distance()).isEqualTo(1)
+        );
+    }
+
+    @Test
+    @DisplayName("Car이름만 입력받아서 생성 TEST")
+    void makeNameCar() {
+        Car car = new Car(new Name("test"));
+        assertAll(
+                () -> assertThat(car.carName()).isEqualTo("test"),
+                () -> assertThat(car.distance()).isEqualTo(0)
+        );
+    }
+
+    @Test
+    @DisplayName("Car move Test")
+    void moveCar() {
+        Car car = new Car(new Name("test"));
+        Car movedCar = car.move();
+        assertAll(
+                () -> assertThat(movedCar.carName()).isEqualTo("test"),
+                () -> assertThat(movedCar.distance()).isEqualTo(1)
+        );
+    }
+
+    @Test
+    @DisplayName("상수를 입력받아서 4초과이면 움직이고 4이하면 움직이지 않는 테스트")
+    void cannotMoveCar() {
+        Car car = new Car(new Name("test"));
+        assertAll(
+                () -> assertThat(car.isMovable(4)).isFalse(),
+                () -> assertThat(car.isMovable(5)).isTrue()
+
+        );
+    }
+
+}
