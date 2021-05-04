178148827aae557c11badfef9b8ee44d614ca248
최홍희 <vvshinevv@nhntoast.com>
2021-05-04 15:21:18 +0900

    feat : Exception 메시지 정리
    RacingCarGameExceptionMessage 클래스 추가 : final 한 변수로 변경

---
+++ src/main/java/com/racingcar/exception/RacingCarGameExceptionMessage.java
@@ -0,0 +1,7 @@
+package com.racingcar.exception;
+
+public class RacingCarGameExceptionMessage {
+
+    public static final String INVALID_CAR_NAME = "자동차 이름의 길이가 유효하지 않습니다.";
+    public static final String INVALID_RANDOM_VALUE = "랜덤을 생성된 값이 유효하지 않습니다.";
+}
