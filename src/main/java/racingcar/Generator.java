38f35b0e9c6bb2e9fc8ef8abccb3541fb67724a8
junseok <ixtears23@icloud.com>
2021-05-04 23:08:09 +0900

    refactor(random) : 문자열 하드코딩 제거
    1과 9는 변하지 않고
    1이 최소 숫자, 9가 최대 숫자이기 때문에
    1은 MIN_NUMBER, 9는 MAX_NUMBER 로 표시

--- src/main/java/racingcar/Generator.java
+++ src/main/java/racingcar/Generator.java
@@ -4,10 +4,13 @@ import java.util.Random;
 
 public class Generator {
 
+    private static final int MAX_NUMBER = 9;
+    private static final int MIN_NUMBER = 1;
+
     private Generator() {}
 
     public static int randomNumber() {
 
-        return new Random().nextInt(9) + 1;
+        return new Random().nextInt(MAX_NUMBER) + MIN_NUMBER;
     }
 }
