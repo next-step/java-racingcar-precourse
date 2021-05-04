ca70fb3b553fab50e8012721bec7c2088c25fe27
2carrot <changgeun.lee1@cj.net>
2021-05-04 12:29:37 +0900

    refactor : 우승자 관련 일급콜렉션 사용
    - 우승자 관련 별도 클래스로 분리

---
+++ src/main/java/racingcar/Winners.java
@@ -0,0 +1,18 @@
+package racingcar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Winners {
+	List<String> winners = new ArrayList<>();
+
+	public void add(String name) {
+		winners.add(name);
+	}
+
+	@Override
+	public String toString() {
+		return Arrays.toString(winners.toArray()).replaceAll("\\[","").replaceAll("]","");
+	}
+}
