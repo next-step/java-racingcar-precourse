019dd0a13845d51a3ad01c9bbfdc825649c6867f
insoo <leeis0613@gmail.com>
2021-05-04 20:28:29 +0900

    feat : 입력값을 통한 자동차 생성

---
+++ src/main/java/controller/CarController.java
@@ -0,0 +1,24 @@
+package controller;
+
+import domain.Car;
+import validation.CarValidation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarController {
+    public static String[] splitByComma(String inputCarName) {
+        String[] car = inputCarName.split(",");
+        CarValidation.doThings(car);
+        return car;
+    }
+
+    public static List<Car> generateCars(String inputCarName) {
+        List<Car> cars = new ArrayList<>();
+        for (String carName : splitByComma(inputCarName)) {
+            Car car = new Car(carName);
+            cars.add(car);
+        }
+        return cars;
+    }
+}
