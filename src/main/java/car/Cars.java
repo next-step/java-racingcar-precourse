35ff1774ac9c67be986d1551cde190bb9fa54e5b
frostnight <frostnightsong@gmail.com>
2021-05-04 21:10:11 +0900

    test: 자동차 경주 테스트 전체 자동차에 대해서 경주 테스트

---
+++ src/main/java/car/Cars.java
@@ -0,0 +1,35 @@
+package car;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Cars {
+
+	private List<Car> carList;
+
+	public Cars(String names) {
+		this.carList = mapCarList(names);
+	}
+
+	public static List<Car> mapCarList(String names) {
+		List<Car> carList = new ArrayList<>();
+		List<String> carNameList = Arrays.asList(names.split(","));
+		for (String name : carNameList) {
+			carList.add(new Car(name));
+		}
+		return carList;
+	}
+
+	public void race(List<Integer> movePoints) {
+		for (int i = 0; i < movePoints.size(); i++) {
+			Car car = carList.get(i);
+			car.updateMoveIndex(car.move(movePoints.get(i)));
+			carList.set(i, car);
+		}
+	}
+
+	List<Car> callCarList() {
+		return this.carList;
+	}
+}
---
+++ src/test/java/car/CarsTest.java
@@ -0,0 +1,46 @@
+package car;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarsTest {
+
+	private Cars cars;
+
+	@BeforeEach
+	void setUp() {
+		cars = new Cars("pobi,crong,honux");
+	}
+
+	@Test
+	@DisplayName("자동차들이 전부 움직였는지 확인")
+	void moveForwardAll() {
+		cars.race(Arrays.asList(4, 5, 8));
+		for (Car car : cars.callCarList()) {
+			assertThat(car.getCarIndex().getIndex()).isEqualTo(1);
+		}
+	}
+
+	@Test
+	@DisplayName("자동차들이 일부만 움직였는지 확인")
+	void moveForwardPartly() {
+		cars.race(Arrays.asList(4, 3, 8));
+		assertThat(cars.callCarList().get(0).getCarIndex().getIndex()).isEqualTo(1);
+		assertThat(cars.callCarList().get(1).getCarIndex().getIndex()).isEqualTo(0);
+		assertThat(cars.callCarList().get(2).getCarIndex().getIndex()).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("자동차들이 전혀 움직이지 않았는지 확인")
+	void moveStop() {
+		cars.race(Arrays.asList(3, 1, 2));
+		for (Car car : cars.callCarList()) {
+			assertThat(car.getCarIndex().getIndex()).isEqualTo(0);
+		}
+	}
+}
