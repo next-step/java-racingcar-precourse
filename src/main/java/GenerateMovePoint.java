b35b0ac3faa25197e5aac4d8e838e4bc150fd386
frostnight <frostnightsong@gmail.com>
2021-05-04 22:53:35 +0900

    refactor: 이동 점수 반환 리스트로 변환

--- src/main/java/GenerateMovePoint.java
+++ src/main/java/GenerateMovePoint.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class GenerateMovePoint {
 
 	private static final int RANDOM_MAX_VAL = 10;
 
-	public static int makeMovePoint() {
-		return new Random().nextInt(RANDOM_MAX_VAL);
+	public List<Integer> makeMovePointList(int carCount) {
+		List<Integer> movePointList = new ArrayList<>(carCount);
+		for (int i = 0; i < carCount; i++) {
+			movePointList.add(new Random().nextInt(RANDOM_MAX_VAL));
+		}
+		return movePointList;
 	}
 }
