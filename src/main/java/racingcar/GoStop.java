597aefa0615d5ef2ce2a13aadcfb4eb5b522ed99
ShinHan <tlsgks8668@naver.com>
2021-05-04 19:54:56 +0900

    feat: GO, STOP 기능에 대한 enum 생성

--- src/main/java/racingcar/GoStop.java
+++ src/main/java/racingcar/GoStop.java
@@ -1,2 +1,33 @@
-package racingcar;public enum GoStop {
+package racingcar;
+
+import java.util.Random;
+
+public enum GoStop {
+    GO, STOP;
+
+    private static Random random = new Random();
+
+    public static GoStop checkGo(int no) {
+        if (no > 9 || no < 0) {
+            throw new IllegalArgumentException("전진하는 조건은 0에서 9사이의 수만 입력할 수 있습니다.");
+        }
+
+        if (no < 4) {
+            return STOP;
+        }
+
+        return GO;
+    }
+
+    public static GoStop getRandomNumber() {
+        return checkGo(random.nextInt(10));
+    }
+
+    public boolean isGo() {
+        return GoStop.GO == this;
+    }
+
+    public boolean isStop() {
+        return GoStop.STOP == this;
+    }
 }
