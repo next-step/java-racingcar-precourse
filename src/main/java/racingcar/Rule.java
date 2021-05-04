9977afd813136ac264a1e8b1cff4ec5317344db0
junseok <ixtears23@icloud.com>
2021-05-04 23:11:58 +0900

    refactor(rule) : 하드코딩된 숫자 멤버 변수로 변경
    변하지 않는 수이기 때문에
    하드코딩을 제거하고 멤버 변수 상수로 이동 시킴.

--- src/main/java/racingcar/Rule.java
+++ src/main/java/racingcar/Rule.java
@@ -2,9 +2,11 @@ package racingcar;
 
 public class Rule {
 
+    private static final int FORWARD_NUMBER = 4;
+
     private Rule() {}
 
     public static boolean forward(int number) {
-        return number >= 4;
+        return number >= FORWARD_NUMBER;
     }
 }
