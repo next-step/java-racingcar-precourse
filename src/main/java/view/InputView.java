3d46b428f168f9155012441f3e1c5f454f3040d5
ohgillwhan <soora_genius@naver.com>
2021-05-04 21:21:58 +0900

    refact : InputView의 맞춤법 수정

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -14,13 +14,13 @@ public class InputView {
     }
 
     public CarNames carNames() {
-        System.out.println(String.format("경주할자동차이름을입력하세요.(이름은쉼표(%s)기준으로구분)", CarNames.NAME_SPLITTER));
+        System.out.println(String.format("경주할 자동차 이름을 입력하세요.(이름은 쉼표(%s) 기준으로 구분)", CarNames.NAME_SPLITTER));
 
         return new CarNames(scanner.next());
     }
 
     public TryCount tryCount() {
-        System.out.println("시도할회수는몇회인가요?");
+        System.out.println("시도할 회수는 몇회인가요?");
 
         return new TryCount(scanner.next());
     }
