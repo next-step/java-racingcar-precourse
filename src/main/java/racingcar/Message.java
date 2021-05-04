54570a2e5c1c685d0344b6be2fa9a5a6a2455482
ShinHan <tlsgks8668@naver.com>
2021-05-04 19:57:30 +0900

    feat: 출력 메세지 클래스 생성

--- src/main/java/racingcar/Message.java
+++ src/main/java/racingcar/Message.java
@@ -1,2 +1,10 @@
-package racingcar;public class Message {
+package racingcar;
+
+public class Message {
+    public static final String INPUT_CAR_NAME = "경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)";
+    public static final String ATTEMPT_COUNT = "시도할 회수는 몇 회인가요?";
+    public static final String GAME_RESULT = "실행 결과";
+    public static final String WIN_MESSAGE = "이(가) 최종 우승했습니다.";
+    public static final String CAR_NAME_5_MORE_THAN_ERROR = "자동차 이름은 5자 이하로만 가능합니다.";
+    public static final String ATTEMPT_COUNT_NUMBER_ERROR = "시도할 횟수는 숫자만 입력이 가능합니다.";
 }
