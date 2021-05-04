8ce08dc2fccc63d88c9e6ed87604dde2a17c9d76
sangho lee <sangholee.dev@gmail.com>
2021-05-04 21:58:19 +0900

    fix: 자동차 이름 5자 포함 안되던 문제 수정

--- src/main/java/com/github/sangholeedev/javaracingcarprecourse/car/CarName.java
+++ src/main/java/com/github/sangholeedev/javaracingcarprecourse/car/CarName.java
@@ -4,7 +4,7 @@ public class CarName {
 	private final String name;
 
 	public CarName(String name) {
-		if(name.length() >= 5) {
+		if(name.length() > 5) {
 			throw new IllegalArgumentException("이름이 5자가 넘습니다.");
 		}
 		this.name = name;
