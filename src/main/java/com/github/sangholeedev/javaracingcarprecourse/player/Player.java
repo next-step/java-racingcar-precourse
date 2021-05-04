f50d672e933e05ad8c25500e6ac8c94ceaa46ffa
sangho lee <sangholee.dev@gmail.com>
2021-05-04 21:57:46 +0900

    feat: 사용자 자동차이름 리스트 분류 추가

--- src/main/java/com/github/sangholeedev/javaracingcarprecourse/player/Player.java
+++ src/main/java/com/github/sangholeedev/javaracingcarprecourse/player/Player.java
@@ -1,4 +1,30 @@
 package com.github.sangholeedev.javaracingcarprecourse.player;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.github.sangholeedev.javaracingcarprecourse.car.CarName;
+
 public class Player {
+	private List<CarName> carNames;
+	private static final String SPLIT_WORD = ",";
+
+	public Player(final String carNamesString) {
+		this.carNames = splitCarNameString(carNamesString);
+		if(carNames.isEmpty()) {
+			throw new IllegalArgumentException("잘못된 이름입니다.");
+		}
+	}
+
+	private List<CarName> splitCarNameString(String carNamesString) {
+		List<CarName> carNames = new ArrayList<>();
+		for (String carName : carNamesString.split(SPLIT_WORD)) {
+			carNames.add(new CarName(carName));
+		}
+		return carNames;
+	}
+
+	public List<CarName> getCarNames() {
+		return carNames;
+	}
 }
--- src/test/java/com/github/sangholeedev/javaracingcarprecourse/player/PlayerTest.java
+++ src/test/java/com/github/sangholeedev/javaracingcarprecourse/player/PlayerTest.java
@@ -6,10 +6,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
+
 	@Test
-	@DisplayName("사용자_생성테스트")
-	void 사용자_생성테스트() {
-		Player player = new Player();
-		assertThat(player).isNotNull();
+	@DisplayName("사용자_경주자동차이름_입력")
+	void 사용자_경주자동차이름_입력() {
+		final String carNameString = "pobi,crong,honux";
+		Player player = new Player(carNameString);
+		assertThat(player.getCarNames()).hasSize(3);
 	}
 }
