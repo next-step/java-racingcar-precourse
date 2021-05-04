d518d7e4f95cf22a64024d8a3b33e0c956212704
홍창의 <redcrow4@gmail.com>
2021-05-04 22:25:37 +0900

    test: 레이스 1회 수행 및 주행거리 표시

--- src/test/java/CarsTest.java
+++ src/test/java/CarsTest.java
@@ -1,11 +1,24 @@
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarsTest {
 
-	private static Cars classCars = new Cars(null, 10);
+	private static Cars classCars;
+
+	@BeforeAll
+	static void createCars() {
+		List<Car> carList = new ArrayList<>();
+		for (int i = 0; i < 10; i++) {
+			carList.add(new Car("자동차" + i));
+		}
+		classCars = new Cars(carList, 10);
+	}
 
 	@Test
 	@DisplayName("자동차 이름 목록 검증")
@@ -25,6 +38,17 @@ class CarsTest {
 		assertThat(classCars.isValidCar(carNamesArr1)).withFailMessage("carNamesArr1").isTrue();
 		assertThat(classCars.isValidCar(carNamesArr2)).withFailMessage("carNamesArr2").isFalse();
 		assertThat(classCars.isValidCar(carNamesArr3)).withFailMessage("carNamesArr3").isFalse();
+	}
+
+	@Test
+	@DisplayName("레이스 1회 수행 및 주행거리 표시")
+	public void goRace() {
+		classCars.goRace();
+		List<Car> carList = classCars.getCarList();
+		for (Car car : carList) {
+			assertThat(car.getCarMileage())
+				.isBetween(0, 1);
+		}
 	}
 
 }
