0c6421148112d437539ff1bdcfa036ccf11b7b3d
symba <sybarits@gmail.com>
2021-10-07 21:27:05 +0900

    feat: 게임 안내 문구 출력 기능 구현
    게임 안내 문구 출력을 위한 클래스 구현

---
+++ src/main/java/racinggame/GamePrint.java
@@ -0,0 +1,26 @@
+package racinggame;
+
+import racinggame.car.CarList;
+
+public class GamePrint {
+
+    public void printCarNameInput() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+    }
+
+    public void printTryCountInput() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public void printResult() {
+        System.out.println("실행 결과");
+    }
+
+    public void printProcessing(CarList list) {
+        System.out.println(list.getProcessResult());
+    }
+
+    public void printWinner(CarList list) {
+        System.out.println("최종 우승자는 " + list.getWinners() + " 입니다.");
+    }
+}
