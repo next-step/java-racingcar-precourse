3c97f1c25abf87f868fd1a5fcc85d2a1b850ec53
kcri <67crystalk@naver.com>
2021-10-08 22:26:44 +0900

    feat : 게임 출력에 관한 기능

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,12 +1,13 @@
 package racinggame;
 
 import racinggame.view.InputView;
+import racinggame.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
         // TODO 자동차 경주 게임 구현
         RacingGame game = InputView.inputCarNames();
         game.setTryCnt(Integer.parseInt(InputView.inputTryCount()));
-        game.run();
+        OutputView.printRacingGame(game);
     }
 }
