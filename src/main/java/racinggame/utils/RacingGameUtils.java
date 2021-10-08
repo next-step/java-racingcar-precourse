f6aba1fdd8ce242c956fd192fcad54bb0a99a2b3
seonghyeoklee <dltjdgur327@gmail.com>
2021-10-09 04:19:31 +0900

    feat : 쉼표 기준으로 구분한 자동차 이름의 앞뒤 공백을 제거한다.

--- src/main/java/racinggame/utils/RacingGameUtils.java
+++ src/main/java/racinggame/utils/RacingGameUtils.java
@@ -3,17 +3,23 @@ package racinggame.utils;
 public class RacingGameUtils {
 
     private static final String SPLIT_REGEX = ",";
+    private static final int RACING_CAR_NAME_LENGTH_LIMIT = 5;
 
     public static String[] racingCarNameSplit(String racingCarNameInput) {
         return racingCarNameInput.split(SPLIT_REGEX);
     }
 
-    public static boolean isValidRacingCarNameLength(String racingCarNameInput) {
-        for (String racingCarName : racingCarNameSplit(racingCarNameInput)) {
-            if (racingCarName.trim().length() > 5) {
-                return false;
-            }
+    public static boolean isValidRacingCarNameLength(String racingCarName) {
+        if (racingCarName.length() > RACING_CAR_NAME_LENGTH_LIMIT) {
+            return false;
         }
         return true;
+    }
+
+    public static String[] trim(String[] target) {
+        for (int i = 0; i < target.length; i++) {
+            target[i] = target[i].trim();
+        }
+        return target;
     }
 }
--- src/test/java/racinggame/utils/RacingGameUtilsTest.java
+++ src/test/java/racinggame/utils/RacingGameUtilsTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static racinggame.utils.RacingGameUtils.isValidRacingCarNameLength;
-import static racinggame.utils.RacingGameUtils.racingCarNameSplit;
+import static racinggame.utils.RacingGameUtils.*;
 
 public class RacingGameUtilsTest {
 
@@ -16,22 +15,39 @@ public class RacingGameUtilsTest {
                         , "pobi,crong,honux,elice,mark:5" }
                         , delimiter = ':')
     void 입력받은_자동차_이름을_쉼표_기준으로_구분한다(String racingCarNameInput, int lengthInput) {
-        String[] racingCarNameSplit = racingCarNameSplit(racingCarNameInput);
-
-        assertThat(racingCarNameSplit.length).isEqualTo(lengthInput);
+        assertThat(racingCarNameSplit(racingCarNameInput).length).isEqualTo(lengthInput);
     }
 
     @ParameterizedTest(name = "자동차 이름 [{0}] 길이가 5자 초과면 False 이다")
-    @ValueSource(strings = { "pobi,crong,javaji,javamaster"
+    @ValueSource(strings = {  "javaji,javamaster"
                             , "abraham,aaliyah" })
     void 쉼표_기준으로_구분한_자동차_이름의_길이가_5자_초과_False(String racingCarNameInput) {
-        assertThat(isValidRacingCarNameLength(racingCarNameInput)).isFalse();
+        for (String racingCarName : racingCarNameSplit(racingCarNameInput)) {
+            assertThat(isValidRacingCarNameLength(racingCarName)).isFalse();
+        }
     }
 
     @ParameterizedTest(name = "자동차 이름 [{0}] 길이가 5자 이하면 True 이다")
-    @ValueSource(strings = { "pobi,crong,honux"
+    @ValueSource(strings = {  "pobi,crong,honux"
                             , "raham,liyah" })
     void 쉼표_기준으로_구분한_자동차_이름의_길이가_5자_이하_True(String racingCarNameInput) {
-        assertThat(isValidRacingCarNameLength(racingCarNameInput)).isTrue();
+        for (String racingCarName : racingCarNameSplit(racingCarNameInput)) {
+            assertThat(isValidRacingCarNameLength(racingCarName)).isTrue();
+        }
     }
+
+    @ParameterizedTest(name = "자동차 이름 [{0}] 공백을 제거한다")
+    @CsvSource(value = {  "pobi ,crong ,honux :pobi,crong,honux"
+                        , " pobi, crong, honux:pobi,crong,honux"
+                        , " pobi , crong , honux :pobi,crong,honux" }
+                        , delimiter = ':')
+    void 쉼표_기준으로_구분한_자동차_이름의_앞뒤_공백을_제거한다(String before, String after) {
+        String[] beforeNames = trim(racingCarNameSplit(before));
+        String[] afterNames = racingCarNameSplit(after);
+
+        for (int i = 0; i < beforeNames.length; i++) {
+            assertThat(beforeNames[i]).isEqualTo(afterNames[i]);
+        }
+    }
+
 }
