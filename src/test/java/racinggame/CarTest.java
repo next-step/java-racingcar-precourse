04458855b3905a2374b4ffac7845ed58b1554e33
herbtea <herbtea30@gmail.com>
2021-10-08 17:30:09 +0900

    refactor : 자동차 객체 테스트 코드 리팩토링

--- src/test/java/racinggame/CarTest.java
+++ src/test/java/racinggame/CarTest.java
@@ -1,10 +1,19 @@
 package racinggame;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CarTest {
+
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("dony");
+    }
+
     @Test
     void 자동차_객체_생성() {
         assertEquals("자동차 이름은 필수 입니다. 입력해 주세요.", assertThrows(IllegalArgumentException.class, () -> new Car()).getMessage());
@@ -12,22 +21,23 @@ public class CarTest {
 
     @Test
     void 자동차_이름을_입력하여_객체_생성() {
-        Car car = new Car("dony");
         assertEquals(car.getName(), "dony");
     }
 
     @Test
     void 자동차_이름_길이_오류체크() {
-        assertEquals("자동차 이름은 1~5자로 입력해 주세요.", assertThrows(IllegalArgumentException.class, () -> new Car("donydony")).getMessage());
-        assertEquals("자동차 이름은 1~5자로 입력해 주세요.", assertThrows(IllegalArgumentException.class, () -> new Car("도니도니도니")).getMessage());
-        assertEquals("자동차 이름은 1~5자로 입력해 주세요.", assertThrows(IllegalArgumentException.class, () -> new Car("dony!!")).getMessage());
+        assertCarNameLengthOneToFive("donydony");
+        assertCarNameLengthOneToFive("도니도니도니");
+        assertCarNameLengthOneToFive("dony!!");
+    }
+
+    private void assertCarNameLengthOneToFive(String name) {
+        assertEquals("자동차 이름은 1~5자로 입력해 주세요.", assertThrows(IllegalArgumentException.class, () -> new Car(name)).getMessage());
     }
 
     @Test
     void 자동차_이름_중복체크() {
-        String name = "dony";
-        Car car1 = new Car(name);
-        Car car2 = new Car(name);
-        assertTrue(car1.equals(car2));
+        Car car1 = new Car("dony");
+        assertTrue(car.equals(car1));
     }
 }
