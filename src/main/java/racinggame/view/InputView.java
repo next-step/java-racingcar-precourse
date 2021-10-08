2bc961b3d9b237e207eb3c5a17735c232698784f
uyoung <uyoung5410@gmail.com>
2021-10-09 06:34:20 +0900

    feat: 자동차 이름을 입력받아 자동차 생성기능 구현

---
+++ src/main/java/racinggame/view/InputView.java
@@ -0,0 +1,28 @@
+package racinggame.view;
+
+import nextstep.utils.Console;
+import racinggame.domain.Cars;
+
+public class InputView {
+    public Cars inputCarName() {
+        try {
+            System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+            String carNamesInput = Console.readLine();
+            validateCarNamesString(carNamesInput);
+            return new Cars(splitCarNamesString(carNamesInput));
+        } catch (IllegalArgumentException iae) {
+            System.out.println(iae.getMessage());
+            return inputCarName();
+        }
+    }
+
+    private void validateCarNamesString(String carNamesInput) {
+        if (carNamesInput.trim().length() == 0) {
+            throw new IllegalArgumentException("[ERROR] 이름들을 쉼표(,)로 구분하여 입력해주세요.");
+        }
+    }
+
+    private String[] splitCarNamesString(String carNamesString) {
+        return carNamesString.split(",");
+    }
+}
