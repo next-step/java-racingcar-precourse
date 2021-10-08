5b10ceb04d913206fc55cd57cc42345764bc4417
pih6585 <pih6585@naver.com>
2021-10-08 13:25:30 +0900

    feat : 자동차 이름을 ,로 구분하여 생성하는 기능 추가

--- src/main/java/racinggame/model/Cars.java
+++ src/main/java/racinggame/model/Cars.java
@@ -1,14 +1,38 @@
 package racinggame.model;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class Cars {
 
+	private static final String REGEX = ",";
+
 	private final List<Car> cars;
 
 	public Cars(List<Car> cars) {
 		this.cars = cars;
+	}
+
+	public Cars(String name) {
+		String[] carsName = toSplit(name);
+		cars = toList(carsName);
+	}
+
+	private String[] toSplit(String carNames) {
+		return carNames.split(REGEX);
+	}
+
+	private List<Car> toList(String[] carNames) {
+		List<Car> cars = new ArrayList<>();
+		for (String carName : carNames) {
+			cars.add(new Car(carName));
+		}
+		return cars;
+	}
+
+	public List<Car> getCars() {
+		return cars;
 	}
 
 	@Override
--- src/test/java/racinggame/model/CarsTest.java
+++ src/test/java/racinggame/model/CarsTest.java
@@ -6,6 +6,8 @@ import java.util.Arrays;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class CarsTest {
 
@@ -15,5 +17,14 @@ class CarsTest {
 		Cars cars = new Cars(Arrays.asList(new Car("aaa"), new Car("bbb")));
 
 		assertThat(cars).isEqualTo(new Cars(Arrays.asList(new Car("aaa"), new Car("bbb"))));
+	}
+
+	@ParameterizedTest
+	@DisplayName("자동차이름들은 , 구분자로 분리된다. ")
+	@CsvSource(value = {"aa,bb,cc;3", "aa,bb;2", "aa.bb;1", "aa:bb,cc;2"}, delimiter = ';')
+	public void toSplit(String name, int nameCount) {
+		Cars cars = new Cars(name);
+
+		assertThat(cars.getCars().size()).isEqualTo(nameCount);
 	}
 }
