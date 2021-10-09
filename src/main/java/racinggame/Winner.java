f3aa786065b049dd2cf807f3da003db31a7fda36
herbtea <herbtea30@gmail.com>
2021-10-09 17:01:36 +0900

    feat : TDD 자동차 경주게임 우승자 테스트 및 우승자 판별기능 구현

---
+++ src/main/java/racinggame/Winner.java
@@ -0,0 +1,37 @@
+package racinggame;
+
+import java.util.Collections;
+import java.util.Comparator;
+
+/**
+ * Name : Winner <br/>
+ * Description : 우승자 클래스
+ */
+public class Winner {
+    public static final String NAME_SEPARATOR = ",";
+    private Cars users;
+
+    public Winner(Cars users) {
+        this.users = users;
+    }
+
+    public String getWinner() {
+        StringBuilder winnerName = new StringBuilder();
+        int maxPosition = Collections.max(users.getUsers(), Comparator.comparingInt(Car::getPosition)).getPosition();
+        for (Car user : users.getUsers()) {
+            mapWinnerName(winnerName, maxPosition, user);
+        }
+
+        return replaceLastSeparator(winnerName);
+    }
+
+    private String replaceLastSeparator(StringBuilder result) {
+        return result.substring(0, result.lastIndexOf(NAME_SEPARATOR));
+    }
+
+    private void mapWinnerName(StringBuilder winnerName, int maxPosition, Car user) {
+        if (maxPosition == user.getPosition()) {
+            winnerName.append(user.getName()).append(",");
+        }
+    }
+}
---
+++ src/test/java/racinggame/WinnerTest.java
@@ -0,0 +1,54 @@
+package racinggame;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class WinnerTest {
+
+    private Cars users;
+    private Winner winner;
+    private Car dony;
+    private Car chuny;
+    private Car junny;
+
+    @BeforeEach
+    void setUp() {
+        dony = new Car("dony");
+        chuny = new Car("chuny");
+        junny = new Car("junny");
+        users = new Cars(Arrays.asList(dony, chuny, junny));
+        winner = new Winner(users);
+    }
+
+    @Test
+    void 우승자_1명_확인() {
+        dony.move(4);
+        chuny.move(3);
+        junny.move(2);
+        dony.move(6);
+        chuny.move(7);
+        junny.move(8);
+        assertEquals(dony.getPosition(), 2);
+        assertEquals(chuny.getPosition(), 1);
+        assertEquals(junny.getPosition(), 1);
+        assertEquals(winner.getWinner(), "dony");
+    }
+
+    @Test
+    void 우승자_2명_확인() {
+        dony.move(4);
+        chuny.move(5);
+        junny.move(2);
+        dony.move(7);
+        chuny.move(9);
+        junny.move(4);
+        assertEquals(dony.getPosition(), 2);
+        assertEquals(chuny.getPosition(), 2);
+        assertEquals(junny.getPosition(), 1);
+        assertEquals(winner.getWinner(), "dony,chuny");
+    }
+}
