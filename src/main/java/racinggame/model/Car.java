91192129c7b8e54d8f2a6efb3b1b790ffc43f117
SC <pch838811@gmail.com>
2021-10-09 14:42:18 +0900

    feat : 자동차를 생성
    - 자동차의 이름에 대한 유효성 검증

---
+++ src/main/java/racinggame/model/Car.java
@@ -0,0 +1,42 @@
+package racinggame.model;
+
+import java.util.Optional;
+
+public class Car {
+
+	private static final int NAME_MAX_LENGTH = 5;
+	private static final String NAME_EXCEPTION_MESSAGE = "자동차의 이름이 잘못되었습니다.";
+	private static final String NAME_LENGTH_EXCEPTION_MESSAGE = "자동차는 %d글자 이하의 이름이어야 합니다.";
+
+	private static final String EMPTY_STRING = "";
+	private final String name;
+
+	private Car(String name) {
+		validateName(name);
+		this.name = name;
+	}
+
+	public static Car valueOf(String name) {
+		return new Car(name);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	private void validateName(String name) {
+		if (isNullOrEmpty(name)) {
+			throw new IllegalArgumentException(NAME_EXCEPTION_MESSAGE);
+		}
+		if (name.length() > NAME_MAX_LENGTH) {
+			throw new IllegalArgumentException(String.format(NAME_LENGTH_EXCEPTION_MESSAGE, NAME_MAX_LENGTH));
+		}
+	}
+
+	private boolean isNullOrEmpty(String name) {
+		return Optional.ofNullable(name)
+			.map(String::trim)
+			.orElse(EMPTY_STRING)
+			.isEmpty();
+	}
+}
