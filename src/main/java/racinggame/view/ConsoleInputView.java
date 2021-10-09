6ece9536fa85a8329824e586007c9ef91500b27a
kimdonggyu-dev <donggyu.dev@gmail.com>
2021-10-09 16:44:34 +0900

    refactor: 이름 입력 문구 수정

--- src/main/java/racinggame/view/ConsoleInputView.java
+++ src/main/java/racinggame/view/ConsoleInputView.java
@@ -6,7 +6,7 @@ import racinggame.domain.exceptions.BusinessException;
 public class ConsoleInputView {
 
     public String enterNames() {
-        System.out.println("경주할자동차이름을입력하세요.(이름은쉼표(,)기준으로구분)");
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return Console.readLine();
     }
 
