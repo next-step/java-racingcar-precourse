ae412023e871168fc52dcee36a894cf638cfda74
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-10-10 02:46:53 +0900

    feat: 자동차의 대한 이름, 이동결과, 랭킹을 보유한 객체

---
+++ src/main/java/racinggame/domain/Car.java
@@ -0,0 +1,47 @@
+package racinggame.domain;
+
+import racinggame.common.Constant;
+
+public class Car {
+
+    private String name;
+
+    private String result;
+
+    private int rank;
+
+    public Car(String name) {
+        this.name = name;
+        this.result = Constant.STR_EMPTY;
+    }
+
+    public void move(int number) {
+        if (number >= 4) {
+            this.result += "-";
+        }
+        if (number < 4) {
+            this.result += "";
+        }
+    }
+
+    public void initial() {
+        this.rank = 1;
+    }
+
+    public void compare(Car car) {
+        if (this.result.length() < car.result.length()) {
+            this.rank += 1;
+        }
+    }
+
+    public String firstRank() {
+        if (this.rank == 1) {
+            return this.name;
+        }
+        return Constant.STR_EMPTY;
+    }
+
+    public String status() {
+        return this.name + " : " + this.result;
+    }
+}
