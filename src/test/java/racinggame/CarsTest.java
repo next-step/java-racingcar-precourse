2f60455018928e20a79c86ba0f97af5263927633
lewisseo91 <togokr@naver.com>
2021-10-09 23:51:31 +0900

    feat(test): CarsTest 테스트 케이스 추가
    CarsTest.java 테스트 케이스들 추가
     - 전반적으로 mockRandom 에서 random 숫자 return 을 고정하기 위해 소스 참고
       - mokRandoms -> when -> thenReturn 이용
     - cars a,b,c 이름으로 생성 후 단일 전진 테스트
     - cars a,b,c 이름으로 생성 후 단일 멈춤 테스트 추가
     - cars a,b,c 이름으로 생성 후 a,b,c 전진 테스트 추가
     - cars a,b,c 이름으로 생성 후 a,b,c 멈춤 테스트 추가
     - cars a,b,c 이름으로 생성 후 a,b 전진 c 멈춤 테스트 추가

---
+++ src/test/java/racinggame/CarsTest.java
@@ -0,0 +1,90 @@
+package racinggame;
+
+import nextstep.utils.Randoms;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mockStatic;
+
+public class CarsTest {
+    private Cars cars;
+
+    @BeforeEach
+    void setup() {
+        cars = new Cars(Arrays.asList("a", "b", "c"));
+    }
+
+    @Test
+    void cars_a_전진() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms
+                    .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(4);
+            cars.play();
+
+            assertTrue(cars.getCars().get(0).isMovableForward());
+            assertThat(cars.getCars().get(0).getMoveCount()).isEqualTo(1);
+        }
+    }
+
+    @Test
+    void cars_a_후퇴() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms
+                    .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(0);
+            cars.play();
+
+            assertFalse(cars.getCars().get(0).isMovableForward());
+            assertThat(cars.getCars().get(0).getMoveCount()).isEqualTo(0);
+        }
+    }
+
+    @Test
+    void cars_a_b_c_전진() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms
+                    .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(4, 5, 9);
+            cars.play();
+
+            assertThat(cars.getCars().get(0).getMoveCount()).isEqualTo(1);
+            assertThat(cars.getCars().get(1).getMoveCount()).isEqualTo(1);
+            assertThat(cars.getCars().get(2).getMoveCount()).isEqualTo(1);
+        }
+    }
+
+    @Test
+    void cars_a_b_c_후퇴() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms
+                    .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(1, 3, 0);
+            cars.play();
+
+            assertThat(cars.getCars().get(0).getMoveCount()).isEqualTo(0);
+            assertThat(cars.getCars().get(1).getMoveCount()).isEqualTo(0);
+            assertThat(cars.getCars().get(2).getMoveCount()).isEqualTo(0);
+        }
+    }
+
+    @Test
+    void cars_a_b_전진_c_후퇴() {
+        try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+            mockRandoms
+                    .when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+                    .thenReturn(4, 5, 0);
+            cars.play();
+
+            assertThat(cars.getCars().get(0).getMoveCount()).isEqualTo(1);
+            assertThat(cars.getCars().get(1).getMoveCount()).isEqualTo(1);
+            assertThat(cars.getCars().get(2).getMoveCount()).isEqualTo(0);
+        }
+    }
+}
