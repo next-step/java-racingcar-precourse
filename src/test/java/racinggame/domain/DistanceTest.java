058d31ef9c3712e8e75b537d01bfb62f77a488ba
kimdonggyu-dev <donggyu.dev@gmail.com>
2021-10-09 19:22:38 +0900

    test: Distance 테스트코드 추가

--- src/test/java/racinggame/domain/DistanceTest.java
+++ src/test/java/racinggame/domain/DistanceTest.java
@@ -2,6 +2,10 @@ package racinggame.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DistanceTest {
@@ -9,11 +13,26 @@ class DistanceTest {
     @Test
     void 생성() {
         assertEquals(Distance.of(3), Distance.of(3));
+        assertEquals(Distance.of(Distance.of(3)), Distance.of(3));
+        assertEquals(Distance.of(3).value(), 3);
+    }
+
+    @Test
+    void 이동시_거리_하나증가() {
+        assertEquals(Distance.move(Distance.of(3)), Distance.of(4));
+        assertEquals(Distance.move(Distance.of(3)).value(), Distance.of(4).value());
     }
 
     @Test
     void 이동거리_증가() {
         assertEquals(Distance.move(Distance.of(3)), Distance.of(4));
+    }
+
+    @Test
+    void 정렬시_오름차순_검증() {
+        List<Distance> distances = Arrays.asList(Distance.of(5), Distance.of(3), Distance.of(4));
+        Collections.sort(distances);
+        assertEquals(distances, Arrays.asList(Distance.of(3), Distance.of(4), Distance.of(5)));
     }
 
 }
