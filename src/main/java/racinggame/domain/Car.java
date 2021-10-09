d938710185249f10a4416043377c4a9ce99b3c64
gunan <power4889@gmail.com>
2021-10-09 19:05:43 +0900

    refactor(Car): 로컬상수 이름을 VALUE -> NUMBER로 일관성있게 통일.

--- src/main/java/racinggame/domain/Car.java
+++ src/main/java/racinggame/domain/Car.java
@@ -4,8 +4,8 @@ import nextstep.utils.Randoms;
 
 public class Car {
 	private static final int MOVABLE_MIN_NUMBER = 4;
-	private static final int RANDOM_MIN_VALUE = 0;
-	private static final int RANDOM_MAX_VALUE = 9;
+	private static final int RANDOM_MIN_NUMBER = 0;
+	private static final int RANDOM_MAX_NUMBER = 9;
 	private final Name name;
 	private final Distance distance;
 
@@ -21,7 +21,7 @@ public class Car {
 	}
 
 	private boolean isMovable() {
-		return Randoms.pickNumberInRange(RANDOM_MIN_VALUE, RANDOM_MAX_VALUE) >= MOVABLE_MIN_NUMBER;
+		return Randoms.pickNumberInRange(RANDOM_MIN_NUMBER, RANDOM_MAX_NUMBER) >= MOVABLE_MIN_NUMBER;
 	}
 
 	public Distance getDistance() {
