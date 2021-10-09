4e095ad33c1ae6ba24a6aaf0a66163b9e1a6fc8d
kimdonggyu-dev <donggyu.dev@gmail.com>
2021-10-09 19:19:07 +0900

    test: Name 테스트 코드 추가

--- src/test/java/racinggame/domain/NameTest.java
+++ src/test/java/racinggame/domain/NameTest.java
@@ -6,14 +6,16 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.NoSuchElementException;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class NameTest {
 
     @Test
     void 생성() {
-        assertDoesNotThrow(() -> Name.of("abc").equals(Name.of("abc")));
+        assertEquals(Name.of("abc"), Name.of("abc"));
+        assertEquals(Name.of(Name.of("abc")), Name.of("abc"));
+        assertEquals(Name.of("abc").value(), "abc");
     }
 
     @ParameterizedTest
