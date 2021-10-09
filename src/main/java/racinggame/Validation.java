d11f737239bb0abc8796b74f00892ecb35372e48
김민지 <21600069@handong.edu>
2021-10-09 16:43:21 +0900

    fix : 자동차 이름 글자수 Error msg 표시

--- src/main/java/racinggame/Validation.java
+++ src/main/java/racinggame/Validation.java
@@ -2,7 +2,14 @@ package racinggame;
 
 public class Validation {
 
+
+    public static final String ERROR_MSG = "[ERROR] 자동차 이름은 5글자 이하로 해주세요";
+
     public Boolean checkCarNmaeLen(String carName) {
-        return carName.length() <= 5;
+        if (carName.length() <= 5) {
+            return true;
+        }
+        System.out.println(ERROR_MSG);
+        return false;
     }
 }
