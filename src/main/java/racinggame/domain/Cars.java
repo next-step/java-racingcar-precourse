de10ca13df6262110b4b90bf58678bfe1fda8a42
gunan <power4889@gmail.com>
2021-10-09 19:07:03 +0900

    feat(Cars): Car의 일급컬렉션 클래스인 Cars 추가.

---
+++ src/main/java/racinggame/domain/Cars.java
@@ -0,0 +1,43 @@
+package racinggame.domain;
+
+import java.util.List;
+
+public class Cars {
+	private final List<Car> cars;
+
+	public Cars(List<Car> cars) {
+		this.cars = cars;
+		checkCarsSize();
+		if (cars.size() >= 2) {
+			checkSameName();
+		}
+	}
+
+	public List<Car> getCars() {
+		return cars;
+	}
+
+	public void move() {
+		for (Car car : cars) {
+			car.move();
+		}
+	}
+
+	private void checkCarsSize() {
+		if (cars == null || cars.size() <= 0) {
+			throw new IllegalArgumentException("자동차를 한 대 이상 등록해주세요");
+		}
+	}
+
+	private void checkSameName() {
+		for (int i = 0; i < cars.size(); i++) {
+			checkSameName(cars.get(i), cars.get((i + 1) % cars.size()));
+		}
+	}
+
+	private void checkSameName(Car car, Car nextCar) {
+		if (car.getName().equals(nextCar.getName())) {
+			throw new IllegalArgumentException("자동차 이름은 중복될 수 없습니다.");
+		}
+	}
+}
---
+++ src/test/java/racinggame/domain/CarsTest.java
@@ -0,0 +1,42 @@
+package racinggame.domain;
+
+import nextstep.utils.Randoms;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mockStatic;
+
+public class CarsTest {
+	@Test
+	void moveTest() {
+		try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+			mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+					.thenReturn(4, 3, 4);
+			Cars cars = new Cars(Arrays.asList(new Car(Name.of("pobi")), new Car(Name.of("crong")), new Car(Name.of("honux"))));
+			cars.move();
+			assertThat(cars.getCars().get(0).getDistance())
+					.isEqualTo(Distance.of(1));
+			assertThat(cars.getCars().get(1).getDistance())
+					.isEqualTo(Distance.of(0));
+			assertThat(cars.getCars().get(2).getDistance())
+					.isEqualTo(Distance.of(1));
+		}
+	}
+
+	@Test
+	void checkCarsSizeTest() {
+		assertThatThrownBy(() -> new Cars(new ArrayList<>()))
+				.isInstanceOf(IllegalArgumentException.class);
+	}
+
+	@Test
+	void checkSameNameTest() {
+		assertThatThrownBy(() -> new Cars(Arrays.asList(new Car(Name.of("pobi")), new Car(Name.of("crong")), new Car(Name.of("pobi")))))
+				.isInstanceOf(IllegalArgumentException.class);
+	}
+}
