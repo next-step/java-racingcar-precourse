7ff9ae0e5d74928a371186e7ff398ed0254cd0ca
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-10-10 02:52:57 +0900

    test: 입력된 자동차 이름의 대한 자동차 객체 저장, 5자리 이상 입력된 자동차 이름의 대한 검증

---
+++ src/test/java/racinggame/domain/CarFacrotyTest.java
@@ -0,0 +1,22 @@
+package racinggame.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class CarFacrotyTest {
+
+    @Test
+    void 자동차_등록() {
+        CarFacroty carFacroty = new CarFacroty("퐁당퐁,퐁당퐁당,퐁당퐁당퐁");
+        Cars cars = carFacroty.enter();
+        assertThat(cars.getCarsSize()).isEqualTo(3);
+    }
+
+    @Test
+    void 자동차_5자리이상_등록() {
+        assertThatThrownBy(() -> new CarFacroty("퐁당퐁당,퐁당퐁당퐁,퐁당퐁당퐁당")).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR]");
+    }
+}
