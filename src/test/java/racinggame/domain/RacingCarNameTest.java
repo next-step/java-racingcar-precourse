3d86ff6489e2220e67b1e1305bf7f9a2b4701e05
seonghyeoklee <dltjdgur327@gmail.com>
2021-10-10 02:28:01 +0900

    test : RacingCarName 생성 및 유효성 테스트 코드 추가

---
+++ src/test/java/racinggame/domain/RacingCarNameTest.java
@@ -0,0 +1,26 @@
+package racinggame.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static racinggame.utils.RacingGameUtils.ERROR_MESSAGE;
+
+class RacingCarNameTest {
+    @ParameterizedTest(name = "{0} 자동차 이름 객체를 생성한다")
+    @ValueSource(strings = {"crong"})
+    void 자동차_이름_객체를_생성한다(String racingCarNameInput) {
+        assertThat(new RacingCarName(racingCarNameInput).getRacingCarName()).isEqualTo(racingCarNameInput);
+    }
+
+    @ParameterizedTest(name = "{0} 자동차 이름 객체 유효성을 검증한다")
+    @ValueSource(strings = {"javaji", ""})
+    void 자동차_이름_객체_유효성을_검증한다(String racingCarNameInput) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> {
+                    new RacingCarName(racingCarNameInput);
+                }).withMessage(ERROR_MESSAGE);
+
+    }
+}
