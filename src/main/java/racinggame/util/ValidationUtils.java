c7a1632b69ded2c8c051cdbd62620fb526a9ae55
jongwoo <jongwoo.dev@gmail.com>
2021-10-10 00:06:27 +0900

    refactor: validate car name
    차 이름 유효성 검사 최대 길이 상수로 추출

--- src/main/java/racinggame/util/ValidationUtils.java
+++ src/main/java/racinggame/util/ValidationUtils.java
@@ -2,8 +2,10 @@ package racinggame.util;
 
 public class ValidationUtils {
 
+	public static final int MAX_CAR_NAME = 5;
+
 	public static boolean validCarName(String string) {
-		return string != null && !"".equals(string) && string.length() <= 5;
+		return string != null && !"".equals(string) && string.length() <= MAX_CAR_NAME;
 	}
 
 }
