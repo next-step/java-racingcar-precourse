334ae9eaa44bdb2b497966732ec8baaeaaafb81a
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-10-10 02:49:06 +0900

    feat: 자주 사용하는 상수

---
+++ src/main/java/racinggame/common/Constant.java
@@ -0,0 +1,14 @@
+package racinggame.common;
+
+public final class Constant {
+
+    public static final String REGEX_COMMA = "\\,";
+    public static final String STR_EMPTY = "";
+    public static final String STR_COMMA = ",";
+    public static final String STR_ERROR = "ERROR";
+    public static final int INT_ZERO = 0;
+    public static final int INT_SIX = 6;
+
+    private Constant() {
+    }
+}
