220b52ee8e29460960d83d594fe397a1966fc7e8
LeeYoungJun <dldudwns72@gmail.com>
2021-10-09 17:59:29 +0900

    test (쉼표로 구분한 N대의 자동차 생성 테스트)

--- src/main/java/domain/Cars.java
+++ src/main/java/domain/Cars.java
@@ -1,2 +1,27 @@
-package domainTest;public class Cars {
+package domain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Cars {
+
+    private static final String DELIMITER = ",";
+    private List<Car> cars;
+
+    public Cars(String names) {
+        List<Car> cars = new ArrayList<>();
+
+        List<String> carNames = Arrays.asList(names.split(DELIMITER));
+
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+
+        this.cars = cars;
+    }
+
+    public Car getCar(int index) {
+        return cars.get(index);
+    }
 }
--- src/test/java/domainTest/CarsTest.java
+++ src/test/java/domainTest/CarsTest.java
@@ -1,16 +1,22 @@
-package domain;
+package domainTest;
 
+import domain.Cars;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CarsTest {
 
-    @Test
+    @ParameterizedTest
     @DisplayName("문자열을 받아 N대의 자동차를 생성 테스트")
-    public void createCars(){
+    @CsvSource(value = {"0:lee", "1:kim", "2:park"}, delimiter = ':')
+    public void createCars(int index, String name){
         String names = "lee,kim,park";
         Cars cars = new Cars(names);
 
-
+        assertThat(cars.getCar(index).getName()).isEqualTo(name);
     }
+
 }
