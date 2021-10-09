b78eaf0b51935132e6bef726455ca0a6907c4bee
ParkJiwoon <bcp0109@gmail.com>
2021-10-09 15:47:46 +0900

    feat: InputError 추가

--- src/main/java/racinggame/model/enums/Error.java
+++ src/main/java/racinggame/model/enums/Error.java
@@ -6,6 +6,8 @@ public enum Error {
     INVALID_MOVE_CONDITION_RANGE("이동 조건 값은 0 이상 9 이하여야 합니다."),
     INVALID_MOVE_NUMBER_SIZE("자동차의 갯수와 이동 숫자의 갯수가 일치하지 않습니다."),
     INVALID_COUNT("시도 횟수는 0 보다 커야 합니다."),
+    INPUT_MUST_NOT_BE_NULL("빈 값을 입력하면 안됩니다."),
+    WRONG_NUMBER_FORMAT("숫자를 입력해야 합니다.")
     ;
 
     private static final String prefix = "[ERROR] ";
