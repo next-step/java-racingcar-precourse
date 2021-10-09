af06334f377aaa9926fdc8f28fafe9d7b311439e
kimdonggyu-dev <donggyu.dev@gmail.com>
2021-10-09 15:28:23 +0900

    feat: Car 도메인 생성

---
+++ src/test/java/racinggame/domain/CarTest.java
@@ -0,0 +1,14 @@
+package racinggame.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class CarTest {
+
+    @Test
+    void 생성() {
+        assertDoesNotThrow(() -> new Car("abc").equals(new Car("abc")));
+    }
+
+}
