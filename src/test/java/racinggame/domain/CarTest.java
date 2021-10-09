9dabe69e63a7b257846645c6eeab933d687dd203
kimdonggyu-dev <donggyu.dev@gmail.com>
2021-10-09 19:53:53 +0900

    test: Car 테스트코드 추가

--- src/test/java/racinggame/domain/CarTest.java
+++ src/test/java/racinggame/domain/CarTest.java
@@ -2,20 +2,39 @@ package racinggame.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CarTest {
 
     @Test
     void 생성() {
-        assertDoesNotThrow(() -> new Car("abc").equals(new Car("abc")));
+        assertEquals(new Car("abc"), new Car("abc"));
     }
 
     @Test
-    void 움직이는_경우() {
-        assertDoesNotThrow(() -> {
-            Car car = new Car("abc");
-            car.move(() -> true);
-        });
+    void 이름_검증() {
+        String name = "abc";
+        Car car = new Car(name);
+        assertEquals(car.getName(), Name.of(name));
+    }
+
+    @Test
+    void 생성시에는_거리가_0이다() {
+        Car car = new Car("abc");
+        assertEquals(car.getDistance(), Distance.of(0));
+    }
+
+    @Test
+    void 전진() {
+        Car car = new Car("abc");
+        car.move(() -> true);
+        assertEquals(car.getDistance(), Distance.of(1));
+    }
+
+    @Test
+    void 정지() {
+        Car car = new Car("abc");
+        car.move(() -> false);
+        assertEquals(car.getDistance(), Distance.of(0));
     }
 }
