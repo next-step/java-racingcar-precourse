33906cc5b74ef787bd53118fff3738435cbd6413
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-10-10 17:31:12 +0900

    feat: 자동차 레이싱 게임 실행 로직

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,21 @@
 package racinggame;
 
+import racinggame.common.enums.Messages;
+import racinggame.controller.CarController;
+import racinggame.domain.CarFacroty;
+import racinggame.domain.Cars;
+import racinggame.view.CarView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 자동차 경주 게임 구현
+        CarView carView = new CarView();
+        CarController carController = new CarController(carView);
+        CarFacroty carFacroty = carController.validInputCarNames();
+        int attempt = carController.validInputNumberGames();
+        Cars cars = carFacroty.enter();
+        carView.printMessage(Messages.PLAY_STATUS.getMessage());
+        carController.calculatePrint(attempt, cars);
+        carView.printResult(cars.winner());
     }
 }
