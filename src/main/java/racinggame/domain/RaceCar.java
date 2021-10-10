df22a0a621bd2a18a131728a1c29b894a628e4c0
cus4703 <cus4703@gmail.com>
2021-10-11 02:10:34 +0900

    refactor: RaceCar equals Override 추가

--- src/main/java/racinggame/domain/RaceCar.java
+++ src/main/java/racinggame/domain/RaceCar.java
@@ -2,6 +2,8 @@ package racinggame.domain;
 
 import racinggame.utils.RaceValidation;
 
+import java.util.Objects;
+
 public class RaceCar {
 
     private String name;
@@ -36,5 +38,18 @@ public class RaceCar {
     private void validation(String name) {
         RaceValidation.validateEmptyName(name);
         RaceValidation.validateLengthOverName(name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        RaceCar raceCar = (RaceCar) o;
+        return moveCount == raceCar.moveCount && Objects.equals(name, raceCar.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, moveCount);
     }
 }
