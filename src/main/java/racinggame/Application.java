1be9b8035fa643642dfd9aad7f1577dfa8667873
jaesungahn91 <jaesungahn91@gmail.com>
2021-10-11 02:34:45 +0900

    feat(Application): 게임 진행 기능

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,27 @@
 package racinggame;
 
+import racinggame.domain.Racing;
+import racinggame.view.Command;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 자동차 경주 게임 구현
+        Command command = new Command();
+        while (game(command));
+    }
+
+    public static boolean game(Command command) {
+        boolean isGameNormal;
+        try {
+            Racing racing = Racing.createRacing(command.askCarNames(), command.askAttemptCount());
+            command.printResultMessage();
+            racing.playRacing(command);
+            command.printWinner(String.join(",", racing.getWinnerStr()));
+            isGameNormal = false;
+        } catch (IllegalArgumentException e) {
+            command.printError();
+            isGameNormal = true;
+        }
+        return isGameNormal;
     }
 }
