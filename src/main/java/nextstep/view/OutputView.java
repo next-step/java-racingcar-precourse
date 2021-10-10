54daff7e2d741bfeba6d0ded7dbc9352bcd36761
leesukjune <robin00q@naver.com>
2021-10-10 17:03:17 +0900

    feat: 출력하는 View ('최종 우승자는 ???,??? 입니다.' 를 출력한다.)

--- src/main/java/nextstep/view/OutputView.java
+++ src/main/java/nextstep/view/OutputView.java
@@ -8,6 +8,8 @@ public class OutputView {
 	private static final String ASKING_RACING_CAR_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	private static final String ASKING_TRY_COUNT_INPUT = "시도할 회수는 몇회인가요?";
 	private static final String EXECUTION_RESULT = "실행 결과";
+	private static final String WINNER_PREFIX = "최종 우승자는 ";
+	private static final String WINNER_POSTFIX = " 입니다.";
 
 	public void printErrorMessage(RuntimeException e) {
 		System.out.println(ERROR_PREFIX + e.getMessage());
@@ -27,5 +29,9 @@ public class OutputView {
 
 	public void printRacingCarsStatus(String racingCarsStatus) {
 		System.out.println(racingCarsStatus);
+	}
+
+	public void printWinners(String winners) {
+		System.out.println(WINNER_PREFIX + winners + WINNER_POSTFIX);
 	}
 }
