c393bad88843aba09696870af9c4cf667b8497fc
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-10-10 17:27:08 +0900

    refactor: final 키워드 추가

--- src/main/java/racinggame/domain/Cars.java
+++ src/main/java/racinggame/domain/Cars.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class Cars {
 
-    private List<Car> carList;
+    private final List<Car> carList;
 
     public Cars(List<Car> carList) {
         this.carList = carList;
