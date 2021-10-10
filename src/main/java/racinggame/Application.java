2ed1f21ff24ec9ddf8a34df6f44de508414e73ad
cus4703 <cus4703@gmail.com>
2021-10-11 02:05:02 +0900

    feat: GameMachine class 생성
    - 게임 클래스
    - 레이스 서비스 호출
    - 레이스 결과 값으로 레이스 내역과 최종 우승자 출력

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,10 @@
 package racinggame;
 
+import racinggame.controller.GameMachine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 자동차 경주 게임 구현
+        GameMachine gameMachine = new GameMachine();
+        gameMachine.runRaceGame();
     }
 }
---
+++ src/main/java/racinggame/controller/GameMachine.java
@@ -0,0 +1,55 @@
+package racinggame.controller;
+
+import racinggame.domain.RaceCar;
+import racinggame.domain.RaceFinishResult;
+import racinggame.domain.RaceRound;
+import racinggame.exception.RaceGameException;
+import racinggame.service.RaceGameService;
+import racinggame.view.InputView;
+import racinggame.view.OutputView;
+
+import java.util.List;
+
+public class GameMachine {
+
+    final private InputView inputView;
+    final private OutputView outputView;
+    final private RaceGameService raceGameService;
+
+    public GameMachine() {
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
+        this.raceGameService = new RaceGameService();
+    }
+
+    public void runRaceGame() {
+        try {
+            List<String> userInputCarsName = inputView.getUserInputCarsName();
+            int userInputTryCount = inputView.getUserInputTryCount();
+            RaceFinishResult raceFinishResult = raceGameService.startRace(userInputCarsName, userInputTryCount);
+            printRaceGameResult(raceFinishResult);
+        } catch (RaceGameException rge) {
+            outputView.printErrorMessage(rge.getMessage());
+        }
+    }
+
+    private void printRaceGameResult(RaceFinishResult raceFinishResult) {
+        printRaceRounds(raceFinishResult.getRaceRounds());
+        printFinalWinners(raceFinishResult.getRaceWinners());
+    }
+
+    private void printFinalWinners(String raceWinners) {
+        outputView.printRaceWinner(raceWinners);
+    }
+
+    private void printRaceRounds(List<RaceRound> raceRounds) {
+        System.out.println("\n실행결과");
+        raceRounds.forEach(round -> printRaceRound(round.getRaceCars()));
+    }
+
+    private void printRaceRound(List<RaceCar> raceCars) {
+        raceCars.forEach(raceCar -> outputView.printRaceRound(raceCar));
+        System.out.println("\n");
+    }
+
+}
