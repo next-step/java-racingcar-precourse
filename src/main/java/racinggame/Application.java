a9cafbe59707552d3ddf13d166ae081d70feccff
symba <sybarits@gmail.com>
2021-10-10 16:25:00 +0900

    feat: 프로그램 메인 메소드 수정
    메인 메소드에서 프로그램 컨트롤러 실행하도록 수정

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,13 @@
 package racinggame;
 
+import racinggame.control.GameController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 자동차 경주 게임 구현
+        GameController game = new GameController();
+        while (true) {
+            game.run();
+        }
     }
 }
