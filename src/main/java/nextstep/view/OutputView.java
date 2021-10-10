74b192c139f40a71c605b0713c6d6739aeba75d4
leesukjune <robin00q@naver.com>
2021-10-10 16:54:45 +0900

    feat: 출력하는 View ('경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)' 을 출력한다.)

--- src/main/java/nextstep/view/OutputView.java
+++ src/main/java/nextstep/view/OutputView.java
@@ -3,8 +3,13 @@ package nextstep.view;
 public class OutputView {
 
 	private static final String ERROR_PREFIX = "[ERROR] ";
+	private static final String ASKING_RACING_CAR_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 
 	public void printErrorMessage(RuntimeException e) {
 		System.out.println(ERROR_PREFIX + e.getMessage());
+	}
+
+	public void printForAskingRacingCarInput() {
+		System.out.println(ASKING_RACING_CAR_INPUT);
 	}
 }
