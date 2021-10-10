2012f55756cee03f345edaa30be4ed7486835fe2
taeyanglim <taeyang7704@naver.com>
2021-10-10 20:58:25 +0900

    refactor: modify CarName default constructor(add validateNameLength Method)

--- src/main/java/racinggame/domain/CarName.java
+++ src/main/java/racinggame/domain/CarName.java
@@ -1,13 +1,24 @@
 package racinggame.domain;
 
+import racinggame.enums.ErrorMessageEnum;
+
 public class CarName {
     private String carName;
+    private final int MAX_NAME_LENGTH = 5;
+    private final int MIN_NAME_LENGTH = 1;
 
     public CarName(final String carName) {
+        validateNameLength(carName);
         this.carName = carName;
     }
 
     public String getCarName() {
         return carName;
+    }
+
+    private void validateNameLength(String carName) {
+        if(carName.length() > MAX_NAME_LENGTH || carName.length() < MIN_NAME_LENGTH) {
+            throw new IllegalArgumentException(ErrorMessageEnum.INPUT_CAR_NAME_SIZE_ERROR.getMessage());
+        }
     }
 }
