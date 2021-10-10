d65bbfbcfa7f4b0b0e7c74855bdad048ec1c1425
DaehyunJoo <dd70296@gmail.com>
2021-10-10 12:12:58 +0900

    feat(Car): 자동차
    - Car Move 로직 추가

--- src/main/java/racinggame/Car.java
+++ src/main/java/racinggame/Car.java
@@ -1,6 +1,12 @@
 package racinggame;
 
+import nextstep.utils.Randoms;
+
 public class Car {
+	public static final int MIN_NUM_IN_RANDOMS = 0;
+	public static final int MAX_NUM_IN_RANDOMS = 9;
+	public static final int MAX_NUM_MOVE_FAIL = 3;
+
 	private final CarName carName;
 	private final CarDistance carDistance;
 
@@ -11,5 +17,17 @@ public class Car {
 
 	public boolean isValid() {
 		return carName.isValid();
+	}
+
+	public void move() {
+		int pickNum = Randoms.pickNumberInRange(MIN_NUM_IN_RANDOMS, MAX_NUM_IN_RANDOMS);
+		if (pickNum <= MAX_NUM_MOVE_FAIL) {
+			return;
+		}
+		carDistance.update();
+	}
+
+	public int getDistance() {
+		return carDistance.getDistance();
 	}
 }
--- src/test/java/racinggame/CarTest.java
+++ src/test/java/racinggame/CarTest.java
@@ -1,18 +1,51 @@
 package racinggame;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.MockedStatic;
+
+import nextstep.utils.Randoms;
 
 public class CarTest {
+	private MockedStatic<Randoms> randoms;
+	private CarDistance carDistance;
+
+	@BeforeEach
+	void setUp() {
+		randoms = mockStatic(Randoms.class);
+		carDistance = new CarDistance(0);
+	}
+
+	@AfterEach
+	void finish() {
+		randoms.close();
+	}
+
 	@ParameterizedTest
 	@CsvSource(value = {"p,true", "crong,true", "honuxx,false"})
 	void 유효한_자동차인지_확인(String name, boolean isValid) {
 		CarName carName = new CarName(name);
-		CarDistance carDistance = new CarDistance(0);
 		Car car = new Car(carName, carDistance);
 
 		assertThat(car.isValid()).isEqualTo(isValid);
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"0,0", "3,0", "4,1", "9,1"})
+	void 자동차_전진(int pickNumber, int expectedDistance) {
+		Car car = new Car(new CarName("poby"), carDistance);
+
+		when(Randoms.pickNumberInRange(anyInt(), anyInt())).thenReturn(pickNumber);
+		car.move();
+
+		assertThat(car.getDistance()).isEqualTo(expectedDistance);
 	}
 }
