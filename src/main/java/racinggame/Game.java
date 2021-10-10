e8a323452c67d28b327f4f177d3ade83d2920ef0
minssan9 <minssan9@gmail.com>
2021-10-10 19:09:59 +0900

    feat(1-gameclass) add game class

---
+++ src/main/java/racinggame/Game.java
@@ -0,0 +1,22 @@
+package racinggame;
+
+import nextstep.utils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Game {
+    int maxPlace = 0;
+    List<Car> winnerCars = new ArrayList<>();
+
+    public void play() {
+        int rollCount = 0;
+        List<Car> cars = new ArrayList<>();
+        // carClass, gameClass
+        // rollCount
+        // moveCar, printCarsPlace
+        // printWinner
+    }
+
+}
