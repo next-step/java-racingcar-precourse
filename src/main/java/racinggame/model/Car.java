811a7942a792ea168d66e11275bc1cb00f00df98
praivesi <praivesi@gmail.com>
2021-10-10 16:23:58 +0900

    feat(Car): 현재 위치값을 가진 Car 자동차 클래스를 추가 (기능목록 1-1)

---
+++ src/main/java/racinggame/model/Car.java
@@ -0,0 +1,17 @@
+package racinggame.model;
+
+public class Car {
+    private String name;
+    private int location;
+
+    public Car(String name, int location){
+        this.name = name;
+        this.location = location;
+    }
+
+    public int forward() {
+        this.location++;
+
+        return this.location;
+    }
+}
