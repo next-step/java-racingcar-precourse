34c340e79210347e41e686d56327ddbb29a5e882
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-10-10 17:27:40 +0900

    refactor: 예외 처리 메세지 추가

--- src/main/java/racinggame/common/enums/Messages.java
+++ src/main/java/racinggame/common/enums/Messages.java
@@ -6,7 +6,9 @@ public enum Messages {
     CAR_NAME_INPUT("", "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     NUMBER_ATTEMPT("", "시도할 회수는 몇회인가요?"),
     PLAY_STATUS("", "실행 결과"),
-    SIZE_CHECK(Constant.STR_ERROR, "자동차 이름은 5자라 이하로 입력해야 합니다.");
+    DUPL_CHECK(Constant.STR_ERROR, "동일한 자동차 이름이 존재합니다."),
+    CAR_SIZE_CHECK(Constant.STR_ERROR, "자동차 이름은 5자라 이하로 입력해야 합니다."),
+    GAME_NUMBER_CHRACTER_CHECK(Constant.STR_ERROR, "게임 횟수는 숫자로 입력해야 합니다.");
 
     private String code;
     private String detail;
