c55065a18cacdc40c8c83007a28b07b29aee6148
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-10-10 17:36:04 +0900

    fix: exception 예외 처리 변경

--- src/test/java/racinggame/domain/CarFacrotyTest.java
+++ src/test/java/racinggame/domain/CarFacrotyTest.java
@@ -2,6 +2,8 @@ package racinggame.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.NoSuchElementException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -16,7 +18,7 @@ class CarFacrotyTest {
 
     @Test
     void 자동차_5자리이상_등록() {
-        assertThatThrownBy(() -> new CarFacroty("퐁당퐁당,퐁당퐁당퐁,퐁당퐁당퐁당")).isInstanceOf(IllegalArgumentException.class)
+        assertThatThrownBy(() -> new CarFacroty("퐁당퐁당,퐁당퐁당퐁,퐁당퐁당퐁당")).isInstanceOf(NoSuchElementException.class)
                 .hasMessageContaining("[ERROR]");
     }
 }
