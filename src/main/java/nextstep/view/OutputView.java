402c78299ce6f5baae00d3263ff3dbffe6485745
leesukjune <robin00q@naver.com>
2021-10-10 16:57:45 +0900

    feat: 출력하는 View ('시도할 회수는 몇회인가요?' 을 출력한다.)

--- src/main/java/nextstep/view/OutputView.java
+++ src/main/java/nextstep/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
 
 	private static final String ERROR_PREFIX = "[ERROR] ";
 	private static final String ASKING_RACING_CAR_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String ASKING_TRY_COUNT_INPUT = "시도할 회수는 몇회인가요?";
 
 	public void printErrorMessage(RuntimeException e) {
 		System.out.println(ERROR_PREFIX + e.getMessage());
@@ -11,5 +12,9 @@ public class OutputView {
 
 	public void printForAskingRacingCarInput() {
 		System.out.println(ASKING_RACING_CAR_INPUT);
+	}
+
+	public void printForAskingTryCountInput() {
+		System.out.println(ASKING_TRY_COUNT_INPUT);
 	}
 }
