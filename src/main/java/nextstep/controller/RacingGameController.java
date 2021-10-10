c8c43a2c2105bdd873f891864dfb9a8468f0b9c6
leesukjune <robin00q@naver.com>
2021-10-10 17:29:39 +0900

    feat: 레이싱게임 컨트롤러 (시도회수 객체 (TryCount) 를 생성한다.)
    시도회수 객체 (TryCount) 를 생성한다.
    - 입력받는 View 를 통해 시도횟수를 입력받는다.
    - 시도횟수 객체를 생성한다.
    - 올바르지 않은 포맷을 입력받은 경우 에러를 출력한다.

--- src/main/java/nextstep/controller/RacingGameController.java
+++ src/main/java/nextstep/controller/RacingGameController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import nextstep.move.RandomMovingStrategy;
 import nextstep.racingcar.RacingCar;
 import nextstep.racingcar.RacingCars;
+import nextstep.trycount.TryCount;
 import nextstep.view.InputView;
 import nextstep.view.OutputView;
 
@@ -13,6 +14,30 @@ public class RacingGameController {
 
 	public static void play() {
 		final RacingCars racingCars = getRacingCars();
+		final TryCount tryCount = getTryCount();
+	}
+
+	private static TryCount getTryCount() {
+		TryCount tryCount = null;
+		while (tryCount == null) {
+			tryCount = createTryCount();
+		}
+
+		return tryCount;
+	}
+
+	private static TryCount createTryCount() {
+		try {
+			OutputView.printForAskingTryCountInput();
+			return createTryCountByTryCount(InputView.getTryCountInput());
+		} catch (RuntimeException e) {
+			OutputView.printErrorMessage(e);
+			return null;
+		}
+	}
+
+	private static TryCount createTryCountByTryCount(int tryCountInput) {
+		return new TryCount(tryCountInput);
 	}
 
 	private static RacingCars getRacingCars() {
