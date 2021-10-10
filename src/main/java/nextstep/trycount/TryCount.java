1d106dfd6ccc9347c031a467a1eaba8cffa3bfe3
leesukjune <robin00q@naver.com>
2021-10-10 15:58:54 +0900

    feat: 시도횟수 도메인 (시도횟수를 감소시킨다.)
    감소시킨 시도횟수는 0보다 작을 수 없다.
    - 0보다작은 경우, InvalidTryCountException 을 던진다.

--- src/main/java/nextstep/trycount/TryCount.java
+++ src/main/java/nextstep/trycount/TryCount.java
@@ -1,5 +1,7 @@
 package nextstep.trycount;
 
+import java.util.Objects;
+
 import nextstep.exception.InvalidTryCountException;
 
 public class TryCount {
@@ -17,5 +19,29 @@ public class TryCount {
 
 	private boolean invalid(int tryCount) {
 		return tryCount < MIN_TRY_COUNT;
+	}
+
+	public TryCount decrease() {
+		final int decreased = tryCount - 1;
+		if (invalid(decreased)) {
+			throw new InvalidTryCountException();
+		}
+
+		return new TryCount(decreased);
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		TryCount tryCount1 = (TryCount)o;
+		return tryCount == tryCount1.tryCount;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(tryCount);
 	}
 }
--- src/test/java/nextstep/trycount/TryCountTest.java
+++ src/test/java/nextstep/trycount/TryCountTest.java
@@ -1,6 +1,7 @@
 package nextstep.trycount;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,28 @@ class TryCountTest {
 
 		// when then
 		assertThatThrownBy(() -> new TryCount(-1))
+			.isInstanceOf(InvalidTryCountException.class)
+			.hasMessageContaining(InvalidTryCountException.INVALID_TRY_COUNT);
+	}
+
+	@DisplayName("시도횟수를 감소시킨다.")
+	@Test
+	void decrease() {
+		// given
+		final int tryCount = 2;
+
+		// when then
+		assertEquals(new TryCount(tryCount - 1), new TryCount(tryCount).decrease());
+	}
+
+	@DisplayName("시도횟수를 0 미만으로 감소시킬 수 없다.")
+	@Test
+	void decrease_invalid() {
+		// given
+		final TryCount tryCount = new TryCount(0);
+
+		// when then
+		assertThatThrownBy(tryCount::decrease)
 			.isInstanceOf(InvalidTryCountException.class)
 			.hasMessageContaining(InvalidTryCountException.INVALID_TRY_COUNT);
 	}
