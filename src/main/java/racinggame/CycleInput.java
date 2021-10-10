14a020b989717b584f0b3ae95d9f6182392007bc
yh22953905 <yh22953905@gmail.com>
2021-10-10 16:30:31 +0900

    feat: 입력 - 몇 번의 이동

---
+++ src/main/java/racinggame/CycleInput.java
@@ -0,0 +1,41 @@
+package racinggame;
+
+import java.util.NoSuchElementException;
+
+import nextstep.utils.Console;
+
+public class CycleInput {
+	private static int cycle;
+	static final String GUIDE_MESSAGE = "시도할 회수는 몇회인가요?";
+	static final String ERROR_MESSAGE = "1 이상의 숫자를 입력해주세요.";
+
+	private CycleInput() {
+		cycle = getInput();
+	}
+
+	public static CycleInput init() {
+		return new CycleInput();
+	}
+
+	public static int value() {
+		return cycle;
+	}
+
+	private int getInput() throws NoSuchElementException, IllegalStateException {
+		System.out.println(GUIDE_MESSAGE);
+		String readLine = Console.readLine();
+		if (!isMoreThanOne(readLine)) {
+			System.out.println(ERROR_MESSAGE);
+			return getInput();
+		}
+		return Integer.parseInt(readLine);
+	}
+
+	private boolean isMoreThanOne(String readLine) {
+		try {
+			return Integer.parseInt(readLine) > 0;
+		} catch (NumberFormatException e) {
+			return false;
+		}
+	}
+}
---
+++ src/test/java/racinggame/CycleInputTest.java
@@ -0,0 +1,53 @@
+package racinggame;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import nextstep.test.NSTest;
+
+public class CycleInputTest extends NSTest {
+	@BeforeEach
+	void beforeEach() {
+		setUp();
+	}
+
+	@Test
+	void 정상_숫자_입력() {
+		assertSimpleTest(() -> {
+			run("10");
+			CycleInput.init();
+			verify(CycleInput.GUIDE_MESSAGE);
+		});
+		assertThat(CycleInput.value()).isEqualTo(10);
+	}
+
+	@Test
+	void 문자_입력() {
+		assertSimpleTest(() -> {
+			run("number", "100");
+			CycleInput.init();
+			verify(CycleInput.GUIDE_MESSAGE, CycleInput.ERROR_MESSAGE);
+		});
+	}
+
+	@Test
+	void _1_이하인_숫자_입력() {
+		assertSimpleTest(() -> {
+			run("0", "1000");
+			CycleInput.init();
+			verify(CycleInput.GUIDE_MESSAGE, CycleInput.ERROR_MESSAGE);
+		});
+	}
+
+	@AfterEach
+	void tearDown() {
+		outputStandard();
+	}
+
+	@Override
+	protected void runMain() {
+	}
+}
