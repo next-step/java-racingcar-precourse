05c7f8f1dd161aa75234ace34c0b2656af4e3ffb
leesukjune <robin00q@naver.com>
2021-10-10 17:00:03 +0900

    feat: 출력하는 View ('실행 결과' 를 출력한다.)

--- src/main/java/nextstep/view/OutputView.java
+++ src/main/java/nextstep/view/OutputView.java
@@ -1,10 +1,13 @@
 package nextstep.view;
 
+import nextstep.constants.CommonConstants;
+
 public class OutputView {
 
 	private static final String ERROR_PREFIX = "[ERROR] ";
 	private static final String ASKING_RACING_CAR_INPUT = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
 	private static final String ASKING_TRY_COUNT_INPUT = "시도할 회수는 몇회인가요?";
+	private static final String EXECUTION_RESULT = "실행 결과";
 
 	public void printErrorMessage(RuntimeException e) {
 		System.out.println(ERROR_PREFIX + e.getMessage());
@@ -16,5 +19,9 @@ public class OutputView {
 
 	public void printForAskingTryCountInput() {
 		System.out.println(ASKING_TRY_COUNT_INPUT);
+	}
+
+	public void printExecutionResult() {
+		System.out.println(CommonConstants.NEWLINE + EXECUTION_RESULT);
 	}
 }
