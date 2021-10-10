631f224a4ea5256e060cd1233edcd9230d8e4564
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-10-10 17:30:23 +0900

    refactor: 예외처리 로직 추가의 따른 메소드 수정

--- src/main/java/racinggame/controller/CarController.java
+++ src/main/java/racinggame/controller/CarController.java
@@ -1,10 +1,13 @@
 package racinggame.controller;
 
+import racinggame.common.Constant;
 import racinggame.common.enums.Messages;
 import racinggame.domain.CarFacroty;
 import racinggame.domain.Cars;
 import racinggame.utils.Utils;
 import racinggame.view.CarView;
+
+import java.util.NoSuchElementException;
 
 public class CarController {
 
@@ -14,18 +17,40 @@ public class CarController {
         this.carView = carView;
     }
 
-    public void playGame() {
+    public CarFacroty validInputCarNames() {
         carView.printMessage(Messages.CAR_NAME_INPUT.getMessage());
-        CarFacroty carFacroty = new CarFacroty(Utils.getInputString());
-        carView.printMessage(Messages.NUMBER_ATTEMPT.getMessage());
-        int attempt = Utils.getStringToInt(Utils.getInputString());
-        Cars cars = carFacroty.enter();
-        carView.printMessage(Messages.PLAY_STATUS.getMessage());
-        calculate(attempt, cars);
-        carView.printResult(cars.winner());
+        String input = Utils.getInputString();
+        CarFacroty carFacroty = null;
+        try {
+            carFacroty = new CarFacroty(input);
+        } catch (NoSuchElementException e) {
+            carView.printMessage(e.getMessage());
+            this.validInputCarNames();
+        }
+        return carFacroty;
     }
 
-    private void calculate(int attempt, Cars cars) {
+    public int validInputNumberGames() {
+        carView.printMessage(Messages.NUMBER_ATTEMPT.getMessage());
+        String input = Utils.getInputString();
+        int result = 0;
+        try {
+            result = this.validChracter(input);
+        } catch (NoSuchElementException e) {
+            carView.printMessage(e.getMessage());
+            this.validInputNumberGames();
+        }
+        return result;
+    }
+
+    private int validChracter(String input) {
+        if(!Utils.getCharacterCheck(input)) {
+            throw new NoSuchElementException(Messages.GAME_NUMBER_CHRACTER_CHECK.getMessage(Constant.STR_ERROR));
+        }
+        return Utils.getStringToInt(input);
+    }
+
+    public void calculatePrint(int attempt, Cars cars) {
         for (int i = 0; i < attempt; i++) {
             cars.play(Utils.getRandomList(cars.getCarsSize()));
             carView.printStatus(cars.getCars());
