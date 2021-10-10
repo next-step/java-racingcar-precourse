f255ad394919ad142d603103dff365025e3e0b6c
codeleesh <38518639+codeleesh@users.noreply.github.com>
2021-10-10 17:26:58 +0900

    refactor: final 키워드 추가 및 파라미터 이름 변경

--- src/main/java/racinggame/domain/CarFacroty.java
+++ src/main/java/racinggame/domain/CarFacroty.java
@@ -3,36 +3,44 @@ package racinggame.domain;
 import racinggame.common.Constant;
 import racinggame.common.enums.Messages;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class CarFacroty {
 
-    private String input;
+    private final String inputName;
 
-    public CarFacroty(String input) {
-        validName(input);
+    public CarFacroty(String inputName) {
+        validSizeName(inputName);
+        validDuplicateName(inputName);
 
-        this.input = input;
+        this.inputName = inputName;
     }
 
-    public void validName(String input) {
-        String[] inputList = input.split(Constant.REGEX_COMMA);
-        for (String str : inputList) {
-            extracted(str);
+    public void validSizeName(String input) {
+        String[] inputArray = input.split(Constant.REGEX_COMMA);
+        for (String str : inputArray) {
+            validString(str);
         }
     }
 
-    private void extracted(String str) {
+    private void validString(String str) {
         if (str.length() == Constant.INT_ZERO || str.length() >= Constant.INT_SIX) {
-            throw new IllegalArgumentException(Messages.SIZE_CHECK.getMessage(Constant.STR_ERROR));
+            throw new NoSuchElementException(Messages.CAR_SIZE_CHECK.getMessage(Constant.STR_ERROR));
+        }
+    }
+
+    public void validDuplicateName(String input) {
+        String[] inputArray = input.split(Constant.REGEX_COMMA);
+        Set<String> inputSet = new HashSet<>(Arrays.asList(inputArray));
+        if( inputArray.length != inputSet.size() ) {
+            throw new NoSuchElementException(Messages.DUPL_CHECK.getMessage(Constant.STR_ERROR));
         }
     }
 
     public Cars enter() {
         List<Car> cars = new ArrayList<>();
-        String[] inputList = input.split(Constant.REGEX_COMMA);
-        for (String str : inputList) {
+        String[] inputNameList = inputName.split(Constant.REGEX_COMMA);
+        for (String str : inputNameList) {
             cars.add(new Car(str));
         }
         return new Cars(cars);
