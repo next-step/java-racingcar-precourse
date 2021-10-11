f1bb755d7e0ffeee9698e1e29b250bc3474ecbc2
Ttungga <torb2002@gmail.com>
2021-10-12 03:21:41 +0900

    feat: 메시지 출력 기능
    - 클래스 생성: RacinggameView
    - 자동차 이름 입력 메시지 출력: showRequestCarNameInputMessage()
    - 전진 횟수 입력 메시지 출력: showRequestMoveCountInputMessage()
    - 자동차 그룹 전진 결과 메시지 출력: showPlayResultStartMessage(), showPlayResultMessage(), createVisualizedDistance()
    - 게임 우승자 안내 메시지 출력: showWinnerMessage()
    - 에러 메시지 출력: showErrorMessage()

--- src/main/java/racinggame/message/ErrorMessage.java
+++ src/main/java/racinggame/message/ErrorMessage.java
@@ -1,6 +1,7 @@
 package racinggame.message;
 
 public enum ErrorMessage {
+	ERROR_MESSAGE_PREFIX("[ERROR]: "),
 	INVALID_CAR_NAME("자동차 이름은 1자 이상 5자 이하여야 합니다."),
 	INVALID_CAR_DISTANCE("자동차의 이동거리는 0 이상 9 이하여야 합니다."),
 	DUPLICATE_CAR_NAMES("자동차 이름은 중복될 수 없습니다."),
---
+++ src/main/java/racinggame/view/RacinggameView.java
@@ -0,0 +1,39 @@
+package racinggame.view;
+
+import racinggame.domain.CarStatus;
+import racinggame.message.ErrorMessage;
+import racinggame.message.PlayMessage;
+
+public class RacinggameView {
+	public void showRequestCarNameInputMessage() {
+		System.out.println(PlayMessage.REQUEST_CAR_NAME_INPUT.getValue());
+	}
+
+	public void showRequestMoveCountInputMessage() {
+		System.out.println(PlayMessage.REQUEST_MOVE_COUNT_INPUT.getValue());
+	}
+
+	public void showErrorMessage(String errorName) {
+		System.out.println(ErrorMessage.ERROR_MESSAGE_PREFIX + ErrorMessage.valueOf(errorName).getValue());
+	}
+
+	public void showPlayResultStartMessage() {
+		System.out.println(PlayMessage.PLAY_RESULT_START);
+	}
+
+	public void showPlayResultMessage(CarStatus carStatus) {
+		System.out.println(carStatus.getCarName() + " : " + createVisualizedDistance(carStatus.getCarPosition()));
+	}
+
+	private String createVisualizedDistance(int carPosition) {
+		String visualizedDistance = "";
+		for (int i = 0; i < carPosition; i++) {
+			visualizedDistance += PlayMessage.DISTANCE_VISUALIZATION_UNIT.getValue();
+		}
+		return visualizedDistance;
+	}
+
+	public void showWinnerMessage(String winnerNames) {
+		System.out.println(PlayMessage.WINNER_PREFIX + winnerNames + PlayMessage.WINNER_SUFFIX);
+	}
+}
