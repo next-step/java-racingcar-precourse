a763d85971ee33fe7a53deeae8b3995fcf7ba228
Ttungga <torb2002@gmail.com>
2021-10-12 04:08:11 +0900

    chore: 오타 수정

--- src/main/java/racinggame/message/PlayMessage.java
+++ src/main/java/racinggame/message/PlayMessage.java
@@ -2,7 +2,7 @@ package racinggame.message;
 
 public enum PlayMessage {
 	CAR_NAME_SEPARATOR(","),
-	REQUEST_CAR_NAME_INPUT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분"),
+	REQUEST_CAR_NAME_INPUT("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
 	REQUEST_MOVE_COUNT_INPUT("시도할 회수는 몇회인가요?"),
 	PLAY_RESULT_START("실행 결과"),
 	DISTANCE_VISUALIZATION_UNIT("-"),
