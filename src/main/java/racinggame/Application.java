bafb231eb0daaca1a9e73e3e88fd36996258190a
JaeHan-Kim <hgal8877@gmail.com>
2021-10-11 23:19:30 +0900

    doc (racinggame): 자동차 기능 문서 추가

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,14 @@
 package racinggame;
 
+import racinggame.domain.RacingGame;
+
 public class Application {
     public static void main(String[] args) {
+        // System.out.println("a,".substring(0, 1));
         // TODO 자동차 경주 게임 구현
+        RacingGame racingGame = new RacingGame();
+        racingGame.gameSetting();
+        racingGame.startRacing();
+        racingGame.awardRacing();
     }
 }
