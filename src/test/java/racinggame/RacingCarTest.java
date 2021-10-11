801f50549daaf1a474a65f70a6a99e82c6392c33
devhak2 <devhak2@gmail.com>
2021-10-11 16:31:53 +0900

    style(RacingCarTest): RacingCarTest 리팩토링
    Random Mock을 Test 멤버로 추출

--- src/test/java/racinggame/RacingCarTest.java
+++ src/test/java/racinggame/RacingCarTest.java
@@ -3,42 +3,50 @@ package racinggame;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 
 import nextstep.utils.Randoms;
 
 public class RacingCarTest {
+	private static MockedStatic<Randoms> mockRandoms;
+
+	@BeforeEach
+	void setUp() {
+		mockRandoms = mockStatic(Randoms.class);
+		mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+			.thenReturn(4, 3, 0, 9);
+	}
+
+	@AfterEach
+	void close() {
+		mockRandoms.close();
+	}
+
 	@Test
 	void 전진_정지_테스트() {
 		RacingCar car = new RacingCar("pobi");
 		Position position;
 
-		try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
-			mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
-				.thenReturn(4, 3, 0, 9);
+		position = car.move();
+		position = car.move();
+		position = car.move();
+		position = car.move();
 
-			position = car.move();
-			position = car.move();
-			position = car.move();
-			position = car.move();
-
-			assertThat(position.getX()).isEqualTo(2);
-		}
+		assertThat(position.getX()).isEqualTo(2);
 	}
 
 	@Test
 	void 전진_정지_여러번_테스트() {
 		RacingCar car = new RacingCar("pobi");
 		Position position;
+		mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+			.thenReturn(4, 3, 0, 9);
 
-		try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
-			mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
-				.thenReturn(4, 3, 0, 9);
+		position = car.moves(4);
 
-			position = car.moves(4);
-
-			assertThat(position.getX()).isEqualTo(2);
-		}
+		assertThat(position.getX()).isEqualTo(2);
 	}
 }
