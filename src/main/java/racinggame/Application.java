369da4dd3734b15d684e36fe32994ac0da82dfa7
Junha Kim <kimjh4930@gmail.com>
2021-10-11 14:28:48 +0900

    feat(Application) : 의존성주입, Application 실행

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,15 +1,23 @@
 package racinggame;
 
+import racinggame.circuit.RacingCircuit;
 import racinggame.device.input.ConsoleKeyboard;
 import racinggame.device.output.ConsolePrinter;
+import racinggame.rule.RacingRule;
 import racinggame.ui.RacingGame;
+import racinggame.ui.Rule;
 
 public class Application {
 	public static void main(String[] args) {
-		// RacingGame racingGame = new RacingGame(
-		//     new ConsoleKeyboard(),
-		//     new ConsolePrinter());
-		//
-		// racingGame.play();
+		Rule rule = new RacingRule();
+		RacingCircuit circuit = new RacingCircuit(rule);
+		RacingGame racingGame = new RacingGame(
+			new ConsoleKeyboard(),
+			new ConsolePrinter(),
+			circuit,
+			rule
+		);
+
+		racingGame.play();
 	}
 }
