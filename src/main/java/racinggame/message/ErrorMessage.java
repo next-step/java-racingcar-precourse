6268f95cd3c61a12aff085f2ff5f1d51ecb29ca8
ShinHan <tlsgks8668@naver.com>
2021-10-11 16:59:24 +0900

    feat: 차 이름을 입력하지 않았을 때 에러 메시지 추가

--- src/main/java/racinggame/message/ErrorMessage.java
+++ src/main/java/racinggame/message/ErrorMessage.java
@@ -5,6 +5,7 @@ public enum ErrorMessage {
     OUT_OF_NAME_LENGTH_ERROR ("차 이름은 5자 이하로 입력해주세요."),
     ONLY_NUMBER_ATTEMPT_COUNT_ERROR ("시도 횟수는 숫자만 입력할 수 있습니다."),
     MORE_THAN_ZERO_ATTEMPT_COUNT_ERROR ("시도 횟수는 1이상의 숫자여야만 합니다."),
+    NOT_BLANK_NAME ("이름은 5자 이내로 꼭 입력해야 합니다."),
     ERROR ("[ERROR] ");
 
     private String message;
