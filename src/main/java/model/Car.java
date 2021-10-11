eb6ce96dfc7525c83c7c2b092694e254c1508b5a
chunghyun lee <sinna94@naver.com>
2021-10-11 12:41:12 +0900

    feat: 랜덤 값에 따라 자동차 전진 기능 추가

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,14 +1,36 @@
 package model;
 
+import nextstep.utils.Randoms;
+
 public class Car {
-    private String name;
-    private int distance = 0;
+	private final String name;
+	private int distance = 0;
 
-    public Car(String name) {
-        this.name = name;
-    }
+	public Car(String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
+
+	public int getDistance() {
+		return distance;
+	}
+
+	private int getRandomNumber() {
+		return Randoms.pickNumberInRange(0, 9);
+	}
+
+	private boolean isPossibleForward() {
+		int randomNumber = getRandomNumber();
+		return randomNumber >= 4;
+	}
+
+	public void move() {
+		if (isPossibleForward()) {
+			distance += 1;
+		}
+	}
+
 }
--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -1,15 +1,42 @@
 package model;
 
+import nextstep.utils.Randoms;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mockStatic;
 
 class CarTest {
 
+	private Car testCar;
+	private final String carName = "test-car";
+
+	@BeforeEach
+	void init() {
+		testCar = new Car(carName);
+	}
+
 	@Test
 	void createCarTest() {
-		String carName = "test-car";
-		Car car = new Car(carName);
-		assertEquals(carName, car.getName());
+		assertEquals(carName, testCar.getName());
+	}
+
+	@Test
+	@DisplayName("자동차 이동 검증")
+	void carMoveTest() {
+		try (final MockedStatic<Randoms> mockRandoms = mockStatic(Randoms.class)) {
+			mockRandoms.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+				.thenReturn(3, 4);
+			testCar.move();
+			assertThat(testCar.getDistance()).isZero();
+			testCar.move();
+			assertThat(testCar.getDistance()).isEqualTo(1);
+		}
 	}
 }
