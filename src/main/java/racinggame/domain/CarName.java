21f461685df3e8617b7aa78cf2796794cf401cf6
BottleH <guys1017@korea.ac.kr>
2021-10-11 12:56:21 +0900

    feat(CarName): 자동차 이름 객체 추가
    - 길이5자 유효성 체크기능

---
+++ src/main/java/racinggame/domain/CarName.java
@@ -0,0 +1,19 @@
+package racinggame.domain;
+
+import static racinggame.common.Message.*;
+
+import racinggame.exception.OutOfCarNameLengthException;
+
+public class CarName {
+
+	private static final int MAX_LENGTH = 5;
+
+	private String carName;
+
+	public CarName(String carName) {
+		if (carName.length() > MAX_LENGTH) {
+			throw new OutOfCarNameLengthException(CAR_NAME_LENGTH_ERROR.getMessage());
+		}
+		this.carName = carName;
+	}
+}
---
+++ src/test/java/racinggame/domain/CarNameTest.java
@@ -0,0 +1,20 @@
+package racinggame.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import racinggame.exception.OutOfCarNameLengthException;
+
+@DisplayName("자동차 이름 Test")
+class CarNameTest {
+
+	@Test
+	@DisplayName("자동차 이름 길이 유효성 체크 테스트")
+	public void CarNameLengthTest() {
+		assertThrows(OutOfCarNameLengthException.class, () -> {
+			CarName carName = new CarName("람보르기니페라리");
+		});
+	}
+}
