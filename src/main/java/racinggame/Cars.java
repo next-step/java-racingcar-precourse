cdc0c32fed0447aa72df0bc9b26e741c872e66f4
박창혁 <changchang73@pionnet.co.kr>
2021-10-12 00:58:55 +0900

    feat: 자동차 경기 진행 수 추가
    1. 자동차 경기 진행 수 추가

--- src/main/java/racinggame/Cars.java
+++ src/main/java/racinggame/Cars.java
@@ -10,12 +10,24 @@ public class Cars {
 	private static final int MIN_NUMBER = 0;
 	private static final int MAX_NUMBER = 9;
 	private final List<Car> racers;
+	private final int raceCount;
 
-	public Cars(List<String> names) {
-		this.racers = initialize(names);
+	public Cars(List<String> names, String number) {
+		this.racers = initializeCarName(names);
+		this.raceCount = initializeRaceCount(number);
 	}
 
-	private List<Car> initialize(List<String> names) {
+	private int initializeRaceCount(String number) {
+		int count;
+		try {
+			count = Integer.parseInt(number);
+		}catch(Exception e){
+			throw new NumberFormatException("숫자를 입력해주시기 바랍니다.");
+		}
+		return count;
+	}
+
+	private List<Car> initializeCarName(List<String> names) {
 		List<Car> cars = new ArrayList<>();
 		for (String name : names) {
 			cars.add(new Car(name));
@@ -27,10 +39,14 @@ public class Cars {
 		return racers.size();
 	}
 
-	public void race(int number) {
-		do {
+	public void race() {
+		int count = this.raceCount;
+		printStartMsg();
+		while(count-- > 0){
 			racing();
-		} while (number-- > 0);
+			printNewLine();
+		}
+		printEndMsg("");
 	}
 
 	private void racing() {
@@ -38,5 +54,17 @@ public class Cars {
 			car.race(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER));
 			car.printCarResults();
 		}
+	}
+
+	private void printEndMsg(String msg) {
+		System.out.println("최종 우승자는 " + msg + "입니다.");
+	}
+
+	private void printNewLine() {
+		System.out.println("");
+	}
+
+	private void printStartMsg() {
+		System.out.println("실행 결과");
 	}
 }
--- src/test/java/racinggame/CarsTest.java
+++ src/test/java/racinggame/CarsTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 public class CarsTest {
 	@Test
 	void 자동차_이름_분할(){
-		Cars cars = new Cars(Arrays.asList("chang,hyuk,park".split(",")));
+		Cars cars = new Cars(Arrays.asList("chang,hyuk,park".split(",")), "3");
 		assertThat(cars.getNumberOfCars()).isEqualTo(3);
 	}
 }
