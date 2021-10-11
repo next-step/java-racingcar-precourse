5f364f7ab02545974ff4590557737c8b08d87650
박창혁 <changchang73@pionnet.co.kr>
2021-10-12 00:15:33 +0900

    feat: 자동차 랜덤 숫자 추가

--- src/main/java/racinggame/CarName.java
+++ src/main/java/racinggame/CarName.java
@@ -11,4 +11,8 @@ public class CarName {
 		}
 		this.name = name;
 	}
+
+	public String getName() {
+		return name;
+	}
 }
--- src/main/java/racinggame/Cars.java
+++ src/main/java/racinggame/Cars.java
@@ -7,6 +7,8 @@ import nextstep.utils.Randoms;
 
 public class Cars {
 
+	private static final int MIN_NUMBER = 0;
+	private static final int MAX_NUMBER = 9;
 	private final List<Car> racers;
 
 	public Cars(List<String> names) {
@@ -15,7 +17,7 @@ public class Cars {
 
 	private List<Car> initialize(List<String> names) {
 		List<Car> cars = new ArrayList<>();
-		for(String name : names){
+		for (String name : names) {
 			cars.add(new Car(name));
 		}
 		return cars;
@@ -25,4 +27,16 @@ public class Cars {
 		return racers.size();
 	}
 
+	public void race(int number) {
+		do {
+			racing();
+		} while (number-- > 0);
+	}
+
+	private void racing() {
+		for (Car car : this.racers) {
+			car.race(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER));
+			car.printCarResults();
+		}
+	}
 }
