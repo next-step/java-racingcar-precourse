10323cb1abb26390a5f6dfe32011309b8f06e617
olivejua <tmfrl4710@gmail.com>
2021-10-11 18:05:06 +0900

    feat: create cars validation for car names

---
+++ src/main/java/racinggame/Car.java
@@ -0,0 +1,25 @@
+package racinggame;
+
+import nextstep.utils.Randoms;
+
+public class Car {
+	private RacingBar racingBar;
+	private final String name;
+
+	public Car(String name) {
+		validateLengthOf(name);
+		this.name = name;
+		racingBar = new RacingBar(0);
+	}
+
+	public void race() {
+		int randomNo = Randoms.pickNumberInRange(0, 9);
+		racingBar.addBy(randomNo);
+	}
+
+	private void validateLengthOf(String carName) {
+		if (5 < carName.length()) {
+			throw new IllegalArgumentException("자동차의 이름은 5자 이하여야 합니다");
+		}
+	}
+}
---
+++ src/main/java/racinggame/Cars.java
@@ -0,0 +1,30 @@
+package racinggame;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Cars {
+	private final List<Car> cars = new ArrayList<>();
+
+	public Cars(List<String> carNames) {
+		validateDuplicatedNames(carNames);
+
+		for (String carName : carNames) {
+			cars.add(new Car(carName));
+		}
+	}
+
+	public int size() {
+		return cars.size();
+	}
+
+	private void validateDuplicatedNames(List<String> carNames) {
+		Set<String> nameSet = new HashSet<>(carNames);
+
+		if (nameSet.size() != carNames.size()) {
+			throw new IllegalArgumentException("중복된 이름은 사용할 수 없습니다");
+		}
+	}
+}
