9b1d867730c50651e08565c38783d877ad4407f2
JaeHan-Kim <hgal8877@gmail.com>
2021-10-11 23:16:16 +0900

    fix (racinggame): 전체 바퀴수랑 현재 바퀴수를 비교하도록 수정

--- src/main/java/racinggame/domain/Laps.java
+++ src/main/java/racinggame/domain/Laps.java
@@ -1,13 +1,23 @@
 package racinggame.domain;
 
 public class Laps {
+    private int currentLaps;
     private int laps;
 
     public Laps(int laps) {
         this.laps = laps;
+        this.currentLaps = 1;
     }
 
     public int getLaps() {
         return laps;
+    }
+
+    public boolean isGoal() {
+        return currentLaps > laps;
+    }
+
+    public void passStartLine() {
+        currentLaps++;
     }
 }
