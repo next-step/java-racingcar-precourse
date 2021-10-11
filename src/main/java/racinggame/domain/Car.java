bc8fc05c195ccf35577777febfc3b79aff9c060e
gunan <power4889@gmail.com>
2021-10-12 01:45:24 +0900

    feat(Car): equals 오버라이드.
    -Name이 같으면 같은 Car이다.

--- src/main/java/racinggame/domain/Car.java
+++ src/main/java/racinggame/domain/Car.java
@@ -2,6 +2,8 @@ package racinggame.domain;
 
 import nextstep.utils.Randoms;
 
+import java.util.Objects;
+
 public class Car {
 	private static final int MOVABLE_MIN_NUMBER = 4;
 	private static final int RANDOM_MIN_NUMBER = 0;
@@ -30,5 +32,18 @@ public class Car {
 
 	public Name getName() {
 		return name;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Car car = (Car) o;
+		return Objects.equals(name, car.name);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
 	}
 }
