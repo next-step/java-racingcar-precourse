5055ad0e4fcce38a35ea33bd0deafc9b3f8e312d
김혜린 <sjgpfls@gmail.com>
2021-10-11 14:10:26 +0900

    test: CarName -> Nametest 이름 길이 확인

--- src/test/java/racinggame/domain/NameTest.java
+++ src/test/java/racinggame/domain/NameTest.java
@@ -13,4 +13,12 @@ public class NameTest {
 		Name name = Name.of("abcde");
 		assertThat(name.getValue()).isEqualTo("abcde");
 	}
+
+	@DisplayName("이름이 5자 이하가 아닌 경우 IllegalArgumentException 발생하는지 확인")
+	@Test
+	void isValidLength() {
+		assertThatThrownBy(() -> Name.of("abcdef"))
+			.isInstanceOf(IllegalArgumentException.class)
+			.hasMessageContaining("[ERROR] 이름은 5자 이하로 입력해주세요");
+	}
 }
