c474f45a8a28d04965f74dd0daf182735c105904
ShinHan <tlsgks8668@naver.com>
2021-10-11 16:19:24 +0900

    feat: 이동한 거리 원시값 분리
    test: 거리에 대한 테스트

---
+++ src/test/java/racinggame/domain/DistanceTest.java
@@ -0,0 +1,35 @@
+package racinggame.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DistanceTest {
+
+    private Distance distance;
+
+    @BeforeEach
+    void Distance() {
+        distance = new Distance();
+    }
+
+    @Test
+    @DisplayName("생성할 수 있다.")
+    void create() {
+        assertThat(distance.getDistance()).isEqualTo(0);
+        assertThat(distance.getPosition()).isEqualTo("");
+    }
+
+    @Test
+    @DisplayName("한 칸 전진할 수 있다.")
+    void go_distance() {
+        distance.increaseDistance();
+
+        assertThat(distance.getDistance()).isEqualTo(1);
+        assertThat(distance.getPosition()).isEqualTo("-");
+    }
+}
