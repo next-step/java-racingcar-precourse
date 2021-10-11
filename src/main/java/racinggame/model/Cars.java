4409183182cd298477c77786a049ac14abb35ca0
oeunkyoung <ou1112@naver.com>
2021-10-11 17:30:21 +0900

    feat : 자동차 이름 입력 기능 수정
     - 자동차 이름 입력받을 때 자동차 이름의 길이에 대한 검증 로직 추가

---
+++ src/main/java/racinggame/model/Cars.java
@@ -0,0 +1,29 @@
+package racinggame.model;
+
+import java.util.ArrayList;
+
+import static racinggame.common.Constant.*;
+import static racinggame.common.ValidationUtils.*;
+
+public class Cars {
+    private static ArrayList<Car> cars;
+
+    public Cars() {
+    }
+
+    public Cars(ArrayList<Car> cars) {
+        this.cars = cars;
+    }
+
+    public static ArrayList<Car> makeCarsByInputName(String inputNames) {
+        cars = new ArrayList<Car>();
+        String[] carNames = inputNames.split(NAME_REGEX);
+        for(String name : carNames) {
+            if(!validName(name)) {
+                throw new IllegalArgumentException(ERROR_PREFIX + ERR_CAR_NAME_LEN);
+            }
+            cars.add(new Car(name, 0));
+        }
+        return cars;
+    }
+}
