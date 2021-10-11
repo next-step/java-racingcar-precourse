716926aa6d8d2979dee4e1e782a085ab8691312e
praivesi <praivesi@gmail.com>
2021-10-11 16:41:26 +0900

    feat(GameConsole): 사용자에게 게임의 총 횟수를 입력받는 기능을 GameConsole 클래스에 추가(기능목록 6번)

--- src/main/java/racinggame/view/GameConsole.java
+++ src/main/java/racinggame/view/GameConsole.java
@@ -6,27 +6,43 @@ import racinggame.model.Cars;
 import racinggame.model.RacingGame;
 
 public class GameConsole {
-    private Cars playerCars;
     private RacingGame racingGame;
 
+    private Cars playerCars;
+    private int gameTurnCnt;
+
     public GameConsole() {
-        this.playerCars = new Cars();
         this.racingGame = new RacingGame();
+
+        this.playerCars = new Cars();
+        this.gameTurnCnt = 0;
     }
 
     public void setup() {
+        readPlayerNames();
+        readGameTurnCount();
+    }
+
+    private void readPlayerNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String input = Console.readLine();
 
-        String[] userNames = input.split(",", 0);
+        String[] playerNames = input.split(",", 0);
 
-        for (String userName : userNames) {
-            this.playerCars.add(new Car(userName, 0));
+        for (String playerName : playerNames) {
+            this.playerCars.add(new Car(playerName, 0));
         }
+    }
+
+    private void readGameTurnCount() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+        String input = Console.readLine();
+
+        this.gameTurnCnt = Integer.parseInt(input);
     }
 
     public void playGame(){
         // TODO: 추후 사용자에게 입력받은 게임 턴수를 turnCnt로 전달해야 함
-        Cars winnerCars = this.racingGame.play(this.playerCars, 10);
+        Cars winnerCars = this.racingGame.play(this.playerCars, this.gameTurnCnt);
     }
 }
