3dccf5c6bda04831d1cb30a0429970e13c0fbc63
taeyanglim <taeyang7704@naver.com>
2021-10-11 18:50:05 +0900

    feat: add console output message in MessageEnum

--- src/main/java/racinggame/enums/MessageEnum.java
+++ src/main/java/racinggame/enums/MessageEnum.java
@@ -2,7 +2,10 @@ package racinggame.enums;
 
 public enum MessageEnum {
     INITIAL_INPUT_MESSAGE("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
-    INITIAL_INPUT_NUMBER_OF_ATTEMPTS("시도할 회수는 몇회인가요?");
+    INITIAL_INPUT_NUMBER_OF_ATTEMPTS("시도할 회수는 몇회인가요?"),
+    EXECUTION_RESULT_MESSAGE("\n실행결과"),
+    WINNERS_MESSAGE("최종 우승자는 "),
+    END_OF_SENTENCE_MESSAGE(" 입니다.");
 
     String message;
 
