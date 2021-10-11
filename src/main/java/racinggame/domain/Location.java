62fb481f52c2f8923c20f3d6f50c008cb67a6c1e
JaeHan-Kim <hgal8877@gmail.com>
2021-10-11 23:02:33 +0900

    fix (racinggame): 위치의 비교를 위해 equals, hashCode, comparable 구현

--- src/main/java/racinggame/domain/Location.java
+++ src/main/java/racinggame/domain/Location.java
@@ -1,10 +1,16 @@
 package racinggame.domain;
 
-public class Location {
+import java.util.Objects;
+
+public class Location implements Comparable<Location> {
 
     public static final int START_LOCATION = 0;
 
     private int location;
+
+    public Location() {
+        this(START_LOCATION);
+    }
 
     public Location(int location) {
         this.location = location;
@@ -16,5 +22,36 @@ public class Location {
 
     public void moveLocation() {
         location++;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Location location1 = (Location) o;
+        return location == location1.location;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(location);
+    }
+
+    @Override
+    public int compareTo(Location o) {
+        if (this.getLocation() > o.getLocation()) {
+            return 1;
+        }
+        if (this.getLocation() == o.getLocation()) {
+            return 0;
+        }
+        return -1;
+    }
+
+    public Location compareToLocation(Location o) {
+        if (o.compareTo(this) == 1) {
+            return o;
+        }
+        return this;
     }
 }
