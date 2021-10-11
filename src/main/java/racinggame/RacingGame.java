ec69d3be7b4ef8bc90760622b546ae26776215cc
wyjax <wyjax.dev@gmail.com>
2021-10-11 23:54:21 +0900

    feat: 게임 진행 클래스 개발

---
+++ src/main/java/racinggame/RacingGame.java
@@ -0,0 +1,42 @@
+package racinggame;
+
+import racinggame.domain.*;
+
+public class RacingGame {
+    private RacingGameView gameView;
+    private RacingCars racingCars;
+    private Round round;
+
+    public RacingGame() {
+        this.gameView = new RacingGameView();
+    }
+
+    public void init() {
+        initRacingCars();
+        initRound();
+    }
+
+    private void initRacingCars() {
+        String racingCarsName = gameView.viewCarName();
+        this.racingCars = new RacingCars(racingCarsName);
+    }
+
+    private void initRound() {
+        int round = gameView.viewRoundCount();
+        this.round = new Round(round);
+    }
+
+    public void start() {
+        while (round.next()) {
+            RaceRecords raceRecords = racingCars.playRound();
+            raceRecords.report();
+        }
+        end();
+    }
+
+    private void end() {
+        RaceResults raceResults = racingCars.results();
+        Winners winners = new Winners(raceResults);
+        winners.report();
+    }
+}
