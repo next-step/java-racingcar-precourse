63f511e877dad634b92ed2cfc6a2506614293881
DaehyunJoo <dd70296@gmail.com>
2021-10-11 23:16:25 +0900

    feat(RacingGameController): 유저 입력 프로세스 추가
    - 경주할 자동차 이름 입력 프로세스 및 예외처리 추가
    - 시도할 횟수 이름 입력 프로세스 및 예외처리 추가

--- src/main/java/racinggame/RacingGameController.java
+++ src/main/java/racinggame/RacingGameController.java
@@ -1,6 +1,39 @@
 package racinggame;
 
+import java.util.Optional;
+
 public class RacingGameController {
+	private final UserView userView;
+	private final RacingGameView racingGameView;
+
+	private Cars cars;
+	private TryNumber tryNumber;
+
+	public RacingGameController() {
+		this.userView = new UserView();
+		this.racingGameView = new RacingGameView();
+	}
+
 	public void play() {
+		inputUserName();
+		inputTryNumber();
+	}
+
+	private void inputUserName() {
+		boolean isInvalid = true;
+		while (isInvalid) {
+			cars = new Cars(userView.inputCarNames());
+			isInvalid = userView.showCarNamesErrorMsgIfInvalid(!cars.isValid());
+		}
+	}
+
+	private void inputTryNumber() {
+		boolean isInvalid = true;
+		Optional<TryNumber> optionalTryNumber = Optional.empty();
+		while (isInvalid) {
+			optionalTryNumber = TryNumber.of(userView.inputTryNumber());
+			isInvalid = userView.showTryNumberErrorMsgIfInvalid(!optionalTryNumber.isPresent());
+		}
+		tryNumber = optionalTryNumber.get();
 	}
 }
--- src/main/java/racinggame/UserView.java
+++ src/main/java/racinggame/UserView.java
@@ -16,4 +16,18 @@ public class UserView {
 		System.out.println(MSG_INPUT_NAMES);
 		return Console.readLine();
 	}
+
+	public boolean showCarNamesErrorMsgIfInvalid(boolean isInvalid) {
+		if (isInvalid) {
+			System.out.println("[ERROR]: 잘못된 자동차 이름입니다. 5자 이하의 자동차 이름을 입력해주세요.");
+		}
+		return isInvalid;
+	}
+
+	public boolean showTryNumberErrorMsgIfInvalid(boolean isInvalid) {
+		if (isInvalid) {
+			System.out.println("[ERROR]: 시도 횟수가 잘못 되었습니다. 0이상의 숫자를 입력해주세요.");
+		}
+		return isInvalid;
+	}
 }
