49cfa24e9c9bcdceed6c26ef25518e8c52bada91
minho <lights8615@gmail.com>
2021-10-13 02:14:28 +0900

    fix: 0 이상의 시도횟수만 입력 가능하도록 수정

--- src/main/java/racinggame/domain/AttemptNumber.java
+++ src/main/java/racinggame/domain/AttemptNumber.java
@@ -4,6 +4,9 @@ public class AttemptNumber {
     private int number;
 
     public AttemptNumber(int number) {
+        if (number < 1) {
+            throw new IllegalArgumentException("1회 이상의 횟수를 입력해주세요.");
+        }
         this.number = number;
     }
 
