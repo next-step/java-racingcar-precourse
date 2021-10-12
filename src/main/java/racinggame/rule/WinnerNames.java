420d033cbb9c470874eb754dcfb13bdcfcdfec6c
Junha Kim <kimjh4930@gmail.com>
2021-10-12 17:08:20 +0900

    test(WinnerNames) : 테스트 코드 추가

--- src/main/java/racinggame/rule/WinnerNames.java
+++ src/main/java/racinggame/rule/WinnerNames.java
@@ -2,6 +2,7 @@ package racinggame.rule;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class WinnerNames {
 	private final List<String> names = new ArrayList<>();
@@ -41,4 +42,18 @@ public class WinnerNames {
 		return names.size() - 1;
 	}
 
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		WinnerNames that = (WinnerNames)o;
+		return names.equals(that.names);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(names);
+	}
 }
---
+++ src/test/java/racinggame/rule/winnerrules/FarAwayWinRuleTest.java
@@ -0,0 +1,76 @@
+package racinggame.rule.winnerrules;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import racinggame.circuit.FinalRecord;
+import racinggame.circuit.LapRecords;
+import racinggame.racingcar.LapRecord;
+import racinggame.racingcar.Location;
+import racinggame.racingcar.Name;
+import racinggame.rule.WinnerDecisionRule;
+import racinggame.rule.WinnerNames;
+
+class FarAwayWinRuleTest {
+	private WinnerDecisionRule rule;
+
+	@BeforeEach
+	void beforeEach() {
+		rule = new FarAwayWinRule();
+	}
+
+	@DisplayName("가장 멀린간 레이싱카를 우승자로 정한다.")
+	@Test
+	void winner() {
+		//given
+		WinnerNames winnerNames = new WinnerNames();
+		winnerNames.addName("abc1");
+
+		//when then
+		assertThat(rule.judgeWinner(finalRecordWinner())).isEqualTo(winnerNames);
+	}
+
+	@DisplayName("우승자가 여러명인 경우, ',' 로 구분해 표시한다.")
+	@Test
+	void winners() {
+		//given
+		WinnerNames winnerNames = new WinnerNames();
+		winnerNames.addName("abc1");
+		winnerNames.addName("abc2");
+
+		//when
+		WinnerNames actualResult = rule.judgeWinner(finalRecordWinners());
+
+		//then
+		assertThat(actualResult).isEqualTo(winnerNames);
+		assertThat(actualResult.getNamesWithDelimiter(",")).isEqualTo("abc1,abc2");
+	}
+
+	private FinalRecord finalRecordWinner() {
+		FinalRecord finalRecord = new FinalRecord();
+
+		LapRecords lapRecords = new LapRecords();
+		lapRecords.add(new LapRecord(new Name("abc1"), new Location(3)));
+		lapRecords.add(new LapRecord(new Name("abc2"), new Location(2)));
+
+		finalRecord.add(lapRecords);
+
+		return finalRecord;
+	}
+
+	private FinalRecord finalRecordWinners() {
+		FinalRecord finalRecord = new FinalRecord();
+
+		LapRecords lapRecords = new LapRecords();
+		lapRecords.add(new LapRecord(new Name("abc1"), new Location(3)));
+		lapRecords.add(new LapRecord(new Name("abc2"), new Location(3)));
+
+		finalRecord.add(lapRecords);
+
+		return finalRecord;
+	}
+
+}
