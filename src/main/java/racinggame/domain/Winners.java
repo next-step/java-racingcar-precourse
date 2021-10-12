dba5162ac40725be70e74b0a72472ace054fc196
LeeYoungJun <dldudwns72@gmail.com>
2021-10-13 01:41:03 +0900

    refactor (최대값 구하기 로직 변경)

--- src/main/java/racinggame/domain/Winners.java
+++ src/main/java/racinggame/domain/Winners.java
@@ -1,2 +1,42 @@
-package racinggame.domain;public class Winners {
+package racinggame.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Winners {
+
+    private static final String COMMA = ",";
+    private Cars cars;
+
+    public Winners(Cars resultCars) {
+        this.cars = resultCars;
+    }
+
+    public String getWinners(){
+        int maxMoveCount = putMaxMoveCount();
+
+        List<String> carList = new ArrayList<>();
+
+        for (int i = 0; i < cars.size(); i++) {
+            addWinner(cars.getCar(i),maxMoveCount,carList);
+        }
+
+        return String.join(COMMA,carList);
+    }
+
+    private void addWinner(Car car, int maxMoveCount, List<String> carList) {
+       if(car.getMoveCount() == maxMoveCount){
+           carList.add(car.getName());
+       }
+    }
+
+    private int putMaxMoveCount(){
+        int maxMoveCount = Integer.MIN_VALUE;
+
+        for (int i = 0; i < cars.size(); i++) {
+            maxMoveCount = Math.max(maxMoveCount,cars.getCar(i).getMoveCount());
+        }
+
+        return maxMoveCount;
+    }
 }
