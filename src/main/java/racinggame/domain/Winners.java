1917f1d3b9b06b784802bff8da73fada84578dc3
wyjax <wyjax.dev@gmail.com>
2021-10-12 21:08:50 +0900

    feat: N번 출력을 1번 출력하도록 변경

--- src/main/java/racinggame/domain/Winners.java
+++ src/main/java/racinggame/domain/Winners.java
@@ -1,28 +1,23 @@
 package racinggame.domain;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Winners {
     List<Winner> winners;
 
-    public Winners(RaceResults raceResults) {
-        this.winners = judgeWinners(raceResults);
+    public Winners(List<Winner> winners) {
+        initWinners(winners);
     }
 
-    private List<Winner> judgeWinners(RaceResults raceResults) {
-        List<Winner> winners = new ArrayList<>();
-        int leadDistance = raceResults.calculateLeadDistance();
-        for (RaceResult raceResult : raceResults.getRaceResults()) {
-            addWinner(winners, raceResult, leadDistance);
+    private void initWinners(List<Winner> winners) {
+        if (winners != null && winners.size() > 0) {
+            this.winners = winners;
+            this.winners.sort(Comparator.comparing(Winner::getName));
+            return;
         }
-        return winners;
-    }
-
-    private void addWinner(List<Winner> winners, RaceResult raceResult, int leadDistance) {
-        if (raceResult.isLeadRacer(leadDistance)) {
-            winners.add(new Winner(raceResult.getName()));
-        }
+        this.winners = new ArrayList<>();
     }
 
     public void report() {
@@ -33,9 +28,9 @@ public class Winners {
     private String getWinnersName() {
         StringBuilder sb = new StringBuilder();
         for (Winner winner : winners) {
-            sb.append(winner.getName() + " ");
+            sb.append(winner.getName())
+                    .append(" ");
         }
-
         return sb.toString().trim().replaceAll(" ", ",");
     }
 }
