8251e57e5e003bdbb138640d1444c02499fb8986
JaeHan-Kim <hgal8877@gmail.com>
2021-10-12 23:15:53 +0900

    feat (racinggame): 작업한 자동차 경주 클레스를 main 메서드에 작성

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -4,7 +4,6 @@ import racinggame.domain.RacingGame;
 
 public class Application {
     public static void main(String[] args) {
-        // System.out.println("a,".substring(0, 1));
         // TODO 자동차 경주 게임 구현
         RacingGame racingGame = new RacingGame();
         racingGame.gameSetting();
