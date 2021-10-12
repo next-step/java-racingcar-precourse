5e2b842e32868ab61b5787b8d3cea56f36fe4ad0
cothis <minho1030@naver.com>
2021-10-12 17:12:58 +0900

    feat(Cars): 우승자 구하기 및 플레이 기능 추가

--- src/main/java/racinggame/Cars.java
+++ src/main/java/racinggame/Cars.java
@@ -1,9 +1,8 @@
 package racinggame;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Cars {
 	List<Car> cars;
@@ -16,12 +15,53 @@ public class Cars {
 		this.cars = cars;
 	}
 
-	public static Cars createByNames(String names) {
-		return new Cars(Arrays.stream(names.split(",")).map(Car::new)
-															 .collect(Collectors.toList()));
+	public static Cars createByNames(String nameStr) {
+		String[] names = nameStr.split(",");
+		List<Car> carList = new ArrayList<>();
+		for (String name : names) {
+			carList.add(new Car(name));
+		}
+		return new Cars(carList);
 	}
 
 	public boolean isEmpty() {
 		return this.cars.isEmpty();
+	}
+
+	public void play() {
+		for (Car car : cars) {
+			car.play();
+		}
+	}
+
+	public Car get(int index) {
+		return cars.get(index);
+	}
+
+	private void addSamePositionCars(int position, Car currentCar, List<Car> cars) {
+		if (currentCar.getPosition() == position) {
+			cars.add(currentCar);
+		}
+	}
+
+	private List<Car> getSpecificPositionCars(int position) {
+		List<Car> specificPositionCars = new ArrayList<>();
+		for (Car car: cars) {
+			addSamePositionCars(position, car, specificPositionCars);
+		}
+		return specificPositionCars;
+	}
+
+	private int getMaxPosition() {
+		int maxValue = 0;
+		for (Car car : cars) {
+			maxValue = Math.max(car.getPosition(), maxValue);
+		}
+		return maxValue;
+	}
+
+	public List<Car> getWinners() {
+		int maxPosition = getMaxPosition();
+		return getSpecificPositionCars(maxPosition);
 	}
 }
--- src/test/java/racinggame/CarsTest.java
+++ src/test/java/racinggame/CarsTest.java
@@ -1,12 +1,16 @@
 package racinggame;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
+import static org.mockito.Mockito.*;
 
 import java.util.Arrays;
 import java.util.Collections;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+import nextstep.utils.Randoms;
 
 public class CarsTest {
 	private Cars cars;
@@ -38,5 +42,34 @@ public class CarsTest {
 		String names = "pobi,crong";
 		cars = Cars.createByNames(names);
 		assertThat(cars.isEmpty()).isFalse();
+	}
+
+	@Test
+	@DisplayName("각 자동차 play 기능")
+	void playCars() {
+		try (MockedStatic<Randoms> mockedStatic = mockStatic(Randoms.class)) {
+			mockedStatic.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+				.thenReturn(3, 0, 6);
+
+			cars = Cars.createByNames("pobi,crong,honux");
+			cars.play();
+			assertThat(cars.get(0).getPosition()).isEqualTo(0);
+			assertThat(cars.get(1).getPosition()).isEqualTo(0);
+			assertThat(cars.get(2).getPosition()).isEqualTo(1);
+		}
+	}
+
+	@Test
+	@DisplayName("우승자 구하기")
+	void getWinners() {
+		try (MockedStatic<Randoms> mockedStatic = mockStatic(Randoms.class)) {
+			mockedStatic.when(() -> Randoms.pickNumberInRange(anyInt(), anyInt()))
+				.thenReturn(3, 0, 6);
+
+			cars = Cars.createByNames("pobi,crong,honux");
+			cars.play();
+
+			assertThat(cars.getWinners()).isEqualTo(Collections.singletonList(cars.get(2)));
+		}
 	}
 }
