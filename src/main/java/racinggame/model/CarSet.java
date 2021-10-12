f5466451e725a18dbbb5a55ff5bca0b3f8d70a95
lattedy <stary7stars@naver.com>
2021-10-12 14:26:00 +0900

    feat: CarSet 구현
    * 자동차 추가 및 중복확인

---
+++ src/main/java/racinggame/model/CarSet.java
@@ -0,0 +1,50 @@
+package racinggame.model;
+
+import nextstep.utils.Randoms;
+import racinggame.exception.IllegalInputException;
+import racinggame.util.Constant;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class CarSet {
+    private static final CarSet carSet = new CarSet();
+    private ArrayList<Car> cars;
+    private Set<String> setForDuplicateCheck;
+
+    private CarSet() {
+        this.cars = new ArrayList<>();
+        this.setForDuplicateCheck = new HashSet<>();
+    }
+
+    public static CarSet getCarSetInstance() {
+        return carSet;
+    }
+
+    public boolean addCar(String name) {
+        try {
+            isDuplicate(name);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        setForDuplicateCheck.add(name);
+        cars.add(new Car(name));
+        return true;
+    }
+
+    private void isDuplicate(String name) throws IllegalInputException {
+        if (setForDuplicateCheck.contains(name)) {
+            cars.clear();
+            setForDuplicateCheck.clear();
+            throw new IllegalInputException("[ERROR] " + name + "이 중복됩니다.");
+        }
+    }
+
+    public ArrayList<Car> getCars() {
+        return cars;
+    }
+
+    
+}
