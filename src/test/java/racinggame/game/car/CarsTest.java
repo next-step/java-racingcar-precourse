3d0f0e3dd2454f1ed5c3dc34757d7af3ee4c57cf
SunghunDev <sunghun@sunghun.dev>
2021-10-12 21:56:09 +0900

    test: cars 테스트 케이스 작성

--- src/test/java/racinggame/game/car/CarsTest.java
+++ src/test/java/racinggame/game/car/CarsTest.java
@@ -3,12 +3,26 @@ package racinggame.game.car;
 import org.junit.jupiter.api.Test;
 import racinggame.BaseTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class CarsTest extends BaseTest<Cars> {
 
     @Test
     void testGetCarList() {
+        int size = 3;
+        Cars cars = new Cars(getDefaultCarNames(size));
+        assertThat(cars.getCarList().size() == size).isTrue();
+    }
 
+
+    private List<String> getDefaultCarNames(int size) {
+        List<String> carNames = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            carNames.add("car-0" + i);
+        }
+        return carNames;
     }
 }
