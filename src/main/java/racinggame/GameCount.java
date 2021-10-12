3fc4efed652eda6c4fa74ef0149fb713b1fc3063
cothis <minho1030@naver.com>
2021-10-12 21:46:12 +0900

    feat(GameCount): GameCount class 추가

---
+++ src/main/java/racinggame/GameCount.java
@@ -0,0 +1,13 @@
+package racinggame;
+
+public class GameCount {
+	private final int gameCount;
+
+	public GameCount(int gameCount) {
+		this.gameCount = gameCount;
+	}
+
+	public int getCount() {
+		return gameCount;
+	}
+}
