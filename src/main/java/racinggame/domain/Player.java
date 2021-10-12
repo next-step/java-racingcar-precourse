8606cfef88305a523e9832984f40753c5e0a700f
minho <lights8615@gmail.com>
2021-10-13 03:33:05 +0900

    fix: 횟수 입력 시 숫자가 아닌 경우도 에러 확인하도록 수정

--- src/main/java/racinggame/domain/Player.java
+++ src/main/java/racinggame/domain/Player.java
@@ -1,10 +1,17 @@
 package racinggame.domain;
 
 import nextstep.utils.Console;
+import racinggame.exception.RacingGameErrorCode;
+import racinggame.exception.RacingGameException;
 
 public class Player {
     public AttemptNumber inputAttemptNumber() {
-        return new AttemptNumber(Integer.parseInt(Console.readLine()));
+        String input = Console.readLine();
+        try {
+            return new AttemptNumber(Integer.parseInt(input));
+        } catch (Exception e) {
+            throw new RacingGameException(RacingGameErrorCode.INVALID_NUMBER);
+        }
     }
 
     public String inputNames() {
--- src/main/java/racinggame/exception/RacingGameErrorCode.java
+++ src/main/java/racinggame/exception/RacingGameErrorCode.java
@@ -2,6 +2,7 @@ package racinggame.exception;
 
 public enum RacingGameErrorCode {
     INVALID_CAR_NAME("5글자 초과 또는 비어있는 이름으로 자동차를 생성할 수 없습니다."),
+    INVALID_NUMBER("유효한 숫자를 입력해주세요."),
     INVALID_ATTEMPT_NUMBER("1회 이상의 횟수를 입력해주세요.");
 
     private final String message;
