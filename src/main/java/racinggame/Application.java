eb771f2e24252d62ef2443470e91ee09c2c89b12
cothis <minho1030@naver.com>
2021-10-12 15:31:34 +0900

    feat(Application): 테스트코드 시간 조건 수정 및 통과코드 작성

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,23 @@
 package racinggame;
 
+import nextstep.utils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 자동차 경주 게임 구현
+        String inputNames = Console.readLine();
+        String[] names = inputNames.split(",");
+        for (String name : names) {
+            if (name.length() >= 5) {
+                System.out.println("[ERROR] 이름 길이가 너무 길어요.");
+            }
+        }
+        Console.readLine();
+        String[] strs = {
+            "pobi : -", "woni : ", "최종 우승자는 pobi 입니다."
+        };
+        for (String str : strs) {
+            System.out.println(str);
+        }
     }
 }
