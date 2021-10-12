c7567af2e31bc1f62340a7af691ccdee2deb5213
rpgworld <cloud_data@naver.com>
2021-10-13 08:16:34 +0900

    feat : 자동차 클래스 구현(생성, 이동-전진,정지) 및 테스트 완료

---
+++ src/main/java/racinggame/domain/Car.java
@@ -0,0 +1,35 @@
+package racinggame.domain;
+
+import nextstep.utils.Randoms;
+
+public class Car {
+    private static final int MIN_BOUND = 1;
+    private static final int MAX_BOUND = 9;
+    private static final int MOVING_FORWARD = 4;        // 4 이상 전진 / 3이하 정지
+    private static final int CAR_NAME_MAX_LENGTH = 5;   // 자동차 이름 가능 최대글자수
+
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        if (name == null || name.trim().equals(""))
+            throw new IllegalArgumentException("자동차 이름이 빈값일 수 없습니다.");
+        if (name.length() > CAR_NAME_MAX_LENGTH)
+            throw new IllegalArgumentException("자동차 이름은 5글자 이하만 가능합니다.");
+
+        this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
+        if (getRandomNo() >= MOVING_FORWARD)
+            this.position++;
+    }
+
+    protected int getRandomNo() {
+        return Randoms.pickNumberInRange(MIN_BOUND, MAX_BOUND);
+    }
+}
---
+++ src/test/java/racinggame/domain/CarTest.java
@@ -0,0 +1,53 @@
+package racinggame.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CarTest {
+
+    @Test
+    @DisplayName(value = "1~9의 랜덤값을 가져오는가")
+    @RepeatedTest(value = 5, name = "실행횟수 {currentRepetition}/{totalRepetitions}")
+    public void 랜덤값을가져오니1_9() {
+        Car car = new Car("pobi");
+        assertThat(car.getRandomNo()).isGreaterThan(0).isLessThan(10);
+    }
+
+    @Test
+    @DisplayName(value = "이름이 5글자 이상일때, 오류 발생하는가")
+    public void 글자5글자이상오류() {
+        assertThatThrownBy(() -> {
+            new Car("fighting!");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName(value = "4 이상일 때 전진하는가")
+    public void 전진하니() {
+        Car car = new Car("pobi") {
+            @Override
+            protected int getRandomNo() {
+                return 4;
+            }
+        };
+        car.move();
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName(value = "3 이하일 때 정지하는가")
+    public void 정지하니() {
+        Car car = new Car("pobi") {
+            @Override
+            protected int getRandomNo() {
+                return 3;
+            }
+        };
+        car.move();
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+}
