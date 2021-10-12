fa03e84a897b8a66b04dbc42f7f9b2b9681614b0
yh22953905 <yh22953905@gmail.com>
2021-10-12 14:19:40 +0900

    refactor: RacingGame 에서 Winners 분리

--- src/main/java/racinggame/RacingGame.java
+++ src/main/java/racinggame/RacingGame.java
@@ -1,9 +1,5 @@
 package racinggame;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-
 import nextstep.utils.Randoms;
 
 public class RacingGame {
@@ -31,7 +27,7 @@ public class RacingGame {
 			race();
 		}
 
-		printWinners(getWinners());
+		printWinners(Winners.init(cars));
 	}
 
 	/**
@@ -50,60 +46,17 @@ public class RacingGame {
 
 	/**
 	 * @author : yh22953905
-	 * @Date : 2021/10/12 1:16 오전
-	 * @Description : 최종 우승자를 가져옴
-	 *
-	 **/
-	private List<Car> getWinners() {
-		sortCarsByDistance();
-
-		return pickWinners();
-	}
-
-	/**
-	 * @author : yh22953905
-	 * @Date : 2021/10/12 1:18 오전
-	 * @Description : 자동차들을 distance 순으로 정렬
-	 *
-	 **/
-	private void sortCarsByDistance() {
-		cars.value().sort(Comparator.comparingInt((Car car) -> car.distance().value()).reversed());
-	}
-
-	/**
-	 * @author : yh22953905
-	 * @Date : 2021/10/12 1:18 오전
-	 * @Description : 정렬된 자동차들을 토대로 최종 우승자들을 선별
-	 *
-	 **/
-	private List<Car> pickWinners() {
-		List<Car> winners = new ArrayList<>();
-		int winnerDistance = 0;
-
-		for (Car car : cars.value()) {
-			if (winnerDistance > car.distance().value()) {
-				return winners;
-			}
-			winnerDistance = car.distance().value();
-			winners.add(car);
-		}
-
-		return winners;
-	}
-
-	/**
-	 * @author : yh22953905
 	 * @Date : 2021/10/12 1:19 오전
 	 * @Description : 최종 우승자들을 출력
 	 *
 	 **/
-	private void printWinners(List<Car> winners) {
+	private void printWinners(Winners winners) {
 		String winnersName = "";
 
-		for (Car winner : winners) {
+		for (Car winner : winners.value()) {
 			winnersName += winner.carName().value() + ",";
 		}
 
-		System.out.println(String.format("최종 우승자는 %s 입니다.", winnersName.substring(0,winnersName.lastIndexOf(","))));
+		System.out.println(String.format("최종 우승자는 %s 입니다.", winnersName.substring(0, winnersName.lastIndexOf(","))));
 	}
 }
---
+++ src/main/java/racinggame/Winners.java
@@ -0,0 +1,50 @@
+package racinggame;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Winners {
+	private static List<Car> winners = new ArrayList<>();
+
+	private Winners(Cars cars) {
+		sortCarsByDistance(cars);
+		pickWinners(cars);
+	}
+
+	public static Winners init(Cars cars) {
+		return new Winners(cars);
+	}
+
+	public List<Car> value() {
+		return winners;
+	}
+
+	/**
+	 * @author : yh22953905
+	 * @Date : 2021/10/12 1:18 오전
+	 * @Description : 자동차들을 distance 순으로 정렬
+	 *
+	 **/
+	private void sortCarsByDistance(Cars cars) {
+		cars.value().sort(Comparator.comparingInt((Car car) -> car.distance().value()).reversed());
+	}
+
+	/**
+	 * @author : yh22953905
+	 * @Date : 2021/10/12 1:18 오전
+	 * @Description : 정렬된 자동차들을 토대로 최종 우승자들을 선별
+	 *
+	 **/
+	private void pickWinners(Cars cars) {
+		int winnerDistance = 0;
+
+		for (Car car : cars.value()) {
+			if (winnerDistance > car.distance().value()) {
+				break;
+			}
+			winnerDistance = car.distance().value();
+			winners.add(car);
+		}
+	}
+}
