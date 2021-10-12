742fffecb6a970073cde67f2f38d6ccd125a2895
kkwan0226 <kkwan0226@gmail.com>
2021-10-12 09:42:34 +0900

    style(NSTest.java): import 순서 변경 및 개행 추가 Before: import > import static After: import static > import(external) > import(internal)
    캠퍼스 핵데이 Java 코딩 컨벤션 적용에 따른 import 순서 변경 및 개행 추가

--- src/test/java/nextstep/test/NSTest.java
+++ src/test/java/nextstep/test/NSTest.java
@@ -1,9 +1,9 @@
 package nextstep.test;
 
-import nextstep.utils.Randoms;
-import org.assertj.core.util.Strings;
-import org.junit.jupiter.api.function.Executable;
-import org.mockito.MockedStatic;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -13,11 +13,11 @@ import java.time.Duration;
 import java.util.Arrays;
 import java.util.NoSuchElementException;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
-import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mockStatic;
+import org.assertj.core.util.Strings;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.MockedStatic;
+
+import nextstep.utils.Randoms;
 
 public abstract class NSTest {
     private static final Duration TEST_TIMEOUT = Duration.ofSeconds(1L);
