5040710938fe5fddb5b09bbf698b4c98271f87a2
Junha Kim <kimjh4930@gmail.com>
2021-10-13 05:12:50 +0900

    refactor : ui 분리 리팩토링 완료

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,24 +1,10 @@
 package racinggame;
 
-import racinggame.circuit.RacingCircuit;
-import racinggame.racinggame.RacingGame;
-import racinggame.racinggame.Rule;
-import racinggame.rule.RacingRule;
-import racinggame.rule.winnerrules.FarAwayWinRule;
-import racinggame.ui.input.ConsoleKeyboard;
-import racinggame.ui.output.ConsolePrinter;
+import racinggame.ui.RacingGameScreen;
 
 public class Application {
 	public static void main(String[] args) {
-		Rule rule = new RacingRule(new FarAwayWinRule());
-
-		RacingGame racingGame = new RacingGame(
-			new ConsoleKeyboard(),
-			new ConsolePrinter(),
-			new RacingCircuit(),
-			rule
-		);
-
-		racingGame.play();
+		RacingGameScreen screen = new RacingGameScreen();
+		screen.on();
 	}
 }
--- src/main/java/racinggame/ui/RacingGameScreen.java
+++ src/main/java/racinggame/ui/RacingGameScreen.java
@@ -1,14 +1,25 @@
 package racinggame.ui;
 
+import racinggame.circuit.FinalRecord;
+import racinggame.circuit.RacingCircuit;
+import racinggame.dice.TenSidedDice;
+import racinggame.racinggame.Circuit;
+import racinggame.racinggame.RacingGame;
+import racinggame.racinggame.Rule;
+import racinggame.rule.RacingRule;
+import racinggame.rule.winnerrules.FarAwayWinRule;
 import racinggame.ui.config.RacingGameConfig;
 import racinggame.ui.input.ConsoleKeyboard;
 import racinggame.ui.output.ConsolePrinter;
 
 public final class RacingGameScreen {
 	private Settings settings;
+	private FinalRecord finalRecord;
 
 	public void on() {
 		gameSetting();
+		play();
+		printResult();
 	}
 
 	private void gameSetting() {
@@ -16,5 +27,30 @@ public final class RacingGameScreen {
 			new ConsoleKeyboard(),
 			new ConsolePrinter())
 			.configGame();
+	}
+
+	private void play (){
+		Circuit circuit = new RacingCircuit(
+			new TenSidedDice(),
+			new RacingRule(
+				new FarAwayWinRule()
+			)
+		);
+
+		RacingGame racingGame = new RacingGame(circuit);
+		finalRecord = racingGame.play(settings.getRacingCars(), settings.getLap());
+	}
+
+	private void printResult (){
+		Rule rule = new RacingRule(new FarAwayWinRule());
+		ConsolePrinter printer = new ConsolePrinter();
+
+		String result = new StringBuilder()
+			.append("실행결과\n")
+			.append(finalRecord.readFinalRecord())
+			.append("최종 우승자는 ").append(rule.judgeWinners(finalRecord).getNamesWithDelimiter(",")).append(" 입니다.")
+			.toString();
+
+		printer.print(result);
 	}
 }
