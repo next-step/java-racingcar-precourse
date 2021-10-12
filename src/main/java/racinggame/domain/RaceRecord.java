18d16d6442c044e29d0604b52e2fa6f28d3d1dce
wyjax <wyjax.dev@gmail.com>
2021-10-12 21:06:51 +0900

    feat: status 추가

--- src/main/java/racinggame/domain/RaceRecord.java
+++ src/main/java/racinggame/domain/RaceRecord.java
@@ -1,16 +1,16 @@
 package racinggame.domain;
 
+import racinggame.type.MovingStatus;
+
 public class RaceRecord {
     private CarName carName;
     private Distance distance;
+    private MovingStatus status;
 
     public RaceRecord(RacingCar racingCar) {
         this.carName = new CarName(racingCar.getName());
         this.distance = new Distance(racingCar.getDistance());
-    }
-
-    public void report() {
-        System.out.println(reportMessage());
+        this.status = racingCar.getMovingStatus();
     }
 
     public String reportMessage() {
