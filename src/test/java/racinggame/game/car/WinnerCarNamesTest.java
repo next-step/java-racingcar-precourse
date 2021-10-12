3ecc2f3ce317971b3ab4b093fab5b243c291dac4
SunghunDev <sunghun@sunghun.dev>
2021-10-12 22:18:31 +0900

    test: WinnerCarNames 테스트 케이스 작성

---
+++ src/test/java/racinggame/game/car/WinnerCarNamesTest.java
@@ -0,0 +1,57 @@
+package racinggame.game.car;
+
+import org.junit.jupiter.api.Test;
+import racinggame.BaseTest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WinnerCarNamesTest extends BaseTest<Car> {
+
+    @Test
+    void test() {
+        int winnerCount = 2;
+        List<String> winnerCarNameList = createWinnerCarNames(winnerCount);
+
+        int winnerForwardMovementCount = 10;
+
+        List<Car> cars = new ArrayList<>();
+        cars.addAll(createCars(winnerCarNameList, winnerForwardMovementCount));
+        cars.addAll(createCars(2, 0));
+
+        WinnerCarNames winnerCarNames = new WinnerCarNames(cars, winnerForwardMovementCount);
+        for (int i = 0; i < winnerCount; i++) {
+            assertThat(winnerCarNames.getWinnerCarNames().contains(winnerCarNameList.get(i))).isTrue();
+        }
+    }
+
+    private List<Car> createCars(List<String> carNames, int forwardMovementCount) {
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < carNames.size(); i++) {
+            Car car = new Car(carNames.get(i));
+            setPrivateField(car, "forwardMovementCount", forwardMovementCount);
+            cars.add(car);
+        }
+        return cars;
+    }
+
+    private List<Car> createCars(int size, int forwardMovementCount) {
+        List<Car> cars = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            Car car = new Car("c" + i);
+            setPrivateField(car, "forwardMovementCount", forwardMovementCount);
+            cars.add(car);
+        }
+        return cars;
+    }
+
+    private List<String> createWinnerCarNames(int size) {
+        List<String> carNames = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            carNames.add("w" + i);
+        }
+        return carNames;
+    }
+}
