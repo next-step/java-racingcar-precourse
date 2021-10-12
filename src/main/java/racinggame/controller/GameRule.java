8ee0d65a9a50990168ca209dbf5ecd61e0b1c131
BottleH <guys1017@korea.ac.kr>
2021-10-12 13:27:07 +0900

    feat(GameRule): GameRule 컨트롤러 추가
    - 게임 도중, 자동차 이동 로직
    - README.md 기능 완료 표시

---
+++ src/main/java/racinggame/controller/GameRule.java
@@ -0,0 +1,30 @@
+package racinggame.controller;
+
+import racinggame.domain.Car;
+import racinggame.domain.MoveCount;
+import racinggame.domain.RacingCars;
+
+public class GameRule {
+
+	private RacingCars racingCars;
+	private MoveCount moveCount;
+
+	public GameRule(RacingCars racingCars, MoveCount moveCount) {
+		this.racingCars = racingCars;
+		this.moveCount = moveCount;
+	}
+
+	public void gameStart() {
+		int count = this.moveCount.getCount();
+		for (int i = 0; i < count; i++) {
+			racing();
+		}
+	}
+
+	private void racing() {
+		for (Car car : this.racingCars.getRacingCars()) {
+			car.moveCar();
+			System.out.println(car.currentCars());
+		}
+	}
+}
---
+++ src/test/java/racinggame/controller/GameRuleTest.java
@@ -0,0 +1,27 @@
+package racinggame.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import racinggame.domain.Car;
+import racinggame.domain.MoveCount;
+import racinggame.domain.RacingCars;
+
+@DisplayName("게임 룰 컨트롤러 Test")
+class GameRuleTest {
+
+	@Test
+	@DisplayName("게임 시작 Test")
+	void gameStart() {
+		RacingCars racingCars = new RacingCars();
+		MoveCount moveCount = new MoveCount(3);
+		Car lam = new Car("람보르기니");
+		Car truck = new Car("트럭");
+		racingCars.addCar(lam);
+		racingCars.addCar(truck);
+		GameRule gameRule = new GameRule(racingCars, moveCount);
+		gameRule.gameStart();
+	}
+}
