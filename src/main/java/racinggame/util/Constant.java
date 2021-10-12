e3ecf9f3689704ef86bbb572a10a2b3e313db849
lattedy <stary7stars@naver.com>
2021-10-12 14:27:00 +0900

    feat: Constant 정의
    * 자동차 이름 길이
    * 게임 횟수
    * 랜덤값 관련 값

---
+++ src/main/java/racinggame/util/Constant.java
@@ -0,0 +1,11 @@
+package racinggame.util;
+
+public class Constant {
+    public static final int CAR_NAME_LENGTH = 5; // 자동차 이름 길이 제한
+
+    public static final int PLAY_COUNT_LIMIT = 200; // 게임 횟수 제한
+
+    public static final int LOWER_BOUND_OF_MOVE_POSSIBILITY = 0; // 랜덤값의 최소값
+    public static final int UPPER_BOUND_OF_MOVE_POSSIBILITY = 9; // 랜덤값의 최대값
+    public static final int BORDER_OF_MOVE_POSSIBILITY = 4; // 몇 이상일 경우 움직일지 경계값
+}
