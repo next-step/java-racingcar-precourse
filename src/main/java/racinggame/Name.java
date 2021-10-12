2bfd9ebd3df22266f13d4388df30f6ea6a9d6520
CodingNoJam <hanwoodww@gmail.com>
2021-10-13 06:16:44 +0900

    feat: 경주에 참가하는 자동차들 이름 입력받기 기능구현
    - 화면 출력을 담당할 Printer 클래스 작성
    - 쉼표(,)로 자동차 이름 구분
    - Name 클래스에 자리수 검증 로직 추가(1~5자리만 허용)

--- src/main/java/racinggame/Name.java
+++ src/main/java/racinggame/Name.java
@@ -4,10 +4,18 @@ public class Name {
 	private final String name;
 
 	public Name(String name) {
+		validateLength(name);
 		this.name = name;
 	}
 
 	public String value() {
 		return this.name;
 	}
+
+	private void validateLength(String name) {
+		if (name.length() < 1 || name.length() > 5) {
+			throw new IllegalArgumentException("[ERROR] 이름은 1~5자 사이로만 가능합니다.");
+		}
+	}
+
 }
---
+++ src/main/java/racinggame/Printer.java
@@ -0,0 +1,13 @@
+package racinggame;
+
+import nextstep.utils.Console;
+
+public class Printer {
+	private static final String ENTER_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+	private static final String DELIMITER = ",";
+
+	public static String[] enterCarNames() {
+		System.out.println(ENTER_CAR_NAMES);
+		return Console.readLine().split(DELIMITER);
+	}
+}
