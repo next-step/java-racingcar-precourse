6cbe6e2cc67b6b1ef31ef0b93273bb2193dcdbe6
lattedy <stary7stars@naver.com>
2021-10-12 14:27:58 +0900

    feat: Exception 구현
    * Input관련 exception 구현

---
+++ src/main/java/racinggame/exception/IllegalInputException.java
@@ -0,0 +1,9 @@
+package racinggame.exception;
+
+public class IllegalInputException extends Exception {
+    public IllegalInputException(){}
+
+    public IllegalInputException(String message) {
+        super(message);
+    }
+}
