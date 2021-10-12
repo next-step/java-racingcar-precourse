582f11f49cdf0dae17fafd9543e15e91baeea136
donsonioc2010 <whddnjs822@gmail.com>
2021-10-13 01:10:22 +0900

    refactor : stream제거

--- src/main/java/racinggame/DataInput.java
+++ src/main/java/racinggame/DataInput.java
@@ -6,33 +6,31 @@ import racinggame.Enum.StartGuide;
 import racinggame.car.domain.CarName;
 import racinggame.car.utils.CarsUtil;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class DataInput {
 
     public static List<CarName> inputCarName() {
-        while (true) {
-            try {
-                System.out.println(StartGuide.NAME_MSG.getMsg());
-                return Arrays.asList(CarsUtil.makeCarStringToAry(Console.readLine())).stream()
-                        .map(name -> new CarName(name))
-                        .collect(Collectors.toList());
-            }catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
+        try {
+            System.out.println(StartGuide.NAME_MSG.getMsg());
+            String[] inputCarNames = CarsUtil.makeCarStringToAry(Console.readLine());
+            List<CarName> rtnData = new ArrayList<>();
+            for(String carName : inputCarNames) {
+                rtnData.add(new CarName(carName));
             }
+            return rtnData;
+        }catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
         }
     }
 
     public static int inputDoingRound() {
-        while(true) {
-            try {
-                System.out.println(StartGuide.ROUND_MSG.getMsg());
-                return Integer.parseInt(Console.readLine());
-            }catch (NumberFormatException e) {
-                System.out.println(Error.WRONG_INPUT_NUMBER.getMsg());
-            }
+        try {
+            System.out.println(StartGuide.ROUND_MSG.getMsg());
+            return Integer.parseInt(Console.readLine());
+        }catch (NumberFormatException e) {
+            throw new NumberFormatException(Error.WRONG_INPUT_NUMBER.getMsg());
         }
     }
 }
