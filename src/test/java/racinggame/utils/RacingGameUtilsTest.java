7d556f1bd2878f300c13c0d02160f88fe0ce4afb
seonghyeoklee <dltjdgur327@gmail.com>
2021-10-13 21:55:38 +0900

    test : 메소드 리팽토링으로 테스트 코드 이동

--- src/test/java/racinggame/utils/RacingGameUtilsTest.java
+++ src/test/java/racinggame/utils/RacingGameUtilsTest.java
@@ -2,12 +2,7 @@ package racinggame.utils;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
-import racinggame.domain.RacingCar;
 
-import java.util.NoSuchElementException;
-
-import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static racinggame.utils.RacingGameUtils.*;
 
@@ -22,28 +17,6 @@ public class RacingGameUtilsTest {
         assertThat(racingCarNameSplit(racingCarNameInput).length).isEqualTo(lengthInput);
     }
 
-    @ParameterizedTest(name = "자동차 이름 [{0}] 길이가 0자 이하면 IllegalArgumentException 발생한다")
-    @ValueSource(strings = {","})
-    void 쉼표_기준으로_구분한_자동차_이름의_길이가_0자_이하_IllegalArgumentException_발생한다(String racingCarNameInput) {
-        for (String racingCarName : racingCarNameSplit(racingCarNameInput)) {
-            assertThatIllegalArgumentException()
-                    .isThrownBy(() -> {
-                        isValidRacingCarNameLengthLimit(racingCarName);
-                    });
-        }
-    }
-
-    @ParameterizedTest(name = "자동차 이름 [{0}] 길이가 5자 초과면 IllegalArgumentException 발생한다")
-    @ValueSource(strings = {"javaji,javamaster"})
-    void 쉼표_기준으로_구분한_자동차_이름의_길이가_5자_초과_IllegalArgumentException_발생한다(String racingCarNameInput) {
-        for (String racingCarName : racingCarNameSplit(racingCarNameInput)) {
-            assertThatExceptionOfType(NoSuchElementException.class)
-                    .isThrownBy(() -> {
-                        isValidRacingCarNameLengthLimit(racingCarName);
-                    });
-        }
-    }
-
     @ParameterizedTest(name = "자동차 이름 [{0}] 공백을 제거한다")
     @CsvSource(value = {  "pobi ,crong ,honux :pobi,crong,honux"
                         , " pobi, crong, honux:pobi,crong,honux"
@@ -55,62 +28,6 @@ public class RacingGameUtilsTest {
 
         for (int i = 0; i < beforeNames.length; i++) {
             assertThat(beforeNames[i]).isEqualTo(afterNames[i]);
-        }
-    }
-
-    @ParameterizedTest(name = "자동차 이름 [{0}] 길이가 5자 초과면 메세지를 출력한다")
-    @ValueSource(strings = {"javaji,javamaster"})
-    void 자동차_이름의_길이가_5자_초과_메세지를_출력한다(String racingCarName) {
-        assertThatExceptionOfType(NoSuchElementException.class)
-                .isThrownBy(() -> {
-                    isValidRacingCarNameLengthLimit(racingCarName);
-                })
-                .withMessage(ERROR_MESSAGE);
-    }
-
-    @ParameterizedTest(name = "경주의 시도할 횟수[{0}]는 0보다 큰 숫자를 입력해야 한다")
-    @ValueSource(ints = {0, -10})
-    void 경주의_시도할_횟수는_0_보다_큰_숫자를_입력해야_한다(int tryCount) {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> {
-                    isValidTryCountGreaterThanZero(tryCount);
-                })
-                .withMessage(ERROR_MESSAGE);
-    }
-
-    @ParameterizedTest(name = "[{0}] 경주의 시도할 횟수 입력값은 숫자만 유효하다")
-    @ValueSource(strings = {"a", "B", "%", "한글", "10.5"})
-    void 입력값은_숫자만_유효하다(String readLine) {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> {
-                    isValidOnlyNumber(readLine);
-                })
-                .withMessage(ERROR_MESSAGE);
-    }
-
-    @ParameterizedTest(name = "[{0}] 경주의 시도할 횟수 입력값은 0보다 큰 숫자만 유효하다")
-    @ValueSource(strings = {"1", "5", "10", "100"})
-    void 입력값은_0_보다_큰_숫자만_유효하다(String readLine) {
-        assertThatCode(() -> isValidTryCount(readLine)).doesNotThrowAnyException();
-    }
-
-    @ParameterizedTest(name = "랜덤값이 [{0}] 이상이면 자동가 전진한다")
-    @ValueSource(ints = {CHECK_NUMBER_VALUE})
-    void 자동차가_전진한다(int pickNumber) {
-        RacingCar racingCar = new RacingCar("pobi");
-        if (isMoved(pickNumber)) {
-            racingCar.move();
-            assertThat(racingCar.getMovesCount()).isEqualTo(1);
-        }
-    }
-
-    @ParameterizedTest(name = "랜덤값이 [{0}] 이하면 자동가 멈춘다")
-    @ValueSource(ints = {CHECK_NUMBER_VALUE - 1})
-    void 자동차가_멈춘다(int pickNumber) {
-        RacingCar racingCar = new RacingCar("pobi");
-        if (!isMoved(pickNumber)) {
-            racingCar.stop();
-            assertThat(racingCar.getMovesCount()).isEqualTo(0);
         }
     }
 }
