b812af87960a7218a7462499662e01f157451a47
hanna <gkssk4163@hanmail.net>
2021-10-13 19:56:52 +0900

    feat: 사용자 입력처리 기능보완
    자동차 입력 공백제거 기능추가 및 숫자입력 오류메세지 수정

--- src/main/java/racinggame/view/UserConsole.java
+++ src/main/java/racinggame/view/UserConsole.java
@@ -3,12 +3,16 @@ package racinggame.view;
 import nextstep.utils.Console;
 
 public class UserConsole {
-	public static final String TRIAL_NUM_ERROR_MESSAGE = "[ERROR] 시도 횟수는 2147483647 이하로 입력가능합니다.";
+	public static final String TRIAL_NUM_ERROR_MESSAGE = "[ERROR] 시도 횟수는 2147483647 이하의 숫자로 입력가능합니다.";
 
 	public static String[] readCarNames() {
 		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-		String carNames = Console.readLine();
-		return carNames.split(",");
+		String strCarNames = Console.readLine();
+		String[] carNames = strCarNames.split(",");
+		for (int i = 0; i < carNames.length; i++) {
+			carNames[i] = carNames[i].trim();
+		}
+		return carNames;
 	}
 
 	public static int readTrialNumber() {
