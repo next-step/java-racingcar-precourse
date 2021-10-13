18bcb8cd00d43da25aba4c64101482bcc44bac5c
jin338s <jin3378s@gmail.com>
2021-10-13 12:31:08 +0900

    Feat : 시스템 운영에 필요한 문자열과 유틸 함수 추가

---
+++ src/main/java/racinggame/SystemMessages.java
@@ -0,0 +1,7 @@
+package racinggame;
+
+public class SystemMessages {
+    public final static String INPUT_RACING_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    public final static String INPUT_RACING_COUNT = "시도할 회수는 몇회인가요?";
+    public final static String RACING_RESULT = "실행 결과";
+}
---
+++ src/main/java/racinggame/Utils.java
@@ -0,0 +1,13 @@
+package racinggame;
+
+public class Utils {
+    public static String getDistanceDash(int count) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < count; i++) {
+            result.append("-");
+        }
+
+        return result.toString();
+    }
+}
