9ced66cf762eade1373c5e7d43626a00bb2e3ce0
박창혁 <changchang73@pionnet.co.kr>
2021-10-13 22:13:13 +0900

    style: cars 변수명 변경
    변수명 혼용되어 사용되어 정리

--- src/main/java/racinggame/model/Cars.java
+++ src/main/java/racinggame/model/Cars.java
@@ -9,28 +9,32 @@ public class Cars {
 
 	private static final int MIN_NUMBER = 0;
 	private static final int MAX_NUMBER = 9;
-	private final List<Car> racers;
+	private final List<Car> cars;
 	private Distance maximumTravelDistance;
 
 	public Cars(List<String> names) {
-		this.racers = initializeCarName(names);
+		this.cars = initializeCarName(names);
 		this.maximumTravelDistance = new Distance(0);
 	}
 
 	private List<Car> initializeCarName(List<String> names) {
-		List<Car> cars = new ArrayList<>();
+		List<Car> racers = new ArrayList<>();
 		for (String name : names) {
-			cars.add(new Car(name));
+			racers.add(new Car(name));
 		}
-		return cars;
+		return racers;
+	}
+
+	public List<Car> getCars() {
+		return this.cars;
 	}
 
 	public int getNumberOfCars() {
-		return racers.size();
+		return cars.size();
 	}
 
 	public void race() {
-		for (Car car : this.racers) {
+		for (Car car : this.cars) {
 			car.move(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER));
 			measureMaximumTrableDistance(car.getDistance());
 		}
@@ -49,4 +53,23 @@ public class Cars {
 	public int getMaximumTravelDistance() {
 		return maximumTravelDistance.getDistance();
 	}
+
+	public List<String> getWinners() {
+		List<String> winners = new ArrayList<>();
+		for (Car car : this.cars) {
+			addWinner(winners, car);
+		}
+		return winners;
+	}
+
+	private void addWinner(List<String> winners, Car car) {
+		if (isWinner(car)) {
+			winners.add(car.getName());
+		}
+	}
+
+	private boolean isWinner(Car car) {
+		return this.maximumTravelDistance.getDistance() == car.getDistance();
+	}
+
 }
