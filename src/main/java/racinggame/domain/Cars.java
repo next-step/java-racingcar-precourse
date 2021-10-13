f07923df47c87cf4a928e7bd1ee8ec9e21bb4d7f
gaeul <toheart@gmail.com>
2021-10-13 19:48:59 +0900

    refactor: moveAllCars 메소드 이동

--- src/main/java/racinggame/domain/Cars.java
+++ src/main/java/racinggame/domain/Cars.java
@@ -1,15 +1,29 @@
 package racinggame.domain;
 
+import nextstep.utils.Randoms;
+
 import java.util.List;
 
 public class Cars {
+    private static final int MIN_GAS_POINT_RANGE = 0;
+    private static final int MAX_GAS_POINT_RANGE = 9;
+
     private final List<Car> cars;
 
     public Cars(List<Car> cars) {
         this.cars = cars;
     }
 
-    public List<Car> getCars() {
-        return cars;
+    public LapResult move() {
+        LapResult lapResult = new LapResult();
+        for (Car car : cars) {
+            int gasPoint = generateGasPoint();
+            lapResult.add(car.move(gasPoint));
+        }
+        return lapResult;
+    }
+
+    private int generateGasPoint() {
+        return Randoms.pickNumberInRange(MIN_GAS_POINT_RANGE, MAX_GAS_POINT_RANGE);
     }
 }
--- src/main/java/racinggame/domain/RacingGame.java
+++ src/main/java/racinggame/domain/RacingGame.java
@@ -1,10 +1,6 @@
 package racinggame.domain;
 
-import nextstep.utils.Randoms;
-
 public class RacingGame {
-    private static final int MIN_GAS_POINT_RANGE = 0;
-    private static final int MAX_GAS_POINT_RANGE = 9;
     private final Cars cars;
 
     public RacingGame(Cars cars) {
@@ -14,21 +10,8 @@ public class RacingGame {
     public GameResult run(LapCount lapCount) {
         GameResult gameResult = new GameResult();
         for (int i = 0; i < lapCount.getValue(); i++) {
-            gameResult.add(moveAllCars());
+            gameResult.add(cars.move());
         }
         return gameResult;
-    }
-
-    private LapResult moveAllCars() {
-        LapResult lapResult = new LapResult();
-        for (Car car : cars.getCars()) {
-            int gasPoint = generateGasPoint();
-            lapResult.add(car.move(gasPoint));
-        }
-        return lapResult;
-    }
-
-    private int generateGasPoint() {
-        return Randoms.pickNumberInRange(MIN_GAS_POINT_RANGE, MAX_GAS_POINT_RANGE);
     }
 }
