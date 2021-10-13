19cafec47a7c7040917038b020f8d55e369f1592
sonyoon7 <sonyoon7@gmail.com>
2021-10-13 18:28:34 +0900

    레이싱게임 테스트 테스트 코드 타임리미트 시간 늘림

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,7 +1,18 @@
 package racinggame;
 
+import racinggame.domain.game.Game;
+import racinggame.domain.users.User;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 자동차 경주 게임 구현
+
+        Game racingGame = new Game(new User());
+        String[] carNames = racingGame.userInputCarNames();
+        int round = racingGame.userInputRound();
+        racingGame.init(round, carNames);
+
+        racingGame.play();
+
     }
 }
