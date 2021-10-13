ac59b7fe5990e37b8d5bd3a90bac14f90e37b42c
박창혁 <changchang73@pionnet.co.kr>
2021-10-13 17:00:29 +0900

    style: Name class -> CarName class 명 변경

--- src/main/java/racinggame/model/CarName.java
+++ src/main/java/racinggame/model/CarName.java
@@ -1,4 +1,4 @@
-package racinggame;
+package racinggame.model;
 
 public class CarName {
 	private final int MAX_LENGTH = 5;
@@ -6,13 +6,13 @@ public class CarName {
 	private final String name;
 
 	public CarName(String name) {
-		if(name.length() > MAX_LENGTH){
-			throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+		if (name.length() > MAX_LENGTH) {
+			throw new IllegalArgumentException(MAX_LENGTH + "자 이하만 가능합니다.");
 		}
 		this.name = name;
 	}
 
 	public String getName() {
-		return name;
+		return this.name;
 	}
 }
--- src/test/java/racinggame/CarNameTest.java
+++ src/test/java/racinggame/CarNameTest.java
@@ -4,7 +4,9 @@ import static org.assertj.core.api.AssertionsForClassTypes.*;
 
 import org.junit.jupiter.api.Test;
 
-public class NameTest {
+import racinggame.model.CarName;
+
+public class CarNameTest {
 	@Test
 	void 자동차_이름_오류() {
 		Throwable thrown = catchThrowable(() -> {
