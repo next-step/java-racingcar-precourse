29b8f7eb50403423a0e61df199dc5bd6e9dc716c
seonghyeoklee <dltjdgur327@gmail.com>
2021-10-13 21:57:20 +0900

    test : move 메소드 리팩토링으로 테스트 코드 수정

--- src/test/java/racinggame/domain/RacingCarsTest.java
+++ src/test/java/racinggame/domain/RacingCarsTest.java
@@ -8,14 +8,13 @@ import java.util.NoSuchElementException;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static racinggame.utils.RacingGameUtils.ERROR_MESSAGE;
-import static racinggame.utils.RacingGameUtils.getRacingCarList;
+import static racinggame.domain.GameErrorMessage.ERROR_RACING_CAR_NAME_LENGTH_LIMIT_MESSAGE;
 
 class RacingCarsTest {
     @ParameterizedTest(name = "자동차 일급콜렉션 객체를 생성한다")
     @ValueSource(strings = {"pobi,crong,honux"})
     void 자동차_일급콜렉션_객체를_생성한다(String racingCarNameInput) {
-        RacingCars racingCars = new RacingCars(getRacingCarList(racingCarNameInput));
+        RacingCars racingCars = new RacingCars(racingCarNameInput);
 
         assertThat(racingCars.getRacingCars()).isInstanceOf(List.class);
         assertThat(racingCars.getRacingCars().size()).isEqualTo(3);
@@ -26,22 +25,22 @@ class RacingCarsTest {
     void 자동차_일급콜렉션_객체_유효성을_검증한다(String racingCarNameInput) {
         assertThatExceptionOfType(NoSuchElementException.class)
                 .isThrownBy(() -> {
-                    new RacingCars(getRacingCarList(racingCarNameInput));
-                }).withMessage(ERROR_MESSAGE);
+                    new RacingCars(racingCarNameInput);
+                }).withMessage(ERROR_RACING_CAR_NAME_LENGTH_LIMIT_MESSAGE.getErrorMessage());
     }
 
     @ParameterizedTest(name = "가장 큰 전진 횟수를 반환한다")
     @ValueSource(strings = {"pobi,crong,honux"})
     void 가장_큰_전진_횟수를_반환한다(String racingCarNameInput) {
-        RacingCars racingCars = new RacingCars(getRacingCarList(racingCarNameInput));
+        RacingCars racingCars = new RacingCars(racingCarNameInput);
         List<RacingCar> racingCarList = racingCars.getRacingCars();
         /* 1회 */
-        racingCarList.get(0).move();
+        racingCarList.get(0).movingForward();
         racingCarList.get(1).stop();
         racingCarList.get(2).stop();
         /* 2회 */
-        racingCarList.get(0).move();
-        racingCarList.get(1).move();
+        racingCarList.get(0).movingForward();
+        racingCarList.get(1).movingForward();
         racingCarList.get(2).stop();
 
         assertThat(racingCars.getMaxMovesCount()).isEqualTo(2);
@@ -50,59 +49,25 @@ class RacingCarsTest {
     @ParameterizedTest(name = "우승자를 선별하여 반환한다(공동 우승 가능)")
     @ValueSource(strings = {"pobi,crong,honux"})
     void 우승자를_선별하여_반환한다(String racingCarNameInput) {
-        RacingCars racingCars = new RacingCars(getRacingCarList(racingCarNameInput));
+        RacingCars racingCars = new RacingCars(racingCarNameInput);
         List<RacingCar> racingCarList = racingCars.getRacingCars();
         /* 1회 */
-        racingCarList.get(0).move();
+        racingCarList.get(0).movingForward();
         racingCarList.get(1).stop();
         racingCarList.get(2).stop();
         /* 2회 */
-        racingCarList.get(0).move();
-        racingCarList.get(1).move();
+        racingCarList.get(0).movingForward();
+        racingCarList.get(1).movingForward();
         racingCarList.get(2).stop();
         /* 3회 */
         racingCarList.get(0).stop();
-        racingCarList.get(1).move();
-        racingCarList.get(2).move();
+        racingCarList.get(1).movingForward();
+        racingCarList.get(2).movingForward();
 
         List<RacingCar> winners = racingCars.getWinners();
 
         assertThat(winners.size()).isEqualTo(2);
         assertThat(winners.get(0).getRacingCarName()).isEqualTo("pobi");
         assertThat(winners.get(1).getRacingCarName()).isEqualTo("crong");
-    }
-
-    @ParameterizedTest(name = "우승자 자동차 이름을 출력한다(공동 우승)")
-    @ValueSource(strings = {"pobi,crong,honux"})
-    void 우승자_자동차_이름을_출력한다_공동_우승(String racingCarNameInput) {
-        RacingCars racingCars = new RacingCars(getRacingCarList(racingCarNameInput));
-        List<RacingCar> racingCarList = racingCars.getRacingCars();
-        /* 1회 */
-        racingCarList.get(0).move();
-        racingCarList.get(1).move();
-        racingCarList.get(2).stop();
-        /* 2회 */
-        racingCarList.get(0).move();
-        racingCarList.get(1).move();
-        racingCarList.get(2).stop();
-
-        assertThat(racingCars.toString()).isEqualTo("최종 우승자는 pobi,crong 입니다.");
-    }
-
-    @ParameterizedTest(name = "우승자 자동차 이름을 출력한다")
-    @ValueSource(strings = {"pobi,crong,honux"})
-    void 우승자_자동차_이름을_출력한다(String racingCarNameInput) {
-        RacingCars racingCars = new RacingCars(getRacingCarList(racingCarNameInput));
-        List<RacingCar> racingCarList = racingCars.getRacingCars();
-        /* 1회 */
-        racingCarList.get(0).move();
-        racingCarList.get(1).move();
-        racingCarList.get(2).stop();
-        /* 2회 */
-        racingCarList.get(0).move();
-        racingCarList.get(1).stop();
-        racingCarList.get(2).stop();
-
-        assertThat(racingCars.toString()).isEqualTo("최종 우승자는 pobi 입니다.");
     }
 }
