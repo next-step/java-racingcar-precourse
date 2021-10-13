2122256d6efc470cc0738eb43e6a0a363c4d49fa
LeeYoungJun <dldudwns72@gmail.com>
2021-10-13 19:46:27 +0900

    test (Name 클래스 테스트)

--- src/main/java/racinggame/domain/exceptions/CarsException.java
+++
@@ -1,6 +0,0 @@
-package racinggame.domain;
-
-public class CarsException extends IllegalArgumentException {
-    public CarsException(String s) {
-    }
-}
--- src/test/java/domainTest/NameTest.java
+++ src/test/java/domainTest/NameTest.java
@@ -1,2 +1,30 @@
-package domainTest;public class NameTest {
+package domainTest;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racinggame.domain.Car;
+import racinggame.domain.Name;
+import racinggame.domain.exceptions.CarException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class NameTest {
+
+    @Test
+    @DisplayName("이름이 5자 이하 일 때 정상 생성")
+    public void generatorNmae() {
+        Car car = new Car("lee");
+        assertThat(car.getName()).isEqualTo("lee");
+    }
+
+
+    @Test
+    @DisplayName("이름이 5자 초과인 자동차 생성시 에러 발생")
+    public void errorCarNameOverFive() {
+        assertThatThrownBy(() -> {
+            Name name = new Name("leeyoungjun");
+        }).isInstanceOf(CarException.class);
+    }
+
 }
