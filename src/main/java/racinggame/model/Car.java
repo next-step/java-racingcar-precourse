6761845a1d977341c378c51965d1160bc6a7a9c2
lattedy <stary7stars@naver.com>
2021-10-13 20:59:37 +0900

    refactor: 안쓰는 인자 제거

--- src/main/java/racinggame/model/Car.java
+++ src/main/java/racinggame/model/Car.java
@@ -32,14 +32,14 @@ public class Car {
         dashes = new String[count];
         StringBuilder accumulatedDash = new StringBuilder("");
         for (int i = 0; i < count; i++) {
-            move(accumulatedDash, i,
+            move(accumulatedDash,
                     Randoms.pickNumberInRange(Constant.LOWER_BOUND_OF_MOVE_POSSIBILITY, Constant.UPPER_BOUND_OF_MOVE_POSSIBILITY),
                     possibility);
             dashes[i] = accumulatedDash.toString();
         }
     }
 
-    private void move(StringBuilder accumulatedDash, int idx, int randVal, int possibility) {
+    private void move(StringBuilder accumulatedDash, int randVal, int possibility) {
         if (randVal >= possibility) {
             accumulatedDash.append("-");
             position++;
