0323c3c44656e170bea631a7316d6c4fd9bb9f74
박창혁 <changchang73@pionnet.co.kr>
2021-10-13 22:50:06 +0900

    feat: 시도할 횟수 기능, 우승자 출력 기능 추가
    1. 사용자에게 시도할 횟수 입력 받는다.
    2. 입력받은 값이 0이상의 숫자인지 판벌한다.
    3. 종료 후 우승자를 출력한다.

--- src/main/java/racinggame/controller/RacingCarGame.java
+++ src/main/java/racinggame/controller/RacingCarGame.java
@@ -1,6 +1,7 @@
 package racinggame.controller;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 import racinggame.model.Car;
 import racinggame.model.Cars;
@@ -10,17 +11,35 @@ import racinggame.view.OutputVIew;
 public class RacingCarGame {
 
 	private static final String CAR_NAME_DELIMITER = ",";
+	private static final String CHECK_NUMBER = "(^[0-9]*$)";
 
 	private final InputVIew inputVIew = new InputVIew();
 	private final OutputVIew outputVIew = new OutputVIew();
 
 	public void gameStart() {
-		Cars cars = null;
+		Cars cars = getCars();
+		int raceCount = getRaceCount();
+		while (raceCount-- > 0) {
+			cars.race();
+			outputInGame(cars);
+		}
+		outputVIew.outputWinners(cars);
+	}
 
+	private int getRaceCount() {
+		int raceCount = -1;
+		while (raceCount < 0) {
+			raceCount = getRaceNumber(inputVIew.inputRaceNumber());
+		}
+		return raceCount;
+	}
+
+	private Cars getCars() {
+		Cars cars = null;
 		while (cars == null) {
 			cars = makeCars(inputVIew.inputCarName());
 		}
-
+		return cars;
 	}
 
 	private Cars makeCars(String names) {
@@ -31,6 +50,35 @@ public class RacingCarGame {
 			outputVIew.outputErrorMessage(e.getMessage());
 		}
 		return cars;
+	}
+
+	private int getRaceNumber(String raceNumber) {
+		int returnNumber;
+		try {
+			returnNumber = stringToInt(raceNumber);
+		} catch (Exception e) {
+			outputVIew.outputErrorMessage(e.getMessage());
+			return -1;
+		}
+		return returnNumber;
+	}
+
+	private int stringToInt(String number) {
+		if (isNotNumber(number)) {
+			throw new NumberFormatException("0 이상의 숫자만 입력해주시기 바랍니다.");
+		}
+		return Integer.parseInt(number);
+	}
+
+	private boolean isNotNumber(String number) {
+		return !Pattern.matches(CHECK_NUMBER, number);
+	}
+
+	private void outputInGame(Cars cars) {
+		for (Car car : cars.getCars()) {
+			outputVIew.outputInGame(car);
+		}
+		outputVIew.outputNewLine();
 	}
 
 }
