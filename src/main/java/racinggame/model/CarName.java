58742c24cd0b8f2506780beb4905952caca88ff7
박창혁 <changchang73@pionnet.co.kr>
2021-10-13 20:24:59 +0900

    feat: 자동차 명 없을경우 오류체크 추가

--- src/main/java/racinggame/model/CarName.java
+++ src/main/java/racinggame/model/CarName.java
@@ -1,15 +1,25 @@
 package racinggame.model;
 
+import java.util.NoSuchElementException;
+
 public class CarName {
 	private final int MAX_LENGTH = 5;
 
 	private final String name;
 
 	public CarName(String name) {
+		validation(name);
+		this.name = name;
+	}
+
+	private void validation(String name) {
+		if (name.isEmpty()) {
+			throw new NoSuchElementException("명이 누락되었습니다.");
+		}
+
 		if (name.length() > MAX_LENGTH) {
 			throw new IllegalArgumentException(MAX_LENGTH + "자 이하만 가능합니다.");
 		}
-		this.name = name;
 	}
 
 	public String getName() {
--- src/test/java/racinggame/CarNameTest.java
+++ src/test/java/racinggame/CarNameTest.java
@@ -2,13 +2,16 @@ package racinggame;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
+import java.util.NoSuchElementException;
+
 import org.junit.jupiter.api.Test;
 
 import racinggame.model.CarName;
 
 public class CarNameTest {
+
 	@Test
-	void 자동차_이름_오류() {
+	void 자동차_이름_5자_이상() {
 		Throwable thrown = catchThrowable(() -> {
 			new CarName("123456");
 		});
@@ -16,4 +19,15 @@ public class CarNameTest {
 		assertThat(thrown)
 			.isInstanceOf(IllegalArgumentException.class);
 	}
+
+	@Test
+	void 자동차_이름_없을_경우() {
+		Throwable thrown = catchThrowable(() -> {
+			new CarName("");
+		});
+
+		assertThat(thrown)
+			.isInstanceOf(NoSuchElementException.class);
+	}
+
 }
