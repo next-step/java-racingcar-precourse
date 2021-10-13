b3442d75de54b92d81dc93781e5f73bbd001ef40
insoo <email@email.com>
2021-10-13 20:43:08 +0900

    feat: car Controller

---
+++ src/main/java/racinggame/controller/CarController.java
@@ -0,0 +1,25 @@
+package racinggame.controller;
+
+import racinggame.domain.Car;
+import racinggame.validation.CarValidation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarController {
+    public static String[] splitByComma(String inputCarName) {
+        String[] car = inputCarName.split(",");
+        CarValidation.isCarValidate(car);
+        return car;
+    }
+
+    public static List<Car> generateCars(String inputCarName) {
+        List<Car> cars = new ArrayList<>();
+        for (String carName : splitByComma(inputCarName)) {
+            Car car = new Car(carName);
+            cars.add(car);
+        }
+        return cars;
+    }
+}
+
