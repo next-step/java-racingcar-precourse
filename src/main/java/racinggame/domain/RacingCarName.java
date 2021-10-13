5b8dce69e510abeeb5e6172d8f60b0833c87562b
seonghyeoklee <dltjdgur327@gmail.com>
2021-10-13 21:40:45 +0900

    refactor : 자동차 이름(String)을 검증하는 클래스

--- src/main/java/racinggame/domain/RacingCarName.java
+++ src/main/java/racinggame/domain/RacingCarName.java
@@ -1,11 +1,15 @@
 package racinggame.domain;
 
-import static racinggame.utils.RacingGameUtils.isValidRacingCarNameLengthLimit;
+import java.util.NoSuchElementException;
+
+import static racinggame.domain.GameConfig.RACING_CAR_NAME_MAX_LENGTH;
+import static racinggame.domain.GameConfig.RACING_CAR_NAME_MIN_LENGTH;
+import static racinggame.domain.GameErrorMessage.ERROR_RACING_CAR_NAME_LENGTH_LIMIT_MESSAGE;
 
 public class RacingCarName {
-    private String racingCarName;
+    private final String racingCarName;
 
-    public RacingCarName(String racingCarName) {
+    public RacingCarName(final String racingCarName) {
         validateRacingCarName(racingCarName);
         this.racingCarName = racingCarName;
     }
@@ -14,7 +18,15 @@ public class RacingCarName {
         return racingCarName;
     }
 
-    public void validateRacingCarName(String racingCarName) {
+    public void validateRacingCarName(final String racingCarName) {
         isValidRacingCarNameLengthLimit(racingCarName);
+    }
+
+    public void isValidRacingCarNameLengthLimit(final String racingCarName) {
+        int length = racingCarName.length();
+        if (length < RACING_CAR_NAME_MIN_LENGTH.getValue() || length > RACING_CAR_NAME_MAX_LENGTH.getValue()) {
+            System.out.println(ERROR_RACING_CAR_NAME_LENGTH_LIMIT_MESSAGE.getErrorMessage());
+            throw new NoSuchElementException(ERROR_RACING_CAR_NAME_LENGTH_LIMIT_MESSAGE.getErrorMessage());
+        }
     }
 }
