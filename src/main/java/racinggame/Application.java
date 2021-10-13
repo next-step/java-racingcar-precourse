1786fab86a9dcf18632780d813792fd24ec6b008
CodingNoJam <hanwoodww@gmail.com>
2021-10-13 17:46:13 +0900

    refactor: 기존의 구현한 기능들 메서드로 묶어서 정리
    - 4개 메서드로 구분해서 분리
    - 경주에 참가하는 자동차 이름 입력 받기
    - 게임 내 이동 횟수 입력 받기
    - 이동 횟수 만큼 경주 시작
    - 경주가 끝나고 최종 결과 출력

--- src/main/java/racinggame/Application.java
+++ src/main/java/racinggame/Application.java
@@ -1,22 +1,40 @@
 package racinggame;
 
 public class Application {
+	private static GameController gameController = new GameController();
+	private static Repeat repeat = Repeat.createRepeat();
+
 	public static void main(String[] args) {
 		// TODO 자동차 경주 게임 구현
-		GameController gameController = new GameController();
-		boolean again = true;
-		while (again) {
+		enterRacingCarNames();
+
+		enterMoveCount();
+
+		startRacingGame();
+
+		printResult();
+	}
+
+	private static void enterRacingCarNames() {
+		while (repeat.isTrue()) {
 			String[] carNames = gameController.printEnterCarNames();
-			again = gameController.createCars(carNames);
+			repeat.set(gameController.createCars(carNames));
 		}
+	}
 
-		again = true;
-		while (again) {
-			again = gameController.printEnterMoveCount();
+	private static void enterMoveCount() {
+		repeat.init();
+		while (repeat.isTrue()) {
+			repeat.set(gameController.printEnterMoveCount());
 		}
+	}
 
+	private static void startRacingGame() {
 		gameController.startRacing();
+	}
 
+	private static void printResult() {
 		gameController.printRacingGameWinners();
 	}
+
 }
