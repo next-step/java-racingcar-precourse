659174a043c5cfd5062dd7f1fb464c2de45ba54f
김민지 <21600069@handong.edu>
2021-10-13 22:04:03 +0900

    test : WinnerTest 수정 및 추가

--- src/test/java/racinggame/WinnerTest.java
+++ src/test/java/racinggame/WinnerTest.java
@@ -1,15 +1,16 @@
 package racinggame;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
 import racinggame.domain.Car;
 import racinggame.domain.Cars;
 import racinggame.domain.RacingResult;
 import racinggame.domain.Winner;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class WinnerTest {
     private RacingResult racingResult;
@@ -23,11 +24,11 @@ public class WinnerTest {
         pororoCar = new Car(cars.cars.get(0));
         rupiCar = new Car(cars.cars.get(1));
         ediCar = new Car(cars.cars.get(2));
-        for (int i = 0; i < 5; i++) {
-            pororoCar.play();
-            rupiCar.play();
-            ediCar.play();
-        }
+
+        pororoCar.setMoveCnt(3);
+        rupiCar.setMoveCnt(2);
+        ediCar.setMoveCnt(1);
+
         racingResult = new RacingResult();
         racingResult.addResult(pororoCar);
         racingResult.addResult(rupiCar);
@@ -38,13 +39,28 @@ public class WinnerTest {
     @DisplayName("mapWinner 메소드는")
     class Describe_mapWinner {
         @Nested
-        @DisplayName("5번의 시도를 했을 때")
-        class Context_with_five_move {
+        @DisplayName("pororoCars가 우승자이므로")
+        class Context_with_winner_car {
             @Test
-            @DisplayName("우승자를 생성한다.")
+            @DisplayName("prr를 매핑한다.")
             void return_winner_map() {
                 Winner winner = new Winner(racingResult);
-                assertThat(winner).isNotNull();
+                assertThat(winner.findWinner(racingResult)).contains("prr").doesNotContain("rupi", "edi");
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("findWinnerCnt 메소드는")
+    class Describe_findWinnerCnt {
+        @Nested
+        @DisplayName("가장 멀리간 우승자의 이동 거리를 찾아주므로")
+        class Context_with_winner_car_move_cnt {
+            @Test
+            @DisplayName("prr의 이동거리인 3을 반환한다.")
+            void return_winner_car_move_count() {
+                Winner winner = new Winner(racingResult);
+                assertThat(winner.findWinnerCnt(racingResult)).isEqualTo(3);
             }
         }
     }
