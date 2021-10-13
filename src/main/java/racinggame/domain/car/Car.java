0ec746e75dc6114a91307b1f62c5ed490cad40f4
최상현 <itchoi0429@gmail.com>
2021-10-13 21:30:00 +0900

    레이싱 게임 - 테스트 코드 전

--- src/main/java/racinggame/domain/car/Car.java
+++ src/main/java/racinggame/domain/car/Car.java
@@ -3,6 +3,7 @@ package racinggame.domain.car;
 public class Car {
     private String carName;
     private int location = 0;
+    private static final int CAR_NAME_LIMIT = 5;
 
     public Car(String carName) {
         this.carName = carName;
@@ -12,31 +13,20 @@ public class Car {
         location++;
     }
 
-
-    /*private Map<String, Integer> carRepositories;
-
-    public Car(String... carNames) {
-        this.carRepositories = new HashMap<>();
-        settingMapByCarNames(carNames);
+    public static boolean isAvailableCarName(String carName) {
+        int length = carName.length();
+        return length > 0 && length <= CAR_NAME_LIMIT;
     }
 
-    private void settingMapByCarNames(String[] carNames) {
-        for (String carName : carNames) {
-            carRepositories.put(carName, 0);
-        }
-    }
-
-    public void moveCarByNum(String carName, int number) {
-        if (isMoveCarByNum(number)) {
-            Integer orDefault = carRepositories.getOrDefault(carName, -1);
-
-            carRepositories.put(carName, carRepositories.get(carName) + 1);
-        }
-    }
-
-    private boolean isMoveCarByNum(int number) {
+    public static boolean canMoveCarByNum(int number) {
         return number > 3 && number < 10;
-    }*/
+    }
 
+    public String getCarName() {
+        return carName;
+    }
 
+    public int getLocation() {
+        return location;
+    }
 }
--- src/main/java/racinggame/domain/game/RacingGame.java
+++ src/main/java/racinggame/domain/game/RacingGame.java
@@ -1,5 +1,7 @@
 package racinggame.domain.game;
 
+import nextstep.utils.Console;
+import nextstep.utils.Randoms;
 import racinggame.domain.car.Car;
 
 import java.util.ArrayList;
@@ -7,34 +9,91 @@ import java.util.List;
 
 public class RacingGame {
     private final int limitCount;
-    List<Car> participationCarList = new ArrayList<>();
+    private List<Car> participationCarList = new ArrayList<>();
+    private List<String> lastGameWinners = new ArrayList<>();
+    private int maxLocation = 0;
 
-    public RacingGame(String[] carNames, int limitCount) {
+    public RacingGame(String strCarNames, int limitCount) {
+        String[] carNames = strCarNames.split(",");
+        createCar(carNames);
         this.limitCount = limitCount;
-        createCar(carNames); // TODO 명칭 변경 필요
+    }
+
+    public void gameStart() {
+        if (lastGameWinners.size() > 0) lastGameWinners.clear();
+
+        for (int i = 0; i < limitCount; i++) {
+            for (Car car : participationCarList) {
+                moveCarByNum(car, getRandomNumber());
+                int location = car.getLocation();
+                if (location > maxLocation) maxLocation = location;
+            }
+
+            logExcuteResult();
+        }
+
+        logGameWinners();
+    }
+
+    private void logExcuteResult() {
+        for (Car car : participationCarList) {
+            System.out.print(car.getCarName() + ": ");
+            for (int i = 0; i < car.getLocation(); i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+
+    private void logGameWinners() {
+        String winners = "";
+        if (maxLocation > 0) {
+            for (Car car : participationCarList) {
+                if (car.getLocation() == maxLocation) winners += "," + car.getCarName();
+            }
+        }
+
+        System.out.println("최종 우승자는 " + winners.replace(",", "") + " 입니다.");
     }
 
     private void createCar(String[] carNames) {
         for (String carName : carNames) {
+            if (!Car.isAvailableCarName(carName)) carName = getNormalCarName(carName);
+
             Car car = new Car(carName);
             participationCarList.add(car);
         }
     }
 
-    public void gameStart() {
+    private String getNormalCarName(String carName) {
+        while (!Car.isAvailableCarName(carName)) {
+            System.out.println("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+            carName = Console.readLine();
+        }
+        return carName;
+    }
 
+    public static boolean isAvailableCarNames(String strCarNames) {
+        String[] carNames = strCarNames.split(",");
+        for (String carName : carNames) {
+            if (!Car.isAvailableCarName(carName)) return false;
+        }
 
+        return true;
     }
 
     public void moveCarByNum(Car car, int number) {
-        if (isMoveCarByNum(number)) {
+        if (Car.canMoveCarByNum(number)) {
             car.moveCar();
         }
     }
 
-    private boolean isMoveCarByNum(int number) {
-        return number > 3 && number < 10;
+    private int getRandomNumber() {
+        return Randoms.pickNumberInRange(0, 9);
     }
+
 
 
 }
