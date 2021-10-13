530923dd24e7d74a7d41a47cbe4ba3e4dc353ad6
seonghyeoklee <dltjdgur327@gmail.com>
2021-10-13 21:56:08 +0900

    test : 자동차의 이동(전진) 테스트 코드 추가

--- src/test/java/racinggame/domain/RacingCarTest.java
+++ src/test/java/racinggame/domain/RacingCarTest.java
@@ -1,5 +1,7 @@
 package racinggame.domain;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -7,7 +9,6 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static racinggame.utils.RacingGameUtils.*;
 
 public class RacingCarTest {
     @ParameterizedTest(name = "입력받은 자동차 이름으로 자동차 객체를 생성한다")
@@ -22,7 +23,7 @@ public class RacingCarTest {
     @ParameterizedTest(name = "자동차 이름 검증이 완료되면, 자동차 리스트를 생성하고 반환한다")
     @CsvSource(value = {"pobi,crong,honux:3"}, delimiter = ':')
     void 자동차_이름_검증이_완료되면_자동차_리스트를_생성하고_반환한다(String racingCarNameInput, int sizeInput) {
-        RacingCars racingCars = new RacingCars(getRacingCarList(racingCarNameInput));
+        RacingCars racingCars = new RacingCars(racingCarNameInput);
 
         assertThat(racingCars.getRacingCars()).isInstanceOf(List.class);
         assertThat(racingCars.getRacingCars().size()).isEqualTo(sizeInput);
@@ -31,16 +32,16 @@ public class RacingCarTest {
     @ParameterizedTest(name = "자동차 경주 결과를 출력한다")
     @ValueSource(strings = {"pobi,crong,honux"})
     void 자동차_경주_결과를_출력한다(String racingCarNameInput) {
-        RacingCars racingCars = new RacingCars(getRacingCarList(racingCarNameInput));
+        RacingCars racingCars = new RacingCars(racingCarNameInput);
 
         List<RacingCar> racingCarList = racingCars.getRacingCars();
         /* 1회 */
-        racingCarList.get(0).move();
+        racingCarList.get(0).movingForward();
         racingCarList.get(1).stop();
         racingCarList.get(2).stop();
         /* 2회 */
-        racingCarList.get(0).move();
-        racingCarList.get(1).move();
+        racingCarList.get(0).movingForward();
+        racingCarList.get(1).movingForward();
         racingCarList.get(2).stop();
 
         racingCarList.forEach(System.out::println);
@@ -48,5 +49,21 @@ public class RacingCarTest {
         assertThat(racingCarList.get(0).toString()).isEqualTo("pobi : --");
         assertThat(racingCarList.get(1).toString()).isEqualTo("crong : -");
         assertThat(racingCarList.get(2).toString()).isEqualTo("honux : ");
+    }
+
+    @Test
+    @DisplayName("자동가 전진한다")
+    void 자동차가_전진한다() {
+        RacingCar racingCar = new RacingCar("pobi");
+        racingCar.movingForward();
+        assertThat(racingCar.getMovesCount()).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("자동가 정지한다")
+    void 자동차가_정지한다() {
+        RacingCar racingCar = new RacingCar("pobi");
+        racingCar.stop();
+        assertThat(racingCar.getMovesCount()).isEqualTo(0);
     }
 }
