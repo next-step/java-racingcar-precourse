03ca6169e01f91eefacb79e1b7b29e1e77b2d1a2
gkfgran5037 <gkfgran5037@naver.com>
2021-10-13 20:16:13 +0900

    feat : 숫자에 따른 이동 상태(전진 또는 정지) 반환

---
+++ src/main/java/racinggame/ActStatus.java
@@ -0,0 +1,39 @@
+package racinggame;
+
+public enum ActStatus {
+    STOP("", 0, 3),
+    GO("-", 4, 9),
+    Empty("", -1 ,-1);
+
+    private String actExpress;
+    private int minNum;
+    private int maxNum;
+
+    ActStatus(String actExpress, int minNum, int maxNum) {
+        this.actExpress = actExpress;
+        this.minNum = minNum;
+        this.maxNum = maxNum;
+    }
+
+    public static ActStatus findByActNumber(int number) {
+        for (ActStatus actStatus :ActStatus.values()) {
+            if (isWithinRange(actStatus, number)) {
+                return actStatus;
+            }
+        }
+        return Empty;
+    }
+
+    private static boolean isWithinRange(ActStatus actStatus, int number) {
+        if(actStatus == Empty) {
+            return false;
+        }
+        if (actStatus.minNum == -1 && actStatus.maxNum == -1) {
+            return false;
+        }
+        if (actStatus.minNum != -1 && actStatus.minNum > number) {
+            return false;
+        }
+        return actStatus.maxNum == -1 || actStatus.maxNum >= number;
+    }
+}
