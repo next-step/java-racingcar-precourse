377ee66bf39cca23859ece5df380894bb75b08fb
bestsilver <dkvkxm99@gmail.com>
2021-10-13 22:51:29 +0900

    feat: 게임에서 자주 쓰이는 리터럴값 정의

---
+++ src/main/java/racinggame/utils/GameRule.java
@@ -0,0 +1,12 @@
+package racinggame.utils;
+
+public class GameRule {
+    public static final String SCORE_SHAPE = "-";
+    public static final String SEPARATOR = ",";
+    public static final int MIN_FORWARD = 4;
+    public static final int MIN_PICK_NUMBER = 0;
+    public static final int MAX_PICK_NUMBER = 9;
+    public static final int MIN_CAR_NAME = 1;
+    public static final int MAX_CAR_NAME = 5;
+}
+
