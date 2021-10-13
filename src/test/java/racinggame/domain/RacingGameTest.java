f1a60ea1f1061fc7f7531d1e86f357e8224002c1
pu1et <pu1etproof@gmail.com>
2021-10-13 21:12:03 +0900

    test : 우승자 판별 기능 테스트 코드 수정

--- src/test/java/racinggame/domain/RacingGameTest.java
+++ src/test/java/racinggame/domain/RacingGameTest.java
@@ -1,34 +1,23 @@
 package racinggame.domain;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class RacingGameTest {
-    ArrayList<String> carNames;
-
-    @BeforeEach
-    void setUp() {
-        carNames = new ArrayList<>();
-        carNames.add("자동차1");
-        carNames.add("자동차2");
-        carNames.add("자동차3");
-    }
-
     @Test
     @DisplayName("가장 많이 이동한 차가 승리한다.")
     void 우승자_판별() {
-        RacingGame racingGame = new RacingGame(carNames, 3);
+        CarNames carNames = new CarNames("자동차1,자동차2,자동차3");
+        RaceNum raceNum = new RaceNum("3");
+        RacingGame racingGame = new RacingGame(carNames, raceNum);
 
-        ArrayList<Car> winners = racingGame.getWinners();
-        int maxGoCount = racingGame.getMaxGoCount();
+        Winners winners = racingGame.getWinners();
+        MaxGoCount maxGoCount = racingGame.getMaxGoCount();
 
-        for (Car winner : winners) {
-            assertThat(winner.getGoCount()).isEqualTo(maxGoCount);
+        for (Car winner : winners.getWinners()) {
+            assertThat(winner.getGoCount().equals(maxGoCount.getMaxGoCount())).isTrue();
         }
     }
 }
