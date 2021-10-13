f0c41142f88e678ea7a82c2b1ccf43b052e23170
right42 <chjm452@naver.com>
2021-10-13 20:03:49 +0900

    refactor: 자동차 이름 검증 테스트 코드 수정
    테스트 시나리오 추가

--- src/test/java/racinggame/domain/CarTest.java
+++ src/test/java/racinggame/domain/CarTest.java
@@ -3,23 +3,25 @@ package racinggame.domain;
 import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import racinggame.exception.InvalidInputException;
 
 class CarTest {
 
-	@Test
-	void 이름은_5글자_이하만_가능하다_정상(){
-		String name = "pobi";
+	@ParameterizedTest
+	@ValueSource(strings = {"pobi", "a"})
+	void 이름은_5글자_이하만_가능하다_정상(String name){
 
 		CarName carName = new CarName(name);
 
 		assertThat(carName).isNotNull();
 	}
 
-	@Test
-	void 이름은_5글자_이하만_가능하다_에러(){
-		String name = "sonata";
+	@ParameterizedTest
+	@ValueSource(strings = {"sonata", ""})
+	void 이름은_5글자_이하만_가능하다_에러(String name){
 
 		assertThatThrownBy(() -> {
 			new CarName(name);
