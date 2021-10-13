c8562babae6b32d2f2a1eccf24a957df1c14242a
kkwan0226 <kkwan0226@gmail.com>
2021-10-13 13:47:57 +0900

    feature(StringUtils): 라운드 입력시 숫자 검증하는 기능 구현 및 ErrorMessage 추가
    - 라운드 입력시 숫자외에 다른 문자가 입력 시 오류를 발생하도록 함
    - 이에 따른 error message 추가
    ChangeFiles:
    - StringUtils.java
    - ErrorMessage.java

--- src/main/java/racinggame/code/ErrorMessage.java
+++ src/main/java/racinggame/code/ErrorMessage.java
@@ -3,7 +3,8 @@ package racinggame.code;
 public enum ErrorMessage {
     INVALID_CAR_NAME_LENGTH("Check the length of the car name"),
     INVALID_CAR_NAME_NULL("The car name cannot be null"),
-    INVALID_CAR_LIST_SIZE("Please check the number of cars for the racing game");
+    INVALID_CAR_LIST_SIZE("Please check the number of cars for the racing game"),
+    INVALID_INPUT_ROUND("Check the input value or type.");
 
     private final String errorMessage;
 
--- src/main/java/racinggame/util/StringUtils.java
+++ src/main/java/racinggame/util/StringUtils.java
@@ -1,5 +1,7 @@
 package racinggame.util;
 
+import java.util.regex.Pattern;
+
 import racinggame.code.CarConfig;
 import racinggame.code.ErrorMessage;
 import racinggame.exception.RacinggameException;
@@ -15,6 +17,13 @@ public class StringUtils {
     public static void validateStringNull(String inputString) {
         if (inputString == null) {
             throw new RacinggameException(ErrorMessage.INVALID_CAR_NAME_NULL.getValue());
+        }
+    }
+
+    public static void validateStringOnlyNumber(String inputString) {
+        Pattern isNumber = Pattern.compile("^[1-9]*?");
+        if (!isNumber.matcher(inputString).matches()) {
+            throw new RacinggameException(ErrorMessage.INVALID_INPUT_ROUND.getValue());
         }
     }
 }
