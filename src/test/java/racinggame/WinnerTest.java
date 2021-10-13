7762cba6fb4b8b84a5a3c4628f7ad98642baab5d
gkfgran5037 <gkfgran5037@naver.com>
2021-10-13 21:10:59 +0900

    test : 자동차들의 위치값 중 최대 위치값 테스트

---
+++ src/test/java/racinggame/WinnerTest.java
@@ -0,0 +1,53 @@
+package racinggame;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class WinnerTest {
+    private Car carA;
+    private Car carB;
+    private Car carC;
+    private Car carD;
+    private Car carE;
+
+    @BeforeEach
+    void setup() {
+        carA = new Car("carA");
+        carA.move(0);
+        carA.move(0);
+        carB = new Car("carB");
+        carB.move(0);
+        carB.move(0);
+        carC = new Car("carC");
+        carC.move(1);
+        carC.move(7);
+        carD = new Car("carD");
+        carD.move(7);
+        carD.move(7);
+        carE = new Car("carE");
+        carE.move(9);
+        carE.move(7);
+    }
+
+    @Test
+    void max_position_0() {
+        Winner winner = new Winner(Arrays.asList(carA, carB));
+        assertThat(winner.calMaxPosition()).isEqualTo(0);
+    }
+
+    @Test
+    void max_position_1() {
+        Winner winner = new Winner(Arrays.asList(carA, carB, carC));
+        assertThat(winner.calMaxPosition()).isEqualTo(1);
+    }
+
+    @Test
+    void max_position_2() {
+        Winner winner = new Winner(Arrays.asList(carA, carB, carC, carD, carE));
+        assertThat(winner.calMaxPosition()).isEqualTo(2);
+    }
+}
