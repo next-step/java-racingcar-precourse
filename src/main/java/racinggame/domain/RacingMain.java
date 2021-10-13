72d22ba9856ce25683af91b2346d766332cc1b3b
rpgworld <cloud_data@naver.com>
2021-10-13 20:29:59 +0900

    refactor : View 부분 분리 - GameView 클래스 구현 및 분리 완료(From RacingMain Class)

--- src/main/java/racinggame/domain/RacingMain.java
+++ src/main/java/racinggame/domain/RacingMain.java
@@ -1,7 +1,6 @@
 package racinggame.domain;
 
-import nextstep.utils.Console;
-import racinggame.utils.ValidationUtils;
+import racinggame.view.GameView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,66 +9,28 @@ public class RacingMain {
 
     private static final String ERROR_MESSAGE = "[ERROR]";
 
+    private GameView gameView;
     private RacingGame racingGame;
     private List<Car> cars;
     private int tryNo = 0;
 
     public void run() {
-        inputCars();
-        inputTryNo();
+        // 레이싱 게임 생성
+        gameView = new GameView();
+        racingGame = new RacingGame(
+                RacingGame.initCars(gameView.inputCars()),
+                gameView.inputTryNo()
+        );
 
-        racingGame = new RacingGame(cars, tryNo);
-
+        // 레이싱 반복
         while(racingGame.racing()) {
             racingGame.race();
-            List<Car> cars = racingGame.getCars();
-            printCars(cars);
-            System.out.println("");
+            gameView.printCars(racingGame.getCars());
         }
 
-        printWinners(racingGame.getCars());
-    }
-
-    private void inputCars() {
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-
-        String names = Console.readLine();
-        while (!ValidationUtils.validateCarsNames(names)
-                || ValidationUtils.duplicateCarNames(names))
-        {
-            System.out.println(ERROR_MESSAGE);
-            names = Console.readLine();
-        }
-        cars = RacingGame.initCars(names);
-    }
-
-    private void inputTryNo() {
-        System.out.println("시도할 횟수는 몇회인가요?");
-
-        String tryNo = Console.readLine();
-        while(!ValidationUtils.validateTryNo(tryNo)) {
-            System.out.println(ERROR_MESSAGE);
-            tryNo = Console.readLine();
-        }
-        this.tryNo = Integer.parseInt(tryNo);
-    }
-
-    private void printCars(List<Car> cars) {
-        for (Car car : cars) {
-            String carStatus = String.format("%s : %s", car.getName(), changePositionToBar(car.getPosition()));
-            System.out.println(carStatus);
-        }
-    }
-
-    private String changePositionToBar(int position) {
-        String result = "";
-        for(int i = 0; i < position; i++)
-            result += "-";
-        return result;
-    }
-
-    private void printWinners(List<Car> cars) {
-        System.out.println(String.format("최종 우승자는 %s 입니다.", findWinnersNames(cars, getMaxPosition(cars))));
+        // 우승자 출력
+        int maxPosition = getMaxPosition(racingGame.getCars());
+        gameView.printWinners(findWinnersNames(racingGame.getCars(), maxPosition));
     }
 
     private int getMaxPosition(List<Car> cars) {
---
+++ src/main/java/racinggame/view/GameView.java
@@ -0,0 +1,75 @@
+package racinggame.view;
+
+import nextstep.utils.Console;
+import racinggame.domain.Car;
+import racinggame.utils.ValidationUtils;
+
+import java.util.List;
+
+public class GameView {
+    private static final String ERROR_MESSAGE = "[ERROR]";
+
+    // 자동차 이름 입력받기
+    public String inputCars() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+
+        String names = Console.readLine();
+        while (!validateCarsNames(names))
+        {
+            System.out.println(ERROR_MESSAGE);
+            names = Console.readLine();
+        }
+        return names;
+    }
+    // 자동차 이름 유효성 검사
+    private boolean validateCarsNames(String names) {
+        if (!ValidationUtils.validateCarsNames(names)) {
+            System.out.println(ERROR_MESSAGE + " 자동차 이름은 5글자 이하로 입력해주세요.");
+            return false;
+        }
+        if (ValidationUtils.duplicateCarNames(names)) {
+            System.out.println(ERROR_MESSAGE + " 중복되지 않는 자동차 이름을 이력해주세요.");
+            return false;
+        }
+        return true;
+    }
+
+    // 시도횟수 입력받기
+    public int inputTryNo() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+        String tryNo = Console.readLine();
+        while(!validateTryNo(tryNo)) {
+            tryNo = Console.readLine();
+        }
+        return Integer.parseInt(tryNo);
+    }
+
+    private boolean validateTryNo(String tryNo) {
+        if (!ValidationUtils.validateTryNo(tryNo)) {
+            System.out.println(ERROR_MESSAGE + " 시도 횟수는 1 이상의 숫자만 입력해주세요.");
+            return false;
+        }
+        return true;
+    }
+
+    private String changePositionToBar(int position) {
+        String result = "";
+        for(int i = 0; i < position; i++)
+            result += "-";
+        return result;
+    }
+
+    // 자동차들 위치 출력
+    public void printCars(List<Car> cars) {
+        for (Car car : cars) {
+            String carStatus = String.format("%s : %s", car.getName(), changePositionToBar(car.getPosition()));
+            System.out.println(carStatus);
+        }
+        System.out.println();
+    }
+
+    // 승자 출력
+    public void printWinners(String winners) {
+        System.out.println(String.format("최종 우승자는 %s 입니다.", winners));
+    }
+}
