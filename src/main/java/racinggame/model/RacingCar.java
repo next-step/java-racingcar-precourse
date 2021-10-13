1d232224402ee29a47d3f103d378b8eba66eab01
dharana7723 <dharana7723@gmail.com>
2021-10-13 19:12:27 +0900

    feat(RacingCar): 차이름 부여 및 getName 추가

--- src/main/java/racinggame/model/RacingCar.java
+++ src/main/java/racinggame/model/RacingCar.java
@@ -1,4 +1,17 @@
 package racinggame.model;
 
-public class RacingCar {
+public class RacingCar{
+    private String carName;
+
+    public RacingCar() {
+    }
+
+    public String getCarName(){
+        return this.carName;
+    }
+
+    public void setCarName(String inputCarName){
+        this.carName = inputCarName;
+    }
 }
+
