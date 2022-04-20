79fe3d30e5c46481e188e74312ef2920e8bb5645
Jinmo Im <jinmo.im@gmail.com>
2022-04-20 23:05:32 +0900

    refactor: 메시지 분리, GameMessage class를 enum으로 변경

--- src/main/java/racingcar/game/Game.java
+++ src/main/java/racingcar/game/Game.java
@@ -50,11 +50,12 @@ class Game {
     }
 
     void play() {
-        String line = GameUtil.readLineWithPrompt("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n");
+        String line = GameUtil.readLineWithPrompt(GameMessage.PROMPT_INPUT_CAR_NAMES.get());
         Cars cars = GameUtil.parseCars(line);
         if (cars == null) return;
 
-        Turns turns = new Turns(GameUtil.readLineWithPrompt("시도할 회수는 몇회인가요?\n"));
+        line = GameUtil.readLineWithPrompt(GameMessage.PROMPT_INPUT_NUMBER_OF_TURNS.get());
+        Turns turns = new Turns(line);
         process(cars, turns);
         Console.print(GameMessage.traces(states));
 
--- src/main/java/racingcar/game/GameMessage.java
+++ src/main/java/racingcar/game/GameMessage.java
@@ -2,11 +2,25 @@ package racingcar.game;
 
 import racingcar.game.car.Car;
 
-public class GameMessage {
+enum GameMessage {
+
+    PROMPT_INPUT_CAR_NAMES("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n"),
+    PROMPT_INPUT_NUMBER_OF_TURNS("시도할 회수는 몇회인가요?\n");
+
+    private final String message;
+
+    GameMessage(String message) {
+        this.message = message;
+    }
+
     static String traces(GameStates states) {
         return String.join("",
                 states.map(state ->
                         String.join("",
                                 state.getCars().map(Car::toString)) + "\n"));
+    }
+
+    String get() {
+        return this.message;
     }
 }
