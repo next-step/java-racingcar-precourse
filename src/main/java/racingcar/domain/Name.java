5994ccd990e90879498d254d0ff728c98c0365bb
sooman-che <straicus@gmail.com>
2022-04-20 22:36:48 +0900

    fix: Name object validation
    - delegate to controller

--- src/main/java/racingcar/domain/Name.java
+++ src/main/java/racingcar/domain/Name.java
@@ -1,21 +1,16 @@
 package racingcar.domain;
 
+import racingcar.error.Error;
+
 public class Name {
     private String text;
 
     public Name(String text) {
-        validateLength(text);
         this.text = text;
     }
 
-    private void validateLength(String text) {
-        if (text.length() > 5) {
-            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
-        }
-    }
-
-
-    public String getText() {
+    @Override
+    public String toString() {
         return text;
     }
 }
