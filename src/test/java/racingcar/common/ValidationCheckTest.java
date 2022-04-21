fc7870ca5df09f6c7599e7a584f3cea29ac44b17
KimTH94 <kcc940602@gmail.com>
2022-04-22 01:07:10 +0900

    test(ValidationCheckTest) add 시도 횟수 숫자 정합성 검사

--- src/test/java/racingcar/common/ValidationCheckTest.java
+++ src/test/java/racingcar/common/ValidationCheckTest.java
@@ -1,17 +1,22 @@
 package racingcar.common;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static racingcar.common.ValidationCheck.carNamelengthValidation;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
 public class ValidationCheckTest {
+    public ValidationCheck validationCheck = new ValidationCheck();
+
     @Test
     void 자동차_이름_길이_정합성_검사() {
-        assertThat(carNamelengthValidation("car")).isEqualTo(true);
-        assertThat(carNamelengthValidation("racingCar")).isEqualTo(false);
+        assertThat(validationCheck.carNamelengthValidation("car")).isEqualTo(true);
+        assertThat(validationCheck.carNamelengthValidation("racingCar")).isEqualTo(false);
+    }
+
+    @Test
+    void 시도_횟수_숫자_정합성_검사() {
+        assertThat(validationCheck.numberValidation("123")).isEqualTo(true);
+        assertThat(validationCheck.numberValidation("abc")).isEqualTo(false);
     }
 
 }
