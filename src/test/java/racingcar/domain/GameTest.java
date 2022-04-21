077d774be582b9b0f7c24d8c9bcb6a38ea64bba0
KimTH94 <kcc940602@gmail.com>
2022-04-21 22:31:49 +0900

    test(GameTest) create 자동차 이름 입력 받기 테스트

---
+++ src/test/java/racingcar/domain/GameTest.java
@@ -0,0 +1,19 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+    @Test
+    void 자동차_세팅() {
+        String[] racingCarName = {"자동차1", "자동차2", "자동차3", "car4"};
+        RacingCars racingCars = new RacingCars(racingCarName);
+        for (int index = 0; index < racingCarName.length; index++) {
+            assertThat(racingCarName[index]).isEqualTo(racingCars.getNameByIndex(index + 1));
+        }
+    }
+
+
+}
