75952426298c0c1814ea6c9057dcd7409fe4320a
KimTH94 <kcc940602@gmail.com>
2022-04-22 01:09:42 +0900

    feat(ValidationCheck) add 시도 횟수 숫자 정합성 검사

--- src/main/java/racingcar/common/ValidationCheck.java
+++ src/main/java/racingcar/common/ValidationCheck.java
@@ -3,10 +3,19 @@ package racingcar.common;
 import java.util.regex.Pattern;
 
 public class ValidationCheck {
-    public static int CAR_NAME_MAX_LENGTH = 5;
+    public static final int CAR_NAME_MAX_LENGTH = 5;
+    public static final String NUMBER_PATTERN = "^[0-9]*$" ;
 
     public static boolean carNamelengthValidation(String carName) {
         if (CAR_NAME_MAX_LENGTH < carName.length()) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean numberValidation(String value) {
+        if (!Pattern.matches(NUMBER_PATTERN, value)) {
+            // throw new IllegalArgumentException(SystemMessage.NUMBER_EXCEPTION);
             return false;
         }
         return true;
