39e86073e252063f2d7eb756692f5216d44fc9f3
Jinmo Im <jinmo.im@gmail.com>
2022-04-22 11:32:43 +0900

    refactor(GameExceptionHandler): 메서드 분리

--- src/main/java/racingcar/game/GameExceptionHandler.java
+++ src/main/java/racingcar/game/GameExceptionHandler.java
@@ -1,15 +1,23 @@
 package racingcar.game;
 
+import racingcar.game.util.Console;
+
 public class GameExceptionHandler {
     private static final String PREFIX = "[ERROR] ";
 
+    private static void printException(Exception e) {
+        Console.println(PREFIX + e.getMessage());
+    }
+
+    private static void handleException(Runnable r) {
+        try {
+            r.run();
+        } catch (Exception e) {
+            printException(e);
+        }
+    }
+
     public static Runnable wrap(Runnable r) {
-        return () -> {
-            try {
-                r.run();
-            } catch (Exception e) {
-                System.out.println(PREFIX + e.getMessage());
-            }
-        };
+        return () -> handleException(r);
     }
 }
