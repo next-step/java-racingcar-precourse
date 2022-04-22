18b638314c6574e843df72bb05b46fd4ff3fab78
sooman-che <straicus@gmail.com>
2022-04-22 11:29:07 +0900

    fix: modifications due to changes in test results.
    - update the test results correctly
    - change the result format in output view

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -1,13 +1,9 @@
 package racingcar.view;
 
-import racingcar.domain.Name;
-
 public class OutputView {
     public static void showResult(String winnerNames) {
-        final String message = String.format("최종 우승자는 %s 입니다.", winnerNames);
+        final String message = String.format("최종 우승자: %s", winnerNames);
         System.out.println(message);
     }
 
-    public static void progressView() {
-    }
 }
