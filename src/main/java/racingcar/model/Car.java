b8f46e690c8d0154ca3c0e493851eb11324dcf14
wxxsoo <wxxsox@gmail.com>
2022-04-22 20:16:11 +0900

    feat: Random값이 4이상이면 자동차 전진

--- src/main/java/racingcar/model/Car.java
+++ src/main/java/racingcar/model/Car.java
@@ -1,13 +1,21 @@
 package racingcar.model;
 
 public class Car {
-    private final String name;
     private static final int NAME_MAX_LENGTH = 5;
     private static final int NAME_MIN_LENGTH = 1;
+    private static final int CAN_MOVE = 4;
+    private final String name;
+    private int distance = 0;
 
     public Car(String name) {
         validateName(name);
         this.name = name;
+    }
+
+    public void move(int randomNumber) {
+        if(isMove(randomNumber)) {
+            distance++;
+        }
     }
 
     public void validateName(String name) {
@@ -18,5 +26,13 @@ public class Car {
         if(name.length() > NAME_MAX_LENGTH || name.length() < NAME_MIN_LENGTH) {
             throw new IllegalArgumentException();
         }
+    }
+
+    boolean isMove(int randomNumber) {
+        return randomNumber >= CAN_MOVE;
+    }
+
+    public int getDistance() {
+        return distance;
     }
 }
--- src/test/java/racingcar/model/CarTest.java
+++ src/test/java/racingcar/model/CarTest.java
@@ -1,10 +1,10 @@
 package racingcar.model;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 public class CarTest {
 
@@ -12,5 +12,13 @@ public class CarTest {
     @ValueSource(strings = {"123456", ""})
     void 자동차_이름은_1자_이상_5자_이하(String input) {
         assertThatThrownBy(() -> new Car(input)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:0", "2:0", "3:0", "4:1", "5:1", "6:1", "7:1", "8:1", "9:1"}, delimiter = ':')
+    void random값이_4이상이면_전진(int input, int expected) {
+        Car car = new Car("hello");
+        car.move(input);
+        assertThat(car.getDistance()).isEqualTo(expected);
     }
 }
