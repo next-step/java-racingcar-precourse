41039e1fe65847da7e8d3ea573761780c11620ec
Jinmo Im <jinmo.im@gmail.com>
2022-04-22 10:59:03 +0900

    refactor: IntRange()의 default 생성자 접근제한자 변경 public to private
    default 생성자는 외부에서 사용할 일이 없으므로

--- src/main/java/racingcar/game/IntRange.java
+++ src/main/java/racingcar/game/IntRange.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class IntRange extends AbstractImmutableList<Integer> {
-    public IntRange() {
+    private IntRange() {
         super();
     }
 
