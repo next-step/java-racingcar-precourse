d8160aae752cee67a746a588fdad6539217bc495
wxxsoo <wxxsox@gmail.com>
2022-04-23 01:03:10 +0900

    feat: 잘못된 값을 입력할 경우 에러 출력 후 그 부분부터 다시 입력

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -10,18 +10,16 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class RacingController {
-    private static final String DELIMITER = ",";
 
     public void start() {
-        String carNames = InputView.inputCarNames();
-        List<Car> cars = generateCars(carNames);
+        List<Car> cars = generateCars(InputView.inputCarNames());
         String moveCount = InputView.inputMoveCount();
         Racing racing = new Racing(moveCount, cars);
         Winner winner = new Winner(racing.race());
         winner.printWinners();
     }
 
-    public List<Car> generateCars(String carNames) {
-        return Arrays.stream(carNames.split(DELIMITER)).map(Car::new).collect(Collectors.toList());
+    public List<Car> generateCars(String[] carNames) {
+        return Arrays.stream(carNames).map(Car::new).collect(Collectors.toList());
     }
 }
--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,18 +1,40 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import racingcar.model.Car;
+import racingcar.model.Racing;
+
+import java.util.Arrays;
 
 public class InputView {
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_MOVE_COUNT_MESSAGE = "시도할 회수는 몇회인가요?";
+    private static final String CAR_NAME_DELIMITER = ",";
 
-    public static String inputCarNames() {
-        System.out.println(INPUT_CAR_NAMES_MESSAGE);
-        return Console.readLine();
+    public static String[] inputCarNames() {
+        String[] input = readInput(INPUT_CAR_NAMES_MESSAGE).split(CAR_NAME_DELIMITER);
+        try {
+            Arrays.stream(input).forEach(Car::validateNameLength);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            input = inputCarNames();
+        }
+        return input;
     }
 
     public static String inputMoveCount() {
-        System.out.println(INPUT_MOVE_COUNT_MESSAGE);
+        String input = readInput(INPUT_MOVE_COUNT_MESSAGE);
+        try {
+            Racing.validNumber(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            input = inputMoveCount();
+        }
+        return input;
+    }
+
+    private static String readInput(String message) {
+        System.out.println(message);
         return Console.readLine();
     }
 }
