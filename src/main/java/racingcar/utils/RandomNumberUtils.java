e291781f05ed86b714938de8290d11bf506cbc90
ul8415 <ul8415@gmail.com>
2022-04-23 19:36:18 +0900

    feat: 랜덤 넘버가 0~9 사이 인지 검증 메서드 추가
    - missionutils.Randoms의 pickNumberInRange 메서드 사용

---
+++ src/main/java/racingcar/utils/RandomNumberUtils.java
@@ -0,0 +1,15 @@
+package racingcar.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumberUtils {
+
+    private RandomNumberUtils() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static int getRandomNumberFromZeroToNine() {
+        return Randoms.pickNumberInRange(0,9);
+    }
+
+}
