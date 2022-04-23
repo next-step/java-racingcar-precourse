36128e56e7f8be804a2268ee8fad349b2c68de14
dev.dyllis <dev.dyllis@gmail.com>
2022-04-23 18:21:53 +0900

    test: CarTest init 테스트케이스 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -8,16 +8,24 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class CarTest {
 
     @Test
+    void init() {
+        Car car = new Car("pobi");
+
+        assertThat(car.getName()).isEqualTo("pobi");
+        assertThat(car.getDistance()).isEqualTo(0);
+    }
+
+    @Test
+    void validateName() {
+        assertThrows(IllegalArgumentException.class, () -> new Car("pobiii"));
+    }
+
+    @Test
     void go() {
         Car car = new Car();
         car.go(3);
         car.go(4);
 
         assertThat(car.getDistance()).isEqualTo(1);
-    }
-
-    @Test
-    void validateName() {
-        assertThrows(IllegalArgumentException.class, () -> new Car("pobiii"));
     }
 }
