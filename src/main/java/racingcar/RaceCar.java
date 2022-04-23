fa285853fc4f55c2f2b8729ffb7d0bd28a27fe78
dev.dyllis <dev.dyllis@gmail.com>
2022-04-23 19:32:01 +0900

    feat: 자동차 레이싱 게임 진행 클래스 추가(RaceCar)

---
+++ src/main/java/racingcar/RaceCar.java
@@ -0,0 +1,52 @@
+package racingcar;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class RaceCar {
+    private CarHub hub;
+    private TryCount tryCount;
+
+    public void run() {
+        init();
+        simulate();
+    }
+
+    private void init() {
+        hub = initCarHub();
+        tryCount = initTryCount();
+    }
+
+    private CarHub initCarHub() {
+        while(true) {
+            try {
+                ViewHandler.printNameInput();
+                return new CarHub(Console.readLine());
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private TryCount initTryCount() {
+        while (true) {
+            try {
+                ViewHandler.printTryCountInput();
+                return new TryCount(Console.readLine());
+            } catch(IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void simulate() {
+        ViewHandler.printSimulateResultTitle();
+
+        for (int count = 0; count < tryCount.getValue(); count++) {
+            hub.goCars();
+            ViewHandler.printSimulateResult(hub.getCars());
+        }
+
+        final Winners winners = new Winners(hub.getCars());
+        ViewHandler.printWinners(winners.getWinners());
+    }
+}
