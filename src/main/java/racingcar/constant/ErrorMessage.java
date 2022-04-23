aa1096958c6f5be53d58cbb13f502784954cdd95
ul8415 <ul8415@gmail.com>
2022-04-23 19:31:26 +0900

    feat: 자동차 이름 들에 중복이 없는지 검증 메서드 추가
    - HashSet을 이용해 중복을 제거하고 원본과 사이즈를 비교해서 중복이 있는 경우 IllegalArgumentException 발생하도록 처리
    - ErrorMessage에 중복 에러 메세지 추가

--- src/main/java/racingcar/constant/ErrorMessage.java
+++ src/main/java/racingcar/constant/ErrorMessage.java
@@ -5,6 +5,7 @@ public class ErrorMessage {
     private static final String ERROR_HEAD = "[ERROR] ";
 
     public static final String CAR_NAME_LIMIT = ERROR_HEAD + "자동차 이름은 길이 1~5 사이로 입력해주세요.";
+    public static final String CAR_NAME_DUPLICATION = ERROR_HEAD + "자동차 이름은 중복을 허용하지 않습니다.";
 
 
     private ErrorMessage() {
--- src/main/java/racingcar/vo/CarNames.java
+++ src/main/java/racingcar/vo/CarNames.java
@@ -1,5 +1,6 @@
 package racingcar.vo;
 
+import java.util.HashSet;
 import java.util.List;
 import racingcar.constant.CarNameLength;
 import racingcar.constant.ErrorMessage;
@@ -12,6 +13,7 @@ public class CarNames {
         for (String carName : carNameList) {
             validateCarNameLength(carName);
         }
+        validateDuplication(carNameList);
         this.carNameList = carNameList;
     }
 
@@ -19,6 +21,14 @@ public class CarNames {
         int carNameLength = carName.length();
         if (carNameLength < CarNameLength.MIN || carNameLength > CarNameLength.MAX) {
             throw new IllegalArgumentException(ErrorMessage.CAR_NAME_LIMIT);
+        }
+    }
+
+    private void validateDuplication(List<String> carNameList){
+        int inputCarNameSize = carNameList.size();
+        int deleteDuplicationCarNameSize = new HashSet<>(carNameList).size();
+        if(inputCarNameSize > deleteDuplicationCarNameSize){
+            throw new IllegalArgumentException(ErrorMessage.CAR_NAME_DUPLICATION);
         }
     }
 
