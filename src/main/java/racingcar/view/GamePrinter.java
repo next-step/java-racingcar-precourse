5dd1c73678736ddd9f374da2e3c23b96b1e08b92
hugehoo <tbnsok40@gmail.com>
2022-04-24 10:44:42 +0900

    feature/ GamePrinter 예외케이스 메시지 추가

--- src/main/java/racingcar/view/GamePrinter.java
+++ src/main/java/racingcar/view/GamePrinter.java
@@ -1,7 +1,5 @@
 package racingcar.view;
 
-import racingcar.model.Winner;
-
 import java.util.List;
 
 public class GamePrinter {
@@ -9,6 +7,8 @@ public class GamePrinter {
     private final static String CAR_NAMES = "경주할 자동차 이름(이름은 쉼표(,) 기준으로 구분)";
     private final static String GAME_ROUNDS = "시도할 회수";
     private final static String ERROR = "[ERROR]";
+    private final static String CAR_NAME_EMPTY = "자동차 이름이 비어있습니다. 다시 입력해주세요";
+    private final static String DUPLICATE_NAME = "동일한 이름의 자동차가 존재합니다. 다시 입력해주세요";
 
     public static void printInitMessage() {
         System.out.println(GAME_INIT_MESSAGE);
@@ -30,6 +30,14 @@ public class GamePrinter {
         System.out.println(ERROR + " 이름은 다섯글자까지 입력할 수 있습니다. 다시 입력해주세요");
     }
 
+    public static void printEmptyName() {
+        System.out.println(CAR_NAME_EMPTY);
+    }
+
+    public static void printDuplicateName() {
+        System.out.println(DUPLICATE_NAME);
+    }
+
     public static void printCarAndDashes(String playGame, Integer dashLine) {
         System.out.printf("%s : %s %n", playGame, getDashLines(dashLine));
     }
@@ -45,12 +53,6 @@ public class GamePrinter {
 
     public static void printWinner(List<String> winnerNominates) {
         String winners = String.join(", ", winnerNominates);
-        System.out.printf("최종 우승자: %s", winners);
-    }
-
-    public static void printWinner(Winner winner) {
-        // winnerNominates list 로 만드는 함수만들기.
-        String winners = String.join(", ", winner.winnerNominates);
         System.out.printf("최종 우승자: %s", winners);
     }
 }
