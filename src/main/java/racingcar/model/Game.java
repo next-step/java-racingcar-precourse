b4b4a1a90098c3593e794769acb36b4bb023fb2d
deokmoon <deok2moon@gmail.com>
2022-04-25 00:40:41 +0900

    Feat: 게임 결과 확인 테스트코드 및 프로덕트코드 구현 게임 결과처리하는 테스트코드 및 프로덕트코드 구현

--- src/main/java/racingcar/model/Game.java
+++ src/main/java/racingcar/model/Game.java
@@ -1,20 +1,27 @@
 package racingcar.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
+    private final int INIT_MAX = 0;
+
     private int numberOfExecutions;
+    private int maxCnt;
     private CarList carList;
+    private CarList winnerList;
 
     public Game(String numberOfExecutions) {
         checkInputParameter(numberOfExecutions);
         this.numberOfExecutions = Integer.valueOf(numberOfExecutions);
+        this.maxCnt = INIT_MAX;
     }
 
     public Game(String numberOfExecutions, List<Car> carList) {
         checkInputParameter(numberOfExecutions);
         this.numberOfExecutions = Integer.valueOf(numberOfExecutions);
         this.carList = new CarList(carList);
+        this.maxCnt = INIT_MAX;
     }
 
     private void checkInputParameter(String str) {
@@ -34,5 +41,64 @@ public class Game {
 
     public CarList getCarList() {
         return carList;
+    }
+
+    public void start() {
+        for (int i = 0; i < numberOfExecutions; i++) {
+            eachCarExcecute(this.carList);
+            System.out.println();
+        }
+        List<Car> winnerList = new ArrayList<>();
+        for (int i = 0; i < carList.getCarList().size(); i++) {
+            makeResult(carList.getCarByIndex(i), winnerList);
+        }
+        setWinnerList(winnerList);
+        pritOutGameResult();
+    }
+
+    private void pritOutGameResult() {
+        System.out.print("최종 우승자: ");
+        System.out.print(getWinnerList().getCarByIndex(0).getCarName());
+        for (int i = 1; i < getWinnerList().getCarList().size(); i++) {
+            System.out.print(", ");
+            System.out.print(getWinnerList().getCarByIndex(i).getCarName());
+        }
+        System.out.println();
+    }
+
+    private void makeResult(Car car, List<Car> winnerList) {
+        if (car.getMoveCnt() > maxCnt) {
+            maxCnt = car.getMoveCnt();
+            winnerList.clear();
+            winnerList.add(car);
+            return;
+        }
+        if (car.getMoveCnt() == maxCnt) {
+            winnerList.add(car);
+            return;
+        }
+    }
+
+    private void eachCarExcecute(CarList carList) {
+        for (int i = 0; i < carList.getCarList().size(); i++) {
+            carList.getCarByIndex(i).move();
+            printOutGameProcess(carList.getCarByIndex(i));
+        }
+    }
+
+    private void printOutGameProcess(Car car) {
+        System.out.print(car.getCarName() + " : ");
+        for (int i = 0; i < car.getMoveCnt(); i++) {
+            System.out.print("-");
+        }
+        System.out.println();
+    }
+
+    public void setWinnerList(List<Car> winnerList) {
+        this.winnerList = new CarList(winnerList);
+    }
+
+    public CarList getWinnerList() {
+        return this.winnerList;
     }
 }
--- src/test/java/racingcar/model/GameTest.java
+++ src/test/java/racingcar/model/GameTest.java
@@ -1,14 +1,50 @@
 package racingcar.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.mockStatic;
 
 public class GameTest {
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    private static MockedStatic<Randoms> randoms;
+    private static MockedStatic.Verification verification = () -> Randoms.pickNumberInRange(anyInt(), anyInt());
+    private static OutputStream captor;
+    private static PrintStream standardOut;
+
+    @BeforeAll
+    static void before() {
+        randoms = mockStatic(Randoms.class);
+        standardOut = System.out;
+        captor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(captor));
+    }
+
+    @AfterAll
+    public static void after() {
+        randoms.close();
+        System.setOut(standardOut);
+        System.out.println(output());
+    }
+
+    private static String output() {
+        return captor.toString().trim();
+    }
+
     @Test
     void 입력받은_횟수로_세팅여부_테스트() {
         // given
@@ -45,20 +81,47 @@ public class GameTest {
     void 게임수행과정_출력_테스트() {
         // given
         List<Car> list = new ArrayList<>();
-        list.add(new Car("car1"));
-        list.add(new Car("car2"));
-        Game game = new Game("2", list);
+        list.add(new Car("pobi"));
+        list.add(new Car("nobi"));
+        list.add(new Car("dobi"));
+        Game game = new Game("1", list);
+        randoms.when(verification).thenReturn(MOVING_FORWARD, STOP, MOVING_FORWARD);
         // when
-
+        game.start();
         // then
+        assertThat(output()).contains("pobi : -", "nobi : ", "dobi : -");
     }
 
     @Test
-    void 게임수행결과_출력_테스트() {
+    void 게임우승자_세팅_확인_테스트() {
         // given
-
+        List<Car> list = new ArrayList<>();
+        list.add(new Car("pobi"));
+        list.add(new Car("nobi"));
+        list.add(new Car("dobi"));
+        Game game = new Game("1", list);
+        list.remove(1);
         // when
-
+        game.setWinnerList(list);
         // then
+        assertThat(game.getWinnerList().getCarByIndex(0).getCarName()).isEqualTo("pobi");
+        assertThat(game.getWinnerList().getCarByIndex(1).getCarName()).isEqualTo("dobi");
+    }
+
+    @Test
+    void 게임우승자_START메서드활용_확인_테스트() {
+        // given
+        List<Car> list = new ArrayList<>();
+        list.add(new Car("pobi"));
+        list.add(new Car("nobi"));
+        list.add(new Car("dobi"));
+        Game game = new Game("1", list);
+        randoms.when(verification).thenReturn(MOVING_FORWARD, STOP, MOVING_FORWARD);
+        // when
+        game.start();
+        // then
+        assertThat(game.getWinnerList().getCarByIndex(0).getCarName()).isEqualTo("pobi");
+        assertThat(game.getWinnerList().getCarByIndex(1).getCarName()).isEqualTo("dobi");
+        assertThat(output()).contains("최종 우승자: pobi, dobi");
     }
 }
