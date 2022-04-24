bd04e6b43a00c0301fdd4d8247f3adc6fcfc2e0e
KimTH94 <kcc940602@gmail.com>
2022-04-24 19:47:49 +0900

    test(RacingCarsTest) create RacingCars 객체 테스트

---
+++ src/test/java/racingcar/domain/RacingCarsTest.java
@@ -0,0 +1,35 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class RacingCarsTest {
+    String[] racingCarName;
+    RacingCars racingCars;
+
+    @BeforeEach
+    public void create() {
+        racingCarName = new String[]{"자동차1","자동차2","자동차3"};
+        racingCars = new RacingCars(racingCarName);
+    }
+
+    @Test
+    void RacingCars_객체_생성_테스트() {
+        for (int index = 0; index < racingCarName.length; index++) {
+            assertThat(racingCarName[index]).isEqualTo(racingCars.getNameByIndex(index + 1));
+            assertThat(0).isEqualTo(racingCars.getGoCountByIndex(index + 1));
+        }
+    }
+
+    @Test
+    void 자동차_객체_가져오기() {
+        RacingCar racingCar = new RacingCar("자동차1");
+        assertThat(racingCar).usingRecursiveComparison().isEqualTo(racingCars.getRacingCarByIndex(1));
+        racingCar = new RacingCar("자동차2");
+        assertThat(racingCar).usingRecursiveComparison().isEqualTo(racingCars.getRacingCarByIndex(2));
+        racingCar = new RacingCar("자동차3");
+        assertThat(racingCar).usingRecursiveComparison().isEqualTo(racingCars.getRacingCarByIndex(3));
+    }
+}
