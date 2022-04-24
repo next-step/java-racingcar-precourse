fd6cdbb0a1547de6570c047ed26621e96dcaa761
jylim <jylim@12cm.co.kr>
2022-04-24 12:17:50 +0900

    feat: 자동차 입력 시 유효성 검증 추가

--- src/main/java/racingcar/domain/car/ParticipatingCars.java
+++ src/main/java/racingcar/domain/car/ParticipatingCars.java
@@ -6,8 +6,8 @@ import java.util.List;
 public class ParticipatingCars {
     private List<Car> cars;
 
-    public ParticipatingCars(String[] allCarName) {
-//        CarNameValidation.validation(allCarName);
+    public ParticipatingCars(String[] allParticipateCars) {
+        CarNameValidation.validation(allParticipateCars);
 //        RacePreparation racePreparation = new RacePreparation();
 //        this.cars = racePreparation.carRegistration(allCarName);
     }
