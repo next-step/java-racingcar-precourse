56df0d125c2a11807e04a51f4ca48f34da5f8540
jhm <sonyjangho@gmail.com>
2022-04-24 23:42:55 +0900

    기능 단위별 테스트코드 작성 2.최종우승자동차 확인

--- src/main/java/racingcar/util/RacingCarUtil.java
+++ src/main/java/racingcar/util/RacingCarUtil.java
@@ -1,11 +1,17 @@
 package racingcar.util;
 
+import java.util.List;
+import org.junit.platform.commons.util.StringUtils;
+import racingcar.domain.RacingCar;
+
 public class RacingCarUtil {
 
   public static final int GO_STATUS = 4;
 
   public static boolean isGoOrStop(int cnt) {
-    if(isGo(cnt)) return true;
+    if (isGo(cnt)) {
+      return true;
+    }
     return false;
   }
 
@@ -13,4 +19,49 @@ public class RacingCarUtil {
     return cnt >= GO_STATUS;
   }
 
+  public static String getCarName(List<RacingCar> racingCarList) {
+    RacingCar racingCar = racingCarList.get(0);
+    String maxCarNames = setCarName(null, racingCar.getCarName());
+    for (int idx = 1; idx < racingCarList.size(); idx++) {
+      RacingCar comparisonCar = racingCarList.get(idx);
+      if (isEqualRackingCar(racingCar, comparisonCar) && isZero(comparisonCar)) {
+        maxCarNames = setCarName(racingCar.getCarName(), comparisonCar.getCarName());
+        racingCar = comparisonCar;
+      }
+      if (isMaxRackingCar(racingCar, comparisonCar)) {
+        maxCarNames = setCarName(null, comparisonCar.getCarName());
+        racingCar = comparisonCar;
+      }
+    }
+    return maxCarNames;
+  }
+
+  //0 인지 체크
+  private static boolean isZero(RacingCar comparisonCar) {
+    if (comparisonCar.getGoCount() != 0) {
+      return true;
+    }
+    return false;
+  }
+
+  private static boolean isMaxRackingCar(RacingCar racingCar, RacingCar comparisonCar) {//기존
+    if (racingCar.getGoCount() < comparisonCar.getGoCount()) {
+      return true;
+    }
+    return false;
+  }
+
+  private static boolean isEqualRackingCar(RacingCar racingCar, RacingCar comparisonCar) {
+    if (racingCar.getGoCount() == comparisonCar.getGoCount()) {
+      return true;
+    }
+    return false;
+  }
+
+  private static String setCarName(String carName, String addCarName) {
+    if (StringUtils.isNotBlank(carName)) {
+      return carName + ", " + addCarName;
+    }
+    return addCarName;
+  }
 }
--- src/test/java/racingcar/RacingCarUtil.java
+++ src/test/java/racingcar/RacingCarUtil.java
@@ -9,7 +9,9 @@ public class RacingCarUtil {
   public static final int GO_STATUS = 4;
 
   public static boolean isGoOrStop(int cnt) {
-    if(isGo(cnt)) return true;
+    if (isGo(cnt)) {
+      return true;
+    }
     return false;
   }
 
@@ -19,15 +21,15 @@ public class RacingCarUtil {
 
   public static String getCarName(List<RacingCar> racingCarList) {
     RacingCar racingCar = racingCarList.get(0);
-    String maxCarNames = setCarName(null,racingCar.getCarName());
-    for (int idx = 1; idx < racingCarList.size(); idx++ ) {
+    String maxCarNames = setCarName(null, racingCar.getCarName());
+    for (int idx = 1; idx < racingCarList.size(); idx++) {
       RacingCar comparisonCar = racingCarList.get(idx);
       if (isEqualRackingCar(racingCar, comparisonCar) && isZero(comparisonCar)) {
-        maxCarNames = setCarName(racingCar.getCarName(),comparisonCar.getCarName());
+        maxCarNames = setCarName(racingCar.getCarName(), comparisonCar.getCarName());
         racingCar = comparisonCar;
       }
       if (isMaxRackingCar(racingCar, comparisonCar)) {
-        maxCarNames = setCarName(null,comparisonCar.getCarName());
+        maxCarNames = setCarName(null, comparisonCar.getCarName());
         racingCar = comparisonCar;
       }
     }
@@ -36,16 +38,23 @@ public class RacingCarUtil {
 
   //0 인지 체크
   private static boolean isZero(RacingCar comparisonCar) {
-    if (comparisonCar.getGoCount() != 0) return true;
+    if (comparisonCar.getGoCount() != 0) {
+      return true;
+    }
     return false;
   }
+
   private static boolean isMaxRackingCar(RacingCar racingCar, RacingCar comparisonCar) {//기존
-    if (racingCar.getGoCount() < comparisonCar.getGoCount()) return true;
+    if (racingCar.getGoCount() < comparisonCar.getGoCount()) {
+      return true;
+    }
     return false;
   }
 
   private static boolean isEqualRackingCar(RacingCar racingCar, RacingCar comparisonCar) {
-    if (racingCar.getGoCount() == comparisonCar.getGoCount()) return true;
+    if (racingCar.getGoCount() == comparisonCar.getGoCount()) {
+      return true;
+    }
     return false;
   }
 
