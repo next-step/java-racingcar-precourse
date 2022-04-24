2ce7386d61efa921b911302517bb060be127325b
Timothy <timothy@jungnanum.com>
2022-04-24 17:27:43 +0900

    style: Enum의 상수명 수정
    수정 : REQUEST_INPUT_GAME_TRY_COUNT -> REQUEST_INPUT_CAR_RACE_TIMES

--- src/main/java/racingcar/domain/enumtype/InterfaceMsg.java
+++ src/main/java/racingcar/domain/enumtype/InterfaceMsg.java
@@ -1,9 +1,9 @@
 package racingcar.domain.enumtype;
 
 public enum InterfaceMsg {
-	GAME_INFO("자동차 경주 게임에 오신 것을 환영합니다.\n자동차 경주 게임은 경주할 자동차 이름과 시도할 회수를 입력받습니다."),
+	GAME_INFO("자동차 경주 게임에 오신 것을 환영합니다.\n자동차 경주 게임은 경주할 자동차 이름과 시도할 경주의 회수를 입력받습니다."),
 	REQUEST_INPUT_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분) : "),
-	REQUEST_INPUT_GAME_TRY_COUNT("시도할 회수는 몇회인가요? : ")
+	REQUEST_INPUT_CAR_RACE_TIMES("시도할 회수는 몇회인가요? : ")
 	;
 
 	private String value;
