5a16bdb39ae8b1034cb6c5990d55338e47318d1b
KimTH94 <kcc940602@gmail.com>
2022-04-24 22:29:44 +0900

    refactor(GameTest) modify 공통 부분 정리

--- src/test/java/racingcar/domain/GameTest.java
+++ src/test/java/racingcar/domain/GameTest.java
@@ -2,13 +2,21 @@ package racingcar.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GameTest {
+    String[] racingCarName;
+    RacingCars racingCars;
+
+    @BeforeEach
+    public void create() {
+        racingCarName = new String[]{"자동차1","자동차2","자동차3"};
+        racingCars = new RacingCars(racingCarName);
+    }
+
     @Test
     void 자동차_세팅() {
-        String[] racingCarName = {"자동차1", "자동차2", "자동차3", "car4"};
-        RacingCars racingCars = new RacingCars(racingCarName);
         for (int index = 0; index < racingCarName.length; index++) {
             assertThat(racingCarName[index]).isEqualTo(racingCars.getNameByIndex(index + 1));
         }
