21eeed4c35ce0a84ff536fe02c2fd8e2acc376eb
prosjl <dlatjd0707@naver.com>
2022-04-24 17:43:12 +0900

    [feat] - util클래스 추가
    - 사용자 input
    - random 생성
    - split

---
+++ src/main/java/racingcar/util/Utils.java
@@ -0,0 +1,24 @@
+package racingcar.util;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Utils {
+
+    private Utils(){}
+
+    public static int getRandom(int min, int max){
+        return Randoms.pickNumberInRange(min, max);
+    }
+
+    public static String getInput(String printValue){
+        System.out.println(printValue);
+        return Console.readLine();
+    }
+
+    public static String[] splitStr(String str, String div){
+        return str.split(div);
+    }
+}
+
+
