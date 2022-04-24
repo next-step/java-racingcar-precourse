f354a574e333bdc89621cb2b3097a2d996912197
hugehoo <tbnsok40@gmail.com>
2022-04-24 10:16:43 +0900

    feature/ GameRound - 게임 라운드 마다 수행할 연산 책임

--- src/main/java/racingcar/model/GameRound.java
+++ src/main/java/racingcar/model/GameRound.java
@@ -1,7 +1,13 @@
 package racingcar.model;
 
-public class GameRound {
+import camp.nextstep.edu.missionutils.Randoms;
+import racingcar.view.GamePrinter;
 
+import java.util.Map;
+
+public class GameRound {
+    int MIN_NUMBER = 0;
+    int MAX_NUMBER = 9;
     public int rounds;
 
     public GameRound(int round) {
@@ -11,4 +17,25 @@ public class GameRound {
         this.rounds = round;
     }
 
+    public void playEachRound(Map<String, Integer> racingCarRecords) {
+        racingCarRecords.forEach((car, dashLine) -> {
+            int dashCount = moveForward(racingCarRecords, car);
+            if (dashLine != dashCount) {
+                racingCarRecords.put(car, dashCount);
+            }
+            GamePrinter.printCarAndDashes(car, dashCount);
+        });
+    }
+
+    private int moveForward(Map<String, Integer> racingCarRecords, String car) {
+        int originCount = racingCarRecords.get(car);
+        if (isMove()) {
+            return originCount + 1;
+        }
+        return originCount;
+    }
+
+    private boolean isMove() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER) >= 4;
+    }
 }
