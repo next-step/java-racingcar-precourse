36e6d5c149da456b3126eae875b2bedb940d27e9
hugehoo <tbnsok40@gmail.com>
2022-04-24 10:48:21 +0900

    feature/ GameController class - 1급 컬렉션 클래스 생성

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,37 @@
+package racingcar.controller;
+
+import racingcar.model.GameRound;
+import racingcar.model.RacingCar;
+import racingcar.model.Winner;
+import racingcar.view.GamePrinter;
+import racingcar.view.GameScanner;
+
+
+public class GameController {
+
+    private RacingCar racingCar;
+    private GameRound gameRound;
+    private final Winner winner = new Winner();
+
+    public void initGame() {
+        GamePrinter.printInitMessage();
+        try {
+            racingCar = new RacingCar(GameScanner.scanCarNames());
+            gameRound = new GameRound(GameScanner.scanGameRounds());
+        } catch (IllegalArgumentException e) {
+            initGame();
+        }
+    }
+
+    public void processGame() {
+        int rounds = gameRound.rounds;
+        while (rounds > 0) {
+            gameRound.playEachRound(racingCar.racingCarRecords);
+            rounds -= 1;
+        }
+    }
+
+    public void endGame() {
+        winner.getWinner(racingCar.racingCarRecords);
+    }
+}
