8471cf16d8a0642774ab47146f6da315900abf6b
ul8415 <ul8415@gmail.com>
2022-04-24 13:13:21 +0900

    feat: 입력받은 자동차이름(문자열)에 대한 자동차 목록 생성 메서드 추가
    - 입력받은 문자열을 ,로 split 이후 RacingCars 객체 생성

--- src/main/java/racingcar/service/RacingCarPlayService.java
+++ src/main/java/racingcar/service/RacingCarPlayService.java
@@ -1,10 +1,15 @@
 package racingcar.service;
 
+import java.util.List;
+import racingcar.constant.Delimiter;
+import racingcar.utils.SplitUtils;
 import racingcar.vo.RacingCars;
 
 public class RacingCarPlayService {
 
     public RacingCars registerRacingCars(String inputCarNames) {
-        return null;
+        List<String> inputCarNameList = SplitUtils.splitToList(inputCarNames, Delimiter.COMMA);
+        return new RacingCars(inputCarNameList);
     }
+
 }
