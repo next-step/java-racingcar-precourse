659528f6c8dbf2aa48156b4a4750f4778db9b43d
KimTH94 <kcc940602@gmail.com>
2022-04-25 00:15:44 +0900

    test(ResultTest) add 공동 우승

--- src/test/java/racingcar/domain/ResultTest.java
+++ src/test/java/racingcar/domain/ResultTest.java
@@ -16,5 +16,17 @@ public class ResultTest {
         assertThat(result.getResult()).contains("alice");
     }
 
+    @Test
+    void 공동_우승자_결과() {
+        String[] racingCarsName = {"alice", "lisa", "paul"};
+        RacingCars racaingCars = new RacingCars(racingCarsName);
+        RacingCar racingCar = racaingCars.getRacingCarByIndex(1);
+        racingCar.goRacingCar();
 
+        racingCar = racaingCars.getRacingCarByIndex(3);
+        racingCar.goRacingCar();
+
+        Result result = new Result(racaingCars);
+        assertThat(result.getResult()).contains("alice", "paul");
+    }
 }
