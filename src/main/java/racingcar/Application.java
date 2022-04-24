2840d5a83cd1774eb1535bbe4ee237f96ac182af
deokmoon <deok2moon@gmail.com>
2022-04-25 02:41:09 +0900

    Feat: UI부분 Main메서드 구현 에러 발생 시 해당부분 재수행 하도록 재귀로 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,43 @@
 package racingcar;
 
-public class Application {
-    public static void main(String[] args) {
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.Controller.Controller;
+import racingcar.model.Car;
+import racingcar.util.Validation;
 
+import java.util.List;
+
+public class Application {
+    private static Validation validation = new Validation();
+    private static Controller controller = new Controller();
+    private static List<Car> carList;
+
+    public static void main(String[] args) {
+        callMakeGameCarList();
+        callStartGame();
+    }
+
+    private static void callStartGame() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+        String numberOfExecution = Console.readLine();
+        try {
+            controller.startGame(carList, numberOfExecution);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            callStartGame();
+        }
+    }
+
+    private static void callMakeGameCarList() {
+        System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,) 기준으로 구분");
+        String[] names = Console.readLine().split(",");
+        try {
+            validation.hasNotEnoughParticipant(names);
+            validation.isDuplicated(names);
+            carList = controller.makeCarList(names);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            callMakeGameCarList();
+        }
     }
 }
