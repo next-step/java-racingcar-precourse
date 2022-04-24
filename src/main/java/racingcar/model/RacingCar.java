20fb3d09cc6dcd58c87eb1bb96d085538631b396
hugehoo <tbnsok40@gmail.com>
2022-04-24 10:45:53 +0900

    featuer/ RacingCar 생성시 유효성 체크 추가

--- src/main/java/racingcar/model/RacingCar.java
+++ src/main/java/racingcar/model/RacingCar.java
@@ -1,20 +1,41 @@
 package racingcar.model;
 
+import org.junit.platform.commons.util.StringUtils;
 import racingcar.view.GamePrinter;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
+
 
 public class RacingCar {
 
+    // TODO : 불변 지키게 하려면 어떻게 할 것인가?
     public Map<String, Integer> racingCarRecords = new LinkedHashMap<>();
+    private final List<String> cacheDuplicateCars = new ArrayList<>();
+
 
     public RacingCar(String[] cars) {
-            for (String car : cars) {
-                validateInputLength(car);
-                String refinedCar = trimNameSpaces(car);
-                racingCarRecords.put(refinedCar, 0);
-            }
+        for (String car : cars) {
+            validateEmpty(car);
+            String refinedCar = trimNameSpaces(car);
+            validateDuplicate(refinedCar);
+            validateInputLength(refinedCar);
+            racingCarRecords.put(refinedCar, 0);
+        }
+    }
+
+    private void validateDuplicate(String car) {
+        if (cacheDuplicateCars.contains(car)) {
+            GamePrinter.printDuplicateName();
+            throw new IllegalArgumentException();
+        }
+        cacheDuplicateCars.add(car);
+    }
+
+    private void validateEmpty(String read) {
+        if (StringUtils.isBlank(read)) {
+            GamePrinter.printEmptyName();
+            throw new IllegalArgumentException();
+        }
     }
 
     private String trimNameSpaces(String car) {
@@ -24,10 +45,8 @@ public class RacingCar {
     private void validateInputLength(String car) {
         if (car.length() > 5 || car.length() < 1) {
             GamePrinter.printErrorValidNames();
-            // TODO : 예외처리 어떻게할거?
             throw new IllegalArgumentException("[ERROR]");
         }
-
     }
 
 }
