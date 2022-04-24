84edbe84bd72c23c1c3856b2a417fc4a366b31cd
Gukjin Han <pioneer.genie@gmail.com>
2022-04-25 07:47:58 +0900

    fix(main): application main
    - test code bug fix
    before : 최종 우승자는 pobi 입니다.
    after : 최종 우승자: pobi
    - added race controller start

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,10 @@
 package racingcar;
 
+import racingcar.race.RaceController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        RaceController raceController = new RaceController();
+        raceController.start();
     }
 }
