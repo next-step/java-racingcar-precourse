db956f92f54a45d6d067a464de072aaea8ca8756
KimTH94 <kcc940602@gmail.com>
2022-04-25 01:02:19 +0900

    refactor(SystemMessage) modify 코드 정리

--- src/main/java/racingcar/common/SystemMessage.java
+++ src/main/java/racingcar/common/SystemMessage.java
@@ -1,6 +1,7 @@
 package racingcar.common;
 
 public class SystemMessage {
+
     public static final String INPUT_RACING_CAR_NAME = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String INPUT_TIMES = "시도할 회수는 몇 회인가요?";
     public static final String RESULT = "실행결과";
