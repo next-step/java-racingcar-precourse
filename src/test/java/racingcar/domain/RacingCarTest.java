e83210fc6317510db37df9968a9c6faf12e4c1f6
KimTH94 <kcc940602@gmail.com>
2022-04-24 18:40:15 +0900

    test(RacingCarTest) create 자동차 객체 테스트

---
+++ src/test/java/racingcar/domain/RacingCarTest.java
@@ -0,0 +1,14 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class RacingCarTest {
+    @Test
+    void RacingCar_객체_생성() {
+        RacingCar racingCar = new RacingCar("자동차1");
+        assertThat("자동차1").isEqualTo(racingCar.getName());
+        assertThat(0).isEqualTo(racingCar.getGoCount());
+    }
+}
