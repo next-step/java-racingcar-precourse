e12a497372df1c4c666156327f99649f5ccd020e
Gukjin Han <pioneer.genie@gmail.com>
2022-04-24 11:28:41 +0900

    feat(cars): modify cars
    - change cars to first collection
    - added test

--- src/main/java/racingcar/car/Car.java
+++ src/main/java/racingcar/car/Car.java
@@ -1,7 +1,11 @@
 package racingcar.car;
 
 public class Car {
+    public static int SCORE_MIN = 0;
+    public static int SCORE_MAX = 9;
     public static int GO_MIN_SCORE = 4;
+    public static String ERROR_SCORE_UNDERFLOW = "[ERROR] 너무 작은 값입니다. %d";
+    public static String ERROR_SCORE_OVERFLOW = "[ERROR] 너무 큰 값입니다. %d";
 
     String name;
     int move;
@@ -11,8 +15,17 @@ public class Car {
     }
 
     public void inputScore(int score){
+        validScoreRange(score);
         if (score >= GO_MIN_SCORE){
             go();
+        }
+    }
+    private void validScoreRange(int score){
+        if (score < SCORE_MIN){
+            throw new IllegalArgumentException(String.format(ERROR_SCORE_UNDERFLOW, score));
+        }
+        if (score > SCORE_MAX){
+            throw new IllegalArgumentException(String.format(ERROR_SCORE_OVERFLOW, score));
         }
     }
 
--- src/main/java/racingcar/car/Cars.java
+++ src/main/java/racingcar/car/Cars.java
@@ -1,13 +1,27 @@
 package racingcar.car;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Cars {
     List<Car> cars;
     public Cars(List<Car> cars){
         this.cars = cars;
     }
-    public List<Car> getCars(){
-        return this.cars;
+    public void inputScores(){
+        for (Car car : cars){
+            car.inputScore(Randoms.pickNumberInRange(0, 9));
+        }
+    }
+
+    public Map<String, Integer> getCarMoves(){
+        Map<String, Integer> moveByCar = new HashMap<String, Integer>();
+        for (Car car : cars){
+            moveByCar.put(car.getName(), car.getMove());
+        }
+        return moveByCar;
     }
 }
