cb36fc3eb014cbee4b328f151e9bf4e69df10def
diqksrk <diqksrk123@naver.com>
2022-04-25 19:07:44 +0900

    test : Add test functions that test the Racing Car Game Controller And application

--- src/test/java/racingcar/ApplicationTest.java
+++ src/test/java/racingcar/ApplicationTest.java
@@ -25,12 +25,77 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 다중라운드로_진행() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni, na", "3");
+                    assertThat(output()).contains("최종 우승자: pobi");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,MOVING_FORWARD,MOVING_FORWARD,MOVING_FORWARD, STOP, STOP
+        );
+    }
+
+    @Test
+    void 다중라운드_공동우승자_진행() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni, na", "3");
+                    assertThat(output()).contains("최종 우승자: pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD, MOVING_FORWARD,MOVING_FORWARD,MOVING_FORWARD,MOVING_FORWARD, MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 이름_입력_실패후_게임_진행() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,wonidsadsadd,woni", "pobi,woni", "1");
+                    assertThat(output()).contains("최종 우승자: pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 라운드_입력_실패후_게임_진행() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "d", "3");
+                    assertThat(output()).contains("최종 우승자: pobi");
+                },
+                MOVING_FORWARD, STOP
+        );
+    }
+
+    @Test
+    void 공동우승자_처리() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("pobi,woni", "1");
+                    assertThat(output()).contains("최종 우승자: pobi, woni");
+                },
+                MOVING_FORWARD, MOVING_FORWARD
+        );
+    }
+
+    @Test
     void 이름에_대한_예외_처리() {
         assertSimpleTest(
             () -> {
                 runException("pobi,javaji");
                 assertThat(output()).contains(ERROR_MESSAGE);
             }
+        );
+    }
+
+    @Test
+    void 라운드값에_대한_예외_처리() {
+        assertSimpleTest(
+                () -> {
+                    runException("pobi,jav", "d");
+                    assertThat(output()).contains(ERROR_MESSAGE);
+                }
         );
     }
 
