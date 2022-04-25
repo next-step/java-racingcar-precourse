5e26d45cdc340767cd746f6a6feb3e4719cb333c
diqksrk <diqksrk123@naver.com>
2022-04-25 19:05:55 +0900

    feat :Add functions to  print the winner, After the winner is decided

--- src/main/java/racingcar/controller/RacingCarGame.java
+++ src/main/java/racingcar/controller/RacingCarGame.java
@@ -1,35 +1,73 @@
 package racingcar.controller;
 
-import racingcar.model.RacingCarGameValidator;
-import racingcar.model.RacingCars;
+import org.junit.platform.commons.util.StringUtils;
+import racingcar.model.*;
 import racingcar.view.RacingCarGameInputReader;
+import racingcar.view.RacingCarGameOutputWriter;
 
 public class RacingCarGame {
     private RacingCars racingCars;
     private RacingCarGameValidator racingCarGameValidator;
+    private RacingCarGameReferee racingCarGameReferee;
+    private GameRoundRandomNumberGanerator gameRoundRandomNumberGanerator;
+    private RacingCarGameOutputWriter racingCarGameOutputWriter;
+    private RacingCarGameRule racingCarGameRule;
+    private RacingCarGameError racingCarGameError;
 
-    public  RacingCarGame() {
+    public RacingCarGame() {
         this.racingCars = new RacingCars();
         this.racingCarGameValidator = new RacingCarGameValidator();
+        this.racingCarGameReferee = new RacingCarGameReferee();
+        this.gameRoundRandomNumberGanerator = new GameRoundRandomNumberGanerator();
+        this.racingCarGameOutputWriter = new RacingCarGameOutputWriter();
+        this.racingCarGameRule = new RacingCarGameRule();
+        this.racingCarGameError = new RacingCarGameError();
     }
 
     public void start() {
-        while (!racingCarGameValidator.isValid()) {
-            racingCarGameValidator.racingCarNamesValidate(RacingCarGameInputReader.inquireRacingCarNames());
-        }
-        racingCarGameValidator.initValidator();
-        while (!racingCarGameValidator.isValid()) {
-            racingCarGameValidator.racingCarGameRoundsValidate(RacingCarGameInputReader.inquireRacingCarGameRounds());
-        }
-
-
-//        for (String carName : RacingCarGameInputReader.inquireRacingCarNames().split(",")) {
-//            racingCars.addRacingCar(carName);
-//        }
-
-//        System.out.println("racingCars = " + racingCars);
+        settingRacingCars();
+        decideRacingCarGameRound();
+        racingCarGameReferee.racingCarGameRoundsProcessing(racingCars, racingCarGameRule.getRacingCarGameRound(), gameRoundRandomNumberGanerator);
+        RacingCars winnerRacingCars = racingCarGameReferee.calculateRacingCarGameResult(racingCars);
+        racingCarGameOutputWriter.answerRacingCarGameResult(winnerRacingCars);
     }
 
+    private void settingRacingCars() {
+        decideRacingCarNames();
 
+        for (String racingCarName : racingCarGameRule.getRacingCarGameNames().split(",")) {
+            racingCars.addRacingCar(new RacingCar(racingCarName));
+        }
+    }
 
+    private void decideRacingCarNames() {
+        String racingCarNames = new String();
+        racingCarGameValidator.initValidator();
+
+        while (!racingCarGameValidator.isValid()) {
+            racingCarNames = RacingCarGameInputReader.inquireRacingCarNames();
+            racingCarGameValidator.racingCarNamesValidate(racingCarNames, racingCarGameError);
+            processRacingGameError();
+        }
+        racingCarGameRule.setRacingCarGameNames(racingCarNames);
+    }
+
+    private void decideRacingCarGameRound() {
+        String racingCarGameRound = new String();
+        racingCarGameValidator.initValidator();
+
+        while (!racingCarGameValidator.isValid()) {
+            racingCarGameRound = RacingCarGameInputReader.inquireRacingCarGameRounds();
+            racingCarGameValidator.racingCarGameRoundsValidate(racingCarGameRound, racingCarGameError);
+            processRacingGameError();
+        }
+        racingCarGameRule.setRacingCarGameRound(Integer.parseInt(racingCarGameRound));
+    }
+
+    private void processRacingGameError() {
+        if (StringUtils.isNotBlank(racingCarGameError.getDefaultMessage())) {
+            racingCarGameOutputWriter.answer(racingCarGameError.getDefaultMessage());
+            racingCarGameError.initErorr();
+        }
+    }
 }
--- src/main/java/racingcar/view/RacingCarGameOutputWriter.java
+++ src/main/java/racingcar/view/RacingCarGameOutputWriter.java
@@ -1,8 +1,23 @@
 package racingcar.view;
 
+import racingcar.model.RacingCar;
+import racingcar.model.RacingCars;
+
 public class RacingCarGameOutputWriter {
 
     public void answer(String message) {
         System.out.println(message);
+    }
+
+    public void answerRacingCarGameResult(RacingCars racingCars) {
+        StringBuilder stringBuilder = new StringBuilder();
+        int winnerNumber = racingCars.getRacingCars().size();
+
+        stringBuilder.append("최종 우승자: ");
+        for (RacingCar racingCar : racingCars.getRacingCars()) {
+            stringBuilder.append(racingCar.getName() + ", ");
+        }
+
+        System.out.println(stringBuilder.substring(0, stringBuilder.length() - 2));
     }
 }
