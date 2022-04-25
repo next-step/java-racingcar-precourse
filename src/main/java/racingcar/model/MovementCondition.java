69f53291d067927c7937abe876c32e8491404d99
Celina <luv.mins99@gmail.com>
2022-04-25 18:00:20 +0900

    refactor(RandomMovement): 전진 조건 관련 구현 패키지 및 이름 수정
    - 클래스의 의미를 조금 더 명확하게 하기 위함

---
+++ src/main/java/racingcar/model/MovementCondition.java
@@ -0,0 +1,5 @@
+package racingcar.model;
+
+public interface MovementCondition {
+    int moving();
+}
--- src/main/java/racingcar/model/RandomMovement.java
+++ src/main/java/racingcar/model/RandomMovement.java
@@ -1,13 +1,13 @@
-package racingcar.utils;
+package racingcar.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomGenerator {
+public class RandomMovement implements MovementCondition {
 
     private static final int MIN_NUMBER = 0;
     private static final int MAX_NUMBER = 9;
 
-    public static int race() {
+    public int moving() {
         return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
 }
