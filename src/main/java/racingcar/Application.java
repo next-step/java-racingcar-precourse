a3c6775478ea923a4db3545ec2b18e312ea9e178
sghaha <sghaha@naver.com>
2022-10-07 18:04:13 +0900

    예외처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,14 +7,9 @@ import racingcar.service.GameService;
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-		String input = Console.readLine();
+		Game game = getGame();
 
-		Game game = new Game(input);
-
-		System.out.println("시도할 회수는 몇 회인가요");
-		String turnCountStr = Console.readLine();
-		int turnCount = Integer.parseInt(turnCountStr);
+		int turnCount = getTurnCount();
 
 		System.out.println("실행 결과");
 		GameService gameService = new GameService();
@@ -22,5 +17,36 @@ public class Application {
 
 		gameService.printWinner(game);
 	}
+
+	public static Game getGame() {
+		try {
+			String input = getCarName();
+			Game game = new Game(input);
+			return game;
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return getGame();
+		}
+	}
+
+	public static String getCarName() {
+		System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+		String input = Console.readLine();
+
+		return input;
+	}
+
+	public static int getTurnCount() {
+		System.out.println("시도할 회수는 몇 회인가요");
+		String turnCountStr = Console.readLine();
+		try {
+			int turnCount = Integer.parseInt(turnCountStr);
+			return turnCount;
+		} catch (NumberFormatException e) {
+			System.out.println("[ERROR] 시도 횟수는 숫자여야 한다.");
+			return getTurnCount();
+		}
+	}
+
 
 }
