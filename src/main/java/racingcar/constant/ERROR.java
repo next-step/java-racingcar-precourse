f167411e817af4b6335da0fe37e853d1631d319b
sevoon0909 <sevoon0909@icloud.com>
2022-10-08 22:46:34 +0900

    refactor : 상수값 추가

--- src/main/java/racingcar/constant/ERROR.java
+++ src/main/java/racingcar/constant/ERROR.java
@@ -1,6 +1,9 @@
 package racingcar.constant;
 
+import static racingcar.constant.SETTING.*;
+
 public class ERROR {
 	public static final String EXCESS_LENGTH = "자동차의 이름은 5자리 이상 입력 할 수 없습니다.";
 	public static final String EMPTY_NAME = "자동차의 이름은 빈값을 입력 할 수 없습니다" ;
+	public static final String COUNT_MISS = String.format("횟수는 %d이상 %d이하만 가능합니다.",MIN_ROUND,MAX_ROUND);
 }
--- src/main/java/racingcar/constant/SETTING.java
+++ src/main/java/racingcar/constant/SETTING.java
@@ -1,5 +1,9 @@
 package racingcar.constant;
 
 public class SETTING {
+
 	public static final int ROUND_START = 1;
+	public static final int MIN_ROUND = 1;
+	public static final int MAX_ROUND = 100;
+
 }
