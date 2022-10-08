2766de5bccdc77271e37ef6418f47c1e3b85d505
sevoon0909 <sevoon0909@icloud.com>
2022-10-08 18:56:39 +0900

    test(domain): 자동차 거리 합산 확인

--- src/test/java/racingcar/domain/CarNameTest.java
+++ src/test/java/racingcar/domain/CarNameTest.java
@@ -2,8 +2,19 @@ package racingcar.domain;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static racingcar.constant.ERROR.*;
+import static racingcar.constant.SETTING.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
+
+import racingcar.constant.SETTING;
+import racingcar.domain.car.MyCarInfo;
+import racingcar.domain.car.Component;
+import racingcar.domain.car.MyCar;
+import racingcar.domain.car.MyCarInfos;
+import racingcar.domain.car.factor.CarName;
 
 class CarNameTest {
 
@@ -36,4 +47,27 @@ class CarNameTest {
 
 		assertEquals(EMPTY_NAME,exception.getMessage());
 	}
+
+	@Test
+	public void 자동차_거리_합_확인(){
+
+		List<Component> myCarDistances = new ArrayList<>();
+		myCarDistances.add(new MyCar(CarName.of("doing"),1));
+		myCarDistances.add(new MyCar(CarName.of("doing"),2));
+
+		MyCarInfo myCarInfo = new MyCarInfo(myCarDistances);
+
+		assertEquals(3, myCarInfo.getDistances());
+
+		List<Component> myCarDistances2 = new ArrayList<>();
+		myCarDistances2.add(new MyCar(CarName.of("poing"),1));
+		myCarDistances2.add(new MyCar(CarName.of("poing"),3));
+
+		MyCarInfo myCarInfo2 = new MyCarInfo(myCarDistances2);
+
+		assertEquals(4, myCarInfo2.getDistances());
+
+	}
+
+
 }
