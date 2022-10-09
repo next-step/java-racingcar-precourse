a3b122e5bf9074772109af009e6d216643f14679
gywjd <72644229+gywjd@users.noreply.github.com>
2022-10-09 23:45:00 +0900

    fix: 우승자 출력 수정

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -8,7 +8,7 @@ import domain.RaceResult;
 public class OutputView {
 
     public static final String MESSAGE_INFO_RACE_RESULT = "실행 결과";
-    public static final String MESSAGE_INFO_WINNERS = "최종 우승자: ";
+    public static final String MESSAGE_INFO_WINNERS = "최종 우승자 : ";
 
     public static void printExceptionMessage(String message) {
         System.out.println(message);
