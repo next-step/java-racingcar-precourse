2c56a5d53b12ca5b5e26b55903a41ac67b9b3572
gywjd <72644229+gywjd@users.noreply.github.com>
2022-10-09 14:36:00 +0900

    feat: 전진
    - random 값이 4 이상일 경우 전진, 3 이하면 멈춤
    - 자동차별 주행거리 기록

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import controller.RacingController;
 import domain.CarNames;
 import domain.Laps;
 import view.InputView;
@@ -9,5 +10,8 @@ public class Application {
     public static void main(String[] args) {
         CarNames carNames = InputView.getCarNames();
         Laps laps = InputView.getLaps();
+
+        RacingController racingContoller = new RacingController();
+        racingContoller.startRace(carNames, laps);
     }
 }
