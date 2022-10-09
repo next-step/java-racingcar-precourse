f594a2835d0a1e36e0ae2e704c8f75798ea9387c
DESKTOP-JU2NB2K\rlaal <rlaalsxo1990@gmail.com>
2022-10-09 15:33:31 +0900

    fixed: 자동차이름 생성 로직 수정
    자동차이름 셍상 로직수정
     - 자동차이름에 공백이 포함되있을경우 에러를 throw 하는 기능 추가
     - 자동차이름 길이체크 로직 수정
    
    ref:  [2ab55d6]

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -4,11 +4,16 @@ import java.util.Objects;
 
 public class CarName {
 
+    private final String WHITESPACE = " ";
+    private final int MIN_NAME_LENGTH = 1;
     private final int MAX_NAME_LENGTH = 5;
 
     private final String name;
 
     public CarName(final String name) {
+        if (validateWhitespace(name)) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 공백을 제외하고 작성이 가능합니다.");
+        }
         if (validateCarName(name)) {
             throw new IllegalArgumentException("[ERROR] 자동차 이름은 한글자 이상 다섯글자 이하만 가능합니다.");
         }
@@ -16,7 +21,11 @@ public class CarName {
     }
 
     private boolean validateCarName(final String name) {
-        return name.isEmpty() || name.length() > MAX_NAME_LENGTH;
+        return name.length() < MIN_NAME_LENGTH || name.length() > MAX_NAME_LENGTH;
+    }
+
+    private boolean validateWhitespace(final String name) {
+        return name.contains(WHITESPACE);
     }
 
     public String getName() {
