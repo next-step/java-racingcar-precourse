bd389ad32193097166166a24ca559a0e9ef813b9
gywjd <72644229+gywjd@users.noreply.github.com>
2022-10-09 18:18:00 +0900

    feat: 우승자 안내
    - 실행 결과에 따른 우승자 선정
    - 최종 우승자 출력(한 명 이상일 경우 쉼표로 구분)

--- src/main/java/controller/RacingController.java
+++ src/main/java/controller/RacingController.java
@@ -5,6 +5,7 @@ import domain.CarName;
 import domain.CarNames;
 import domain.Laps;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class RacingController {
@@ -14,13 +15,18 @@ public class RacingController {
     public static final int MOVE_FORWARD_BASE_VALUE = 4;
     public static final String MESSAGE_INFO_RACE_RESULT = "실행 결과";
     private static Map<CarName, String> RECORD;
+    private static Map<CarName, Integer> MILEAGE;
+    public static int MAX_MILEAGE = 0;
 
     public void startRace(CarNames carNames, Laps laps) {
         RECORD = new HashMap<>();
+        MILEAGE = new LinkedHashMap<>();
         int lap = 0;
+        MAX_MILEAGE = 0;
 
         for (CarName name : carNames.getCarNames()) {
             RECORD.put(name, "");
+            MILEAGE.put(name, 0);
         }
 
         System.out.println(MESSAGE_INFO_RACE_RESULT);
@@ -29,6 +35,8 @@ public class RacingController {
             lap++;
             System.out.println();
         }
+
+        announceWinner();
     }
 
     private void progressRace(CarNames carNames) {
@@ -40,11 +48,32 @@ public class RacingController {
     private void moveForward(CarName name) {
         if (Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE) >= MOVE_FORWARD_BASE_VALUE) {
             RECORD.put(name, RECORD.get(name) + "-");
+            MILEAGE.put(name, MILEAGE.get(name) + 1);
         }
         recordResult(name);
     }
 
     private void recordResult(CarName name) {
+        if(MILEAGE.get(name) > MAX_MILEAGE) {
+            MAX_MILEAGE = MILEAGE.get(name);
+        }
         System.out.println(name.getName() + " : " + RECORD.get(name));
+    }
+
+    private String selectWinner(String winners, CarName name) {
+        if(MILEAGE.get(name) == MAX_MILEAGE) {
+            winners += name.getName() + ", ";
+        }
+        return winners;
+    }
+
+    private void announceWinner() {
+        System.out.print("최종 우승자: ");
+        String winners = "";
+        for(CarName name : MILEAGE.keySet()) {
+            winners = selectWinner(winners, name);
+        }
+        winners = winners.substring(0, winners.length() - 2);
+        System.out.println(winners);
     }
 }
