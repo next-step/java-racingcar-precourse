4f7ddf7357b5fed58f209c27078af53e81468da1
snh15978 <snh15978@gmail.com>
2022-10-10 17:27:24 +0900

    feat(Cars): 우승자 추출 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,5 +1,7 @@
 package racingcar.domain;
 
+import java.util.Objects;
+
 public class Car {
     private static final int DEFAULT_MOVE_DISTANCE = 0;
     private static final int NAME_LENGTH_LIMIT = 5;
@@ -40,5 +42,22 @@ public class Car {
 
     public int getMoveDistance() {
         return moveDistance;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return moveDistance == car.moveDistance && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, moveDistance);
     }
 }
--- src/main/java/racingcar/domain/Cars.java
+++ src/main/java/racingcar/domain/Cars.java
@@ -1,5 +1,6 @@
 package racingcar.domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
@@ -17,5 +18,33 @@ public class Cars {
 
     public List<Car> getCars() {
         return cars;
+    }
+
+    public List<Car> extractWinner() {
+        int maxMoveDistance = getMaxMoveDistance(cars);
+
+        List<Car> list = new ArrayList<>();
+        for (Car o : cars) {
+            addWinner(maxMoveDistance, list, o);
+        }
+        return list;
+    }
+
+    private static void addWinner(int maxMoveDistance, List<Car> list, Car o) {
+        if (o.getMoveDistance() == maxMoveDistance) {
+            list.add(o);
+        }
+    }
+
+    private int getMaxMoveDistance(List<Car> cars) {
+        int maxMoveDistance = 0;
+        for (Car car : cars) {
+            maxMoveDistance = compareMoveDistance(maxMoveDistance, car);
+        }
+        return maxMoveDistance;
+    }
+
+    private int compareMoveDistance(int maxMoveDistance, Car car) {
+        return Math.max(car.getMoveDistance(), maxMoveDistance);
     }
 }
