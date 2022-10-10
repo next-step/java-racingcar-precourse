29d5935c396cf95eb8cb1798c84b235bd9da783c
sevoon0909 <sevoon0909@icloud.com>
2022-10-10 13:55:46 +0900

    feat(일급컬렉션) : 초종거리 현재거리를 확인 함을 위한다.

---
+++ src/main/java/racingcar/domain/car/collect/MyCars.java
@@ -0,0 +1,57 @@
+package racingcar.domain.car.collect;
+
+import static racingcar.constant.SETTING.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import racingcar.domain.car.CarName;
+import racingcar.domain.car.MyCar;
+
+public class MyCars {
+
+	private final List<MyCar> myCars;
+
+	public MyCars(List<MyCar> myCars) {
+		this.myCars = new ArrayList(myCars);
+	}
+
+	public Integer  currentDistance(int round){
+		Integer sumDistance = 0;
+		for(int i=0;i<=round;i++){
+			sumDistance+= distance(i);
+		}
+		return sumDistance;
+	}
+
+	public Integer lastDistance(){
+		Integer sumDistance = 0;
+		for(int i=0;i<myCars.size();i++){
+			sumDistance+= distance(i);
+		}
+		return sumDistance;
+	}
+
+	public Integer distance(int round) {
+		return myCarsByRound(round).getDistance();
+	}
+
+	public MyCar myCarsByRound(int round) {
+		return myCars.get(round);
+	}
+
+	public CarName playerCarName(){
+		return myCarsByRound(FIRST_INDEX).getCarName();
+	}
+
+	public List<MyCar> getMyCars() {
+		return myCars;
+	}
+
+	@Override
+	public String toString() {
+		return "MyCars{" +
+			"myCars=" + myCars +
+			'}';
+	}
+}
