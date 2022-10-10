384e91fcc00fef70fd4d2e31cad90b44d24f80a8
jungpil98 <jungpil98@ajou.ac.kr>
2022-10-10 18:31:05 +0900

    fix(RacingGameController) 요구 명세 예외 발생시 재시도되도록 변경

--- src/main/java/racingcar/controller/RacingGameController.java
+++ src/main/java/racingcar/controller/RacingGameController.java
@@ -2,22 +2,55 @@ package racingcar.controller;
 
 import racingcar.domain.*;
 
+import java.util.Objects;
+
 import static camp.nextstep.edu.missionutils.Console.*;
 import static racingcar.view.RacingGameView.*;
 
 public class RacingGameController {
 
     public void start() {
-        printStartRacing();
-        String carNames = readLine();
+        Cars cars = setCars();
+        LabCount labCount = setLabCount();
+        execute(cars, labCount);
+    }
+
+    private Cars setCars() {
+        Cars cars = null;
+        while(Objects.isNull(cars)) {
+            cars = generateCar(cars);
+        }
+        return cars;
+    }
+
+    private LabCount setLabCount() {
+        LabCount labCount = null;
+        while(Objects.isNull(labCount)) {
+            labCount = generateLabCount(labCount);
+        }
+        return labCount;
+    }
+
+    private Cars generateCar(Cars cars) {
         try {
-            Cars cars = Cars.release(carNames);
+            printStartRacing();
+            String carNames = readLine();
+            cars = Cars.release(carNames);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return cars;
+    }
+
+    private LabCount generateLabCount(LabCount labCount) {
+        try {
             printRacingCount();
             String raceCount = readLine();
-            LabCount labCount = new LabCount(raceCount);
-            execute(cars, labCount);
-        } catch(IllegalArgumentException e) {
+            labCount = new LabCount(raceCount);
+        } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
+        } finally {
+            return labCount;
         }
     }
 
