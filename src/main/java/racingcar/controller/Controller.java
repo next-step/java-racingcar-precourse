39b3b6f4c2d8e678fcb565207feadbd3a4e08496
Yian Kim <devyiankim@gmail.com>
2022-10-10 22:52:54 +0900

    feat: 컨트롤러 생성

---
+++ src/main/java/racingcar/controller/Controller.java
@@ -0,0 +1,8 @@
+package racingcar.controller;
+
+public class Controller {
+    private static final String inputCarNames = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String tryCount = "시도할 횟수는 몇 회인가요?";
+    private static final String resultText = "실행결과";
+
+}
