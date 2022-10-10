55c7f3da28d925f083cb6822748f9f0712a895c8
sevoon0909 <sevoon0909@icloud.com>
2022-10-10 13:56:25 +0900

    feat(상수) : 상수값 추가

--- src/main/java/racingcar/constant/SETTING.java
+++ src/main/java/racingcar/constant/SETTING.java
@@ -2,8 +2,14 @@ package racingcar.constant;
 
 public class SETTING {
 
+	public static final int FIRST_INDEX = 0;
+
 	public static final int ROUND_START = 1;
 	public static final int MIN_ROUND = 1;
 	public static final int MAX_ROUND = 100;
+	public static final int START_RANGE = 0;
+	public static final int END_RANGE = 9;
+	public static final int GO = 1;
+	public static final int STOP = 0;
 
 }
