2faf83d1e2c60219d822a6662bdf2e878152a91c
jungpil98 <jungpil98@ajou.ac.kr>
2022-10-10 18:29:04 +0900

    fix(LabCount) 예외처리 검증 케이스 추가

--- src/main/java/racingcar/domain/LabCount.java
+++ src/main/java/racingcar/domain/LabCount.java
@@ -5,7 +5,14 @@ public class LabCount {
     private final int count;
 
     public LabCount(String count) {
+        validate(count);
         this.count = Integer.parseInt(count);
+    }
+
+    private void validate(String count) {
+        if (Integer.parseInt(count) < 1) {
+            throw new IllegalArgumentException("[ERROR] 0 이하의 정수는 입력할 수 없습니다.");
+        }
     }
 
     public int getCount() {
