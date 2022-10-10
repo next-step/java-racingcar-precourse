9a520474afc9a8a5159be277c1aeb8ff98256a87
sevoon0909 <sevoon0909@icloud.com>
2022-10-10 16:41:41 +0900

    feat(main) : 앱실행 메인

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,29 @@
 package racingcar;
 
+import static racingcar.constant.COMMENT.*;
+
+import racingcar.interfaces.RacingInterface;
+import racingcar.util.TextOutPut;
+
 public class Application {
+    public static void processRun(RacingInterface racingInterface){
+
+        while(racingInterface.isAlive()){
+            try{
+                System.out.print(racingInterface.flushOutPut());
+                racingInterface.processRun();
+            }catch (IllegalArgumentException e){
+                TextOutPut textOutPut = new TextOutPut(e);
+                System.out.print(textOutPut.flush());
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        RacingInterface racingInterface = new RacingInterface();
+        processRun(racingInterface);
+
     }
 }
