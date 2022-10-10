ae4580bb7fe62369435fb547bfe6408dfeb11ffd
snh15978 <snh15978@gmail.com>
2022-10-10 17:29:22 +0900

    test(RaceSheet): RaceSheet 테스트 구현

---
+++ src/test/java/racingcar/domain/RaceSheetTest.java
@@ -0,0 +1,35 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RaceSheetTest {
+
+    @Test
+    @DisplayName("각 스냅샷이 힙 영역의 같은값을 안바라보는지 테스트 한다.")
+    void race_sheet_test() {
+
+        RaceSheet racingSheet = new RaceSheet();
+
+        Car carA = new Car("carA");
+        Car carB = new Car("carB");
+        Car carC = new Car("carC");
+
+        Cars cars = new Cars(Arrays.asList(carA, carB, carC));
+
+        racingSheet.addSheet(cars);
+        racingSheet.addSheet(cars);
+
+        List<Cars> racingSheets = racingSheet.getRaceSheets();
+
+        Cars firstSnapshot = racingSheets.get(0);
+        Cars secondSnapshot = racingSheets.get(1);
+
+        assertThat(firstSnapshot).isNotEqualTo(secondSnapshot);
+    }
+}
