f12e4d167414eecc1232419ab543a4de9e694ade
GitJIHO <jiho99322@gmail.com>
2024-05-26 23:32:50 +0900

    feat: Controller 역할을 하는 Application 생성 후 전체적인 코드 종합

---
+++ src/main/java/Application.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Application {
+
+    public static void main(String[] args) {
+        String[] carNames = InputView.getCarNames();
+        int attempts = InputView.getAttempts();
+
+        Race race = new Race(Arrays.asList(carNames), attempts);
+        race.start();
+
+        System.out.println("실행 결과");
+        ResultView.printRaceResult(race.getCars());
+        ResultView.printWinners(race.getWinners());
+    }
+
+}
