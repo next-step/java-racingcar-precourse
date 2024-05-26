702a9788b068fe61940bfe976aebe491005729f3
GitJIHO <jiho99322@gmail.com>
2024-05-26 23:34:03 +0900

    feat: main문에서 예외처리 기능 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,15 +3,18 @@ import java.util.Arrays;
 public class Application {
 
     public static void main(String[] args) {
-        String[] carNames = InputView.getCarNames();
-        int attempts = InputView.getAttempts();
+        try {
+            String[] carNames = InputView.getCarNames();
+            int attempts = InputView.getAttempts();
 
-        Race race = new Race(Arrays.asList(carNames), attempts);
-        race.start();
+            Race race = new Race(Arrays.asList(carNames), attempts);
+            race.start();
 
-        System.out.println("실행 결과");
-        ResultView.printRaceResult(race.getCars());
-        ResultView.printWinners(race.getWinners());
+            System.out.println("실행 결과");
+            ResultView.printRaceResult(race.getCars());
+            ResultView.printWinners(race.getWinners());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
-
 }
