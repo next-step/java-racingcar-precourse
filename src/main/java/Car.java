50dcd33ddc96bed31e5bf91c644ebb227be35771
GitJIHO <jiho99322@gmail.com>
2024-05-26 23:34:32 +0900

    style: ; 누락 수정

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -5,7 +5,7 @@ public class Car {
 
     public Car(String name){
         if(name == null || name.isEmpty() || name.length() >5){
-            throw new IllegalArgumentException("[ERROR] 자동차 이름은 1자 이상 5자 이하만 가능합니다.")
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 1자 이상 5자 이하만 가능합니다.");
         }
         this.name = name;
     }
