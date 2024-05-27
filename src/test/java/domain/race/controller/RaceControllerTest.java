298fe5a266114deaca30e1e500392ad9f313c2e9
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-05-27 22:31:01 +0900

    ✅ test: 자동차 이름 입력 값 검증 테스트 추가
    정상적인 자동차 이름, 5자 길이 이상의 자동차 이름 입력, 콤마가 이상하게 입력된 경우 에 대한 테스트케이스 추가

--- src/test/java/domain/race/controller/RaceControllerTest.java
+++ src/test/java/domain/race/controller/RaceControllerTest.java
@@ -1,12 +1,38 @@
 package domain.race.controller;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RaceControllerTest {
 
+    RaceController raceController = new RaceController();
 
     @Test
-    @DisplayName("정상적인 값이 입력되었을때")
-    void normalInput() {
-        Assertions.assertThat(UtilClass.isFinish("123", "321")).isFalse();
-        Assertions.assertThat(UtilClass.isFinish("321", "321")).isTrue();
+    @DisplayName("잘못된 자동차 이름이 입력된 경우")
+    void invalidCarNames() {
+        String invalidCarNamesFirstComma = ",hello,name";
+        String invalidCarNamesLastComma = "hello,name,";
+        String invalidOverLength = "hello,congratulation";
+        String emptyCarNames = "";
+
+        Assertions.assertThat(raceController.canUseCarNames(invalidCarNamesFirstComma)).isFalse();
+        Assertions.assertThat(raceController.canUseCarNames(invalidCarNamesLastComma)).isFalse();
+        Assertions.assertThat(raceController.canUseCarNames(invalidOverLength)).isFalse();
+        Assertions.assertThat(raceController.canUseCarNames(emptyCarNames)).isFalse();
+    }
+
+    @Test
+    @DisplayName("5자 길이 이상의 자동차 이름이 입력된 경우")
+    void invalidCarNameLength() {
+        String overLengthCarNames = "test1,test12,test123";
+        Assertions.assertThatThrownBy(()->raceController.allLengthIsLessThanFive(overLengthCarNames)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("정상적인 자동차 이름이 입력된 경우")
+    void validCarNameLength() {
+        String overLengthCarNames = "test,test2,test3";
+        Assertions.assertThat(raceController.canUseCarNames(overLengthCarNames)).isTrue();
     }
 }
