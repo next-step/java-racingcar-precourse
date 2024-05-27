dcf813da8ecad2cf2dabfbc251ef485571a04861
thornappl2 <thornappl2@gmail.com>
2024-05-27 19:05:11 +0900

    Feat: 자동차 전진 기능 추가 - 자동차가 전진하는 기능인 move 함수 구현 - 0에서 9사이의 랜덤 값 생성하는 randomNum 추가

---
+++ Model/Car.java
@@ -0,0 +1,23 @@
+package Model;
+
+public class Car {
+    private static final int MINIMUM_NUM = 4;
+    private String name;
+    private int position = 0;
+    private int randomNum = (int)(Math.random() * 10);
+
+    public Car(String name){
+        this.name = name;
+    }
+
+    private int move(int position){
+        if(randomNum >= MINIMUM_NUM){
+            position++;
+        }
+        return position;
+    }
+
+    private int getPosition(){
+        return position;
+    }
+}
