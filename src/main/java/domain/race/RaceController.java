3f3a88d8774944bc62b607f25a120409beab8fc2
yunjunghun0116 <yunjunghun0116@gmail.com>
2024-05-27 18:19:06 +0900

    ✨ feat: 검증 값에 따른 예외 처리
    입력값이 잘못된 경우 IllegalArgumentException을 반환하는데, 이 값을 ERROR 와 함께 출력 후에 다시 입력을 할 수 있도록 false 값을 View에게 리턴해준다.

--- src/main/java/domain/race/RaceController.java
+++ src/main/java/domain/race/RaceController.java
@@ -2,11 +2,23 @@ package domain.race;
 
 public class RaceController {
 
-    public void startRace(String carList, int round) {
-
+    public boolean startRace(String carNames, int round) {
+        try{
+            if(!validationCarNames(carNames)){
+                throw new IllegalArgumentException("자동차 이름이 잘못 입력되었습니다. 다시 입력해 주세요.");
+            }
+            if(!validationRound(round)){
+                throw new IllegalArgumentException("라운드가 잘못 입력되었습니다.. 다시 입력해 주세요.");
+            }
+            // race 시작
+        }catch (IllegalArgumentException exception){
+            System.out.println("[ERROR] "+ exception.getMessage());
+            return false;
+        }
+        return true;
     }
 
-    public boolean validationCarList(String carNames) {
+    public boolean validationCarNames(String carNames) {
         if (carNames.isEmpty()) return false;
         if (carNames.charAt(0) == ',') return false;
         if (carNames.charAt(carNames.length() - 1) == ',') return false;
