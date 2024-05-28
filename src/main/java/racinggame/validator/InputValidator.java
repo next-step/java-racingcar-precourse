16ae2c1e5fb01dcd2542c6e1caf8608e673f275f
ajy4304 <ajy4304@naver.com>
2024-05-28 14:21:03 +0900

    feat(RacingGameInputValidator.java): 검증 클래스 구현
    사용자에게 입력받은 문자열과 숫자를 검증하는 클래스 구현

--- src/main/java/racinggame/validator/InputValidator.java
+++ src/main/java/racinggame/validator/InputValidator.java
@@ -3,6 +3,6 @@ package racinggame.validator;
 public interface InputValidator {
 
     // 사용자에게 입력받은 자동차 이름이 쉼표로 구분되어 있는지
-    boolean isValidateCarNames(String carNames);
-    boolean isValidateInputNum(String inputNum);
+    void isValidateCarNames(String carNames);
+    void isValidateInputNum(String inputNum);
 }
--- src/main/java/racinggame/validator/RacingGameInputValidator.java
+++ src/main/java/racinggame/validator/RacingGameInputValidator.java
@@ -2,5 +2,28 @@ package racinggame.validator;
 
 public class RacingGameInputValidator implements InputValidator{
 
-    public static boolean
+    @Override
+    public void isValidateCarNames(String carNames) {
+        String[] names = carNames.split(",");
+        for (String name : names) {
+            if (name.trim().isEmpty()){
+                throw new IllegalArgumentException("[ERROR] 자동차 이름은 빈 문자열이 올 수 없습니다.");
+            }
+            if (name.trim().length() > 5){
+                throw new IllegalArgumentException("[ERROR] 자동차의 이름은 5자 이하만 가능합니다.");
+            }
+        }
+    }
+
+    @Override
+    public void isValidateInputNum(String inputNum) {
+        try {
+            int count = Integer.parseInt(inputNum);
+            if (count <= 0){
+                throw new IllegalArgumentException("[ERROR] 시도 가능한 수는 양의 정수입니다.");
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 시도 회수는 숫자여야 합니다.");
+        }
+    }
 }
