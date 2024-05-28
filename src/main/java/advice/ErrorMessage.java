ca396aaeb282f9952df3a12b370a1c4e3f569872
Kim Dayeong <gloria030114@gmail.com>
2024-05-28 13:40:25 +0900

    fix: add "[ERROR]" in all error messages
    - add "[ERROR]" to each error message

--- src/main/java/advice/ErrorMessage.java
+++ src/main/java/advice/ErrorMessage.java
@@ -2,9 +2,9 @@ package advice;
 
 public enum ErrorMessage {
 
-    CAR_NAME_OUT_OF_RANGE("자동차 이름은 1자 이상 5자 이하만 가능합니다."),
-    CAR_NAME_DUPLICATE("자동차 이름은 중복될 수 없습니다."),
-    CAR_COUNT_LESS_THAN_TWO("자동차는 2대 이상이어야 합니다.");
+    CAR_NAME_OUT_OF_RANGE("[ERROR] 자동차 이름은 1자 이상 5자 이하만 가능합니다."),
+    CAR_NAME_DUPLICATE("[ERROR] 자동차 이름은 중복될 수 없습니다."),
+    CAR_COUNT_LESS_THAN_TWO("[ERROR] 자동차는 2대 이상이어야 합니다.");
 
     private final String message;
 
