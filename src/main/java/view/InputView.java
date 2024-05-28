17202721d88f6f51394c0aab4fde305f596ef9b6
Kim Dayeong <gloria030114@gmail.com>
2024-05-28 15:14:22 +0900

    feat: add inputAttemptCount() method in InputView
    - add inputAttemptCount() method that receives, verifies, and return number of attempt
    - if invalid input is received, an error message is output and input is received again

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,6 +10,7 @@ public class InputView {
 
     private static Scanner scanner = new Scanner(System.in);
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_ATTEMPT_COUNT = "시도할 회수는 몇회인가요?";
 
     public static void setScanner (Scanner scanner) {
         InputView.scanner = scanner;
@@ -32,10 +33,30 @@ public class InputView {
             InputValidator.validateCarNames(carNameList);
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            carNameList = inputCarNames(); // 에러 발생 시 다시 입력 받음
+            return inputCarNames(); // 에러 발생 시 다시 입력 받음
         }
 
         return carNameList;
     }
 
+    /**
+     * 사용자로부터 시도 횟수를 입력 받는 메서드
+     * @return 시도 횟수
+     */
+    public static int inputAttemptCount() {
+
+            // 사용자 입력
+            System.out.println(INPUT_ATTEMPT_COUNT);
+            String attemptCount = scanner.nextLine();
+
+            // 입력값 검증
+            try {
+                InputValidator.validateAttemptCount(attemptCount);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                return inputAttemptCount(); // 에러 발생 시 다시 입력 받음
+            }
+
+            return Integer.parseInt(attemptCount);
+    }
 }
