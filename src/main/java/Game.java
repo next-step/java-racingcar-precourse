55dc2d736b5c3ed6b3d763a1e4b92c88f4b9002c
BaeJunho <bjh4498@knu.ac.kr>
2024-05-28 16:27:57 +0900

    feat : make makeCars function
    input 문자열을 ',' 기준으로 자르고, 각 문자열을 이름으로 하는 car 객체를 만들어 ArrayList 타입 배열에 저장하는 기능 구현

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,5 +2,10 @@ import java.util.ArrayList;
 
 public class Game {
     private ArrayList<Car> Cars = new ArrayList<>();
-
+    public void makeCars(String str){
+        String[] strArr = str.split(",");
+        for (String s : strArr) {
+            Cars.add(new Car(s));
+        }
+    }
 }
