23a0240123c8051432b379864ca41917cc32a1f4
SeongHun <tjdgns5506@gmail.com>
2024-05-28 12:28:56 +0900

    refactor: @DisplayName 추가

--- src/test/java/RacingGameViewTest.java
+++ src/test/java/RacingGameViewTest.java
@@ -11,11 +11,12 @@ public class RacingGameViewTest {
 
     @DisplayName("자동차 이름 규칙에 맞을 때 테스트")
     @ParameterizedTest
-    @ValueSource(strings = {"car1,car2,car3", "carA,carB", "carX,carY,carZ"})
+    @ValueSource(strings = {"car1,car2,car3", "carA,carB", "12345,1,2"})
     void testIsValidateCarNameInput_valid(String input) {
         assertDoesNotThrow(() -> view.isValidateCarNameInput(input));
     }
 
+    @DisplayName("자동차 이름 규칙과 다를 때 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"car1,carname2,car3", "longname1,longname2", "carX,toolongnameY"})
     void testIsValidateCarNameInput_invalid(String input) {
@@ -26,13 +27,13 @@ public class RacingGameViewTest {
         );
         assertEquals("[ERROR] 이름은 5자 이하만 가능합니다.", thrown.getMessage());
     }
-
+    @DisplayName("맞는 숫자 입력 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"12345", "67890", "11111"})
     void testIsValidateAttemptCountInput_valid(String input) {
         assertDoesNotThrow(() -> view.isValidateAttemptCountInput(input));
     }
-
+    @DisplayName("틀린 숫자 입력 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"123a5", "67b90", "11c11"})
     void testIsValidateAttemptCountInput_invalid(String input) {
