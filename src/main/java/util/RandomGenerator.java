ec9a10116ce476456f7546426cdb88b1a0e1da04
anacnoda77 <sm1108shin@naver.com>
2024-05-28 23:34:06 +0900

    feat(RandomGenerator.java): 난수 생성기 구현

--- src/main/java/util/RandomGenerator.java
+++ src/main/java/util/RandomGenerator.java
@@ -1,5 +1,14 @@
 package util;
 
+import java.util.Random;
+
 public class RandomGenerator {
 
+    private static final Random random = new Random();
+    private static final int ORIGIN = 0;
+    private static final int BOUND = 10;
+
+    public static int createRandomNumber() {
+        return random.nextInt(ORIGIN,BOUND);
+    }
 }
