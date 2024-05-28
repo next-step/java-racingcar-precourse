b5b064b1ab6fd6aa0279c22f7700ba195a89b8ab
anacnoda77 <sm1108shin@naver.com>
2024-05-29 01:18:45 +0900

    feat(MainApplication.java): main 작성
    

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -4,5 +4,6 @@ public class MainApplication {
     public static void main(String[] args) {
 
         GameController gameController = new GameController();
+        gameController.startGame();
     }
 }
