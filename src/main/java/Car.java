43aae42194269fbac6e992f1eb89d49038d94ef7
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-05-28 15:13:21 +0900

    자동차 객체 생성(이름, 이동거리 포함됨)

---
+++ src/main/java/Car.java
@@ -0,0 +1,19 @@
+public class Car {
+
+    private String name;
+    private Integer num;
+
+    public Car(String name) {
+        this.name = name;
+        this.num = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+    
+}
