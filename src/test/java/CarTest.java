f2893742efc1a86840de0b9e3062b4055a1ead90
BaeJunho <bjh4498@knu.ac.kr>
2024-05-28 16:15:31 +0900

    feat : make makeDecision's test
    car 클래스에서 전진 여부를 판단하는 기능에 대한 테스트 코드 작성
    한 번의 실행 시 생성되는 난수가 4 이상일 때 count를 1 증가시키고, 10회의 반복의 결과로 생성되는 car의 route가 count 값과 동일한지 테스트

---
+++ src/test/java/CarTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+public class CarTest {
+    Car car = new Car("test");
+    @Test
+    void makeDecisionTest(){
+        int count = 0;
+        for(int i = 0; i < 10; i++) {
+            car.makeDecision();
+            if(car.getRand_number() >= 4)
+                count++;
+        }
+        int length = car.getName().length();
+        boolean tf = length == count;
+        Assertions.assertTrue(tf);
+    }
+}
