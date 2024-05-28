e6f0848d691eb357deac5e526102d902659c309c
Kim Dayeong <gloria030114@gmail.com>
2024-05-28 15:46:31 +0900

    feat: add RacingCarApplication, add input logic in GameController class
    - add RacingCarApplication, main method(include GameController.playGame())
    - add GameController and playGame() method
    - add input logic in playGame() method

---
+++ src/main/java/controller/GameController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import java.util.List;
+import view.InputView;
+
+public class GameController {
+
+    public static void playGame() {
+
+        List<String> carNames = InputView.inputCarNames();
+        int attemptCount = InputView.inputAttemptCount();
+
+    }
+
+}
