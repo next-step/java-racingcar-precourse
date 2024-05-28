2c78436db53698c665f45f8660235b6c9326e341
anacnoda77 <sm1108shin@naver.com>
2024-05-28 23:48:07 +0900

    Test(GameTest.java): 전진 테스트
    랜덤 숫자에 따라 자동차의 전진 혹은 현상 유지 기능을 구현하기 위한 테스트 코드

---
+++ src/test/java/GameTest.java
@@ -0,0 +1,80 @@
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import controller.GameController;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import model.Car;
+import model.Cars;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import service.GameService;
+import util.RandomGenerator;
+
+public class GameTest {
+
+    GameController gameController;
+    GameService gameService;
+    Cars cars;
+
+    public GameTest() {
+        this.gameController = new GameController();
+        this.gameService = new GameService();
+    }
+
+    @BeforeEach
+    void before() {
+        Car A = new Car("aaa");
+        Car B = new Car("bbb");
+        Car C = new Car("ccc");
+
+        List<Car> list = new ArrayList<>();
+        list.add(A); list.add(B); list.add(C);
+
+        cars = new Cars(list);
+    }
+
+    @Test
+    void move() {
+        Car A = cars.getCar(0);
+        int beforeCount1 = A.getCount();
+        gameService.checkMoveOrStay(4, A);
+        Assertions.assertThat(A.getCount()).isEqualTo(beforeCount1+1);
+
+        Car B = cars.getCar(1);
+        int beforeCount2 = B.getCount();
+        gameService.checkMoveOrStay(9, B);
+        Assertions.assertThat(B.getCount()).isEqualTo(beforeCount2+1);
+
+    }
+
+    @Test
+    void stay() {
+        Car A = cars.getCar(0);
+        int beforeCount1 = A.getCount();
+        gameService.checkMoveOrStay(0, A);
+        Assertions.assertThat(A.getCount()).isEqualTo(beforeCount1);
+
+        Car B = cars.getCar(1);
+        int beforeCount2 = B.getCount();
+        gameService.checkMoveOrStay(3, B);
+        Assertions.assertThat(B.getCount()).isEqualTo(beforeCount2);
+    }
+
+    @Test
+    void randomTest() {
+        Car A = cars.getCar(0);
+        int beforeCount = A.getCount();
+        int randomNumber = RandomGenerator.createRandomNumber();
+        gameService.checkMoveOrStay(randomNumber, A);
+
+        if(randomNumber < 4) {
+            Assertions.assertThat(A.getCount()).isEqualTo(beforeCount);
+        } else {
+            Assertions.assertThat(A.getCount()).isEqualTo(beforeCount+1);
+        }
+
+    }
+
+}
