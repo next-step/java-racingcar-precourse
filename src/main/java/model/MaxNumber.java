e0ab1b14847525f6733cc7fffc5ff905a0e0072c
anacnoda77 <sm1108shin@naver.com>
2024-05-29 01:16:52 +0900

    Feat(MaxNumber.java): MaxNumber 모델 추가
    각 턴이후 가장 많이 앞으로 나아간 자동차의 count를 보관하고 있는 모델

---
+++ src/main/java/model/MaxNumber.java
@@ -0,0 +1,18 @@
+package model;
+
+public class MaxNumber {
+
+    private int maxNumber;
+
+    public MaxNumber(int initNumber) {
+        this.maxNumber = initNumber;
+    }
+
+    public int getMaxNumber() {
+        return maxNumber;
+    }
+
+    public void setMaxNumber(int maxNumber) {
+        this.maxNumber = maxNumber;
+    }
+}
