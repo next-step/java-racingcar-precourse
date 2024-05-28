418cb35170a971f0b0f3218bc4b911a7038c94ee
kanguk <lm0828@naver.com>
2024-05-28 17:14:14 +0900

    feat : Race Model 생성
    feat : Race Model 생성
    
    Model Race 생성, 각 자동차에 대해 랜덤 수에 맞게 전진시키는 moveCars 메소드, 가장 많이 전진한 우승자를 결정하는 getWinner 메소드 구현
    그리고 README 완료체크

---
+++ src/main/java/model/Race.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Race {
+    private final List<Car> cars;
+
+    public Race(List<Car> cars) {
+        this.cars = cars;
+    }
+
+    public void moveCars(RandomNumberGenerator randomNumberGenerator) {
+        for (Car car : cars) {
+            int randomValue = randomNumberGenerator.generate();
+            car.move(randomValue);
+        }
+    }
+
+    public List<Car> getWinners() {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .collect(Collectors.toList());
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
