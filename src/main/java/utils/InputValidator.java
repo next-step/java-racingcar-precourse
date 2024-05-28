e68fd2312f0b2f85620da9e5904aeb32ab176069
Kim Dayeong <gloria030114@gmail.com>
2024-05-28 14:23:43 +0900

    comment: add javadoc in InputValidator
    - add javadoc to InputValidator class
    - add javadoc to validateCarNames() method

--- src/main/java/utils/InputValidator.java
+++ src/main/java/utils/InputValidator.java
@@ -5,10 +5,19 @@ import exceptions.CarNameLengthException;
 import exceptions.DuplicatedCarNameException;
 import java.util.List;
 
+/**
+ * 입력값을 검증하는 클래스
+ */
 public class InputValidator {
 
     private static final int MAX_NAME_LENGTH = 5;
 
+    /**
+     * 자동차 이름들을 검증하는 메서드
+     *
+     * @param names 자동차 이름 리스트
+     * @throws IllegalAccessError 자동차 이름이 유효하지 않을 때 발생
+     */
     public static void validateCarNames(List<String> names) throws IllegalAccessError {
         validateCarName(names);
         validateCarCount(names);
