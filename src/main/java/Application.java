3e9300c022f9533ec3c502b56e86b39c8c0ccd0c
BaeJunho <bjh4498@knu.ac.kr>
2024-05-28 17:50:28 +0900

    feat : make entire game process including input and output
    입출력 기능을 포함한 전체 게임의 과정을 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,5 +2,30 @@ import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
+        // Make Using Objects
+        Game game = new Game();
+        Scanner sc = new Scanner(System.in);
+
+        // Make racing cars process
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String str = sc.next();
+        game.makeCars(str);
+
+        // Choose Racing sequence Count process
+        System.out.println("시도할 회수는 몇회인가요?");
+        int count = sc.nextInt();
+
+        // Print Result process
+        System.out.println("\n실행결과");
+        for(int i = 0; i < count; i++){
+            System.out.println(game.racing());
+        }
+
+        // Print Game's Winner
+        System.out.print("최종 우승자 : ");
+        String[] strings = game.chooseWinner();
+        for(int i = 0; i < strings.length-1; i++)
+            System.out.print(strings[i] + ",");
+        System.out.println(strings[strings.length-1]);
     }
 }
