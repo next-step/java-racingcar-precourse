8a8e733cb5c467595a3a61125e91f702f5eae0b1
BaeJunho <bjh4498@knu.ac.kr>
2024-05-28 18:18:10 +0900

    feat : Make error case's exception handling function
    입력에 대해 발생가능한 오류 (이름 입력의 시작과 끝이 구분자일 때, 횟수 입력의 값이 정수가 아니거나 음수일 때) 에 대한 예외 처리 기능을 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,24 +8,56 @@ public class Application {
 
         // Make racing cars process
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String str = sc.next();
+        String str;
+        // Confirm Name Error Case
+        while (true) {
+            try {
+                str = sc.nextLine();
+                if (str.charAt(0) == ',')
+                    throw new IllegalArgumentException("[ERROR] 입력의 시작이 ',' 일 수 없습니다!");
+                else if (str.charAt(str.length() - 1) == ',')
+                    throw new IllegalArgumentException("[ERROR] 입력의 끝이 ',' 일 수 없습니다!");
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+        }
         game.makeCars(str);
 
         // Choose Racing sequence Count process
         System.out.println("시도할 회수는 몇회인가요?");
-        int count = sc.nextInt();
+        int count;
+        while (true) {
+            try {
+                String temp = sc.next();
+                count = Integer.parseInt(temp);
+                if (count < 0)
+                    throw new IllegalArgumentException("[ERROR] 횟수가 음수일 수 없습니다!");
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("[Error] 정수를 입력해주세요!");
+                continue;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                continue;
+            }
+        }
 
         // Print Result process
         System.out.println("\n실행결과");
-        for(int i = 0; i < count; i++){
+        for (int i = 0; i < count; i++) {
             System.out.println(game.racing());
         }
 
         // Print Game's Winner
         System.out.print("최종 우승자 : ");
         String[] strings = game.chooseWinner();
-        for(int i = 0; i < strings.length-1; i++)
-            System.out.print(strings[i] + ",");
-        System.out.println(strings[strings.length-1]);
+        for (int i = 0; i < strings.length; i++) {
+            if(strings.length-1 == i)
+                System.out.println(strings[i]);
+            else
+                System.out.print(strings[i]);
+        }
     }
 }
