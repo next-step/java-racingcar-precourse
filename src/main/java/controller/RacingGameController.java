374bca780b728d76660d254145b246b4ec419412
SeongHun <tjdgns5506@gmail.com>
2024-05-28 11:22:03 +0900

    feat: CONTROLLER layer 구현 완료
    Model과 View를 조합

---
+++ src/main/java/controller/RacingGameController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import model.RacingGameModel;
+import view.RacingGameView;
+
+public class RacingGameController {
+
+    RacingGameView view = new RacingGameView();
+
+    public void run(){
+        RacingGameModel model = new RacingGameModel(view.getCarNames());
+        int attemptCount = view.getAttemptCount();
+        view.displayRaceResult();
+        for (int i = 0; i < attemptCount; i++) {
+            view.displayUpdatedCars(model.updateCars());
+        }
+        view.displayWinner(model.generateWinner());
+    }
+}
