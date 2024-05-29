c37074906082871200e69f9a8f4fb095c576cc75
pinkbeenyeti <rbgusdn12@gmail.com>
2024-05-29 22:26:19 +0900

    feat: 경주 라운드 횟수 안내 메시지 출력
    사용자가 레이싱 경주를 진행할 라운드를 입력하라고 안내하는 메시지를
    출력한다.

--- src/main/java/View/RacingView.java
+++ src/main/java/View/RacingView.java
@@ -6,5 +6,8 @@ public class RacingView {
     public static void printCarNameMessage() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
-    
+
+    public static void printRacingRoundMessage() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
 }
