64ac68cd17d325d36c9f6805011fc5eba3921430
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-05-30 01:04:29 +0900

    feat: add printResult

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,14 @@
+package view;
+
+import java.util.List;
+import model.Car;
+
+public class OutputView {
+
+    public static void printResult(List<Car> cars) {
+        System.out.println("실행 결과");
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+    }
+}
