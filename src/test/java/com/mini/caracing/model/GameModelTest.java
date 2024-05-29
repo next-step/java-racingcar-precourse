46bfae6ebfcc9e984a75ad0d56dac98dbe910a4e
wgrgwg <kyoeng14697@naver.com>
2024-05-29 17:05:27 +0900

    test(model): add testInitGameModel
    Add testInitGameModel to ensure that initGameModel method initializes game model member variables properly.

--- src/test/java/com/mini/caracing/model/GameModelTest.java
+++ src/test/java/com/mini/caracing/model/GameModelTest.java
@@ -11,5 +11,27 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 class GameModelTest {
-    
+
+    private GameModel gameModel;
+
+    @BeforeEach
+    public void setUp() {
+        gameModel = new GameModel();
+    }
+
+
+    @ParameterizedTest
+    @DisplayName("게임 초기 세팅 테스트")
+    @CsvSource({
+        "'pobi,woni,jun', 5",
+        "'car1,car2,car3', 3"
+    })
+    public void testInitGameModel(String carNameInput, int totalMove) {
+        List<String> carNames = Arrays.asList(carNameInput.split(","));
+        gameModel.initGameModel(carNames, totalMove);
+
+        assertThat(gameModel.getCarDistances()).hasSize(carNames.size());
+        assertThat(gameModel.getCarDistances().keySet()).containsExactlyElementsOf(carNames);
+        assertThat(gameModel.getCarDistances().values()).containsOnly(0);
+    }
 }
