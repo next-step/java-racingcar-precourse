a2b4c34dc43cb734c17b9806f6884769bcb66021
suhyeon <nucc1186@gamil.com>
2024-05-29 22:54:46 +0900

    feat: Read Car name

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -1,2 +1,13 @@
-package View;public class InputView {
+package View;
+
+import Model.Car;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class InputView {
+    private Scanner scanner = new Scanner(System.in);
+
+    String readCarName(){
+        return scanner.nextLine();
+    }
 }
