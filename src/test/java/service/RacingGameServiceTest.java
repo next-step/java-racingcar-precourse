2b7d9ac2d9d989a914edd685df278819f5a215bb
ajy4304 <ajy4304@naver.com>
2024-05-29 23:55:52 +0900

    feat(RacingGameServiceTest.testGetWinners()): getWinners 메서드 테스트 코드 추가
    리플렉션을 이용하여 private 필드인 position을 강제로 수정한 뒤 winners 리스트에 우승자 Car 객체가 담겼는지 테스트하는 코드

--- src/test/java/service/RacingGameServiceTest.java
+++ src/test/java/service/RacingGameServiceTest.java
@@ -3,6 +3,7 @@ package service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -45,5 +46,37 @@ public class RacingGameServiceTest {
         for(Car car : cars){
             assertTrue(car.getPosition() >= 0 && car.getPosition() <= 5);
         }
+    }
+
+    @Test
+    public void testGetWinners()
+        throws NoSuchFieldException, IllegalAccessException {
+        String carNames = "전남대1,전남대2,전남대3";
+        racingGameService.initCar(carNames);
+        List<Car> cars = racingGameService.getCars();
+
+        // 직접 position 을 수정하려면 리플렉션이라는 것을 사용하여 접근해야 한다고 합니다.
+        // 아직 잘 모르지만... 우선 사용해보겠습니다.
+        setPosition(cars.get(0), 2);
+        setPosition(cars.get(1), 3);
+        setPosition(cars.get(2), 3);
+
+        List<Car> winners = racingGameService.getWinners();
+        // winners 리스트 안에 2개가 들어있는지(전남대2, 전남대3이 들어있어야 함)
+        assertEquals(2, winners.size());
+        // 전남대2와 전남대3이 들어있는지
+        assertTrue(winners.stream().anyMatch(car -> car.getName().equals("전남대2")));
+        assertTrue(winners.stream().anyMatch(car -> car.getName().equals("전남대3")));
+    }
+
+    // position 을 직접 조작하기 위한 메서드
+    private void setPosition(Car car, int position) throws NoSuchFieldException, IllegalAccessException{
+        // Car 클래스에서 이름이 position 인 필드를 가져옴
+        // getDeclaredField 는 private 필드도 가져올 수 있음
+        Field positionField = car.getClass().getDeclaredField("position");
+        // private 인 position 필드를 접근 가능하게 허용
+        positionField.setAccessible(true);
+        // position 필드의 값을 설정
+        positionField.set(car, position);
     }
 }
