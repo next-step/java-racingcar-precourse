a55916006deae30db383f92eea9acf71e987c4b1
geist9110 <geist9110@gmail.com>
2024-05-29 13:30:55 +0900

    feat: Add printTryCountInputComment method (function code rc-14)

--- src/main/java/view/Print.java
+++ src/main/java/view/Print.java
@@ -4,4 +4,8 @@ public class Print {
     public static void printCarNameInputComment() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
+
+    public static void printTryCountInputComment() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
 }
