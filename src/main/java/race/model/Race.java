e5f04a648100d0c19c1f4c243ac00d338c7ec2c8
JaeBin <201924576@pusan.ac.kr>
2024-05-29 14:15:17 +0900

    feat(race): create race model

---
+++ src/main/java/race/model/Race.java
@@ -0,0 +1,13 @@
+package race.model;
+
+import java.util.List;
+
+public class Race {
+    private final List<Car> carList;
+
+    public Race(List<Car> carList) {
+        this.carList = carList;
+    }
+
+
+}
