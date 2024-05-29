128e6f845ddff72f33babdb3359ebaaf28f316a2
JaeBin <201924576@pusan.ac.kr>
2024-05-29 14:51:13 +0900

    feat(race): add proceedStep method

--- src/main/java/race/model/Race.java
+++ src/main/java/race/model/Race.java
@@ -1,6 +1,7 @@
 package race.model;
 
 import java.util.List;
+import java.util.Random;
 
 public class Race {
     private final List<Car> carList;
@@ -9,5 +10,11 @@ public class Race {
         this.carList = carList;
     }
 
+    public void proceedStep(CustomRandom random) {
+        for (Car car : carList) {
+            if (random.canMove())
+                car.moveForward();
+        }
+    }
 
 }
