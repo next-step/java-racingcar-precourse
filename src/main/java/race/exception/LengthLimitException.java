7547a90c3aab78ed9f18ddadd218a1ff0747d88c
JaeBin <201924576@pusan.ac.kr>
2024-05-29 18:36:12 +0900

    feat(exception): add length limit exception message

--- src/main/java/race/exception/LengthLimitException.java
+++ src/main/java/race/exception/LengthLimitException.java
@@ -2,4 +2,7 @@ package race.exception;
 
 public class LengthLimitException extends IllegalArgumentException {
 
+    public LengthLimitException() {
+        super("[ERROR] 입력한 자동차의 이름의 길이가 제한된 길이보다 깁니다");
+    }
 }
