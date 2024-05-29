5fcce9cbd847025fd6fdd9516940e16ba8edb6f1
suhyeon <nucc1186@gamil.com>
2024-05-29 23:44:12 +0900

    feat: Print Car Distance

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -1,5 +1,8 @@
 package View;
 
+import Model.Car;
+import java.util.ArrayList;
+
 public class OutputView {
     private static final String QUESTION_CAR_NAME = "경주할 자동차 이름을 입력하세요. (이름은 쉼표, 기준으로 구분)";
     private static final String QUESTION_ROUND = "시도할 회수는 몇 회 인가요?";
@@ -10,5 +13,15 @@ public class OutputView {
 
     public void printQuestRound(){
         System.out.println(QUESTION_ROUND);
+    }
+
+    public void printCarDistance(ArrayList<Car> cars){
+        for(Car car : cars){
+            System.out.print(car.getName() + " : ");
+            for(int i = 0; i < car.getDistance(); i++){
+                System.out.print("-");
+            }
+            System.out.println();
+        }
     }
 }
