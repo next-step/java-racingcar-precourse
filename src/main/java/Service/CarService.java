44226039003f7c5c38319f56e09be8476e23d20e
suhyeon <nucc1186@gamil.com>
2024-05-29 13:19:07 +0900

    feat: initialize CarService

---
+++ src/main/java/Service/CarService.java
@@ -0,0 +1,12 @@
+package Service;
+
+import Model.Car;
+import java.util.ArrayList;
+
+public class CarService {
+    private ArrayList<Car> cars;
+
+    public CarService(){
+        cars = new ArrayList<>();
+    }
+}
