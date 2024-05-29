56a184dbad1497c0bbaa6362c7d150d034229817
ajy4304 <ajy4304@naver.com>
2024-05-30 00:52:33 +0900

    fix(RacingGameResultView.printCarPositions()): println에서 print로 변경
    - 출력 후 줄바꿈이 되는 문제 수정

--- src/main/java/racinggame/view/RacingGameResultView.java
+++ src/main/java/racinggame/view/RacingGameResultView.java
@@ -8,9 +8,9 @@ public class RacingGameResultView {
     // Car 객체의 position 을 시각적으로(-을 이용하여) 표현하는 메서드
     public void printCarPositions(List<Car> cars){
         for (Car car : cars) {
-            System.out.println(car.getName() + ":");
+            System.out.print(car.getName() + ":");
             for (int i = 0; i < car.getPosition(); i++){
-                System.out.println("-");
+                System.out.print("-");
             }
             System.out.println();
         }
