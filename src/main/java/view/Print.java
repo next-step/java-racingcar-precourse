c3b3c0982b900cfc4d35691c06a7653206bd8936
geist9110 <geist9110@gmail.com>
2024-05-29 13:34:00 +0900

    feat: Add printCarMovement method (function code rc-15)

--- src/main/java/view/Print.java
+++ src/main/java/view/Print.java
@@ -1,5 +1,7 @@
 package view;
 
+import model.Car;
+
 public class Print {
     public static void printCarNameInputComment() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -7,5 +9,9 @@ public class Print {
 
     public static void printTryCountInputComment() {
         System.out.println("시도할 회수는 몇회인가요?");
+    }
+
+    public static void printCarMovement(Car car) {
+        System.out.println(car.getName() + " : " + "-".repeat(car.getDistance()));
     }
 }
