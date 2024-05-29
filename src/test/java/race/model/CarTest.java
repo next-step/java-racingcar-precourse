d68c513e5fcc40f8dbc21ade4c978a8a4989dba4
JaeBin <201924576@pusan.ac.kr>
2024-05-29 19:33:06 +0900

    test(car_test): test exceed name length

--- src/test/java/race/model/CarTest.java
+++ src/test/java/race/model/CarTest.java
@@ -1,13 +1,10 @@
 package race.model;
 
-import org.assertj.core.api.Assertions;
-import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import race.exception.LengthLimitException;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarTest {
     @Test
