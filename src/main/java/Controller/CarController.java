80893a92e9741472ffffa5d90ae347d46352d06f
suhyeon <nucc1186@gamil.com>
2024-05-29 23:39:30 +0900

    feat: set Cars to start game

--- src/main/java/Controller/CarController.java
+++ src/main/java/Controller/CarController.java
@@ -1,14 +1,23 @@
 package Controller;
 
-import Model.Car;
 import Service.CarService;
-import java.util.ArrayList;
+import View.InputView;
+import View.OutputView;
 
 public class CarController {
     private CarService carService;
+    private InputView inputView;
+    private OutputView outputView;
 
     public CarController() {
         this.carService = new CarService();
+        this.inputView = new InputView();
+        this.outputView = new OutputView();
     }
 
+    public void setCars(String carNames){
+        for(String carName : carNames.split(",")){
+            carService.addCar(carName);
+        }
+    }
 }
