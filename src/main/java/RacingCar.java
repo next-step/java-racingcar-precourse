68f108fb6cea7d23b484843b7a9de30cd2e78c24
kuma4188 <psb4188@gmail.com>
2024-05-30 00:51:44 +0900

    create Addcar , PrintCar and modify GetCar method

--- src/main/java/RacingCar.java
+++ src/main/java/RacingCar.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class RacingCar {
     public static void main(String[] args) {
+
+        RacingCarVO race = new RacingCarVO();
+        // create object
+        race.GetCar();
+        race.AddCar();
+        race.PrintCar();
 
     }
 }
--- src/main/java/RacingCarVO.java
+++ src/main/java/RacingCarVO.java
@@ -6,26 +6,42 @@ public class RacingCarVO {
     private int try_num;
     private int go_prob;
     private int car_size;
+    private String[] cars;
     Map<String , Integer> race = new HashMap<>();
 
-    public RacingCarVO(int try_num, int car_size) {
-        this.try_num = try_num;
-        this.car_size = car_size;
+
+    public RacingCarVO() {
+        this.try_num = 0;
+        this.car_size = 0;
         this.go_prob=4;
     }// constructor
 
 
-    public RacingCarVO(int try_num, int go_prob, int car_size) {
+    public RacingCarVO(int try_num, int go_prob) {
         this.try_num = try_num;
         this.go_prob = go_prob;
-        this.car_size = car_size;
     }// constructor overloading
 
 
-    public int GetCar(String car) {
-        int n=0;
-        race.put(car, 0);
-        return n;
+    public void GetCar() {
+        int tri = 0;
+        String input;
+        do {
+            tri = 0;
+            Scanner scan = new Scanner(System.in);
+            input = scan.nextLine();
+            cars = input.split(",");
+            for (String car : cars) {
+                tri += FindError(car);
+            }
+        } while (tri != 0);
+    }// get carname
+
+
+    public void AddCar() {
+        for (String car : cars) {
+            race.put(car,0);
+        }
     } //get a carname
 
 
@@ -34,15 +50,19 @@ public class RacingCarVO {
             if (car.length() > 5) {
                 throw new IllegalArgumentException(car + " is not allow");
             }
-            return -1; // 올바른 입력이 들어오면 루프를 종료합니다.
+            return 0; // 올바른 입력이 들어올때
         } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] : Car name must be less than 5 words");
         }
-        return 0;
-    } // find error. 
+        return 1;
+    } // find error.
 
 
-
+    public void PrintCar(){
+        for (String car : cars ){
+            System.out.println(car + " " + race.get(car));
+        }
+    }// print all cars
 
 
 
