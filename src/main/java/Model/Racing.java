2af333b68c7be4775898aca7825155ee6f71882d
pinkbeenyeti <rbgusdn12@gmail.com>
2024-05-29 23:35:26 +0900

    feat: 자동차 리스트 생성
    주어진 자동차 이름들을 토대로 자동차 객체들이 담긴 리스트를 생성한다.

--- src/main/java/Model/Racing.java
+++ src/main/java/Model/Racing.java
@@ -1,5 +1,6 @@
 package Model;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -7,7 +8,12 @@ public class Racing {
     private List<Car> carList;
     private int rounds;
 
-    public Racing(int rounds) {
+    public Racing(List<String> carNames, int rounds) {
+        carList = new ArrayList<>();
+        for(String carName : carNames) {
+            carList.add(new Car(carName, 0));
+        }
+
         ErrorDetect.roundsTest(String.valueOf(rounds));
         this.rounds = rounds;
     }
