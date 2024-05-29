a53e0fcf24dfd96d04f30c5e3975747f2f2e4fd7
suhyeon <nucc1186@gamil.com>
2024-05-29 23:51:48 +0900

    feat: print Execution message

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -7,13 +7,17 @@ import java.util.stream.Collectors;
 public class OutputView {
     private static final String QUESTION_CAR_NAME = "경주할 자동차 이름을 입력하세요. (이름은 쉼표, 기준으로 구분)";
     private static final String QUESTION_ROUND = "시도할 회수는 몇 회 인가요?";
-
+    private static final String EXECUTION_MESSAGE = "실행 결과";
     public void printQuestCarName(){
         System.out.println(QUESTION_CAR_NAME);
     }
 
     public void printQuestRound(){
         System.out.println(QUESTION_ROUND);
+    }
+
+    public void printExeResult(){
+        System.out.println(EXECUTION_MESSAGE);
     }
 
     public void printCarDistance(ArrayList<Car> cars){
