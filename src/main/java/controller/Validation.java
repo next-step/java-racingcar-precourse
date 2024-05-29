2cc84a6ddff04c86761dd29d46f61a78eaef6d5b
geist9110 <geist9110@gmail.com>
2024-05-29 11:19:38 +0900

    feat: Add isCarNameSame method (function code rc-07)
    if carNames has same name then raise IllegalArgumentExceptions

--- src/main/java/controller/Validation.java
+++ src/main/java/controller/Validation.java
@@ -1,9 +1,17 @@
 package controller;
 
+import java.util.Arrays;
+
 public class Validation {
     public void isCarNameLengthUnderFive(String carName) {
         if (carName.length() > 5) {
             throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하여야 합니다.");
+        }
+    }
+
+    public void isCarNameSame(String[] carNames) {
+        if (Arrays.stream(carNames).distinct().count() != carNames.length) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 중복되지 않아야 합니다.");
         }
     }
 }
