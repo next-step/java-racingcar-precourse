f126858a2fb71e193708170b20bd293804d9cc24
ajy4304 <ajy4304@naver.com>
2024-05-29 22:37:50 +0900

    feat(RacingGameServiceTest.testInitCar()): initCar 테스트 코드 추가
    사용자에게 입력받은 이름이 cars 리스트에 제대로 들어가는지 테스트하는 코드

--- src/test/java/service/RacingGameServiceTest.java
+++ src/test/java/service/RacingGameServiceTest.java
@@ -1,2 +1,35 @@
-package service;public class RacingGameServiceTest {
+package service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racinggame.service.RacingGameService;
+import racinggame.domain.Car;
+
+public class RacingGameServiceTest {
+
+    private RacingGameService racingGameService;
+
+    @BeforeEach
+    public void setUp() {
+        racingGameService = new RacingGameService();
+    }
+
+    @Test
+    public void testInitCar(){
+        String carNames = "전남대, 부산대, 경북대, 충남대, 강원대";
+        racingGameService.initCar(carNames);
+        List<Car> cars = racingGameService.getCars();
+
+        assertEquals(5, cars.size());
+        assertEquals("전남대", cars.get(0).getName());
+        assertEquals("부산대", cars.get(1).getName());
+        assertEquals("경북대", cars.get(2).getName());
+        assertEquals("충남대", cars.get(3).getName());
+        assertEquals("강원대", cars.get(4).getName());
+    }
+
+
 }
