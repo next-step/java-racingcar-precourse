005dbd94289285882d533e883460088ef6a96f6a
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-05-30 01:28:11 +0900

    feat: add getWinners

--- src/main/java/model/Race.java
+++ src/main/java/model/Race.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Race {
 
@@ -12,5 +13,16 @@ public class Race {
 
     public void setCars(List<Car> cars) {
         this.cars = cars;
+    }
+
+    public List<Car> getWinners() {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return cars.stream()
+            .filter(car -> car.getPosition() == maxPosition)
+            .collect(Collectors.toList());
     }
 }
