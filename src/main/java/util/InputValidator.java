bd80bea2c646415590ccbfeda33bd0a27ab040ff
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-05-30 02:49:18 +0900

    feat: 자동차 이름 유효성 검증 기능 추가

--- src/main/java/util/InputValidator.java
+++ src/main/java/util/InputValidator.java
@@ -1,6 +1,9 @@
 package util;
 
 import exception.AttemptSizeException;
+import exception.DuplicatedNameException;
+import exception.NameLengthException;
+import java.util.List;
 
 public class InputValidator {
 
@@ -15,13 +18,41 @@ public class InputValidator {
     }
 
     public static boolean checkAttemptSize(int num) {
-        if (num < 1) {
-            try {
+        try {
+            if (num < 1) {
                 throw new AttemptSizeException();
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                return false;
             }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean checkNameLength(List<String> names) {
+        try {
+            for (String name : names) {
+                if (name.length() > 5) {
+                    throw new NameLengthException();
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean checkDuplicatedName(List<String> names) {
+        long count = names.stream().distinct().count();
+
+        try {
+            if (count != names.size()) {
+                throw new DuplicatedNameException();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return false;
         }
         return true;
     }
