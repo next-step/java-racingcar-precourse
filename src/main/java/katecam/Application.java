c6a01523d6d67d90492d8e5de0bb3e80e9c0ee6d
coco3x <inferior3x@gmail.com>
2024-05-30 08:51:26 +0900

    feat: Application

---
+++ src/main/java/katecam/Application.java
@@ -0,0 +1,26 @@
+package katecam;
+
+import katecam.racingcar.adaptor.in.RacingCarController;
+import katecam.racingcar.config.AppConfig;
+
+public class Application {
+    private static final RacingCarController racingCarController = AppConfig.getRacingCarController();
+    private static final RacingCarView racingCarView = AppConfig.getRacingCarView();
+
+    public static void main(String[] args) {
+        boolean valid = false;
+        while (!valid)
+            try{
+                racingCarController.initGame(racingCarView.getCarNames(), racingCarView.getNumberToAttempt());
+                racingCarView.display("\n실행 결과");
+                valid = true;
+            }catch (IllegalArgumentException e){
+                racingCarView.display(e.getMessage());
+            }
+        do{
+            racingCarController.playTurn();
+            racingCarView.display(racingCarController.getTurnResult());
+        }while(!racingCarController.isEnded());
+        racingCarView.display(racingCarController.getTotalResult());
+    }
+}
