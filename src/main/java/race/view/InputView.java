efccec241c97e9c02d16d02cf59aa4715bd1fa9d
JaeBin <201924576@pusan.ac.kr>
2024-05-29 15:49:01 +0900

    feat(input_view): add input prompt method

--- src/main/java/race/view/InputView.java
+++ src/main/java/race/view/InputView.java
@@ -1,5 +1,14 @@
 package race.view;
 
 public class InputView {
+    private InputView() {}
 
+    // 내부 클래스가 호출될 때, Instance 가 생성된다
+    private static class SingletonHelper {
+        private static final InputView INSTANCE = new InputView();
+    }
+
+    public static InputView getInstance() {
+        return SingletonHelper.INSTANCE;
+    }
 }
