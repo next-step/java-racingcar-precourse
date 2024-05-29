690a6afa092dfc8f1f7021ab0a33543f7f3b5b28
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-05-30 00:57:13 +0900

    feat: add inputAttempt

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -10,4 +10,9 @@ public class InputView {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return scanner.nextLine();
     }
+
+    public static int inputAttempt() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+        return scanner.nextInt();
+    }
 }
