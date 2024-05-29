cadf7c06db87f261718395fae2517c1ad4776335
suhyeon <nucc1186@gamil.com>
2024-05-29 12:57:14 +0900

    feat : Car getter and setter

--- src/main/java/Model/Car.java
+++ src/main/java/Model/Car.java
@@ -1,2 +1,27 @@
-package Model;public class Car {
+package Model;
+
+public class Car {
+    private String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
 }
