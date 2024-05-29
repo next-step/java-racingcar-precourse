1b729cae90cf998d9944b0ffa53da22cbf45f08b
pinkbeenyeti <rbgusdn12@gmail.com>
2024-05-29 20:49:12 +0900

    feat: 자동차 이름 검사 기능 추가
    자동차(Car.java) 클래스에 자동차 이름을 검사하는 기능을 추가하였다.
    자동차 이름의 길이가 5자 이하인지, 공백을 포함하지 않는 지를 검사한다.
    제한 사항에 걸릴 경우 예외를 던진다.

--- src/main/java/Model/Car.java
+++ src/main/java/Model/Car.java
@@ -5,6 +5,7 @@ public class Car {
     private int positon;
 
     public Car(String carName, int positon) {
+        nameTest(carName);
         this.carName = carName;
         this.positon = positon;
     }
@@ -23,5 +24,15 @@ public class Car {
 
     public void setPositon(int positon) {
         this.positon = positon;
+    }
+
+    public void nameTest(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+        }
+
+        if (carName.contains(" ")) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 공백을 포함할 수 없습니다.");
+        }
     }
 }
