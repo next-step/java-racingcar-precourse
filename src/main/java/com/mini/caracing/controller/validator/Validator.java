80e009bd0b5b9796d83635c4b5201fd3c94d4047
wgrgwg <kyoeng14697@naver.com>
2024-05-29 09:40:15 +0900

    feat(validator): Add containsBlank() to Validator
    Add containsBlank() method to throw exception if input contains blank.

--- src/main/java/com/mini/caracing/controller/validator/Validator.java
+++ src/main/java/com/mini/caracing/controller/validator/Validator.java
@@ -6,4 +6,10 @@ public class Validator {
             throw new IllegalArgumentException("Input is blank");
         }
     }
+
+    public static void containsBlank(String input) {
+        if (input.contains(" ")) {
+            throw new IllegalArgumentException("Input contains blank");
+        }
+    }
 }
