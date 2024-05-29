945d0ddae903a6e35ab68f56942447b71085f155
ajy4304 <ajy4304@naver.com>
2024-05-29 21:06:49 +0900

    feat(RacingGameService.initCar()): 자동차 객체 생성 메서드 추가
    입력받은 자동차 이름으로 Car 객체를 생성하여 List cars에 넣는 메서드

--- src/main/java/racinggame/service/RacingGameService.java
+++ src/main/java/racinggame/service/RacingGameService.java
@@ -1,2 +1,19 @@
-package racinggame.service;public class RacingGameService {
+package racinggame.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import racinggame.domain.Car;
+
+public class RacingGameService {
+    private List<Car> cars = new ArrayList<>();
+
+    public void initCar(String carNames){
+        for (String carName : carNames.split(",")) {
+            cars.add(new Car(carName.trim()));
+        }
+    }
+
+
+
 }
