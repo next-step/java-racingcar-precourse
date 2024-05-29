4d44bbbb6b5ca4c98d9a55608fbe7a58bd311e45
pinkbeenyeti <rbgusdn12@gmail.com>
2024-05-29 23:26:38 +0900

    feat: 레이싱 라운드 설정
    레이싱 게임의 라운드를 설정하고 라운드의 값에 오류가 있는 지를 검출하는
    기능을 작성하였다.

--- src/main/java/Model/Racing.java
+++ src/main/java/Model/Racing.java
@@ -7,11 +7,8 @@ public class Racing {
     private List<Car> carList;
     private int rounds;
 
-    public void setRounds() {
-        Scanner scanner = new Scanner(System.in);
-        String roundsInput = scanner.nextLine();
-
-        ErrorDetect.roundsTest(roundsInput);
-        rounds = Integer.parseInt(roundsInput);
+    public Racing(int rounds) {
+        ErrorDetect.roundsTest(String.valueOf(rounds));
+        this.rounds = rounds;
     }
 }
