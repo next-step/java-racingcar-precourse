4fd115d65ab8b9644ecb98a75f96129f98d551b1
JaeBin <201924576@pusan.ac.kr>
2024-05-29 19:10:39 +0900

    test(custom_random): delete singleton

--- src/main/java/race/model/CustomRandom.java
+++ src/main/java/race/model/CustomRandom.java
@@ -3,16 +3,6 @@ package race.model;
 import java.util.Random;
 
 public class CustomRandom extends Random {
-    private CustomRandom() {}
-
-    private static final class SingleTonHelper {
-        private static final CustomRandom INSTANCE = new CustomRandom();
-    }
-
-    public static CustomRandom getInstance() {
-        return SingleTonHelper.INSTANCE;
-    }
-
     public boolean canMove() {
         return nextInt(10) >= 4;
     }
