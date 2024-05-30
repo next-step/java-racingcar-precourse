bb12a6a7a97417d490ca1d51eb3d3e7959ec5b4a
pswaao88 <pswaao88@gmail.com>
2024-05-31 01:17:07 +0900

    Feat: Car 클래스 작성

--- src/main/java/Model/Car.java
+++ src/main/java/Model/Car.java
@@ -2,4 +2,25 @@ package Model;
 
 public class Car {
 
+    private String name;
+    private String nowStatus;
+    private int moveCount;
+
+    public Car(String name, String nowStatus, int moveCount) {
+        this.name = name;
+        this.nowStatus = nowStatus;
+        this.moveCount = moveCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNowStatus() {
+        return nowStatus;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
 }
--- src/main/java/VIew/InputView.java
+++ src/main/java/VIew/InputView.java
@@ -1,6 +1,5 @@
 package VIew;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,10 +7,12 @@ import java.io.InputStreamReader;
 public class InputView {
 
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
     //이름을 입력받는 메소드
     public static String inputName() throws IOException {
         return br.readLine();
     }
+
     //숫자를 입력받는 메소드
     public static String inputNumberOfAttempts() throws IOException {
         return br.readLine();
