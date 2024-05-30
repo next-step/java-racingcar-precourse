5159df478085361c717e0cf1a402b649c1373f5b
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-30 21:30:02 +0900

    feat : 시도할 횟수 입력 제한 사항
    입력한 시도할 횟수가 0이하인 경우 예외가 발생하도록 작성했습니다.

--- src/main/java/Model/FindErrors.java
+++ src/main/java/Model/FindErrors.java
@@ -3,19 +3,26 @@ package Model;
 import java.util.*;
 
 public class FindErrors {
-    public void validCarNames(String input){
+
+    public void validCarNames(String input) {
         String[] carNames = input.split(",");
         Set<String> uniqueNames = new HashSet<>();
-        for(String name : carNames){
-            if(name.length() > 5 ){
+        for (String name : carNames) {
+            if (name.length() > 5) {
                 throw new IllegalArgumentException("[ERROR] 자동차의 이름은 5자 이하만 가능합니다.");
             }
-            if(name.contains(" ")){
+            if (name.contains(" ")) {
                 throw new IllegalArgumentException("[ERROR] 자동차의 이름에 띄어쓰기가 있으면 안됩니다.");
             }
-            if(!uniqueNames.add(name)){
+            if (!uniqueNames.add(name)) {
                 throw new IllegalArgumentException("[ERROR] 자동차의 이름에 중복이 있으면 안됩니다.");
             }
+        }
+    }
+
+    public void validAttempts(int n) {
+        if(n <= 0){
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 1 이상이어야 합니다.");
         }
     }
 
