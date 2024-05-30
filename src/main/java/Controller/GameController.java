3148397ce514ae483c9ee0577c14b72ec645365f
pinkbeenyeti <rbgusdn12@gmail.com>
2024-05-30 11:13:26 +0900

    feat: 게임의 실행
    사용자이 입력을 받고 Model과 View를 연결하여 게임을 실행하는 기능을
    추가혀였다.
    아직 최종 승리자를 찾는 부분은 구현하지 않았다.

---
+++ src/main/java/Controller/GameController.java
@@ -0,0 +1,30 @@
+package Controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import java.util.Scanner;
+
+import View.RacingView;
+import Model.Racing;
+
+public class GameController {
+    public void gameStart() {
+        Scanner scanner = new Scanner(System.in);
+
+        RacingView.printCarNameMessage();
+        List<String> carNames = Arrays.asList(scanner.nextLine().split(","));
+
+        RacingView.printRacingRoundMessage();
+        String rounds = scanner.nextLine();
+
+        Racing carRacing = new Racing(carNames, rounds);
+
+        RacingView.printRacingRoundResultMessage();
+        for (int i=0; i<carRacing.getRounds(); i++) {
+            carRacing.playRound();
+            RacingView.printOneRoundResult(carRacing.getCarList());
+        }
+
+    }
+}
