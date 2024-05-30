1391c465a823b375a19854434fdd988fa87ec1d9
dkswoals <a4445656@gmail.com>
2024-05-30 17:39:11 +0900

    fix: 이동 횟수 입력 양식 변경(int -> String)

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -9,9 +9,9 @@ public class InputView {
 
         return sc.nextLine();
     }
-    public int inputCount(){
+    public String inputCount(){
         System.out.println("시도할 횟수는 몇회인가요?");
-        return sc.nextInt();
+        return sc.nextLine();
     }
 
     public void printError(String message){
