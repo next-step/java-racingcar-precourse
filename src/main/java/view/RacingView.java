bc555f8ebccfeb6a6495b294a6da8f9a41598dfe
geon <kky8586696@naver.com>
2024-05-30 20:21:32 +0900

    feat(RacingView): 시도할 회수 입력받는 메서드 추가

--- src/main/java/view/RacingView.java
+++ src/main/java/view/RacingView.java
@@ -10,5 +10,9 @@ public class RacingView {
         System.out.println("경주할 자동차 이름을 입력하세요.");
         return sc.nextLine();
     }
+    public static String printInputTryNum(){
+        System.out.println("시도할 회수는 몇회인가요?");
+        return sc.nextLine();
+    }
 
 }
