e3b6fb5979e351a5218e30d151129cceeac06f6f
stopmin <geemin0@naver.com>
2024-05-30 23:15:35 +0900

    feat: ExceptionMessage Enum

---
+++ src/main/java/domain/ExceptionMessage.java
@@ -0,0 +1,16 @@
+package domain;
+
+public enum ExceptionMessage {
+    INVALID_NAME_LENGTH("이름은 1자 이상 5자 이하만 가능합니다."),
+    DUPLICATED_CAR_NAME("중복된 이름이 존재합니다.");
+
+    private final String message;
+
+    ExceptionMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
