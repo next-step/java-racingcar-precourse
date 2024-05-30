f1f1c24617b98746bc174b776b5bdd54a82b8492
pswaao88 <pswaao88@gmail.com>
2024-05-31 01:36:41 +0900

    Feat: Winner와 WinnerNumber 클래스 작성
    우승자의 전진횟수를 저장하는 WinnerNumber클래스 작성
    우승자를 저장하고 우승자를 더해주는 메소드를 Winner클래스에 작성

--- src/main/java/Model/Winner.java
+++ src/main/java/Model/Winner.java
@@ -1,5 +1,29 @@
 package Model;
 
 public class Winner {
+    private String winner;
+    private boolean firstWinner;
 
+    public Winner() {
+        this.winner = "";
+        this.firstWinner = true;
+    }
+
+    public void addWinner(String winnerCar) {
+        if (firstWinner) {//첫번째 우승자이면 그냥 더하기 연산
+            firstWinner = false;
+            addFirstWinner(winnerCar);
+            return;
+        }
+        //첫번째 이후 우승자라면 ", 우승자" 를 더함
+        addNotFirstWinner(winnerCar);
+    }
+    //단순하게 우승자 더하기
+    public void addFirstWinner(String winnerCar) {
+        winner += winnerCar;
+    }
+    //", "을 포함하여 우승자 더하기
+    public void addNotFirstWinner(String winnerCar) {
+        winner = winner + ", " + winnerCar;
+    }
 }
