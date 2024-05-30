4c9e1e58319b3ac9ffd7b2d8d5ed436b34885466
GitJIHO <jiho99322@gmail.com>
2024-05-31 00:20:52 +0900

    Race 클래스에서 car 리스트와 시도 횟수를 매개변수로 받는 생성자 기능 추가

---
+++ src/main/java/Race.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Race {
+
+    private List<Car> cars;
+    private int attempts;
+
+    public Race(List<String> carNames, int attempts) {
+        this.cars = new ArrayList<>();
+        for (String name : carNames) {
+            cars.add(new Car(name));
+        }
+        this.attempts = attempts;
+    }
+
+}
