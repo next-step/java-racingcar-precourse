cd847ab58905eb02c7cd9aeb85fdc3a02107a03c
geon <kky8586696@naver.com>
2024-05-30 20:29:41 +0900

    feat(RacingController): 차 이름을 쉼표로 구분하여 carlist에 추가하는 addCarlist 추가

--- src/main/java/controller/RacingController.java
+++ src/main/java/controller/RacingController.java
@@ -1,12 +1,34 @@
 package controller;
 
+import model.Car;
+import view.RacingView;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class RacingController {
-    private Scanner sc = new Scanner(System.in);
+    private int tryNum;
+    private List<Car> carlist ;
+
+    public RacingController(){
+        tryNum = 0;
+    }
+    public void addCarlist(String[] carNames){
+        for(String carname : carNames){
+            Car car = new Car(carname);
+            carlist.add(car);
+        }
+
+    }
 
     public void inputCarName(){
+        String inputCars = RacingView.printInputCarName();
+        String[] carNames = inputCars.split(",");
 
+    }
+    public void inputTryNum(){
+        tryNum = RacingView.printInputTryNum();
     }
 
 }
