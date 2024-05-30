d38cb94a8212c5c72c581a97783ae8cc31ab56f0
GitJIHO <jiho99322@gmail.com>
2024-05-31 01:04:15 +0900

    feat: Car 이름 중복시 오류 발생 기능 추가

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Car {
     private static final int MOVENUM = 4;
     private final String name;
@@ -13,6 +17,15 @@ public class Car {
     public void move(int randomNum){
         if(randomNum >= MOVENUM){
             position++;
+        }
+    }
+
+    public void CarDup(List<Car> cars){
+        Map<Car, Integer> elementCount = new HashMap<>();
+        for(Car element : cars){
+            if(elementCount.containsKey(element)){
+                throw new IllegalArgumentException("[ERROR] 자동차 이름은 중복될 수 없습니다.");
+            }
         }
     }
 
