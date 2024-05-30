f83480a133bb0d00e29cae7279d058483fe8a3f6
GitJIHO <jiho99322@gmail.com>
2024-05-31 00:46:34 +0900

    feat: ResultView 클래스에 최종 우승자를 출력하는 printWinners() 기능 구현

--- src/main/java/ResultView.java
+++ src/main/java/ResultView.java
@@ -9,4 +9,9 @@ public class ResultView {
         System.out.println();
     }
 
+    public static void printWinners(List<Car> winners){
+        String result = String.join(", ", winners.stream().map(Car::getName).toArray(String[]::new));
+        System.out.println("최종 우승자 : "+ result);
+    }
+
 }
