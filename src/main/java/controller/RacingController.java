049032a5909231ce5d984c4b4eb5c7158eff0b93
geon <kky8586696@naver.com>
2024-05-30 20:20:37 +0900

    feat(RacingView): 자동차 이름 입력받는 View 추가

--- src/main/java/controller/RacingController.java
+++ src/main/java/controller/RacingController.java
@@ -1,5 +1,12 @@
 package controller;
 
+import java.util.Scanner;
+
 public class RacingController {
+    private Scanner sc = new Scanner(System.in);
+
+    public void inputCarName(){
+
+    }
 
 }
--- src/main/java/view/RacingView.java
+++ src/main/java/view/RacingView.java
@@ -1,4 +1,14 @@
 package view;
 
+import java.util.Scanner;
+
 public class RacingView {
+
+    private static Scanner sc = new Scanner(System.in);
+
+    public static String printInputCarName(){
+        System.out.println("경주할 자동차 이름을 입력하세요.");
+        return sc.nextLine();
+    }
+
 }
