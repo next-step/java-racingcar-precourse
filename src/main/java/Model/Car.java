8d2600decdc640166d493a8c4f7ee6ca7b657893
pswaao88 <pswaao88@gmail.com>
2024-05-31 01:20:15 +0900

    Feat: Player 클래스 작성

--- src/main/java/Model/Car.java
+++ src/main/java/Model/Car.java
@@ -1,17 +1,17 @@
 package Model;
 
 public class Car {
-
+    //자동차의 이름, 현재의 전진 상태, 전진 횟수 변수
     private String name;
     private String nowStatus;
     private int moveCount;
-
+    //생성자로 변수 초기화
     public Car(String name, String nowStatus, int moveCount) {
         this.name = name;
         this.nowStatus = nowStatus;
         this.moveCount = moveCount;
     }
-
+    //각 변수 getter
     public String getName() {
         return name;
     }
