d19570ac17f73472bc57bf49afe54e00aa60f22a
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-31 01:02:09 +0900

    feat : Application의 main()으로 합치기
    작성한 클래스와 메서드들을 이용하여 하나의 main()에다가 합쳤습니다.

---
+++ src/main/java/Application.java
@@ -0,0 +1,26 @@
+
+import Controller.*;
+import Model.*;
+import View.*;
+import java.util.*;
+
+public class Application {
+
+    public static void main(String[] args) {
+        RacingViews views = new RacingViews();
+        InputProcessing inputProcessing = new InputProcessing();
+        MovingCars movingCars = new MovingCars(views, new Random());
+        WinnerCalculator winnerCalculator = new WinnerCalculator();
+        String carNames = inputProcessing.getValidCarNames();
+        String[] carNamesArray = carNames.split(",");
+        RacingCarsVO[] cars = new RacingCarsVO[carNamesArray.length];
+        for(int i = 0; i < carNamesArray.length; i++){
+            cars[i] = new RacingCarsVO(carNamesArray[i]);
+        }
+        int attempts = inputProcessing.getValidAttempts();
+        movingCars.movingCar(cars, attempts);
+        List<String> winners = winnerCalculator.calculateWinners(cars);
+        views.displayWinners(String.join(", ",winners));
+    }
+
+}
