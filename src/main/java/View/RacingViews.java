cdad77f31f2863d9c78b14fab169ef75dbc8973b
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-30 20:53:25 +0900

    feat : 시도할 횟수 입력하기
    '시도할 횟수는 몇 회인가요?'를 출력하고 사용자가 시도할 횟수를 입력할 수
    있게 구현했습니다.

--- src/main/java/View/RacingViews.java
+++ src/main/java/View/RacingViews.java
@@ -14,4 +14,9 @@ public class RacingViews {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
         return sc.nextLine();
     }
+
+    public int getAttempts() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        return sc.nextInt();
+    }
 }
