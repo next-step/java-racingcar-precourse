b399a632f3feffa8ba27217eab3602bb5f91cc0f
yso8296 <66588512+yso8296@users.noreply.github.com>
2024-05-30 22:54:07 +0900

    feat: 테스트 코드 추가

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -1,9 +1,15 @@
 package model;
 
+import static org.assertj.core.api.Assertions.*;
+
+import exception.NameLengthException;
+import java.util.Arrays;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import util.InputValidator;
 
 public class CarTest {
 
@@ -14,7 +20,7 @@ public class CarTest {
     public void checkGetterSetter() {
         car.setName("Pobi");
 
-        Assertions.assertThat(car.getName()).isEqualTo("Pobi");
+        assertThat(car.getName()).isEqualTo("Pobi");
     }
 
     @Test
@@ -23,6 +29,36 @@ public class CarTest {
         car.setPosition(1);
         car.move();
 
-        Assertions.assertThat(car.getPosition()).isEqualTo(2);
+        assertThat(car.getPosition()).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("자동차 이름 5자 넘어가는 경우 false 반환 확인")
+    public void checkNameLength() {
+        List<String> names = Arrays.asList("abcdef", "kakao");
+        boolean result = InputValidator.checkNameLength(names);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("자동차 이름 중복된 경우 false 반환 확인")
+    public void checkDuplicatedName() {
+        List<String> names = Arrays.asList("kakao", "kakao");
+        boolean result = InputValidator.checkDuplicatedName(names);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("시도 횟수 1보다 작은 경우 false 반환 확인")
+    public void checkAttemptSize() {
+        boolean result = InputValidator.checkAttemptSize("-1");
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    @DisplayName("시도 횟수 정수가 아닌 경우 false 반환 확인")
+    public void checkInvalidAttempt() {
+        boolean result = InputValidator.checkValidAttempt("aa");
+        assertThat(result).isFalse();
     }
 }
