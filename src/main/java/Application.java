6ff7c6ac3b8776b45ff0cfcad077224f834ce839
pinkbeenyeti <rbgusdn12@gmail.com>
2024-05-30 13:43:09 +0900

    feat: 프로그렘의 실행
    실제 프로그램을 실행시키는 Application 클래스를 추가하였다.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Application {
+import Controller.GameController;
+
+public class Application {
+    public static void main(String[] args) {
+        GameController gameController = new GameController();
+        gameController.gameStart();
+    }
 }
