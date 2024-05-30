9dcb0a9b50a5d923395450ba13f3370a21cbedb6
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-31 00:42:17 +0900

    feat : 우승자 계산
    이동한 거리를 기준으로 가장 많이 움직인 자동차의 이름을 가져옵니다.

---
+++ src/main/java/Controller/WinnerCalculator.java
@@ -0,0 +1,22 @@
+package Controller;
+
+import Model.RacingCarsVO;
+import java.util.*;
+
+public class WinnerCalculator {
+    public List<String> calculateWinners(RacingCarsVO[] cars) {
+        int maxMove = 0;
+        for(RacingCarsVO car : cars){
+            if(car.getMove() > maxMove) {
+                maxMove = car.getMove();
+            }
+        }
+        List<String> winners = new ArrayList<>();
+        for(RacingCarsVO car : cars){
+            if(car.getMove() == maxMove){
+                winners.add(car.getCarName());
+            }
+        }
+        return winners;
+    }
+}
