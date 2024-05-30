dfa4cba250fc6dd534cd5b9c269f36f4ddbdbb26
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-30 19:23:49 +0900

    feat : 자동차 이름 입력 제한 사항 추가
    자동차의 이름에 중복이 있으면 에러가 발생하는 경우도 추가했습니다. 또한 에러의 경우를 확실하게 구분하고자 경우를 나누었습니다.

--- src/main/java/Model/FindErrors.java
+++ src/main/java/Model/FindErrors.java
@@ -1,11 +1,20 @@
 package Model;
 
+import java.util.*;
+
 public class FindErrors {
     public void validCarNames(String input){
         String[] carNames = input.split(",");
+        Set<String> uniqueNames = new HashSet<>();
         for(String name : carNames){
             if(name.length() > 5 || name.contains(" ")){
-                throw new IllegalArgumentException("[ERROR] 자동차의 이름은 5자 이하만 가능합니다. 또한 자동차의 이름에 띄어쓰기가 있으면 안됩니다.");
+                throw new IllegalArgumentException("[ERROR] 자동차의 이름은 5자 이하만 가능합니다.");
+            }
+            if(name.contains(" ")){
+                throw new IllegalArgumentException("[ERROR] 자동차의 이름에 띄어쓰기가 있으면 안됩니다.");
+            }
+            if(!uniqueNames.add(name)){
+                throw new IllegalArgumentException("[ERROR] 자동차의 이름에 중복이 있으면 안됩니다.");
             }
         }
     }
