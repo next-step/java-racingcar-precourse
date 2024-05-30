99f34951d344d0da562adee2b81b2981bb02349b
canyos <4581974@naver.com>
2024-05-30 12:18:08 +0900

    input tryNum

--- src/main/java/RacingCar.java
+++ src/main/java/RacingCar.java
@@ -3,12 +3,14 @@ import java.util.*;
 public class RacingCar {
     String[] nameList;
     int[] stepList;
+    int tryNum;
 
     public RacingCar(){ }
     public void run(){
         printInputName();
         inputName();
-
+        printTryNum();
+        inputTryNum();
     }
     public void printInputName(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -21,5 +23,13 @@ public class RacingCar {
         this.stepList = new int[this.nameList.length];
     }
 
+    public void printTryNum() {
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
 
+    public void inputTryNum(){
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        this.tryNum = num;
+    }
 }
