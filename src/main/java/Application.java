60fe1e00af7861ccbc655cdaf8ae3402d8a14d58
Dalsungmin <cnww123@naver.com>
2024-05-30 09:53:54 +0900

    makeNumber, goStop 메서드 생
    전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
    난수를 자동차 수만큼 생성해서 carchance리스트를 만들어 부여한다.
    4 이상이라면 cardistance리스트에 1씩 추가해 저장해서 거리를 저장한다.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -6,13 +6,20 @@ public class Application {
     static String[] nameArray;
     static List<String> nameList = new ArrayList<String>();
     static int chance;
+    static List<Integer> arr = new ArrayList<Integer>();
+    static List<Integer> cardistance = new ArrayList<Integer>();
+
     public static void main(String[] args) {
         inputName();
-        if (vaildName() == false){
+        if (vaildName() == false){ // validName 메서드 반환값이 false면 프로그램 종료
             return;
         }
-
         inputChance();
+        makeNumber();
+        for (int i = 0; i < nameArray.length; i++) { //각각의 자동차가 얼마나 전진했는지 저장할 리스트 요소 0으로 초기화
+            cardistance.add(0);
+        }
+        goStop();
 
     }
 
@@ -43,6 +50,27 @@ public class Application {
         Scanner scan = new Scanner(System.in);
         chance = scan.nextInt();
     }
+    static void makeNumber(){ // 0 ~ 9 사이의 난수 생성
+        for (int i = 0; i < nameArray.length; i++) {
+            arr.add((int) ((Math.random() * 10000) % 10)); // 0~1사이에 난수가 생성 되므로 10000을 곱한후 10으로 나눈 나머지로 만든다
+        }
+        System.out.println(arr);
+    }
+    static void goStop(){ //난수값이 4이상인지 판별후 앞으로 전진시키는 메서드
+        List<Integer> carchance = new ArrayList<Integer>(); // 생성된 난수 값을 각각의 자동차에 부여할 리스트 생성
+        for (int i = 0; i < arr.size(); i++) {
+            carchance.add(arr.get(i));
+        }
+        System.out.println(carchance);
 
+        for (int j = 0; j < carchance.size(); j++) { // 부여받은 난수가 4이상이면 cardistance의 값 1씩 추가해서 전진
+            if (carchance.get(j) >= 4) {
+                cardistance.set(j, cardistance.get(j) + 1);
+            }
+        }
+        for (int i = 0; i < carchance.size(); i++) { // cardistance의 값만큼 "_" 반복 출력해 거리 출력
+            System.out.println(nameList.get(i)+" : "+ "_".repeat(cardistance.get(i)));
+        }
+    }
 
 }
