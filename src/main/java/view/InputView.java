ae69315a3a38be171cadcadf248feb0a2e995a54
kmj452 <111856852+kmj452@users.noreply.github.com>
2024-05-31 01:19:55 +0900

    InputView.java 구현 - 사용자 입력 처리

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+    public static String InputCar(){
+        String carname;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
+        carname = scanner.nextLine();
+        return carname;
+    }
+
+    public static int InputNumber(){
+        int number;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("시도할 회수는 몇회인가요?");
+        number = scanner.nextInt();
+        return number;
+    }
+}
