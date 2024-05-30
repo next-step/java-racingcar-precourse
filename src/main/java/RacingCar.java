0442005e398310509b2aa9f644a832bb000c45dd
canyos <4581974@naver.com>
2024-05-30 12:17:16 +0900

    input name

--- src/main/java/RacingCar.java
+++ src/main/java/RacingCar.java
@@ -1,3 +1,25 @@
+import java.util.*;
+
 public class RacingCar {
-    
+    String[] nameList;
+    int[] stepList;
+
+    public RacingCar(){ }
+    public void run(){
+        printInputName();
+        inputName();
+
+    }
+    public void printInputName(){
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public void inputName(){
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        this.nameList = text.split(",");
+        this.stepList = new int[this.nameList.length];
+    }
+
+
 }
