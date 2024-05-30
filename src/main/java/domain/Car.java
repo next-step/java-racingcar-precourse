d47a59426a555aaf11b79a93f23c9a7e84892305
stopmin <geemin0@naver.com>
2024-05-30 22:34:14 +0900

    feat: Car 도메인 생성

---
+++ src/main/java/domain/Car.java
@@ -0,0 +1,24 @@
+package domain;
+
+public class Car {
+
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void moveForward() {
+        this.position++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
