5e98ab2b2979e8a1dd141b8e5e9cf82a86d44a8a
anacnoda77 <sm1108shin@naver.com>
2024-05-31 02:40:13 +0900

    refactor(Cars.java)

--- src/main/java/model/Cars.java
+++ src/main/java/model/Cars.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
+
     private final List<Car> cars;
 
     public Cars(List<Car> cars) {
@@ -21,7 +22,7 @@ public class Cars {
     public static Cars createCars(List<String> carNames) {
         List<Car> cars = new ArrayList<>();
 
-        for (String name:carNames) {
+        for (String name : carNames) {
             cars.add(new Car(name));
         }
 
