ff786e56ef289a8c60d43de6b93c4874d2d07af2
GitJIHO <jiho99322@gmail.com>
2024-05-31 00:18:36 +0900

    feat: Car name이 5자를 초과할 시 오류 발생 기능 추가

---
+++ src/main/java/Car.java
@@ -0,0 +1,20 @@
+public class Car {
+    private static final int MOVENUM = 4;
+    private final String name;
+    private int position = 0;
+
+    public Car(String name){
+        if(name == null || name.isEmpty() || name.length() >5){
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 1자 이상 5자 이하만 가능합니다.");
+        }
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
