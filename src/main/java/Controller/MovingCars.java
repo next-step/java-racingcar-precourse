8511c7bea29feed3c44ec49a80cc609ce0041208
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-31 00:24:38 +0900

    feat : 실행 결과
    사용자가 입력한 이동 횟수만큼 반복하며, 자동차의 전진 조건을 만족하기 위해 0에서 9사이의 무작위 값을 구합니다. 또한, 움직인 결과를 toString()과 RacingViews의 displayResult를 사용하여 출력합니다.

---
+++ src/main/java/Controller/MovingCars.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import Model.RacingCarsVO;
+import View.RacingViews;
+import java.util.*;
+
+public class MovingCars {
+    private RacingViews view;
+    private Random random;
+
+    public MovingCars(RacingViews view, Random random) {
+        this.view = view;
+        this.random = random;
+    }
+
+    public void movingCar(RacingCarsVO[] cars, int attempts) {
+        for(int i = 0; i < attempts; i++){
+            for(RacingCarsVO car : cars) {
+                int move = random.nextInt(10);
+                car.setMove(move);
+                view.displayResult(car.toString());
+            }
+        }
+    }
+}
