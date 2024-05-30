c30f659fec25e9ee5b8d6c9b22094c6d838c4ac1
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-30 21:41:21 +0900

    feat : 사용자의 시도할 횟수 입력에 대한 처리
    while과 try-catch를 사용하여 사용자가 잘못된 값을 입력하면 예외를 발생시키고 다시 값을 입력할 수 있게 하였습니다. 예외는 illegalArgumentException과 InputMismatchException을 사용했습니다.

--- src/main/java/Controller/InputProcessing.java
+++ src/main/java/Controller/InputProcessing.java
@@ -2,6 +2,7 @@ package Controller;
 
 import View.RacingViews;
 import Model.FindErrors;
+import java.util.InputMismatchException;
 
 public class InputProcessing {
     public String getValidCarNames() {
@@ -13,6 +14,22 @@ public class InputProcessing {
                 findErrors.validCarNames(carNames);
                 return carNames;
             }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public int getValidAttempts(){
+        while(true){
+            try{
+                RacingViews racingViews = new RacingViews();
+                FindErrors findErrors = new FindErrors();
+                int attempts = racingViews.getAttempts();
+                findErrors.validAttempts(attempts);
+                return attempts;
+            } catch (InputMismatchException e){
+                System.out.println("[ERROR] 입력 형식이 올바르지 않습니다. 정수로 입력해야 합니다.");
+            } catch (IllegalArgumentException e){
                 System.out.println(e.getMessage());
             }
         }
