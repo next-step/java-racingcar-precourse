3d391b7d2d3e6b62e3061402f0fac3d02c6fd8f6
seojiu <seojiu5598@gmail.com>
2024-05-31 21:42:54 +0900

    feat(InputHandler): 자동차 이름 입력받기

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,8 @@
+import java.util.*;
 public class Application {
     public static void main(String[] args) {
 
+        InputHandler inputHandler= new InputHandler();//사용자 입력 받는 객체 생성
+        Vector<Racer> racers = inputHandler.getRacersFromUser();//자동자 이름 입력받는 메서드 호출
     }
 }
---
+++ src/main/java/InputHandler.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class InputHandler {
+    Scanner scanner=new Scanner(System.in);
+
+    public Vector<Racer> getRacersFromUser() {
+        while (true) {//while 문으로 오류가 발생하지 않을때까지 반복 실행 => 오류가 없으면 return 해서 반복문 종료
+            try {
+                System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+                String names = scanner.nextLine();
+                String[] splitNames = names.split(",");//,으로 나눠서 배열에 저장
+                validateNames(splitNames);////5글자 초과 또는 공백 오류 잡기
+                return createRacers(splitNames);//Racer 모아놓은 벡터 만드는 함수 호출
+
+            } catch (IllegalArgumentException e) {//오류 발생시
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    //5글자 초과 또는 공백 포함 시 오류 발생
+    private void validateNames(String[] splitNames) {
+        for (String name : splitNames) {
+            if (name.length() > 5 || name.contains(" ")) {
+                throw new IllegalArgumentException("[ERROR] 이름은 5자 이내, 공백 불가.");
+            }
+        }
+    }
+
+    //Racer 모아놓은 벡터 만들기
+    private Vector<Racer> createRacers(String[] splitNames) {
+        Vector<Racer> racers = new Vector<>(splitNames.length);
+
+        for (String name : splitNames) {
+            racers.add(new Racer(name));
+        }
+        return racers;
+    }
+
+}
