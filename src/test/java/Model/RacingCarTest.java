91fcfc624c4181d88745243b7ea8eb77519c2302
pykido <animoto1@naver.com>
2024-05-31 23:03:56 +0900

    feat(RacingCarTest.java) : 자동차 경주 게임 기능 단위 테스트

---
+++ src/test/java/Model/RacingCarTest.java
@@ -0,0 +1,42 @@
+package test.java.Model;
+
+import Model.Car;
+import Model.RandomNumber;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
+public class RacingCarTest {
+    @Test
+    @DisplayName("자동차가 전진하는지 테스트")
+    public void moveTest() {
+        // given
+        Car car = new Car("car1");
+
+        // when
+        int randNum = RandomNumber.generateRanNum();
+
+
+        if (randNum >= 4) {
+            car.move();
+            // then
+            assertEquals(1, car.getPosition());
+        }
+        if (randNum < 4) {
+            // then
+            assertEquals(0, car.getPosition());
+        }
+    }
+
+    @Test
+    @DisplayName("자동차 위치를 입력하면 문자열 형태로 반환한다.")
+    public void toUnitTest() {
+        // given
+        Car car = new Car("car1");
+        car.move();
+
+        assertEquals("-", car.getPosUnits(1));
+    }
+}
