abc4dadf9adce3928a9737de6967dfc5d5f55b93
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-31 19:48:51 +0900

    test : 우승자가 여러 명인 경우에 대한 테스트

--- src/test/java/WinnerCalculatorTest.java
+++ src/test/java/WinnerCalculatorTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.*;
 public class WinnerCalculatorTest {
 
     @Test
+    @DisplayName("우승자가 혼자인 경우에 대한 테스트")
     public void calculateWinners_Solo(){
         RacingCarsVO car1 = new RacingCarsVO("kbm");
         RacingCarsVO car2 = new RacingCarsVO("mbk");
@@ -19,5 +20,21 @@ public class WinnerCalculatorTest {
         List<String> winners = calculator.calculateWinners(cars);
         assertEquals(1,winners.size());
         assertTrue(winners.contains("kbm"));
+    }
+
+    @Test
+    @DisplayName("우승자가 여러 명인 경우에 대한 테스트")
+    public void calculateWinners_Multi(){
+        RacingCarsVO car1 = new RacingCarsVO("kbm");
+        RacingCarsVO car2 = new RacingCarsVO("mbk");
+        RacingCarsVO car3 = new RacingCarsVO("bmk");
+        car1.setMove(5);
+        car3.setMove(5);
+        WinnerCalculator calculator = new WinnerCalculator();
+        RacingCarsVO[] cars = new RacingCarsVO[]{car1,car2,car3};
+        List<String> winners = calculator.calculateWinners(cars);
+        assertEquals(2,winners.size());
+        assertTrue(winners.contains("kbm"));
+        assertTrue(winners.contains("bmk"));
     }
 }
