6bc9c14f909edf4130d4f4752348da411bdf6953
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-31 18:25:04 +0900

    test : RacingCarsVO의 객체 생성에 대한 테스트

--- src/test/java/RacingCarsVOTest.java
+++ src/test/java/RacingCarsVOTest.java
@@ -1,3 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import Model.RacingCarsVO;
+import org.junit.jupiter.api.*;
+
 public class RacingCarsVOTest {
+
+    @Test
+    @DisplayName("객체 생성에 대한 테스트")
+    public void RacinvCarsVO_InitializationTest(){
+        RacingCarsVO car = new RacingCarsVO("kbm");
+        assertEquals("kbm",car.getCarName());
+        assertEquals(0,car.getMove());
+    }
 
 }
