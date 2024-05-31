162c944d39b84b2fc0d2233516ee1746b804d5e2
pswaao88 <pswaao88@gmail.com>
2024-05-31 15:21:25 +0900

    Feat: Game클래스에 makeCars메소드 추가
    beforeCheckCarNames에서 올바르게 Car를 생성하면 true 아니면 false리턴후 종료

--- src/main/java/Controller/Game.java
+++ src/main/java/Controller/Game.java
@@ -2,6 +2,9 @@ package Controller;
 
 import Model.Car;
 import Model.Player;
+import Model.Winner;
+import Model.WinnerNumber;
+import Util.CheckNameValidity;
 import VIew.InputView;
 import java.io.IOException;
 import java.util.StringTokenizer;
@@ -9,17 +12,28 @@ import java.util.StringTokenizer;
 public class Game {
 
     private Car[] cars;
+    private String[] beforeCheckCarNames;
     private StringTokenizer carNames;
+    private Player player;
+    private Winner winner;
+    private int winnerNumber;
 
     public String InputCarsName() throws IOException {
         return InputView.inputName();
     }
 
-    public StringTokenizer separateName(String name) {
-        return new StringTokenizer(name, ",");
+    public void separateName(String name) {
+        beforeCheckCarNames = name.split(",");
     }
 
-    
-
-
+    public boolean makeCars(String[] carNames) {
+        cars = new Car[carNames.length];
+        for (int i = 0; i < cars.length; i++) {
+            if (!CheckNameValidity.checkNameValidity(carNames[i])) {
+                return false;
+            }
+            cars[i] = new Car(carNames[i], "", 0);
+        }
+        return true;
+    }
 }
