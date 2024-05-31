ec5fd950a600f95751dfb3cdb60b36774e2b9658
pswaao88 <pswaao88@gmail.com>
2024-05-31 11:43:27 +0900

    Feat: 입력된 숫자가 올바른지 확인하는 메소드 작성
    int 정수형인지체크하는 checkNumberIsInt작성
    1이상인지 체크하는 checkNumberRange작성
    위의 두메소드를 모두 확인하는 checkNumberValidity 메소드 작성

--- src/main/java/Util/CheckNumberValidity.java
+++ src/main/java/Util/CheckNumberValidity.java
@@ -2,4 +2,35 @@ package Util;
 
 public class CheckNumberValidity {
 
+    private CheckNumberValidity() {
+
+    }
+
+    //int자료형으로 변환 할 수 있는지와 1이상인 자연수인지 확인하는 메소드
+    public static boolean checkNumberValidity(String number) {
+        if (!checkNumberIsInt(number)) { //int형이 아닌경우
+            return false;
+        }
+        if (!checkNumberRange(number)) { //음수인 경우
+            return false;
+        }
+        return true; //올바른 값
+    }
+
+    //int 자료형인지 확인하는 메소드
+    private static boolean checkNumberIsInt(String number) {
+        try {
+            Integer.parseInt(number);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    //1이상인 int형 정수인지 확인하는 메소드, checkNumberIsInt이후에 쓰이므로 음수인지만 체크
+    private static boolean checkNumberRange(String number) {
+        int changedNumber = Integer.parseInt(number);
+        return 1 <= changedNumber;
+    }
+
 }
