c5f7c11bf03d872b2b97cac28bf73b22522f4155
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-05-31 18:51:19 +0900

    feat: Update RacingGame&Car Blank Error Handling

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -5,6 +5,9 @@ public class Car {
         if(name.length()>5) {
             throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
         }
+        if (name.contains(" ")) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름에 공백을 포함할 수 없습니다.");
+        }
         this.name=name;
         this.position=0;
     }
--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -43,6 +43,9 @@ public class RacingGame {
             if (name.trim().length() > 5) {
                 throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
             }
+            if (name.contains(" ")) {
+                throw new IllegalArgumentException("[ERROR] 자동차 이름에 공백을 포함할 수 없습니다.");
+            }
             if (!uniqueNames.add(trimmedName)) {
                 throw new IllegalArgumentException("[ERROR] 자동차 이름은 중복될 수 없습니다.");
             }
