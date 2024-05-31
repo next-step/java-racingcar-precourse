2673447bf767af5e5fbf95ba44c53a6e0e57c30f
geist9110 <geist9110@gmail.com>
2024-05-31 19:03:24 +0900

    test: Add car move method test

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -2,8 +2,12 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
@@ -14,5 +18,20 @@ public class CarTest {
         Car car = new Car(carName);
         assertTrue(carName.equals(car.getName()));
         assertTrue(car.getDistance() == 0);
+    }
+
+    static Stream<Arguments> provideTryCountAndCar() {
+        return Stream.of(Arguments.of(1, new Car("pobi")), Arguments.of(2, new Car("woni")),
+                Arguments.of(3, new Car("jun")), Arguments.of(4, new Car("aabbcc")));
+    }
+
+    @ParameterizedTest
+    @DisplayName("distance of car is increased by 1 when car moves")
+    @MethodSource("provideTryCountAndCar")
+    void testCarMove(Integer tryCount, Car car) {
+        for (int i = 0; i < tryCount; i++) {
+            car.move();
+        }
+        assertTrue(car.getDistance() == tryCount);
     }
 }
