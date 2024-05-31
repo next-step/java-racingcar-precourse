41774514b0cd7156ee84f16ec39f0f4be0009045
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-31 19:00:01 +0900

    test : toString()에 대한 테스트

--- src/test/java/RacingCarsVOTest.java
+++ src/test/java/RacingCarsVOTest.java
@@ -28,4 +28,14 @@ public class RacingCarsVOTest {
         car.setMove(3);
         assertEquals(0,car.getMove());
     }
+
+    @Test
+    @DisplayName("toString()에 대한 테스트")
+    public void toStringTest(){
+        RacingCarsVO car = new RacingCarsVO("kbm");
+        car.setMove(7);
+        car.setMove(2);
+        car.setMove(4);
+        assertEquals("kbm : --",car.toString());
+    }
 }
