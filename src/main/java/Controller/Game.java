4f23cb6343174387b78c5326c2f37ba9a2974334
pswaao88 <pswaao88@gmail.com>
2024-05-31 14:10:52 +0900

    Feat: Game클래스에 InputCarsName메소드 추가
    InputView를 이용해 이름을 String 으로 입력받아 리턴

--- src/main/java/Controller/Game.java
+++ src/main/java/Controller/Game.java
@@ -1,5 +1,20 @@
 package Controller;
 
+import Model.Car;
+import Model.Player;
+import VIew.InputView;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
 public class Game {
+
+    private Car[] cars;
+    private StringTokenizer carNames;
+
+    public String InputCarsName() throws IOException {
+        return InputView.inputName();
+    }
+
+
 
 }
--- src/main/java/Util/CheckNameValidity.java
+++ src/main/java/Util/CheckNameValidity.java
@@ -9,26 +9,26 @@ public class CheckNameValidity {
     }
 
     public static boolean checkNameValidity(String name) {
-        if(!checkNameLength(name)) {
+        if(checkNameLength(name)) {// 길이가 6이상
             return false;
         }
-        if(!checkEnglish(name)){
+        if(!checkEnglish(name)){// 영어가 아닌 문자 포함
             return false;
         }
-        if(checkBlank(name)){
+        if(checkBlank(name)){ // 공백 포함
             return false;
         }
         return true;
     }
-
+    //5자리 이하인지 검사
     private static boolean checkNameLength(String name) {
-        return name.length() <= 5;
+        return name.length() > 5;
     }
-
+    //영어만 포함하는지 검사
     private static boolean checkEnglish(String name) {
         return Pattern.matches("^[a-zA-z]*$",name);
     }
-
+    //공백을 포함하면 true 아니면 false
     private static boolean checkBlank(String name) {
         return name.contains(" ");
     }
