82aab20111e3d2b6029b7c233f5ba06cd33b1939
pswaao88 <pswaao88@gmail.com>
2024-05-31 13:01:13 +0900

    이름을 검사하는 클래스 작성
    이름의 길이 영어로만 이루어져있는지 공백을 포함하는지 검사하는 메소드작성

--- src/main/java/Util/CheckNameValidity.java
+++ src/main/java/Util/CheckNameValidity.java
@@ -1,5 +1,37 @@
 package Util;
 
+import java.util.regex.Pattern;
+
 public class CheckNameValidity {
+
+    private CheckNameValidity() {
+
+    }
+
+    public static boolean checkNameValidity(String name) {
+        if(!checkNameLength(name)) {
+            return false;
+        }
+        if(!checkEnglish(name)){
+            return false;
+        }
+        if(checkBlank(name)){
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean checkNameLength(String name) {
+        return name.length() <= 5;
+    }
+
+    private static boolean checkEnglish(String name) {
+        return Pattern.matches("^[a-zA-z]*$",name);
+    }
+
+    private static boolean checkBlank(String name) {
+        return name.contains(" ");
+    }
+
 
 }
