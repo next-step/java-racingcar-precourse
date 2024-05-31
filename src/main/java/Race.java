81ba6420f29debed45a36a626461ec365de71030
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-05-31 16:04:24 +0900

    feat: Update getWinner

--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -27,6 +27,16 @@ public class Race {
         System.out.println();
     }
 
+    public List<Car> getWinner(){
+        int maxPosition=cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+        return cars.stream()
+                .filter(car->car.getPosition()==maxPosition)
+                .collect(Collectors.toList());
+    }
+
     public List<Car> getCars() {
         return new ArrayList<>(cars);
     }
