bf3e9f957d656a9f470331670b6d7094ad986e10
Seunghyun Ko <chris00825@naver.com>
2024-05-31 10:53:54 +0900

    feat: Naming racing car

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,7 +1,12 @@
 import java.util.Scanner;
 
 public class Application {
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        //
+        namingCar();    // 경주 자동차 이름 입력
+    }
+    public static String[] namingCar() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return input.nextLine().split(",");
     }
 }
