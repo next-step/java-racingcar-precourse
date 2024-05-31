c0a269941a48726dffcb084603e741526e2e283b
pykido <animoto1@naver.com>
2024-05-31 23:01:16 +0900

    feat(Input.java) : 사용자 입력 기능 구현

---
+++ src/main/java/RacingCar/VIew/Input.java
@@ -0,0 +1,38 @@
+package VIew;
+
+import Model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static Controller.ValidateInput.validTryCount;
+import static Controller.ValidateInput.validaCar;
+
+public class Input {
+    public static List<Car> inputCars() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String input = scanner.nextLine();
+
+        String[] carNames = input.split(",");
+
+        List<Car> carList = new ArrayList<>();
+        for (int i = 0; i < carNames.length; i++) {
+            validaCar(carNames[i], carList);
+            carList.add(new Car(carNames[i]));
+        }
+
+        return carList;
+    }
+
+    public static int inputTryCount() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("시도할 회수는 몇회인가요?");
+
+        String input = scanner.nextLine();
+        validTryCount(input);
+
+        return Integer.parseInt(input);
+    }
+}
