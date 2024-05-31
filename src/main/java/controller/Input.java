84887c4420fcb9f0fcbac1ba8ab60794925c426a
geist9110 <geist9110@gmail.com>
2024-05-31 17:01:02 +0900

    refactor: Controll IOException in Input class

--- src/main/java/controller/Input.java
+++ src/main/java/controller/Input.java
@@ -3,20 +3,28 @@ package controller;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.List;
 
 public class Input {
 
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static List<String> carNameInput() throws IOException {
-        String carNames = br.readLine();
-        return Arrays.stream(carNames.split(",")).map(String::trim).filter(str -> !str.isEmpty())
-                .toList();
+    public static List<String> carNameInput() {
+        try {
+            List<String> carNames = List.of(br.readLine().split(","));
+            return carNames.stream().map(String::trim).filter(str -> !str.isEmpty()).toList();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 
-    public static String tryCountInput() throws IOException {
-        return br.readLine();
+    public static String tryCountInput() {
+        try {
+            return br.readLine();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
     }
 }
