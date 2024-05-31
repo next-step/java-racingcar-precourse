f49d467f61188930d63a3f9b1e6c98722dc7bdb6
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-31 17:33:53 +0900

    test : 띄어쓰기가 있는 자동차 이름 입력에 대한 테스트

--- src/test/java/FindErrorsTest.java
+++ src/test/java/FindErrorsTest.java
@@ -14,4 +14,13 @@ public class FindErrorsTest {
         });
         assertEquals("[ERROR] 자동차의 이름은 5자 이하만 가능합니다.", exception.getMessage());
     }
+
+    @Test
+    public void validCarNames_Spacing() {
+        String input = "pobi,woni,jun,co k";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            findErrors.validCarNames(input);
+        });
+        assertEquals("[ERROR] 자동차의 이름에 띄어쓰기가 있으면 안됩니다.", exception.getMessage());
+    }
 }
