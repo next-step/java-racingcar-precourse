50a81ef55eec8aa7736bcfa3eb5ffec53bf0c587
geist9110 <geist9110@gmail.com>
2024-05-31 16:32:41 +0900

    refactor: Change public method to private method without total validation method

--- src/main/java/controller/Validation.java
+++ src/main/java/controller/Validation.java
@@ -3,7 +3,7 @@ package controller;
 import java.util.List;
 
 public class Validation {
-    public void isCarNamesLengthUnderFive(List<String> carNames) {
+    private void isCarNamesLengthUnderFive(List<String> carNames) {
         carNames.stream().forEach(carName -> {
             if (carName.length() > 5) {
                 throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하여야 합니다.");
@@ -11,13 +11,13 @@ public class Validation {
         });
     }
 
-    public void isCarNameSame(List<String> carNames) {
+    private void isCarNameSame(List<String> carNames) {
         if (carNames.stream().distinct().count() != carNames.size()) {
             throw new IllegalArgumentException("[ERROR] 자동차 이름은 중복되지 않아야 합니다.");
         }
     }
 
-    public void isCarNamesEmpty(List<String> carNames) {
+    private void isCarNamesEmpty(List<String> carNames) {
         if (carNames.isEmpty()) {
             throw new IllegalArgumentException("[ERROR] 한 명 이상의 참가자가 필요합니다.");
         }
@@ -29,7 +29,7 @@ public class Validation {
         isCarNamesEmpty(carNames);
     }
 
-    public void isTryCountInteger(String tryCount) {
+    private void isTryCountInteger(String tryCount) {
         tryCount.chars().forEach(c -> {
             if (!Character.isDigit(c)) {
                 throw new NumberFormatException("[ERROR] 시도 횟수는 숫자여야 합니다.");
