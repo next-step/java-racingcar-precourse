6aeb2ddd16694a56fad4a58766491d21d10a2e79
geist9110 <geist9110@gmail.com>
2024-05-31 19:02:43 +0900

    test: Combine car name test and distance test

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -1,24 +1,18 @@
 package model;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
     @ParameterizedTest
-    @DisplayName("distance of car is 0 when car is created")
-    @ValueSource(strings = {"pobi", "crong", "honux"})
-    void testCarInitialDistance(String carName) {
+    @DisplayName("is car created with name and distance 0?")
+    @ValueSource(strings = { "pobi", "woni", "jun", "aabbcc" })
+    void testCarCreate(String carName) {
         Car car = new Car(carName);
+        assertTrue(carName.equals(car.getName()));
         assertTrue(car.getDistance() == 0);
-    }
-
-    @ParameterizedTest
-    @DisplayName("name of car is same as input")
-    @ValueSource(strings = {"pobi", "crong", "honux"})
-    void testCarInitialName(String carName) {
-        Car car = new Car(carName);
-        assertTrue(car.getName().equals(carName));
     }
 }
