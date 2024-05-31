838ef4d650d5d61c21291462c07dfa02950094a4
hyeonseo <69136047+hxunsc@users.noreply.github.com>
2024-05-31 14:46:21 +0900

    feat: Create Race

--- src/main/java/Race.java
+++ src/main/java/Race.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Race {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class Race {
+    private List<Car> cars;
+    private Random random;
+
+    public Race(List<String> carNames){
+        this.cars = carNames.stream()
+                .map(Car::new)
+                .collect(Collectors.toList());
+        this.random=new Random();
+    }
+
+    public void moveCars(){
+        for (Car car : cars){
+            car.move(random.nextInt(10));
+        }
+    }
+
+    public List<Car> getCars() {
+        return new ArrayList<>(cars);
+    }
 }
