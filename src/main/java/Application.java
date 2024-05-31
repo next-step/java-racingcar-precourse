4e0c56226915ab674484c966494ed7004eb9b8ac
Seunghyun Ko <chris00825@naver.com>
2024-05-31 10:58:11 +0900

    feat: Create forward try num method

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,10 +3,15 @@ import java.util.Scanner;
 public class Application {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        namingCar();    // 경주 자동차 이름 입력
+        String[] car = namingCar();    // 경주 자동차 이름 입력
+        int tNum = tryNum();           // 전진 시도 횟수 입력
     }
     public static String[] namingCar() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         return input.nextLine().split(",");
+    }
+    public static int tryNum() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return input.nextInt();
     }
 }
