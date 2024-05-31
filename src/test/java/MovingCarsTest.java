4f66c530221ceab03097578fcce5d8af5f92362f
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-31 21:18:34 +0900

    test : 테스트 실행 전에 필요한 초기 설정 작성 및 일관되게 수행하기 위해 사용
    초기 설정을 작성하고 Random이 아닌 CustomRandom을 사용하기 위해서 사용하였습니다.

--- src/test/java/MovingCarsTest.java
+++ src/test/java/MovingCarsTest.java
@@ -1,3 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import Model.MovingCars;
+import Model.RacingCarsVO;
+import View.RacingViews;
+import org.junit.jupiter.api.*;
+
 public class MovingCarsTest {
+
+    private MovingCars movingCars;
+    private RacingViews racingViews;
+
+    @BeforeEach
+    @DisplayName("테스트 실행 전에 필요한 초기 설정 작성 및 일관되게 수행하기 위해 사용")
+    public void setUp(){
+        racingViews = new RacingViews();
+        movingCars = new MovingCars(racingViews,new CustomRandom(5));
+    }
 
 }
