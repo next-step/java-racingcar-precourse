6c7ea7a576fbe0a86c56abd44e1c83b8fd467977
pswaao88 <pswaao88@gmail.com>
2024-05-31 10:15:27 +0900

    Feat: Util패키지의 MoveOrNot클래스 작성
    전달된 수가 0 ~ 3이면 정지 4 ~ 9면 전진하는 메소드를 작성

--- src/main/java/Util/MoveOrNot.java
+++ src/main/java/Util/MoveOrNot.java
@@ -1,5 +1,15 @@
 package Util;
 
 public class MoveOrNot {
+    private MoveOrNot(){
+
+    }
+    //4 ~ 9라면 전진이므로 true, 아니면 false 리턴
+    public static boolean checkMoveOrNot(int randomNumber){
+        if(randomNumber >= 3 && randomNumber <= 9){
+            return true;
+        }
+        return false;
+    }
 
 }
