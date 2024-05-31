dc2ed832879c0f9c1e71cf81a95408f5fc32bade
Seunghyun Ko <chris00825@naver.com>
2024-05-31 11:55:45 +0900

    feat: Create print racing board method

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,8 +4,12 @@ import java.util.Scanner;
 public class Application {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        String[] car = namingCar();    // 경주 자동차 이름 입력
-        int tNum = tryNum();           // 전진 시도 횟수 입력
+        String[] car = namingCar();         // 경주 자동차 이름 입력
+        int tNum = tryNum();                // 전진 시도 횟수 입력
+        System.out.println("\n실행 결과");
+        for(int i=0; i<tNum; i++) {         // 전진 시도 결과 출력
+            racingResult(car);
+        }
     }
     public static String[] namingCar() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -18,5 +22,12 @@ public class Application {
     public static int randNum() {
         Random rand = new Random();
         return rand.nextInt(10);
+    }
+    public static void racingResult(String[] carList) {
+        for(String car: carList) {
+            System.out.print(car + " : ");
+            System.out.println();
+        }
+        System.out.println();
     }
 }
