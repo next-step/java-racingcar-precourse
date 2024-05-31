a970bd99d83b60a95896302eeb6927f2241ddbf2
pswaao88 <pswaao88@gmail.com>
2024-05-31 15:49:07 +0900

    checkNameValidity 중복검사 메소드 추가
    HashMap을 이용해 중복을 검사해 중복이면 T 아니면 F 리턴

--- src/main/java/Util/CheckNameValidity.java
+++ src/main/java/Util/CheckNameValidity.java
@@ -1,9 +1,10 @@
 package Util;
 
+import java.util.HashMap;
 import java.util.regex.Pattern;
 
 public class CheckNameValidity {
-
+    private static HashMap<String, Boolean> sameNameHashMap = new HashMap<>();
     private CheckNameValidity() {
 
     }
@@ -12,25 +13,37 @@ public class CheckNameValidity {
         if(checkNameLength(name)) {// 길이가 6이상
             return false;
         }
-        if(!checkEnglish(name)){// 영어가 아닌 문자 포함
+        if(!checkOnlyEnglish(name)){// 영어가 아닌 문자 포함
             return false;
         }
         if(checkBlank(name)){ // 공백 포함
             return false;
         }
+        if(checkSameName(name)){
+            return false;
+        }
         return true;
     }
-    //5자리 이하인지 검사
+    //5자리를 넘는지 검사
     private static boolean checkNameLength(String name) {
         return name.length() > 5;
     }
-    //영어만 포함하는지 검사
-    private static boolean checkEnglish(String name) {
+    //영어만 포함하는지 검사 (영어만있으면 T / 다른거 포함 F)
+    private static boolean checkOnlyEnglish(String name) {
         return Pattern.matches("^[a-zA-z]*$",name);
     }
     //공백을 포함하면 true 아니면 false
     private static boolean checkBlank(String name) {
         return name.contains(" ");
+    }
+    //중복이면 true 아니면 false
+    private static boolean checkSameName(String name){
+        if(sameNameHashMap.containsKey(name)){
+            sameNameHashMap = new HashMap<>();//중복일때 초기화하지 않으면 다음 입력시에도 영향을 줄수 있어 초기화
+            return true;
+        }
+        sameNameHashMap.put(name,true);
+        return false;
     }
 
 
