585669247c1ed4f8549f29b644df7afe75dde477
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-31 19:44:29 +0900

    test : 우승자가 혼자인 경우에 대한 테스트

--- src/test/java/WinnerCalculatorTest.java
+++ src/test/java/WinnerCalculatorTest.java
@@ -1,3 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import Model.RacingCarsVO;
+import Model.WinnerCalculator;
+import java.util.*;
+import org.junit.jupiter.api.*;
+
 public class WinnerCalculatorTest {
 
+    @Test
+    public void calculateWinners_Solo(){
+        RacingCarsVO car1 = new RacingCarsVO("kbm");
+        RacingCarsVO car2 = new RacingCarsVO("mbk");
+        car1.setMove(4);
+        car1.setMove(5);
+        car2.setMove(4);
+        WinnerCalculator calculator = new WinnerCalculator();
+        RacingCarsVO[] cars = new RacingCarsVO[]{car1,car2};
+        List<String> winners = calculator.calculateWinners(cars);
+        assertEquals(1,winners.size());
+        assertTrue(winners.contains("kbm"));
+    }
 }
