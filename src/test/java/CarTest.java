d27af52a73d451b5696472845b69599a30595ac9
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-05-31 18:28:19 +0900

    refactor: Car객체내 함수 변경으로 인한 테스트 코드 수정

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -7,30 +7,31 @@ import org.junit.jupiter.api.Test;
 class CarTest {
 
     @Test
-    @DisplayName("4미만의 값이 들오올 경우")
-    void DontUpdateNum() {
+    @DisplayName("생성시 이름을 초기화")
+    void checkName() {
 //        given
 //        자동차 객체는 이름을 주면 생성자에 의해 해당이름과 0을 갖는 객체가 생성된다.
         Car car = new Car("test");
 //        when
-//        경계값
-//        updateNum의 입력으로 사용되는 값은 Util.Util.generateRandomNumber에 의해 생성되므로 입력값 검증은 따로 하지 않는다.
-        car.updateNum(3);
+
 //        then
-        assertEquals(car.getNum(), 0);
+        assertEquals(car.getName(), "test");
     }
 
     @Test
-    @DisplayName("4이상의 값이 들오올 경우")
-    void UpdateNum() {
+    @DisplayName("plusNum 실행시 num이 1 증가")
+    void plusNum() {
 //        given
 //        자동차 객체는 이름을 주면 생성자에 의해 해당이름과 0을 갖는 객체가 생성된다.
         Car car = new Car("test");
 //        when
 //        경계값
 //        updateNum의 입력으로 사용되는 값은 Util.Util.generateRandomNumber에 의해 생성되므로 입력값 검증은 따로 하지 않는다.
-        car.updateNum(4);
+        car.plusNum();
+        car.plusNum();
+        car.plusNum();
+
 //        then
-        assertEquals(car.getNum(), 1);
+        assertEquals(car.getNum(), 3);
     }
 }
