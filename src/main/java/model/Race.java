5cca19d922dd730a6f8883515a13a1540450230d
jwoo9928 <astro9928@o.cnu.ac.kr>
2024-05-31 22:06:03 +0900

    feat: race
    feat: Race 클래스 추가
    
    - 경주를 관리하는 Race 클래스 구현
    - 자동차 리스트와 시도 횟수를 생성자로 받아 초기화
    - 경주 시작 메서드 추가 (각 시도마다 자동차 이동 및 결과 출력)
    - 우승자 반환 메서드 추가 (최대 위치 값을 가진 자동차 리스트 반환)

---
+++ src/main/java/model/Race.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.util.List;
+import java.util.Random;
+
+public class Race {
+    private List<Car> cars;
+    private int tryCount;
+
+    public Race(List<Car> cars, int tryCount) {
+        this.cars = cars;
+        this.tryCount = tryCount;
+    }
+
+    public void startRace() {
+        Random random = new Random();
+        for (int i = 0; i < tryCount; i++) {
+            for (Car car : cars) {
+                car.move(random.nextInt(10));
+                System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+            }
+            System.out.println();
+        }
+    }
+
+    public List<Car> getWinners() {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .toList();
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+}
