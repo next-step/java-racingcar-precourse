9de8a8dc77148c3331dc12c35b0e50ef79e1c37e
KBM05haruin30 <wjdrbs1189@gmail.com>
2024-05-31 17:28:23 +0900

    test: 5글자가 넘는 자동차 이름 입력에 대한 테스트
    test코드를 작성하기 위해 FindErrorsTest.java를 만들었습니다. FindErrors에는 validCarNames, validAttempts 메소드가 있는데 그 중 첫 번째로 validCarNames에 대한 테스트를 작성 중입니다."

---
+++ src/test/java/FindErrorsTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import Model.FindErrors;
+import org.junit.jupiter.api.*;
+
+public class FindErrorsTest {
+    private FindErrors findErrors = new FindErrors();
+
+    @Test
+    public void validCarNames_LongNames() {
+        String input = "pobi,woni,jun,cookies";
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            findErrors.validCarNames(input);
+        });
+        assertEquals("[ERROR] 자동차의 이름은 5자 이하만 가능합니다.", exception.getMessage());
+    }
+}
