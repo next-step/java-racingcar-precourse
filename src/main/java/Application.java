40c3bb5f4681d8d1b4950244667b56239e71d2dc
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-05-31 18:10:07 +0900

    refactor: Application 구조 개선

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,15 +1,9 @@
-import Controller.RacingcarController;
-import DTO.Car;
-import Util.Util;
-import View.Print;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import Controller.PlayController;
 
 public class Application {
 
     public static void main(String[] args) {
-        RacingcarController.play();
+        PlayController.play();
     }
 
 }
