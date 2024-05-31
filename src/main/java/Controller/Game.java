a237a20a710ab6df227ddb01888074d3ad0891b9
pswaao88 <pswaao88@gmail.com>
2024-05-31 16:27:32 +0900

    Feat: Game클래스에 Player만드는 메소드 작성
    올바른 값을 입력할때까지 입력후 Player클래스에 저장

--- src/main/java/Controller/Game.java
+++ src/main/java/Controller/Game.java
@@ -5,7 +5,9 @@ import Model.Player;
 import Model.Winner;
 import Model.WinnerNumber;
 import Util.CheckNameValidity;
+import Util.CheckNumberValidity;
 import VIew.InputView;
+import VIew.OutputView;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
@@ -35,9 +37,18 @@ public class Game {
 
     public void makeCorrectCars()throws IOException{
         do {
+            OutputView.outputInputCarNameMessage();
             String names = InputView.inputName();
             separateName(names);
         }while (makeCars());
     }
 
+    public void makeCorrectPlayer()throws IOException{
+        OutputView.outputLine("시도할 회수는 몇회인가요?");
+        String numberOfAttempts = InputView.inputNumberOfAttempts();
+        while(!CheckNumberValidity.checkNumberValidity(numberOfAttempts)){
+            OutputView.outputLine("다시~~~");
+        }
+        Player player = new Player(Integer.parseInt(numberOfAttempts));
+    }
 }
