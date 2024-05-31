dab0209fbb27268198369ca89977715096477bff
pswaao88 <pswaao88@gmail.com>
2024-05-31 10:18:04 +0900

    Feat: 메인클래스인 MainGame 작성
    실질적인 동작을 하는 Application의 run메소드를 실행하는 main메소드작성

--- src/main/java/Controller/Application.java
+++ src/main/java/Controller/Application.java
@@ -1,5 +1,7 @@
 package Controller;
 
 public class Application {
+    public void run(){
 
+    }
 }
--- src/main/java/Controller/MainGame.java
+++ src/main/java/Controller/MainGame.java
@@ -2,4 +2,9 @@ package Controller;
 
 public class MainGame {
 
+    public static void main(String[] args) {
+        Application application = new Application();
+        application.run();
+    }
+
 }
