f67ed564940f43cf5ed524cd8192a54e1a55a4a3
Kim-DongHyuk <80240164+Kdonghs@users.noreply.github.com>
2024-05-31 19:03:50 +0900

    fix: import 최적화

--- src/test/java/UtilTest.java
+++ src/test/java/UtilTest.java
@@ -1,9 +1,6 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import DTO.Car;
 import Util.Util;
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
