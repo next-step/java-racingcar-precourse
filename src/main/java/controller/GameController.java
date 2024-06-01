3159870e576956914e3cdccc51c734c0f42abe7f
J-1ac <tkdwns26@naver.com>
2024-06-02 00:16:54 +0900

    feat: 게임 진행 로직 추가

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,8 +1,41 @@
 package controller;
 
+import java.util.List;
+import model.RacingCar;
+import service.GameService;
+import view.GameView;
+
 public class GameController {
 
+    private final GameService gameService = new GameService();
+    private final GameView gameView = new GameView();
+    private List<RacingCar> racingCars;
+    private int numRounds;
+
     public void startGame() {
-        //todo
+//        사용자로부터 자동차 이름 입력 받기
+        this.racingCars = gameService.getCarNames();
+//        사용자로부터 시도 횟수 입력 받기
+        this.numRounds = gameService.getRounds();
+        while (numRounds > 0) {
+//            라운드별 게임 진행
+            playGameByRound();
+            numRounds--;
+        }
+//        우승자 판단
+        List<String> winners = gameService.determineWinner();
+//        우승자 출력
+        gameView.displaywinner(winners);
+    }
+
+    private void playGameByRound() {
+        for (RacingCar racingCar : racingCars) {
+//            자동차별 0에서 9사이 무작위 난수 생성
+            gameService.generateCarRandomNum(racingCar);
+//            난수값이 4 이상인 경우 전진
+            gameService.determineCarMove(racingCar);
+        }
+//            UI 표기
+        gameView.displayCurrentRound(racingCars);
     }
 }
