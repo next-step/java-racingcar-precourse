69922ec1f3554ecaecaec7afefff6fba13837167
Seunghyun Ko <chris00825@naver.com>
2024-06-01 21:13:55 +0900

    test: Add JUnit5Test setup code

--- src/test/java/JUnit5Test.java
+++ src/test/java/JUnit5Test.java
@@ -1,3 +1,13 @@
+import java.util.ArrayList;
+import org.junit.jupiter.api.BeforeEach;
+
 public class JUnit5Test {
 
+    private ArrayList<Car> carList = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        carList.add(new Car("a"));
+        carList.add(new Car("b"));
+    }
 }
