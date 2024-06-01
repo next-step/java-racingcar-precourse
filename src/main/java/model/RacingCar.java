2818241eba6b7ee80d2498aff68c0474e4701fbe
J-1ac <tkdwns26@naver.com>
2024-06-02 00:19:20 +0900

    feat: RacingCar 모델 구현

--- src/main/java/model/RacingCar.java
+++ src/main/java/model/RacingCar.java
@@ -1,5 +1,22 @@
 package model;
 
 public class RacingCar {
+    private int currentScore;
+    private int totalScore;
 
+    public int getCurrentScore() {
+        return currentScore;
+    }
+
+    public void setCurrentScore(int currentScore) {
+        this.currentScore = currentScore;
+    }
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    public void setTotalScore(int totalScore) {
+        this.totalScore = totalScore;
+    }
 }
