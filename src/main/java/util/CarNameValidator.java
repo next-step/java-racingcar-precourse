d8c2b7895c2866f03a66e4b371219cb5e72e1125
HSHwan <tnghks2866@gmail.com>
2024-06-01 18:28:02 +0900

    fix: 자동차 이름 유효성 검사 중 no split 문자열 넘기도록 허용

--- src/main/java/util/CarNameValidator.java
+++ src/main/java/util/CarNameValidator.java
@@ -7,6 +7,12 @@ public class CarNameValidator implements InputValidator {
 
     @Override
     public void checkInputValue(String input) {
+        // 자동차 이름이 split 안된 경우 무시
+        if (input.contains(",")) {
+            return;
+        }
+
+        // 자동차 이름의 길이가 최대 길이보다 초과하면 예외 발생
         if (input.length() > CAR_NAME_MAX_LENGTH) {
             raiseIllegalArgumentException(CAR_NAME_ERROR_MESSAGE);
         }
