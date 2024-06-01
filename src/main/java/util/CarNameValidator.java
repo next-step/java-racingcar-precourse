0e003dafd9c43829749eb4cdc3225b306559c817
HSHwan <tnghks2866@gmail.com>
2024-06-01 14:15:08 +0900

    style: 유효성 검사 에러 메시지 수정

--- src/main/java/util/CarNameValidator.java
+++ src/main/java/util/CarNameValidator.java
@@ -3,7 +3,7 @@ package util;
 public class CarNameValidator implements InputValidator {
 
     private static final int CAR_NAME_MAX_LENGTH = 5;
-    private static final String CAR_NAME_ERROR_MESSAGE = "이름의 길이가 " + CAR_NAME_MAX_LENGTH + "를 초과하였습니다";
+    private static final String CAR_NAME_ERROR_MESSAGE = "자동차 이름의 길이가 " + CAR_NAME_MAX_LENGTH + "를 초과하였습니다.";
 
     @Override
     public void checkInputValue(String input) {
