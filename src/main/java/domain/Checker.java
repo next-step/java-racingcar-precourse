65204800784a7fc50a3785b2d4c32625384d148e
youcalste03 <luckthb325@naver.com>
2024-06-02 03:44:28 +0900

    feat:중복된 이름 검사 체크

--- src/main/java/domain/Checker.java
+++ src/main/java/domain/Checker.java
@@ -1,6 +1,8 @@
 package domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Checker {
     public void carNameRangeCheck(List<String> names) {  //잘못 입력시 다시 시작해야하므로 StringCar네임 하나 만들것
@@ -9,6 +11,13 @@ public class Checker {
             if(length>5||length<1){
                 throw new IllegalArgumentException("이름의 길이를 잘못 입력");
             }
+        }
+    }
+
+    public void carNameDuplicationCheck(List<String> names){
+        Set<String> set = new HashSet<>(names);
+        if(names.size()>set.size()){
+            throw new IllegalArgumentException("중복된 이름를 입력");
         }
     }
 }
