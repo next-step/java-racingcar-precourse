a60cb969e11e2381382bbd59f1da78b0e39c9653
HSHwan <tnghks2866@gmail.com>
2024-06-01 14:04:13 +0900

    feat: 자동차 이름 유효성 검사 기능 추가

---
+++ src/main/java/util/CarNameValidator.java
@@ -0,0 +1,14 @@
+package util;
+
+public class CarNameValidator implements InputValidator {
+
+    private static final int CAR_NAME_MAX_LENGTH = 5;
+    private static final String CAR_NAME_ERROR_MESSAGE = "이름의 길이가 " + CAR_NAME_MAX_LENGTH + "를 초과하였습니다";
+
+    @Override
+    public void checkInputValue(String input) {
+        if (input.length() > CAR_NAME_MAX_LENGTH) {
+            raiseIllegalArgumentException(CAR_NAME_ERROR_MESSAGE);
+        }
+    }
+}
