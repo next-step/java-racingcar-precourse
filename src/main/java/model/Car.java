0303b814cc366a4e6ddb3b7c3a02c9185f9fe027
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-06-01 20:38:11 +0900

    feat: 자동차 클래스 구현

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,2 +1,26 @@
-package model;public class Car {
+package model;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다.");
+        }
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move() {
+        this.position++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
 }
