596aa1f437a6a6bc364ed3e40644d8e237d1ffda
pswaao88 <pswaao88@gmail.com>
2024-06-01 22:44:13 +0900

    Feat: 자동차이름 테스트코드 작성

--- src/test/java/Controller/ApplicationTest.java
+++ src/test/java/Controller/ApplicationTest.java
@@ -3,6 +3,7 @@ package Controller;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import Util.CheckNameValidity;
 import java.io.IOException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,5 +19,29 @@ class ApplicationTest {
         assertThat(game.getBeforeCheckCarNames()[1]).isEqualTo("B");
         assertThat(game.getBeforeCheckCarNames()[2]).isEqualTo("C");
         assertThat(game.getBeforeCheckCarNames()[3]).isEqualTo("D");
+    }
+
+    @Test
+    @DisplayName("올바른 이름의 입력이 이루어지는지 테스트")
+    void 자동차이름입력테스트()throws IOException{
+        // 올바른 입력
+        String correctName = "pobi";
+        assertThat(CheckNameValidity.checkNameValidity(correctName)).isEqualTo(true);
+        // 공백 포함
+        String incorrectNameBlank = "po bi";
+        assertThat(CheckNameValidity.checkNameValidity(incorrectNameBlank)).isEqualTo(false);
+        // 영어로 이루어져있지 않음
+        String incorrectNameNotEnglish = "4비";
+        assertThat(CheckNameValidity.checkNameValidity(incorrectNameNotEnglish)).isEqualTo(false);
+        // 이름 중복
+        String incorrectSameName = "same,same";
+        Game game = new Game();
+        game.separateName(incorrectSameName);
+        assertThat(game.makeCars()).isEqualTo(false);
+        // 자동차 1대 일때
+        String incorrectNameJustOneCar = "pobi";
+        game = new Game();
+        game.separateName(incorrectSameName);
+        assertThat(game.makeCars()).isEqualTo(false);
     }
 }
