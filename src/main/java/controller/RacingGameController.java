802febdd74a9b694ee24b5d7ca9b56b3c73f09f9
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-06-01 20:54:27 +0900

    feat:컨트롤러 구현

--- src/main/java/controller/RacingGameController.java
+++ src/main/java/controller/RacingGameController.java
@@ -1,2 +1,21 @@
-package controller;public class RacingGameController {
+package controller;
+
+
+import model.RacingGame;
+import view.InputView;
+import view.ResultView;
+
+public class RacingGameController {
+    public static void run() {
+        String[] carNames = InputView.getCarNames();
+        int numberOfAttempts = InputView.getNumberOfAttempts();
+
+        RacingGame game = new RacingGame(carNames, numberOfAttempts);
+        game.play();
+
+        ResultView.printRace(game.getCars());
+        ResultView.printWinners(game.getWinners());
+    }
 }
+
+
