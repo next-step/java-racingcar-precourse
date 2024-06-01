26d2e658a08421a6e02d34c084249362d18e2967
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-06-01 18:59:17 +0900

    테스트 코드 작성

--- src/main/java/Controller/InputValidator.java
+++ src/main/java/Controller/InputValidator.java
@@ -5,13 +5,15 @@ import java.util.List;
 public class InputValidator {
     public static void validateCarNames(List<String> carNames) {
         for (String name : carNames) {
-            if (name.length() > 5) throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+            if (name.length() > 5) throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
 
         }
     }
 
     public static void validateTrialCount(int count) {
-        if (count <= 0) throw new IllegalArgumentException("[ERROR] 시도할 회수는 양의 정수여야 합니다.");
+        if (count <= 0) throw new IllegalArgumentException("시도할 회수는 양의 정수여야 합니다.");
 
     }
+
+
 }
