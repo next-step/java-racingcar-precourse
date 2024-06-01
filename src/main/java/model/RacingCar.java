764596b497013154b127f13455516a1e15026501
J-1ac <tkdwns26@naver.com>
2024-06-02 00:27:49 +0900

    feat: RacingCar carName 속성 추가

--- src/main/java/model/RacingCar.java
+++ src/main/java/model/RacingCar.java
@@ -4,11 +4,14 @@ public class RacingCar {
 
     private int currentScore;
     private int totalScore;
+    private final String carName;
 
-    public RacingCar(int currentScore, int totalScore) {
+    public RacingCar(int currentScore, int totalScore, String carName) {
         this.currentScore = currentScore;
         this.totalScore = totalScore;
+        this.carName = carName;
     }
+
 
     public int getCurrentScore() {
         return currentScore;
@@ -24,5 +27,9 @@ public class RacingCar {
 
     public void setTotalScore(int totalScore) {
         this.totalScore = totalScore;
+    }
+
+    public String getCarName() {
+        return carName;
     }
 }
