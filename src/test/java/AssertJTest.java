bb18976594111ac2986b5202c32f778610e3c9e3
Seunghyun Ko <chris00825@naver.com>
2024-06-01 22:32:21 +0900

    test: Add AssertJ test before each

--- src/test/java/AssertJTest.java
+++ src/test/java/AssertJTest.java
@@ -1,2 +1,17 @@
+import org.junit.jupiter.api.BeforeEach;
+
 public class AssertJTest {
+
+    @BeforeEach
+    void setUp() {
+        RacingGame.racingCar.add(new Car("c1"));
+        RacingGame.racingCar.add(new Car("c2"));
+        RacingGame.racingCar.add(new Car("c3"));
+        RacingGame.racingCar.add(new Car("c4"));
+        RacingGame.racingCar.add(new Car("c5"));
+        RacingGame.racingCar.add(new Car("c6"));
+        for(int i=0; i<10; i++)
+            RacingGame.racing();
+        RacingGame.lineUp();
+    }
 }
