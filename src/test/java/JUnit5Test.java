68fe5e42781419c3ad412dadd7fa8889dde8f968
Seunghyun Ko <chris00825@naver.com>
2024-06-01 21:17:11 +0900

    test: Add test(random number is correct range)

--- src/test/java/JUnit5Test.java
+++ src/test/java/JUnit5Test.java
@@ -1,5 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.ArrayList;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 public class JUnit5Test {
 
@@ -9,5 +13,12 @@ public class JUnit5Test {
     void setUp() {
         carList.add(new Car("a"));
         carList.add(new Car("b"));
+    }
+
+    @Test
+    @DisplayName("randNum() 메서드가 0~9 사이 값을 반환하는가?")
+    void testRandNumRange() {
+        int num = RacingGame.randNum();
+        assertTrue(num >= 0 && num <= 9);
     }
 }
