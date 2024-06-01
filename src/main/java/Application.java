25db21b9b5a6019a58404264b72ea002fccdda47
GitJIHO <jiho99322@gmail.com>
2024-06-01 17:14:50 +0900

    refactor: start() 함수 무의미한 줄바꿈 삭제

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -7,12 +7,9 @@ public class Application {
     public void start(){
         String[] carNames = InputView.getCarNames();
         Car.CarDup(Arrays.asList(carNames));
-
         int attempts = InputView.getAttempts();
-
         Race race = new Race(Arrays.asList(carNames), attempts);
         List<Car> cars = race.getCars();
-
         System.out.println("\n실행 결과");
         Random random = new Random();
         for(int i=0; i<attempts; i++){
@@ -23,7 +20,6 @@ public class Application {
         }
         ResultView.printWinners(race.getWinners());
     }
-
     public static void main(String[] args) {
         try {
             Application application = new Application();
