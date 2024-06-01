f9c48ab434654c3bb9f7392e697bf81fa4199161
pswaao88 <pswaao88@gmail.com>
2024-06-01 22:29:37 +0900

    Feat: , 구분 테스트메소드 작성

--- src/test/java/Controller/ApplicationTest.java
+++ src/test/java/Controller/ApplicationTest.java
@@ -9,10 +9,14 @@ import org.junit.jupiter.api.Test;
 
 class ApplicationTest {
     @Test
-    @DisplayName("이름이 ,로 구분이 잘되는지 테스트")
+    @DisplayName("이름이 , 로 구분이 잘되는지 테스트")
     void 문자열분리테스트()throws IOException {
         Game game = new Game();
         String name = "A,B,C,D";
-
+        game.separateName(name);
+        assertThat(game.getBeforeCheckCarNames()[0]).isEqualTo("A");
+        assertThat(game.getBeforeCheckCarNames()[1]).isEqualTo("B");
+        assertThat(game.getBeforeCheckCarNames()[2]).isEqualTo("C");
+        assertThat(game.getBeforeCheckCarNames()[3]).isEqualTo("D");
     }
 }
