9e3a54790e8b966f686c637a12b38fe24dd24cc7
HSHwan <tnghks2866@gmail.com>
2024-06-01 17:52:34 +0900

    style: 이동 시도 횟수 최솟값 상수 이름 변경

--- src/main/java/util/AttemptNumberValidator.java
+++ src/main/java/util/AttemptNumberValidator.java
@@ -2,12 +2,12 @@ package util;
 
 public class AttemptNumberValidator implements InputValidator {
 
-    private static final int INITIAL_NUMBER = 0;
+    private static final int ATTEMPT_NUMBER_MINIMUM = 0;
     private static final String ATTEMPT_NUMBER_ERROR_MESSAGE = "0 이상의 정수가 아닌 값을 입력받았습니다";
 
     @Override
     public void checkInputValue(String input) {
-        int attemptNumber = INITIAL_NUMBER;
+        int attemptNumber = ATTEMPT_NUMBER_MINIMUM;
 
         try {
             attemptNumber = Integer.parseInt(input);
@@ -15,7 +15,7 @@ public class AttemptNumberValidator implements InputValidator {
             raiseIllegalArgumentException(ATTEMPT_NUMBER_ERROR_MESSAGE);
             return;
         }
-        if (attemptNumber < 0) {
+        if (attemptNumber < ATTEMPT_NUMBER_MINIMUM) {
             raiseIllegalArgumentException(ATTEMPT_NUMBER_ERROR_MESSAGE);
         }
     }
