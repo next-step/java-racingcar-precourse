0f2b5f0301ff5047afed6035936332c582f18796
EunKyung Lee <2dmsrud2002@naver.com>
2024-06-01 12:49:30 +0900

    Feat: Game 클래스 - 자동차 이름, 시도할 횟수 입력
    자동차 이름을 쉼표로 split 되도록 입력 받고, 시도할 횟수를 숫자로 입력
    받아서 처리하는 로직을 구현함. Rule 을 통해 입력이 옳지 않은지 판단하고
    옳지 않으면 IllegalArgumentException을 발생시키며 에러 메시지를 출력하게
    됨.

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,7 +2,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
+    private Input input = new Input();
+    private Output output = new Output();
+    private Rule rule = new Rule();
     private List<Car> racingCars = new ArrayList<>();
+    private int gameCnt;
+
+    public void start() {
+        output.carNameOutput();
+        while (true) {
+            try {
+                String carNameInput = input.carNameInput();
+                rule.carNameIsNull(carNameInput);
+                rule.carNameIsOverSize(carNameInput);
+                createCars(carNameInput);
+                break;
+            } catch (IllegalArgumentException e) {
+                output.errorOutput(e.getMessage());
+            }
+        }
+        output.tryCntOutput();
+        while (true) {
+            try {
+                String tryCntInput = input.tryCntInput();
+                rule.tryCntInputValid(tryCntInput);
+                gameCnt = createCnt(tryCntInput);
+                break;
+            } catch (IllegalArgumentException e) {
+                output.errorOutput(e.getMessage());
+            }
+        }
+    }
 
     private void createCars(String carNameInput) {
         for (String carName : carNameInput.split(",")) {
