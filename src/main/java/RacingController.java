d427b29f8c5008317073f3f2100550fec1fbe2b0
Seunghyun Ko <chris00825@naver.com>
2024-06-01 20:51:38 +0900

    feat: Add function 'carVerify' in RacingController
    Check if there are duplicate car names.

--- src/main/java/RacingController.java
+++ src/main/java/RacingController.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class RacingController {
     public static void createList(String[] carList) {
         for(String car: carList)
@@ -6,9 +10,13 @@ public class RacingController {
     public static void carVerify(String[] carList) {
         if (carList.length <= 1)
             throw new IllegalStateException();
-        for (String c : carList)
+        for (String c : carList) {
             if (c.length() > 5 || c.isEmpty())
                 throw new IllegalArgumentException();
+        }
+        Set<String> set = new HashSet<>(Arrays.asList(carList));
+        if(set.size() != carList.length)
+            throw new IllegalArgumentException();
         createList(carList);
     }
     public static int tryNumVerify(String n) {
