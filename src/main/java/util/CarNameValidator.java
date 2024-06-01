50a3618d351bd1f12e81d16fb488bffc18266bc9
HSHwan <tnghks2866@gmail.com>
2024-06-01 18:41:38 +0900

    feat: 자동차 이름 리스트 유효성 검사 기능 추가

--- src/main/java/util/CarNameValidator.java
+++ src/main/java/util/CarNameValidator.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.util.List;
+
 public class CarNameValidator implements InputValidator {
 
     private static final int CAR_NAME_MAX_LENGTH = 5;
@@ -15,6 +17,12 @@ public class CarNameValidator implements InputValidator {
         // 자동차 이름의 길이가 최대 길이보다 초과하면 예외 발생
         if (input.length() > CAR_NAME_MAX_LENGTH) {
             raiseIllegalArgumentException(CAR_NAME_ERROR_MESSAGE);
+        }
+    }
+
+    public void checkCarNameList(List<String> carNameList) throws IllegalArgumentException {
+        for (String carName : carNameList) {
+            checkInputValue(carName);
         }
     }
 }
