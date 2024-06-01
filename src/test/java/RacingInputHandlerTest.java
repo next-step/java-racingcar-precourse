f103987408abfb87d463625fcf90f831da2cf679
Yongjin <yongjin5352@gmail.com>
2024-06-02 01:46:17 +0900

    test: add tests for tryCntToInt method in RacingInputHandler class

--- src/test/java/RacingInputHandlerTest.java
+++ src/test/java/RacingInputHandlerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class RacingInputHandlerTest {
     @DisplayName("splitCarNames test")
-    @ParameterizedTest(name = "{0}일 때, {3}")
+    @ParameterizedTest(name = "인자가 {0}일 때, {3}")
     @CsvSource({
             "'aaa, bbb,  ccc', false, 'aaa,bbb,ccc', '예외 발생하지 않고 올바르게 변환'",
             "' aaaaa,bbb,  ccc, d', false, 'aaaaa,bbb,ccc,d', '예외 발생하지 않고 올바르게 변환'",
@@ -19,10 +19,34 @@ public class RacingInputHandlerTest {
                         RacingInputHandler.splitCarNames(carNames);
                     })
                     .isInstanceOf(IllegalArgumentException.class);
+
             return;
         }
+
         Assertions.assertThat(RacingInputHandler.splitCarNames(carNames))
                 .isEqualTo(expected.split(","));
     }
 
+    @DisplayName("tryCntToInt test")
+    @ParameterizedTest(name = "인자가 {0}일 때, {3}")
+    @CsvSource({
+            "'1', false, 1, '예외 발생 없이 올바르게 변환'",
+            "' 0 ', false, 0, '예외 발생 없이 올바르게 변환'",
+            "'1001', true, 1001, '1000보다 크므로 예외 발생'",
+            "'d103', true, 0, '숫자만으로 이루어지지 않았으므로 예외 발생'",
+            "'-1', true, 0, '숫자만으로 이루어지지 않았으므로 예외 발생'"
+    })
+    public void tryCntToIntTest(String num, boolean exception, int expected, String testMsg) {
+        if (exception) {
+            Assertions.assertThatThrownBy(() -> {
+                RacingInputHandler.tryCntToInt(num);
+            })
+                    .isInstanceOf(IllegalArgumentException.class);
+
+            return;
+        }
+
+        Assertions.assertThat(RacingInputHandler.tryCntToInt(num))
+                .isEqualTo(expected);
+    }
 }
