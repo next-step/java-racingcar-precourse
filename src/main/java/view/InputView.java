9206659f8b6acb58890261f181de0b12924a96a6
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-06-01 20:38:58 +0900

    fix:글자수 검사 로직 삭제

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -7,14 +7,8 @@ public class InputView {
 
     public static String[] getCarNames() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
-        String[] carNames = scanner.nextLine().split(",");
 
-        for (String name : carNames) {
-            if (name.trim().length() > 5) {
-                throw new IllegalArgumentException("자동차 이름은 5자 이하여야 합니다: " + name);
-            }
-        }
-        return carNames;
+        return scanner.nextLine().split(",");
     }
 
     public static int getNumberOfAttempts() {
