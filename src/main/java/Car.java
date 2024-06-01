9682ae4d1b7edbeed4348e490e73681c2f38d40d
Seunghyun Ko <chris00825@naver.com>
2024-06-01 19:18:41 +0900

    refactor: Modify Car instance process to position feat: Add forward function

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,9 +1,21 @@
 public class Car {
     private String name;
-    private String process;
+    private String position;
 
     public Car(String name) {
         this.name = name;
-        this.process="";
+        this.position="";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPosition() {
+        return this.position;
+    }
+
+    public void forward() {
+        this.position += "-";
     }
 }
