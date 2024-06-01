537b7da07a582f975f51b4bcbcd4f110b5972b51
doubleclip118 <rmrmrmrm12@pusan.ac.kr>
2024-06-01 21:03:06 +0900

    feat:게임 테스트코드

--- src/test/java/RacingGameTest.java
+++ src/test/java/RacingGameTest.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class RacingGameTest {
+import org.junit.jupiter.api.Test;
+import model.Car;
+import model.RacingGame;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RacingGameTest {
+    @Test
+    public void playGame() {
+        String[] names = {"tesla", "bmw", "benz"};
+        RacingGame game = new RacingGame(names, 5);
+        game.play();
+        List<Car> cars = game.getCars();
+
+        for (Car car : cars) {
+            assertThat(car.getPosition()).isBetween(0, 5);
+        }
+    }
 }
