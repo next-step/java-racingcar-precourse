8f54880659c2a6d734eb9db7502fc10e2bd63ce0
UiChan Lee <junglkls1234@gmail.com>
2024-06-02 22:06:52 +0900

    Feat: "자동차 목록 입력 받기"

--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -1,2 +1,14 @@
+import java.util.*;
 public class RacingGame {
+    Scanner scanner = new Scanner(System.in);
+    private List<Car> carList;
+    private void makeCarList() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String[] carNames = scanner.nextLine().split(",");
+        carList = new ArrayList<>();
+        for (String name : carNames) {
+            carList.add(new Car(name.trim()));
+        }
+    }
+
 }
