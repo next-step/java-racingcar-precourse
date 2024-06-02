759bb5e563ac4fff4bf90f09bfe6c6430ff0d66b
rbm0524 <rbm0524@naver.com>
2024-06-03 08:09:57 +0900

    feat(InputHandler, InputValidator, OutputHandler) : Google Java Style Guide 적용

--- src/main/java/InputValidator.java
+++ src/main/java/InputValidator.java
@@ -4,10 +4,11 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class InputValidator {
-    public static boolean validateCarName(Vector<Car> cars) throws IllegalArgumentException {
-        List<Car> invalidCar =  cars.stream().filter(Car -> Car.getName().length() > 5).toList();
 
-        if(invalidCar.size() > 0) {
+    public static boolean validateCarName(Vector<Car> cars) throws IllegalArgumentException {
+        List<Car> invalidCar = cars.stream().filter(Car -> Car.getName().length() > 5).toList();
+
+        if (invalidCar.size() > 0) {
             cars.clear();
             throw new IllegalArgumentException("[ERROR] : 자동차 이름의 글자 수는 5글자 이하만 가능합니다.");
         }
@@ -15,7 +16,7 @@ public class InputValidator {
     }
 
     public static boolean validateAttemptCount(int number) throws IllegalArgumentException {
-        if(number < 0) {
+        if (number < 0) {
             throw new IllegalArgumentException("[ERROR] : 시도할 횟수는 양수만 가능합니다.");
         }
         return true;
