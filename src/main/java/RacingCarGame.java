3c0c887a8aa04d84f926ac2650361040223ccdc4
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-06-03 00:32:59 +0900

    feat: RacingCarGame validName method 입력이 올바른지 판단하는 메서드

--- src/main/java/RacingCarGame.java
+++ src/main/java/RacingCarGame.java
@@ -1,9 +1,22 @@
 import java.util.*;
 
 public class RacingCarGame {
+    private List<RacingCar> carList = new ArrayList<>();
 
     public void errorMessage(){
         throw new IllegalArgumentException("[ERROR] 잘못된 값을 입력했습니다. 다시 입력해주세요.");
+    }
+
+    public boolean validName(String name){
+        boolean state = true;
+        if(name.length() <= 5) {
+            carList.add(new RacingCar(name));
+        }
+        if(name.length() > 5) {
+            state = false;
+            errorMessage();
+        }
+        return state;
     }
 
 
