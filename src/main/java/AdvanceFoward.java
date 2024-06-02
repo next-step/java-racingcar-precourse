711c1a882c4e30737bf968a55aaf08ef8176ff76
rbm0524 <rbm0524@naver.com>
2024-06-02 23:28:25 +0900

    fix(ApplicationFoward) : 이중 반복문을 단일 반복문으로 수정, 출력 메서드 호출하지 않도록 변경

--- src/main/java/AdvanceFoward.java
+++ src/main/java/AdvanceFoward.java
@@ -3,14 +3,9 @@ import java.util.Vector;
 
 public class AdvanceFoward {
     public static void advanceFoward(Vector<Car> cars, int attemptnumber) {
-        for(int i = 0; i < attemptnumber; i++) {
-            for(int j = 0; j < cars.size(); j++) {
+            for(int i = 0; i < cars.size(); i++) {
                 Random random = new Random();
                 cars.stream().filter(c -> random.nextInt() % 10 >= 4).forEach(c -> c.increaseNumber());
             }
-            OutputHandler.fowardOutput(cars);
-        }
-
     }
-
 }
