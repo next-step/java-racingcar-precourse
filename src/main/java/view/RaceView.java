cc46845bc5b6ad46dd2150b8542c3b2f0407054b
kangwlgns <kangji0615@gmail.com>
2024-06-03 04:08:48 +0900

    feat(view): add RaceView

---
+++ src/main/java/view/RaceView.java
@@ -0,0 +1,40 @@
+package view;
+
+import controller.RaceControllerInterface;
+import observer.Observer;
+import service.RaceServiceInterface;
+
+public class RaceView implements Observer, RaceViewInterface {
+    private RaceServiceInterface raceServiceInterface;
+    private RaceControllerInterface raceControllerInterface;
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void userCarsInput() {
+
+    }
+
+    @Override
+    public void userRoundsInput() {
+
+    }
+
+    @Override
+    public void printProgress() {
+
+    }
+
+    @Override
+    public void printProcess() {
+
+    }
+
+    @Override
+    public void printResult() {
+
+    }
+}
