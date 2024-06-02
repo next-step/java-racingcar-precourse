870ca64cfa9781c5761721f0fa652dd1319e3b46
Giheon Do <dgh0001@naver.com>
2024-06-02 19:46:28 +0900

    Feat: playCountInput 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -2,8 +2,6 @@ package view;
 
 import controller.ValidateInput;
 import java.util.Scanner;
-import javax.smartcardio.Card;
-import model.CarDTO;
 import model.GameStatusDTO;
 import util.CarUtility;
 
@@ -12,21 +10,23 @@ public class InputView {
   private final Scanner scanner;
   private final GameStatusDTO gameStatusDTO;
   private final ValidateInput validateInput;
+  private final CarUtility carUtility;
 
   public InputView(Scanner scanner) {
     this.scanner = scanner;
     validateInput = new ValidateInput();
     gameStatusDTO = new GameStatusDTO();
+    carUtility = new CarUtility();
   }
 
   public void InputInit() {
     CarInput();
+    PlayCountInput();
   }
 
   public void CarInput() {
     String[] carNames;
     boolean carInputState;
-    CarUtility carUtility = new CarUtility();
     do {
       System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
       carNames = carUtility.carNamesSplit(scanner.nextLine());
@@ -38,6 +38,13 @@ public class InputView {
   }
 
   public void PlayCountInput() {
-
+    String playCount;
+    boolean playCountInputState;
+    do {
+      System.out.println("시도할 회수는 몇 회인가요?");
+      playCount = scanner.nextLine();
+      playCountInputState = validateInput.playCountValidate(playCount);
+    } while (!playCountInputState);
+    gameStatusDTO.setPlayCount(Integer.parseInt(playCount));
   }
 }
