ac67987245a1084c91538e6013995b1b2b8adac3
kangwlgns <kangji0615@gmail.com>
2024-06-03 03:26:19 +0900

    feat(RaceModelInterface): remove numberOfRounds setter

--- src/main/java/model/RaceModelInterface.java
+++ src/main/java/model/RaceModelInterface.java
@@ -6,6 +6,5 @@ import service.CarServiceInterface;
 public interface RaceModelInterface {
     public int getNumberOfCars();
     public int getNumberOfRounds();
-    public void setNumberOfRounds();
     public ArrayList<CarServiceInterface> getCars();
 }
