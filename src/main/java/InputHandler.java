52ee2d80b81e9babf618f5ff367fcc520210ee44
rbm0524 <rbm0524@naver.com>
2024-06-02 21:29:37 +0900

    Refactor(InputHandler) : 자동차 이름, 시도 횟수를 입력하는 메서드를 분할

--- src/main/java/InputHandler.java
+++ src/main/java/InputHandler.java
@@ -1,18 +1,26 @@
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Scanner;
 import java.util.Vector;
 
 public class InputHandler {
-    public static int input(Vector<Car> cars) throws IOException {
+    public static void CarNameInput(Vector<Car> cars) throws IOException{
         //사용자 입력 받기
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] s = br.readLine().split(",");
+
         for(int i = 0; i < s.length; i++) {
             cars.add(new Car(s[i].trim(), 0));
         }
 
+    }
+
+    public static int attemptCountInput() throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("시도할 횟수는 몇회인가요?");
         int number = Integer.parseInt(br.readLine());
         return number;
