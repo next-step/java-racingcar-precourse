100e29816bec04a8bf5f9d8dc34f7e4a7ef7e3ea
rbm0524 <rbm0524@naver.com>
2024-06-02 17:28:17 +0900

    Comment(Application) : 로직 작성

---
+++ src/main/java/Application.java
@@ -0,0 +1,17 @@
+import static java.lang.Integer.parseInt;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class Application {
+
+    public static void main(String[] args) throws IOException{
+        //사용자 입력 받기
+        // 입력값 유효성 검사
+        //전진하기
+        //우승자 선정
+        //우승자 출력
+
+    }
+
+}
