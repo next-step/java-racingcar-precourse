5ea57ae5e7c657fd4a3782b220837f095416b726
kangwlgns <kangji0615@gmail.com>
2024-06-03 03:01:43 +0900

    feat(CarModelInterface): fix a parameter of setProgress

--- src/main/java/model/CarModelInterface.java
+++ src/main/java/model/CarModelInterface.java
@@ -3,5 +3,5 @@ package model;
 public interface CarModelInterface {
     public String getName();
     public int getProgress();
-    public void setProgress();
+    public void setProgress(int progress);
 }
