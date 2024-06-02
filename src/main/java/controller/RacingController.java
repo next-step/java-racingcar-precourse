98e992ba433d53cd50758884805a84c7c9824193
youcalste03 <luckthb325@naver.com>
2024-06-02 13:14:33 +0900

    feat: RacingController 입력 구현

--- src/main/java/controller/RacingController.java
+++ src/main/java/controller/RacingController.java
@@ -9,6 +9,40 @@ import utils.Checker;
 import view.RacingView;
 
 public class RacingController {
+    private final Racing racing;
+    private final RacingView view;
+    private final Checker checker;
+    public RacingController(){
+        racing=new Racing();
+        view=new RacingView();
+        checker=new Checker();
+    }
+    public void run(){
+        boolean can=false;
+        view.startRacing();
+        while(!can){
+            can=input();
+        }
+        int n=view.inputNumber();
+        for(int i=0; i<n; i++){
+            racing.round();
+            racing.createRoundResult();
+        }
+
+    }
+    public boolean input(){
+        boolean can=true;
+        try{
+            String names=view.inputNames();
+            List<String> carNames=checker.checkCarName(names);
+            racing.makeCars(carNames);
+        }
+        catch(IllegalArgumentException e){
+            System.out.println(e.getMessage());
+            can=false;
+        }
+        return can;
+    }
 
 
 }
