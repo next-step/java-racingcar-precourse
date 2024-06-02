87536e1c716934d4bc2d9482f5e5624dcad583f1
ariimo <159447466+ariimo@users.noreply.github.com>
2024-06-02 19:56:17 +0900

    Feat : 글자수 예외 처리
    5글자 이상이거나 1글자 이하일 때 다시 입력받는다.

--- src/main/java/Set.java
+++ src/main/java/Set.java
@@ -6,17 +6,46 @@ public class Set {
 
     public static List<Car> setCar() {
         List<Car> cars = new ArrayList<>();
-        String[] names;
         Scanner sc = new Scanner(System.in);
-        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
-        names = sc.nextLine().split(",");
-        for(int i=0; i<names.length;i++) {
-            cars.add(new Car(names[i].trim()));
-//            System.out.println("입력 완료.");
-        }
+        while (true) {
+            System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+            String input = sc.nextLine();
+            String[] names = input.split(",");
 
+            try {
+                validateCarName(names);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+                cars.clear();
+                continue;
+            }
+            for (String name : names) {
+                cars.add(new Car(name.trim()));
+            }
+            break;
+        }
         return cars;
     }
+
+    private static void validateCarName(String[] names){
+        for (String name : names) {
+            validateCarName(name.trim());
+
+        }
+    }
+
+
+    private static void validateCarName(String name) {
+
+        if (name.length() < 1) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 1자 이상만 가능합니다.");
+        }
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하만 가능합니다.");
+        }
+        
+    }
+
 
     public static int setAttempt(){
         int attempt;
