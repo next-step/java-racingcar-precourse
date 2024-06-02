4759ff5b37c05b6b242b78f760b0eeb0be5a2e04
rbm0524 <rbm0524@naver.com>
2024-06-02 22:53:36 +0900

    feat(Application) : do-while문에 try-catch 추가, 전진하기, 우승자 출력하는 로직 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -8,29 +8,35 @@ public class Application {
 
     public static void main(String[] args) throws IOException{
         Vector<Car> cars = new Vector<Car>();
-        boolean carnamevalid;
-        boolean attemptcountvalid;
+        boolean carnamevalid = false;
+        boolean attemptcountvalid = false;
         int attemptnumber = 0;
 
         //사용자 입력 받기
         do {
-            InputHandler.CarNameInput(cars);
-            carnamevalid = InputValidator.validateCarName(cars); //유효성 검사
+            try {
+                InputHandler.CarNameInput(cars);
+                carnamevalid = InputValidator.validateCarName(cars); //유효성 검사
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+
         }while(!carnamevalid);
 
         do {
-            attemptnumber = InputHandler.attemptCountInput();
-            attemptcountvalid = InputValidator.validateAttemptCount(attemptnumber); //유효성 검사
+            try {
+                attemptnumber = InputHandler.attemptCountInput();
+                attemptcountvalid = InputValidator.validateAttemptCount(attemptnumber); //유효성 검사
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
         }while(!attemptcountvalid);
 
         //전진하기
-        AdvanceFoward.advanceFoward(cars);
-
-        //우승자 선정
-
+        AdvanceFoward.advanceFoward(cars, attemptnumber);
 
         //우승자 출력
-
+        OutputHandler.winnerOutput(cars);
     }
 
 }
