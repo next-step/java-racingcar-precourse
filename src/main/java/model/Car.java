759b54ee71d41cc707b55c83f1a3a6b363403f70
seoyoung-dev <seoyoung.dev@gmail.com>
2024-06-03 03:07:25 +0900

    feat: Create Car class

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,2 +1,34 @@
-package model;public class Car {
+package model;
+
+public class Car {
+    private final String name;
+    private int distance;
+    private String progressLine;
+
+    public Car (String name) {
+        this.name = name;
+        this.distance = 0;
+        this.progressLine = "";
+    }
+
+    public void moveForward() {
+        this.distance++;
+        this.progressLine += "-";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public String getProgressLine() {
+        return progressLine;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
 }
