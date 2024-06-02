24f86269ab0db1c91754686d1d43f13f32ad5156
J-1ac <tkdwns26@naver.com>
2024-06-02 19:53:58 +0900

    feat: 자동차 이름 입력받기 구현

--- src/main/java/service/GameService.java
+++ src/main/java/service/GameService.java
@@ -1,5 +1,49 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import model.RacingCar;
+
 public class GameService {
 
+    Scanner scanner = new Scanner(System.in);
+
+    public List<RacingCar> getCarNames() {
+        String[] carNames = new String[0];
+        boolean isVallidCarNamesFlag = false;
+
+        while (!isVallidCarNamesFlag) {
+            try {
+                System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)");
+                String userInput = scanner.nextLine();
+                carNames = userInput.split(",");
+                isVallidCarNamesFlag = isValidCarNames(carNames);
+            } catch (IllegalArgumentException e) {
+                System.out.println("ERROR" + e);
+            }
+        }
+
+        return createCarList(carNames);
+    }
+
+    private List<RacingCar> createCarList(String[] carNames) {
+        List<RacingCar> carList = new ArrayList<>();
+
+        for (String carName : carNames) {
+            RacingCar racingCar = new RacingCar(0, 0, carName);
+            carList.add(racingCar);
+        }
+
+        return carList;
+    }
+
+    private boolean isValidCarNames(String[] carNames) {
+        for(String carName : carNames){
+            if(carName.length()>5){
+                throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+        }
+        return true;
+    }
 }
