7b7b184b685214d03332d8add72fe8c3b03bc15a
rbm0524 <rbm0524@naver.com>
2024-06-02 21:27:54 +0900

    Feat(InputValidator) : 사용자 입력에 대한 유효성 검증하는 메서드

---
+++ src/main/java/InputValidator.java
@@ -0,0 +1,23 @@
+import java.util.List;
+import java.util.Vector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class InputValidator {
+    public static boolean validateCarName(Vector<Car> cars) throws IllegalArgumentException {
+        List<Car> invalidCar =  cars.stream().filter(Car -> Car.getName().length() > 5).toList();
+
+        if(invalidCar.size() > 0) {
+            throw new IllegalArgumentException("자동차 이름의 글자 수는 5글자 이하만 가능합니다.");
+        }
+        return true;
+    }
+
+    public static boolean validateAttemptCount(int number) throws IllegalArgumentException {
+        if(number < 0) {
+            throw new IllegalArgumentException("시도할 횟수는 양수만 가능합니다.");
+        }
+        return true;
+    }
+
+}
