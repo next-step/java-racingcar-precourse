2d67933fd855be7672933f0b69eb3d2f73291c8e
seoyoung-dev <seoyoung.dev@gmail.com>
2024-06-03 02:57:02 +0900

    feat: Find winner

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -1,5 +1,6 @@
 package controller;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import model.*;
 import util.RandomGenerator;
@@ -7,11 +8,11 @@ import view.*;
 
 public class GameController {
 
-    public void startGame() {
-        String[] carNamesArray = getCarNames(InputView.getFristInput());
+    public static void startGame() {
+        String[] carNamesArray = getCarNames(InputView.getFirstInput());
         int n = InputView.getSecondInput();
         List<Car> cars = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < carNamesArray.length; i++) {
             Car car = new Car(carNamesArray[i]);
             cars.add(car);
         }
@@ -31,6 +32,24 @@ public class GameController {
             });
             System.out.println();
         }
+        findWinner(n, cars);
+    }
+
+    private static void findWinner(int n, List<Car> cars) {
+        ArrayList<Integer> distances = new ArrayList<>();
+        cars.forEach(car -> {
+            distances.add(car.getDistance());
+        });
+        distances.sort(Comparator.reverseOrder());
+        int maxDistance = distances.get(0);
+        ArrayList<String> winners = new ArrayList<>();
+        cars.forEach(car -> {
+            if (car.getDistance() == maxDistance) {
+                winners.add(car.getName());
+            }
+        });
+        String resultMessage = String.join(", ", winners);
+        OutputView.printWinner(resultMessage);
     }
 
     public static String[] getCarNames(String input) {
