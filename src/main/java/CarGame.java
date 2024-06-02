3b18e4012c0a5c241afcea66f8fdba05710f0b0f
unknown <tnsgh3691@gmail.com>
2024-06-02 20:19:48 +0900

    feat : 자동차 경주
    - 사용자로부터 받은 input을 바탕으로 실제 게임 시작.
    - 또한 랜덤 숫자 생성

--- src/main/java/CarGame.java
+++ src/main/java/CarGame.java
@@ -7,4 +7,27 @@ public class CarGame {
         System.out.println("시도할 회수는 몇회인가요?");
         return sc.nextInt();
     }
+
+    public static int[] runRace(ArrayList<String> carList, int gameCount) {
+        System.out.println("\n실행 결과");
+        Random random = new Random();
+        int[] forwardMovementCountArray = new int[carList.size()];
+
+        for (int i = 0; i < gameCount; i++) {
+            advanceCars(carList, forwardMovementCountArray, random);
+        }
+
+        return forwardMovementCountArray;
+    }
+
+    private static void advanceCars(ArrayList<String> carList, int[] forwardMovementCountArray, Random random) {
+        for (int k = 0; k < carList.size(); k++) {
+            if (random.nextInt(10) >= 4) {
+                forwardMovementCountArray[k]++;
+            }
+        }
+    }
+
+
+
 }
