73afcaa1e7a2db97aa3727d216c04b3e44c748e4
J-1ac <tkdwns26@naver.com>
2024-06-03 00:12:46 +0900

    feat: validateCarNames 자동차 이름 유효성 검사 추가

--- src/main/java/service/GameService.java
+++ src/main/java/service/GameService.java
@@ -9,10 +9,16 @@ import model.RacingCar;
 public class GameService {
 
     public boolean validateCarNames(String inputCarNames) {
+        if(inputCarNames.isEmpty()){
+            throw new IllegalArgumentException("자동차 이름을 입력해 주세요.");
+        }
         String[] carNames = inputCarNames.split(",");
         for (String carName : carNames) {
             if (carName.length() > 5) {
                 throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+            }
+            if (carName.isEmpty()) {
+                throw new IllegalArgumentException("자동차 이름은 비워둘 수 없습니다.");
             }
         }
         return true;
