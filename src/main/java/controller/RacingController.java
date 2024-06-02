d26910e8f6a1afb4a21933da0c48b692bcd0df85
geon <kky8586696@naver.com>
2024-06-02 18:38:12 +0900

    refactor: Race 모델처리 로직을 컨트롤러에서 빼서 구조 변경

--- src/main/java/controller/RacingController.java
+++ src/main/java/controller/RacingController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.Car;
+import model.Race;
 import utils.InputValidation;
 import view.RacingView;
 
@@ -10,76 +11,42 @@ import java.util.List;
 import java.util.Scanner;
 
 public class RacingController {
-    private int tryNum;
-    private List<Car> carlist ;
-
-    public RacingController(){
-        tryNum = 0;
-    }
     public void startRacing(){
-        inputCarName();
-        inputTryNum();
+        Race race;
+        race = inputCarName();
+        race.setTryNum(inputTryNum());
         RacingView.printResultmessage();
-        for(int i = 0;i<tryNum;i++){
-            moveCarByRandNum(carlist);
-            RacingView.moveStatus(carlist);
+        for(int i = 0;i<race.getTryNum();i++){
+            race.moveCarByRandNum(race.getCarlist());
+            RacingView.moveStatus(race.getCarlist());
         }
-        RacingView.printResult(getWinner());
+        RacingView.printResult(race.getWinner());
     }
-    public void addCarlist(String[] carNames){
-        carlist = new ArrayList<>();
-        for(String carname : carNames){
-            Car car = new Car(carname);
-            carlist.add(car);
-        }
-    }
-
-    public void inputCarName(){
+    public Race inputCarName(){
         while(true){
             try{
                 String inputCars = RacingView.printInputCarName();
                 String[] carNames = inputCars.split(",");
                 InputValidation.isValidInput(carNames);//유효성검사
-                addCarlist(carNames);
-                break;
+                Race race = new Race(carNames);
+                return race;
             }catch(IllegalArgumentException e){
                 System.out.println("[ERROR] " + e.getMessage());
             }
         }
     }
-    public void inputTryNum(){
+    public int inputTryNum(){
         while(true){
             try{
                 String inputTryNum = RacingView.printInputTryNum();
                 InputValidation.isNumber(inputTryNum); //정수인지 확인
-                tryNum = Integer.parseInt(inputTryNum);
-                break;
+                int newtryNum = Integer.parseInt(inputTryNum);
+                return newtryNum;
             }catch (IllegalArgumentException e){
                 System.out.println("[ERROR]"+e.getMessage());
             }
         }
     }
-    public void moveCarByRandNum(List<Car> cars){
-        for(int i =0;i<carlist.size();i++){
-            int randNum = (int)(Math.random()*9);
-            cars.get(i).moveCar(randNum);
-        }
-    }
-    public List<Car> getWinner(){
-        int maxMove = getMaxMove();
-        return carlist.stream()
-                .filter(car->car.getMove() == maxMove)
-                .toList();
-    }
-    public int getMaxMove(){
-        return carlist.stream()
-                .mapToInt(Car::getMove)
-                .max()
-                .orElse(0);
-    }
 
-    public List<Car> getCarlist(){
-        return carlist;
-    }
 
 }
--- src/test/java/model/RaceTest.java
+++ src/test/java/model/RaceTest.java
@@ -1,6 +1,5 @@
-package controller;
+package model;
 
-import model.Car;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -9,20 +8,19 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class RacingControllerTest {
-    private RacingController racingController;
+class RaceTest {
+    private Race race;
 
     @BeforeEach
     public void setUp(){
-        racingController = new RacingController();
+        String[] carNames = {"car1","car2"};
+        race = new Race(carNames);
     }
 
     @Test
     @DisplayName("Carlist에 자동차의 이름들이 잘 추가되는지 확인")
     public void testAddCarList(){
-        String[] carNames = {"car1","car2"};
-        racingController.addCarlist(carNames);
-        List<Car> carlist = racingController.getCarlist();
+        List<Car> carlist = race.getCarlist();
         assertEquals(2,carlist.size());
         assertEquals("car1",carlist.get(0).getCarname());
         assertEquals("car2",carlist.get(1).getCarname());
@@ -31,10 +29,8 @@ class RacingControllerTest {
     @Test
     @DisplayName("랜덤값을 파라미터로 전달했는데 자동차가 잘 움직이는지 확인")
     public void testMoveCarByRandNum(){
-        String[] carNames = {"car1","car2"};
-        racingController.addCarlist(carNames);
-        List<Car> carlist = racingController.getCarlist();
-        racingController.moveCarByRandNum(carlist);
+        List<Car> carlist = race.getCarlist();
+        race.moveCarByRandNum(carlist);
         //랜덤값이 주어졌을때 한칸 움직이거나 or 움직이지않거나
         assertTrue(carlist.get(0).getMove()>=0 && carlist.get(0).getMove()<=1);
         assertTrue(carlist.get(1).getMove()>=0 && carlist.get(1).getMove()<=1);
@@ -44,12 +40,10 @@ class RacingControllerTest {
     @Test
     @DisplayName("단일 우승자 확인")
     public void testGetOneWinner(){
-        String[] carNames = {"car1","car2"};
-        racingController.addCarlist(carNames);
-        List<Car> carlist = racingController.getCarlist();
+        List<Car> carlist = race.getCarlist();
         carlist.get(0).setMove(6);
         carlist.get(1).setMove(3);
-        List<Car> winners = racingController.getWinner();
+        List<Car> winners = race.getWinner();
         assertEquals(1,winners.size());
         assertEquals("car1",winners.get(0).getCarname());
 
@@ -58,12 +52,10 @@ class RacingControllerTest {
     @Test
     @DisplayName("복수 우승자 확인")
     public void testGetManyWinner(){
-        String[] carNames = {"car1","car2"};
-        racingController.addCarlist(carNames);
-        List<Car> carlist = racingController.getCarlist();
+        List<Car> carlist = race.getCarlist();
         carlist.get(0).setMove(6);
         carlist.get(1).setMove(6);
-        List<Car> winners = racingController.getWinner();
+        List<Car> winners = race.getWinner();
         assertEquals(2,winners.size());
         assertEquals("car1",winners.get(0).getCarname());
         assertEquals("car2",winners.get(1).getCarname());
