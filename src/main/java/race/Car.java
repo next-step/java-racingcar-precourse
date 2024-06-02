a9295728412bcae32894e219f90d63c14eec83c5
Kiwoong Kim <gw150311@naver.com>
2024-06-02 13:16:39 +0900

    feat: Car 클래스 추가

---
+++ src/main/java/race/Car.java
@@ -0,0 +1,24 @@
+package race;
+
+public class Car {
+    private String name;
+    private int moveCount;
+
+    public Car(String name) {
+        this.name = name;
+        moveCount = 0;
+        random = -1;
+    }
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return name + " : " + "-".repeat(moveCount);
+    }
+}
