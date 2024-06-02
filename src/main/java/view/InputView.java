973310710f668f3774c0d19c55040d402bdff04c
seoyoung-dev <seoyoung.dev@gmail.com>
2024-06-03 03:11:09 +0900

    fix typo: Fix getFirstInput method name

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -6,7 +6,7 @@ public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static String getFristInput() {
+    public static String getFirstInput() {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표를 기준으로 구분)");
         return scanner.nextLine();
     }
@@ -15,5 +15,4 @@ public class InputView {
         System.out.println("시도할 횟수는 몇회인가요?");
         return scanner.nextInt();
     }
-
 }
