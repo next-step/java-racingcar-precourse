2741cc51ad6a4a0b8dda82e889df43be9b83c95c
kangwlgns <kangji0615@gmail.com>
2024-06-03 02:54:55 +0900

    feat(view): add RaceViewInterface

---
+++ src/main/java/view/RaceViewInterface.java
@@ -0,0 +1,9 @@
+package view;
+
+public interface RaceViewInterface {
+    public void userCarsInput();
+    public void userRoundsInput();
+    public void printProgress();
+    public void printProcess();
+    public void printResult();
+}
