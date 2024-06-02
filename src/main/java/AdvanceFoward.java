8202ab58df451b3a0a8190ef08cb5bd74f4d1b17
rbm0524 <rbm0524@naver.com>
2024-06-02 22:54:40 +0900

    fix(AdvanceFoward) : random 객체 for문 안으로 삽입, for문의 지역변수 i,j 조정

--- src/main/java/AdvanceFoward.java
+++ src/main/java/AdvanceFoward.java
@@ -2,15 +2,18 @@ import java.util.Random;
 import java.util.Vector;
 
 public class AdvanceFoward {
-    public static void advanceFoward(Vector<Car> cars) {
-        Random random = new Random();
-        for(int i = 0; i < cars.size(); i++) {
-            int random_number = random.nextInt() % 10;
-            if(random_number >= 4) {
-                cars.elementAt(i).increaseNumber();
+    public static void advanceFoward(Vector<Car> cars, int attemptnumber) {
+        for(int i = 0; i < attemptnumber; i++) {
+            for(int j = 0; j < cars.size(); j++) {
+                Random random = new Random();
+                int random_number = random.nextInt() % 10;
+                if(random_number >= 4) {
+                    cars.elementAt(j).increaseNumber();
+                }
             }
+            OutputHandler.fowardOutput(cars);
         }
-        OutputHandler.FowardOutput(cars);
+
     }
 
 }
