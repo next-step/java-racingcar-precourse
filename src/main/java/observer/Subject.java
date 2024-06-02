86e7f18d71e1df08d6b4100a640909df96d19ade
kangwlgns <kangji0615@gmail.com>
2024-06-03 03:17:12 +0900

    feat(observer): add Subject

---
+++ src/main/java/observer/Subject.java
@@ -0,0 +1,7 @@
+package observer;
+
+public interface Subject {
+    public void addObserver(Observer observer);
+    public void removeObserver(Observer observer);
+    public void notifyObservers();
+}
