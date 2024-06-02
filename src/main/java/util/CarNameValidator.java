1e25db6cbd1f2c56554c1a729a8af24463f47f3c
HSHwan <tnghks2866@gmail.com>
2024-06-03 00:52:40 +0900

    style: 중복 자동차 이름 에러 메시지 수정

--- src/main/java/util/CarNameValidator.java
+++ src/main/java/util/CarNameValidator.java
@@ -6,7 +6,7 @@ public class CarNameValidator implements InputValidator {
 
     private static final int CAR_NAME_MAX_LENGTH = 5;
     private static final String CAR_NAME_ERROR_MESSAGE = "자동차 이름의 길이가 " + CAR_NAME_MAX_LENGTH + "를 초과하였습니다.";
-    private static final String DUPLICATE_CAR_NAME_ERROR_MESSAGE = "중복된 자동차 이름이 있습니다";
+    private static final String DUPLICATE_CAR_NAME_ERROR_MESSAGE = "중복된 자동차 이름이 있습니다.";
 
     @Override
     public void checkInputValue(Object input) {
