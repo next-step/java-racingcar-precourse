2e8170ce67a79d33a349f733ccbb02591da57b48
UiChan Lee <junglkls1234@gmail.com>
2024-06-02 22:08:23 +0900

    Feat: "경주 회수 입력 받기"

--- src/main/java/RacingGame.java
+++ src/main/java/RacingGame.java
@@ -2,6 +2,7 @@ import java.util.*;
 public class RacingGame {
     Scanner scanner = new Scanner(System.in);
     private List<Car> carList;
+    private int tryCount;
     private void makeCarList() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] carNames = scanner.nextLine().split(",");
@@ -9,6 +10,10 @@ public class RacingGame {
         for (String name : carNames) {
             carList.add(new Car(name.trim()));
         }
+    }
+    private void makeCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        tryCount = scanner.nextInt();
     }
 
 }
