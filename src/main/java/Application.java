232d457b89d9144d1c9195f83fcdc724b8be5ad3
seolbin <jsb3038@gmail.com>
2024-06-03 01:39:09 +0900

    feat : Application 기능 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,5 +1,27 @@
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        
+        List<String> carNames = InputHandler.getCarNames();
+        int rounds = InputHandler.getRounds();
+        Race race = new Race(carNames, rounds);
+        race.start();
+        printRaceResult(race.getWinners());
+    }
+
+    private static void printRaceResult(List<Car> winners) {
+        System.out.println("실행 결과");
+        for (Car car : winners) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+        System.out.println("최종 우승자: " + getWinnerNames(winners));
+    }
+
+    private static String getWinnerNames(List<Car> winners) {
+        StringBuilder sb = new StringBuilder();
+        for (Car winner : winners) {
+            sb.append(winner.getName()).append(", ");
+        }
+        return sb.substring(0, sb.length() - 2);
     }
 }
