5a8930523fac5e5d41a90d1705863096ba2ac82f
Giheon Do <dgh0001@naver.com>
2024-06-02 22:48:24 +0900

    Test: PlayCountValidateTest 추가

--- src/test/java/view/PlayCountValidateTest.java
+++ src/test/java/view/PlayCountValidateTest.java
@@ -34,9 +34,21 @@ public class PlayCountValidateTest {
 
   @Test
   @DisplayName("시도 회수가 음의 정수인 케이스")
-  void carNames_errorCase_longerThan5() {
+  void carNames_errorCase_negative_playCount() {
     // given
     String errorCase = "-1";
+    // when
+    ValidateInput validateInput = new ValidateInput();
+    boolean validateResult = validateInput.playCountValidate(errorCase);
+    // then
+    Assertions.assertThat(validateResult).isEqualTo(false);
+  }
+
+  @Test
+  @DisplayName("시도 회수가 0인 케이스")
+  void carNames_errorCase_zero_playCount() {
+    // given
+    String errorCase = "0";
     // when
     ValidateInput validateInput = new ValidateInput();
     boolean validateResult = validateInput.playCountValidate(errorCase);
