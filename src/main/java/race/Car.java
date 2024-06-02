7a3b04a57438cbde92689b79cb5218dae18c16f4
Kiwoong Kim <gw150311@naver.com>
2024-06-02 14:04:15 +0900

    comment : Car 클래스 메서드 주석 추가

--- src/main/java/race/Car.java
+++ src/main/java/race/Car.java
@@ -22,6 +22,9 @@ public class Car {
         return name;
     }
 
+    /**
+     * 랜덤한 숫자를 생성하여 전진 또는 멈춤
+     */
     public void tryMove() {
         int randomNumber = random.nextInt(RANDOM_MAX + 1);
         if (randomNumber >= MOVE_THRESHOLD) {
@@ -29,9 +32,9 @@ public class Car {
         }
     }
 
-    public void move() {
-
-    }
+    /**
+     * @return 자동차의 이름과 현재 이동 상태
+     */
     public String toString() {
         return name + " : " + "-".repeat(moveCount);
     }
