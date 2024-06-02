7929c9c527292f6eeb7dea8b42a1e82e57f02621
noojmogstel <goaudgo@naver.com>
2024-06-03 06:03:52 +0900

    feat(domain): add Car Domain

---
+++ src/main/java/Racing/domain/Car.java
@@ -0,0 +1,31 @@
+package Racing.domain;
+
+public class Car {
+    //자동차의 정보를 가지는 Car 도메인.
+    private String name;
+    private String distanceBar;
+
+    //생성자
+    public Car(String name) {
+        this.name=name;
+        this.distanceBar="";
+
+    }
+
+    //Getter 및 Setter
+    public String getDistanceBar() {
+        return distanceBar;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void plusDistanceBar() {
+        this.distanceBar +="-";
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
