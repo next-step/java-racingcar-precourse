91cf6c13864ba41cc060d185a24ed967c215c0e6
kangwlgns <kangji0615@gmail.com>
2024-06-03 03:33:51 +0900

    feat(RaceServiceInterface): add cars getter

--- src/main/java/service/RaceServiceInterface.java
+++ src/main/java/service/RaceServiceInterface.java
@@ -1,7 +1,10 @@
 package service;
 
+import java.util.ArrayList;
+
 public interface RaceServiceInterface {
     public int getNumberOfCars();
     public int getNumberOfRounds();
     public void setNumberOfRounds();
+    public ArrayList<CarServiceInterface> getCars();
 }
