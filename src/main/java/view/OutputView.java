af06d2785032d4ad2b40c9b5530b3fd4eebe58cc
seoyoung-dev <seoyoung.dev@gmail.com>
2024-06-03 03:02:57 +0900

    feat: Print winner(s)

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public static void printResult(String name, String progressLine) {
         System.out.println(name + " : " + progressLine);
     }
+
+    public static void printWinner(String winners) {
+        System.out.println("최종 우승자 : " +  winners);
+    }
 }
