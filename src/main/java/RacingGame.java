743b0d9a2061bbbed911ddcfd7a9bed722b2cda9
youcalste03 <luckthb325@naver.com>
2024-06-02 13:36:30 +0900

    feat: main구현

---
+++ src/main/java/RacingGame.java
@@ -0,0 +1,9 @@
+import controller.RacingController;
+
+public class RacingGame {
+
+    public static void main(String[] args){
+        RacingController controller=new RacingController();
+        controller.run();
+    }
+}
--- src/main/java/controller/RacingController.java
+++ src/main/java/controller/RacingController.java
@@ -3,9 +3,7 @@ package controller;
 import domain.Car;
 import domain.Racing;
 import java.io.IOException;
-import java.nio.channels.ScatteringByteChannel;
 import java.util.List;
-import javax.swing.text.View;
 import utils.Checker;
 import view.RacingView;
 
