37547f15e08ac5c8b47034cd059124c0a300bd32
안원모 <tommy45231@naver.com>
2024-06-02 17:20:30 +0900

    Test: 3.자동차 이름 입력 및 유효성 검사 테스트 '영문자 제외 다른문자 입력시 test'

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -17,5 +17,11 @@ public class CarTest {
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("1자 이상 5자 이하만 가능합니다.");
     }
+    @Test
+    void carNameShouldOnlyContainEnglishCharacters() {
+        assertThatThrownBy(() -> new Car("자동차"))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("영문자만 가능합니다.");
+    }
 
 }
