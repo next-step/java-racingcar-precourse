5676252b1fa0855c35ebc2cad8b66608f650e07d
Kim Sanghae <pelikan@pusan.ac.kr>
2024-06-02 13:19:13 +0900

    feat: Init CarDTO

---
+++ src/main/java/model/CarDTO.java
@@ -0,0 +1,13 @@
+package model;
+
+public class CarDTO {
+    private final String name;
+
+    public CarDTO(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
