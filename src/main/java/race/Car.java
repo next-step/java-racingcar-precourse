264714126d7b8eecf75c919bd146fc9cc587d8cb
Kiwoong Kim <gw150311@naver.com>
2024-06-02 13:33:36 +0900

    feat: Car 클래스 랜덤이동기능 추가

--- src/main/java/race/Car.java
+++ src/main/java/race/Car.java
@@ -1,12 +1,17 @@
 package race;
 
+import java.util.Random;
+import static race.Parameters.RANDOM_MAX;
+import static race.Parameters.MOVE_THRESHOLD;
+
 public class Car {
     private String name;
     private int moveCount;
-
+    private Random random;
     public Car(String name) {
         this.name = name;
         moveCount = 0;
+        random = new Random();
     }
 
     public int getMoveCount() {
@@ -17,6 +22,16 @@ public class Car {
         return name;
     }
 
+    public void tryMove() {
+        int randomNumber = random.nextInt(RANDOM_MAX + 1);
+        if (randomNumber >= MOVE_THRESHOLD) {
+            ++moveCount;
+        }
+    }
+
+    public void move() {
+
+    }
     public String toString() {
         return name + " : " + "-".repeat(moveCount);
     }
--- src/main/java/race/Parameters.java
+++ src/main/java/race/Parameters.java
@@ -3,4 +3,6 @@ package race;
 public interface Parameters {
     public int NAME_MAX_LENGTH  = 5;
     public int NAME_MIN_LENGTH = 1;
+    public int MOVE_THRESHOLD = 4;
+    public int RANDOM_MAX = 9;
 }
