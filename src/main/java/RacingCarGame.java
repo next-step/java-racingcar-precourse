dc9f732fecb5eecb5281069eca50c4d805b3b77e
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-06-03 00:26:00 +0900

    feat: RacingCarGame errorMessage Method 입력이 잘못되었을 시 exception을 던지는 메서드

--- src/main/java/RacingCarGame.java
+++ src/main/java/RacingCarGame.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class racingCarGame {
+import java.util.*;
+
+public class RacingCarGame {
+
+    public void errorMessage(){
+        throw new IllegalArgumentException("[ERROR] 잘못된 값을 입력했습니다. 다시 입력해주세요.");
+    }
+
+
 }
+
+
+
+
+
