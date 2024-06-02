ef84296b7a0326968718adac9079490442754ee4
rbm0524 <rbm0524@naver.com>
2024-06-02 21:31:38 +0900

    feat(Application) : 사용자의 입력을 받아서 유효성 검사하는 로직 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -2,12 +2,28 @@ import static java.lang.Integer.parseInt;
 
 import java.io.*;
 import java.util.HashMap;
+import java.util.Vector;
 
 public class Application {
 
     public static void main(String[] args) throws IOException{
+        Vector<Car> cars = new Vector<Car>();
+        boolean carnamevalid;
+        boolean attemptcountvalid;
+        int attemptnumber = 0;
+
         //사용자 입력 받기
-        // 입력값 유효성 검사
+        do {
+            InputHandler.CarNameInput(cars);
+            carnamevalid = InputValidator.validateCarName(cars); //유효성 검사
+        }while(!carnamevalid);
+
+        do {
+            attemptnumber = InputHandler.attemptCountInput();
+            attemptcountvalid = InputValidator.validateAttemptCount(attemptnumber); //유효성 검사
+        }while(!attemptcountvalid);
+
+
         //전진하기
         //우승자 선정
         //우승자 출력
