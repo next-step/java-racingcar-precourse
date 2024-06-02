51b745b731bb1a2eca97d08fc787f78b37befae7
kangwlgns <kangji0615@gmail.com>
2024-06-03 03:39:18 +0900

    feat(service): add CarService

---
+++ src/main/java/service/CarService.java
@@ -0,0 +1,19 @@
+package service;
+
+import model.CarModelInterface;
+
+public class CarService implements CarServiceInterface {
+    private CarModelInterface car;
+
+    public void goForward() {
+
+    }
+
+    public void move() {
+
+    }
+
+    public boolean nameValidation() {
+        return true;
+    }
+}
