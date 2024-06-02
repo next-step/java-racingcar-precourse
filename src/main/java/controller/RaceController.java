f80fb0e961772c4d3c83cd0017fc04439290cae2
kangwlgns <kangji0615@gmail.com>
2024-06-03 04:09:55 +0900

    feat(controller): add RaceController

---
+++ src/main/java/controller/RaceController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import service.CarServiceInterface;
+import service.RaceServiceInterface;
+import view.RaceViewInterface;
+
+public class RaceController implements RaceControllerInterface {
+    private RaceServiceInterface race;
+    private RaceViewInterface view;
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void end() {
+
+    }
+
+    @Override
+    public void addCar(CarServiceInterface carServiceInterface) {
+
+    }
+
+    @Override
+    public void setRound(int numberOfRound) {
+
+    }
+}
