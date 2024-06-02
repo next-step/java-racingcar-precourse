3d7fb625cf130228759f717a369fd88db0573e9d
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-06-03 00:58:45 +0900

    feat: Application 메인메서드 작성

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Application {
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        RacingCarGame Game = new RacingCarGame();
+        boolean res = false;
+        while(!res){
+            res = Game.setCarNames();
+        }
+        res = false;
+        while(!res){
+            res = Game.setTryNum();
+        }
+        Game.racingStart();
+        List<String> winner = Game.findWinner();
+        Game.printWinner(winner);
+
+    }
 }
