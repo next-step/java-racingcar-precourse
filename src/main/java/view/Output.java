e5710bb7d6baef67cbbdf2c04c14ab99c72429e0
Kiwoong Kim <gw150311@naver.com>
2024-06-02 15:42:49 +0900

    feat : Output 클래스 printWinners 메서드 추가

--- src/main/java/view/Output.java
+++ src/main/java/view/Output.java
@@ -1,12 +1,22 @@
 package view;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.StringJoiner;
 import race.Car;
 import race.Race;
+import static view.Prompt.RESULT;
+import static view.Prompt.WINNERS;
 
 public class Output {
+
     public static void printException(String msg) {
         System.out.println(msg);
+    }
+
+    public static void printResult() {
+        System.out.println(Prompt.RESULT);
     }
 
     public static void printRaceState(List<Car> cars) {
@@ -14,5 +24,14 @@ public class Output {
             System.out.println(c.toString());
         }
         System.out.println();
+    }
+
+    public static void printWinners(List<String> winnerNames) {
+        StringJoiner sj = new StringJoiner(", ");
+        System.out.print(WINNERS);
+        for (String winnerName : winnerNames) {
+            sj.add(winnerName);
+        }
+        System.out.println(sj.toString());
     }
 }
