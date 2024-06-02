6bc3452d7a7330d7cb6033b5c223bb9b61755787
yoonsu0325 <84390426+yoonsu0325@users.noreply.github.com>
2024-06-03 00:35:12 +0900

    feat: RacingCarGame setCarNames Method 자동차 이름을 입력받는 메서드

--- src/main/java/RacingCarGame.java
+++ src/main/java/RacingCarGame.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 public class RacingCarGame {
     private List<RacingCar> carList = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
 
     public void errorMessage(){
         throw new IllegalArgumentException("[ERROR] 잘못된 값을 입력했습니다. 다시 입력해주세요.");
@@ -18,7 +19,23 @@ public class RacingCarGame {
         }
         return state;
     }
-
+    public boolean setCarNames(){;
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        String line = scanner.nextLine();
+        String[] split = line.split(",");
+        boolean state = true;
+        for (String s : split) {
+            try {
+                state = validName(s);
+            }
+            catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+                state = false;
+                break;
+            }
+        }
+        return state;
+    }
 
 }
 
