c4c77f7a27dfab70c9f7965f69454170a2da4fba
seoyoung-dev <seoyoung.dev@gmail.com>
2024-06-03 03:49:37 +0900

    style: Rename MainApplication class into Application class

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,6 @@
 import controller.GameController;
 
-public class MainApplication {
+public class Application {
     public static void main(String[] args) {
         GameController gameController = new GameController();
         gameController.startGame();
