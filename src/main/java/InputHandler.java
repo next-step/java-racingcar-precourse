b81260bdab0318db2f05b5b2d49c252aefa9ff1c
rbm0524 <rbm0524@naver.com>
2024-06-02 19:32:47 +0900

    Feat(InputHandler) : 사용자 입력을 받는 클래스 작성

---
+++ src/main/java/InputHandler.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Vector;
+
+public class InputHandler {
+    public static int input(Vector<Car> cars) throws IOException {
+        //사용자 입력 받기
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,)로 구분)");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(",");
+        for(int i = 0; i < s.length; i++) {
+            cars.add(new Car(s[i].trim(), 0));
+        }
+
+        System.out.println("시도할 횟수는 몇회인가요?");
+        int number = Integer.parseInt(br.readLine());
+        return number;
+    }
+
+}
