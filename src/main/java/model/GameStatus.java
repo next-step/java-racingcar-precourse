36100ff3851442a44bb6c8b86dbf35edf063b9e0
Awhn <ahnshwan@outlook.com>
2024-06-03 12:52:27 +0900

    fix : GameSettings into GameStatus
    변수 tryGoal을 GameStatus로 옮기고 클래스를 줄였습니다.

--- src/main/java/model/GameStatus.java
+++ src/main/java/model/GameStatus.java
@@ -7,10 +7,25 @@ public class GameStatus {
     HashMap<String, Integer> tryCurrent = new HashMap<>();
     ArrayList winners = new ArrayList();
 
+    int tryGoal;
+
+    public void setTryGoal(int tryGoal) {
+        this.tryGoal = tryGoal;
+    }
+
+    public int getTryGoal() {
+        return tryGoal;
+    }
+
     public void initTryCurrent(String[] args) {
         for (int i = 0; i < args.length; i++) {
             tryCurrent.put(args[i], 0);
         }
+    }
+
+    public void updateTryCurrent(String args) {
+        int currentKey = tryCurrent.get(args);
+        tryCurrent.put(args, currentKey + 1);
     }
 
     public void setWinners(String[] args) {
