77e739a050986538f3ed83ccac0c6c6aa43cf36f
HaegyeongKim <Haegyeon.kim01@gmail.com>
2024-06-03 12:32:10 +0900

    feat: 출력 기능 테스트

--- src/test/java/view/OutputViewTest.java
+++ src/test/java/view/OutputViewTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import vo.Car;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
