07d9e20223cfd46a0f645a18d617ebd20174220f
suhyeon <nucc1186@gamil.com>
2024-06-03 13:25:27 +0900

    feat:check Car total Valid

--- src/main/java/Validator/Validator.java
+++ src/main/java/Validator/Validator.java
@@ -1,6 +1,11 @@
 package Validator;
 
 public class Validator {
+    public void isCarNameValid(String carName) throws IllegalArgumentException{
+        hasCarNameSpace(carName);
+        isCarNameRangeValid(carName);
+        isCarNameLengthValid(carName);
+    }
     public void isCarNameLengthValid(String carName){
         if(carName.length() > 5){
             throw new IllegalArgumentException("[ERROR] 자동차 이름은 길이 5이하 이어야 합니다.");
