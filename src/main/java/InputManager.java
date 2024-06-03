12dd77c7159b42a3389655eb483398757068ce28
NextrPlue <98487008+NextrPlue@users.noreply.github.com>
2024-06-03 15:00:58 +0900

    chore(InputManager.java): 출력값 수정

--- src/main/java/InputManager.java
+++ src/main/java/InputManager.java
@@ -1,4 +1,3 @@
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,7 +8,7 @@ public class InputManager {
     public List<Car> getCars(Scanner scanner) {
         while (true) {
             try {
-                System.out.print("자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분): ");
+                System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
                 String input = scanner.nextLine();
                 List<String> carNames = nameValidator.validateAndGetNames(input);
                 return createCars(carNames);
@@ -30,7 +29,7 @@ public class InputManager {
     public int getAttempts(Scanner scanner) {
         while (true) {
             try {
-                System.out.print("이동 횟수를 입력하세요: ");
+                System.out.println("시도할 회수는 몇회인가요?");
                 String input = scanner.nextLine();
                 int attempts = Integer.parseInt(input);
                 validateGetAttempts(attempts);
