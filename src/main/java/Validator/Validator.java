fb068b203fb728f0f1a958b0c084cd4a15cc651a
suhyeon <nucc1186@gamil.com>
2024-06-03 13:15:06 +0900

    feat: Check CarName is not bigger than 5

--- src/main/java/Validator/Validator.java
+++ src/main/java/Validator/Validator.java
@@ -1,7 +1,11 @@
 package Validator;
 
 public class Validator {
-//    public void {
-//        throw new IllegalArgumentException()
-//    }
+    public void isCarNameLengthValid(String carName){
+        if(carName.length() > 5){
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 길이 5이하 이어야 합니다.");
+        }
+    }
+
+
 }
