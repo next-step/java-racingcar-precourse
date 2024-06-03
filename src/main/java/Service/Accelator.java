38b05e607027c9541b2f4adcc6dc03da98bf7d51
suhyeon <nucc1186@gamil.com>
2024-06-03 14:27:24 +0900

    fix : accel() default to public

--- src/main/java/Service/Accelator.java
+++ src/main/java/Service/Accelator.java
@@ -5,7 +5,7 @@ import java.util.Random;
 public class Accelator {
     private Random random = new Random();
     private static final int CUTOFF = 4;
-    boolean accel(){
+    public boolean accel(){
         if(random.nextInt(10) >= CUTOFF){
             return true;
         }
