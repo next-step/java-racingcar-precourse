bcd81697ba74332640ef151ec8659cf15fceac89
HaegyeongKim <Haegyeon.kim01@gmail.com>
2024-06-03 12:25:30 +0900

    feat: Controller 작성

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,10 @@
+import controller.GameController;
 
 public class Application {
 
+    private final static GameController gameController = new GameController();
+
     public static void main(String[] args) {
+        gameController.run();
     }
 }
---
+++ src/main/java/controller/GameController.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class GameController {
+
+    public void run() {
+    }
+
+
+
+}
