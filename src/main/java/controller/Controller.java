0f38ad5aaf5f02e5ab8feb8b01377ff949c230a0
donghyun kim <gimdonghyun5434@gmail.com>
2024-06-03 11:05:55 +0900

    refactor : Controller의 saveCars메서드를 CarRepository로 옮겼습니다.

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -9,22 +9,13 @@ import view.OutputView;
 
 public class Controller {
   private final Input input = new Input();
-  private final CarRepository cars = new CarRepository();
+  private final CarRepository carRepository = new CarRepository();
   private final OutputView outputView = new OutputView();
   public void run(){
-    saveCars(input.getCarNames());
-    Game game = createGame(cars.findAll(),input.getNumberOfAttempts());
+    carRepository.saveCars(input.getCarNames());
+    Game game = new Game(carRepository.findAll(),input.getNumberOfAttempts());
     game.playGame();
     outputView.printWinners(game.getWinners());
   }
 
-  private void saveCars(List<String> names){
-    for(String name: names) {
-      cars.save(new Car(name));
-    }
-  }
-
-  private Game createGame(List<Car> cars,int numberOfAttempts){
-    return new Game(cars,numberOfAttempts);
-  }
 }
