7144137f4a77aa0ec9eababadc6bc04f6111a4ee
seoyoung-dev <seoyoung.dev@gmail.com>
2024-06-03 10:52:00 +0900

    feat: Create Validator class

--- src/main/java/util/Validator.java
+++ src/main/java/util/Validator.java
@@ -1,2 +1,28 @@
-package util;public class Validator {
+package util;
+
+public class Validator {
+    public static void validateArrayExistence(String[] carArray) {
+        if (carArray.length == 0) {
+            throw new IllegalArgumentException("[ERROR] 1개 이상의 자동자를 입력해야 합니다.");
+        }
+    }
+
+    public static void validateNameLength(String[] carArray) {
+        for (String name: carArray) {
+            if (name.length() > 5) {
+                throw new IllegalArgumentException("[ERROR] 자동자의 이름은 5자 이하이어야 합니다.");
+            }
+            if (name.length() < 1) {
+                throw new IllegalArgumentException("[ERROR] 자동자의 이름은 1자 이상이어야 합니다.");
+            }
+        }
+    }
+
+    public static int validateNumberFormat(String input) {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 시도할 횟수는 숫자로 입력해야 합니다.");
+        }
+    }
 }
