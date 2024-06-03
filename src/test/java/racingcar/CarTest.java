592610adfe9c1fd471b67c386a3999eab9337e4f
minjae <minjae4650@gmail.com>
2024-06-03 19:25:27 +0900

    test: Car 클래스 생성자 테스트 추가
    - Car 객체가 생성될 때, 이름이 5자 초과로 길게 설정되면 적절한 오류가 뜨는지 테스트를 포함했습니다.

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,11 +3,17 @@ package racingcar;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class CarTest {
     @Test
     public void constructorTest() {
         Car car = new Car("mjack");
         assertThat(car.getName()).isEqualTo("mjack");
+    }
+    @Test
+    public void invalidNameException() {
+        assertThatThrownBy(() -> new Car("longname"))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
