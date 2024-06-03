328491bf2f633530f49d184db6ffcd037b95a9c5
Awhn <ahnshwan@outlook.com>
2024-06-03 11:11:48 +0900

    feat : GameStatus class
    make gamestatus class

---
+++ src/main/java/model/GameStatus.java
@@ -0,0 +1,29 @@
+package model;
+import java.util.ArrayList;
+
+import java.util.HashMap;
+
+public class GameStatus {
+    HashMap<String, Integer> tryCurrent = new HashMap<>();
+    ArrayList winners = new ArrayList();
+
+    public void initTryCurrent(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            tryCurrent.put(args[i], 0);
+        }
+    }
+
+    public void setWinners(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            this.winners.add(i, args[i]);
+        }
+    }
+
+    public HashMap<String, Integer> getTryCurrent() {
+        return this.tryCurrent;
+    }
+
+    public ArrayList getWinners() {
+        return this.winners;
+    }
+}
