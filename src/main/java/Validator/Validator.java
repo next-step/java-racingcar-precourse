fc04313c34f5c876e08a71ba5a84a54e041c93a5
suhyeon <nucc1186@gamil.com>
2024-06-03 13:18:35 +0900

    feat: Check CarName range

--- src/main/java/Validator/Validator.java
+++ src/main/java/Validator/Validator.java
@@ -12,4 +12,10 @@ public class Validator {
             throw new IllegalArgumentException("[ERROR] 자동차 이름은 공백을 포함할 수 없습니다.");
         }
     }
+
+    public void isCarNameRangeValid(String carName){
+        if(!carName.matches("[a-zA-z_0-9]")){
+            throw new IllegalArgumentException("[ERROR] 자동차 이름으로 사용할 수 없는 단어가 입력되었습니다.");
+        }
+    }
 }
