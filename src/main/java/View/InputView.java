b1f7ac097105ce42cf5037207f794a8da0855a8c
suhyeon <nucc1186@gamil.com>
2024-06-03 13:55:36 +0900

    feat: add validation to readCarName()

--- src/main/java/View/InputView.java
+++ src/main/java/View/InputView.java
@@ -1,17 +1,30 @@
 package View;
 
 import Model.Car;
+import Validator.Validator;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class InputView {
     private Scanner scanner = new Scanner(System.in);
+    Validator validator = new Validator();
 
     public ArrayList<String> readCarName(){
-        String input = scanner.nextLine();
         ArrayList<String> carNames = new ArrayList<>();
-        for(String carName : input.split(",")){
-            carNames.add(carName);
+        while(true){
+            carNames.clear();
+            String input = scanner.nextLine();
+            try{
+                input = input.replace(", ", ",");
+                for(String carName : input.split(",")){
+                    validator.isCarNameValid(carName);
+                    System.out.println(carName);
+                    carNames.add(carName);
+                }
+                break;
+            } catch(IllegalArgumentException e){
+                System.out.println("[ERROR] 자동차 이름이 잘못되었습니다.");
+            }
         }
         return carNames;
     }
