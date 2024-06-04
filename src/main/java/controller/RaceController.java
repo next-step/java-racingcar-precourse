4d5d5c9addb3f303f6c0db0fb4d5dc400cce7df3
kangwlgns <kangji0615@gmail.com>
2024-06-04 18:20:38 +0900

    docs(RaceController): add comments for addCars

--- src/main/java/controller/RaceController.java
+++ src/main/java/controller/RaceController.java
@@ -26,12 +26,16 @@ public class RaceController implements RaceControllerInterface {
 
     }
 
+    // view에서 입력으로 들어온 차를 생성하도록 race에 전달
     @Override
     public void addCars(String[] cars) {
         try {
+            // 생성 시도
             race.addCars(cars);
         } catch (IllegalArgumentException e) {
+            // 5글자를 넘은 경우, error 메시지를 출력하고,
             view.printCarsErrorMessage(e.getMessage());
+            // 다시 view에 입력을 요청
             view.userCarsInput();
         }
     }
