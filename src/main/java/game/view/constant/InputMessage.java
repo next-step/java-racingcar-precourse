eb3118cf24c645c9f33da8e4ac2bf778234fb630
nove1080 <skwpqjq@gmail.com>
2024-06-05 00:32:43 +0900

    style: InputView RACING_CAR_NAME 이름 변경
    RACING_CAR_NAME -> CAR_NAME

--- src/main/java/game/view/constant/InputMessage.java
+++ src/main/java/game/view/constant/InputMessage.java
@@ -1,7 +1,7 @@
 package game.view.constant;
 
 public enum InputMessage {
-    RACING_CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
+    CAR_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)"),
     ATTEMPT_COUNT("시도할 회수는 몇회인가요?");
 
     private final String description;
