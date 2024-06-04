0a62caee2d0ca1f08f4e908d4cd5f8fb795dfbf3
donghyuun <bomul0524@gmail.com>
2024-06-04 22:25:32 +0900

    fix: field and method fix of GameController

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -4,24 +4,29 @@ import java.util.Scanner;
 public class GameController {
 
     private Scanner scanner;
-    private ArrayList<String> carNamesList = new ArrayList<>();
-    private int tryCount;
 
     public GameController() {
         this.scanner = new Scanner(System.in);
     }
 
-    public void inputCarNames(){
+    public String[] inputCarNames(){
         System.out.println("Enter the names of the cars to race (names should be separated by commas)");
         String names = scanner.nextLine();
         String[] carNames = names.split(",");
-        for (String carName : carNames) {
-            carNamesList.add(carName);
+
+        for (int i = 0; i < carNames.length; i++) {
+            carNames[i] = carNames[i].trim();
         }
 
+        return carNames;
+    }
+
+    public int inputTryCount() {
         System.out.println("Enter count for try");
-        this.tryCount = scanner.nextInt();
+        int tryCount = scanner.nextInt();
         System.out.println("tryCount = " + tryCount);
+
+        return tryCount;
     }
 
     public ArrayList<String> getCarList() {
