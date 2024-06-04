443dd7326a58d8debb2dded1c68a960ef78d5741
donghyuun <bomul0524@gmail.com>
2024-06-04 21:44:45 +0900

    feat: create Car class

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class Car {
+public class Car {
+
+    private String name;
+    private int distance;
+
+    public Car(String name) {
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public void move(){
+        this.distance += 1;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
 }
+
