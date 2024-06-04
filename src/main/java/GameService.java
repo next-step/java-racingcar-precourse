4c3899b40d3cadd74c979bba539efc9d3ae14250
donghyuun <bomul0524@gmail.com>
2024-06-04 22:26:23 +0900

    feat: create GameService class

--- src/main/java/GameService.java
+++ src/main/java/GameService.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class GameService {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class GameService {
+
+    private ArrayList<Car> cars = new ArrayList<>();
+    private int tryCount;
+    private ArrayList<String> winCarsName = new ArrayList<>();
+
+    public GameService(int tryCount, String[] carNames) {
+        this.tryCount = tryCount;
+        for (String carName : carNames) {
+            cars.add(new Car(carName));
+        }
+    }
+
+    public ArrayList<Car> getCars() {
+        return cars;
+    }
+
+    public int getTryCount() {
+        return tryCount;
+    }
+
+    public ArrayList<String> getWinCarsName() {
+        return winCarsName;
+    }
+
+
 }
