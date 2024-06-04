add643497599c7974a1d6e7637bc8af069d9baa1
kangwlgns <kangji0615@gmail.com>
2024-06-04 13:57:15 +0900

    feat(RaceView): add userCarsInput method

--- src/main/java/view/RaceView.java
+++ src/main/java/view/RaceView.java
@@ -1,6 +1,8 @@
 package view;
 
 import controller.RaceControllerInterface;
+import java.util.Arrays;
+import java.util.Scanner;
 import observer.Observer;
 import service.RaceServiceInterface;
 
@@ -14,9 +16,19 @@ public class RaceView implements Observer, RaceViewInterface {
 
     }
 
+    // 자동차 이름을 입력받는 메서드
     @Override
     public void userCarsInput() {
+        // 안내문 출력
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
 
+        // 유저의 입력을 받고 token으로 쪼개기
+        Scanner sc = new Scanner(System.in);
+        String userInput = sc.nextLine();
+        String[] cars = userInput.split(",");
+
+        // Controller에게 유저의 입력을 전달
+        raceControllerInterface.addCars(cars);
     }
 
     @Override
