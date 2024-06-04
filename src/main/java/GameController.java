7de1d8bd63d30b3fc0f13937adb7076b80775da7
donghyuun <bomul0524@gmail.com>
2024-06-04 22:07:05 +0900

    feat: input car names & try count

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -4,13 +4,27 @@ import java.util.Scanner;
 public class GameController {
 
     private Scanner scanner;
-    private ArrayList<Car> carList = new ArrayList<>();
+    private ArrayList<String> carList = new ArrayList<>();
     private int tryCount;
 
     public GameController() {
+        this.scanner = new Scanner(System.in);
     }
 
-    public ArrayList<Car> getCarList() {
+    public void inputCarNames(){
+        System.out.println("Enter the names of the cars to race (names should be separated by commas)");
+        String names = scanner.nextLine();
+        String[] carNames = names.split(",");
+        for (String carName : carNames) {
+            carList.add(carName.trim());
+        }
+
+        System.out.println("Enter count for try");
+        this.tryCount = scanner.nextInt();
+        System.out.println("tryCount = " + tryCount);
+    }
+
+    public ArrayList<String> getCarList() {
         return carList;
     }
 
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,8 @@
 public class main {
     public static void main(String[] args) {
+        // get input(Car names & tryCount) from gameController
+        GameController gameController = new GameController();
+        gameController.inputCarNames();
 
     }
 }
