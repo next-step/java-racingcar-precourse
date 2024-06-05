b515610c0a60273fedb2f31bce3de25c562c8046
kangwlgns <kangji0615@gmail.com>
2024-06-05 16:23:28 +0900

    feat(CarService): add constructor

--- src/main/java/service/CarService.java
+++ src/main/java/service/CarService.java
@@ -1,10 +1,16 @@
 package service;
 
+import model.Car;
 import model.CarModelInterface;
 
 public class CarService implements CarServiceInterface {
 
     private CarModelInterface car;
+
+    // 생성자
+    public CarService(String name, int progress) {
+        car = new Car(name, progress);
+    }
 
     public void goForward() {
 
