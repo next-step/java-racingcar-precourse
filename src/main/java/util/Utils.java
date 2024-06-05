89176deab62e87ae6c82330f2cc3f08e9a9648a5
us4c0d3 <jwseok312@gmail.com>
2024-06-06 01:04:50 +0900

    feat($Utils): Add `Utils.parseCarNames` method

--- src/main/java/util/Utils.java
+++ src/main/java/util/Utils.java
@@ -1,10 +1,16 @@
 package util;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class Utils {
     public static int generateRandomNumber() {
         Random random = new Random();
         return random.nextInt(10);
+    }
+
+    public static List<String> parseCarNames(String input) {
+        return Arrays.asList(input.split(","));
     }
 }
--- src/test/java/util/UtilsTest.java
+++ src/test/java/util/UtilsTest.java
@@ -3,6 +3,9 @@ package util;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +18,20 @@ class UtilsTest {
 
         // then
         assertThat(result).isBetween(0, 9);
+    }
+
+    @Test
+    @DisplayName("parseCarNames test")
+    void parseCarNamesTest() throws Exception {
+        // given
+        String input = "pobi,woni,jun";
+        List<String> expected = new ArrayList<>(Arrays.asList("pobi", "woni", "jun"));
+
+        // when
+        List<String> result = Utils.parseCarNames(input);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+
     }
 }
