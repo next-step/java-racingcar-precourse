ccad177ec82fea4b2fc2afd918394e0d1e1c2b55
dvlprkoji <koji4321@naver.com>
2024-06-05 14:13:19 +0900

    feat: UI 메시지 추가

---
+++ src/main/java/view/message/ErrorMessage.java
@@ -0,0 +1,21 @@
+package view.message;
+
+public enum ErrorMessage {
+    CAR_NAME_LENGTH_EXCEPTION_MESSAGE("자동차 이름은 5자 이하만 가능합니다"),
+    CAR_NAME_DUPLICATE_EXCEPTION_MESSAGE("자동차 이름이 중복되었습니다"),
+    INVALID_TRIAL_NUMBER_EXCEPTION_MESSAGE("1 이상의 숫자만 입력해 주세요")
+
+    ;
+
+    private final String message;
+    private final String errorPrefixMessage = "[ERROR]";
+
+    ErrorMessage(String message) {
+        this.message = this.errorPrefixMessage + message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
