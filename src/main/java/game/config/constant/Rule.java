1c85f3098b33c4d3c2baff645c3200a38cf42b07
nove1080 <skwpqjq@gmail.com>
2024-06-06 01:49:51 +0900

    feat: Rule 상수 클래스 추가

---
+++ src/main/java/game/config/constant/Rule.java
@@ -0,0 +1,11 @@
+package game.config.constant;
+
+public class Rule {
+
+    private Rule() {
+    }
+
+    public static String NAME_DELIMITER = ",";
+    public static int MAX_NAME_LENGTH = 5;
+
+}
