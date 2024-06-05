9195ba0e0fae18f2f9d140cad85a5de28ab72af4
coco3x <inferior3x@gmail.com>
2024-06-05 23:43:05 +0900

    fix: 밸류로 변경

--- src/main/java/katecam/racingcar/domain/Car.java
+++ src/main/java/katecam/racingcar/domain/Car.java
@@ -1,20 +1,31 @@
 package katecam.racingcar.domain;
 
 
-public class Car {
+import java.util.Random;
+
+public final class Car {
     private final String name;
-    private int position;
+    private final int position;
+    private static final Random random;
+    static {
+        random = new Random();
+    }
 
     public Car(String name) {
+        this(name, 0);
+    }
+
+    public Car(String name, int position) {
         this.name = name;
+        this.position = position;
     }
 
     public String getName() {
         return name;
     }
 
-    public void move(){
-        position++;
+    public Car move(){
+        return (random.nextInt(10) >= 4) ? new Car(this.name, this.position + 1) : this;
     }
 
     public int getPosition() {
--- src/main/java/katecam/racingcar/domain/Game.java
+++ src/main/java/katecam/racingcar/domain/Game.java
@@ -1,46 +1,29 @@
 package katecam.racingcar.domain;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Random;
-import java.util.stream.Collectors;
 
 public class Game {
-    private final List<Car> cars;
+    private final Cars cars;
     private final int numberToAttempt;
     private int attemptedNumber;
-    private final Random random;
 
     public Game(List<String> carNames, int carNumber) {
-        this.cars = carNames.stream()
-                .map(Car::new)
-                .toList();
+        this.cars = Cars.of(carNames);
         this.numberToAttempt = carNumber;
-        this.random = new Random();
     }
 
-    //UnmodifiableList
     public List<Car> getCars(){
-        return this.cars;
+        return this.cars.getCars();
     }
 
     public List<Car> getWinners(){
         if (!isEnded()) throw new IllegalStateException("게임 종료 안됨");
-        return this.cars.stream()
-                .collect(Collectors.groupingBy(Car::getPosition))
-                .entrySet().stream()
-                .max(Map.Entry.comparingByKey())
-                .map(Entry::getValue)
-                .orElseGet(List::of);
+        return cars.getWinners();
     }
 
     public void playTurn() {
         if (isEnded()) throw new IllegalStateException("게임이 이미 종료됨");
-        cars.forEach(car->{
-            if (random.nextInt(10) >= 4)
-                car.move();
-        });
+        cars.moveAll();
         attemptedNumber++;
     }
 
