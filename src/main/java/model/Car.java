53d6998b40ea9d78ed4a5cd8216d726a0a3d8eb3
us4c0d3 <jwseok312@gmail.com>
2024-06-06 01:54:40 +0900

    refactor($Car): create constructor for testing
    * 테스트를 위한 생성자 추가(테스트용 RandomNumberGenerator 사용)
    * 기존 생성자는 테스트용 생성자를 호출하며 기존 RandomNumberGenerator 사용

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,15 +1,26 @@
 package model;
 
+import util.ITestRandomNumberGenerator;
+import util.RandomNumberGenerator;
+
 public class Car {
     private String name;
     private int position;
+    private ITestRandomNumberGenerator randomNumberGenerator;
 
+    // 프로덕션용 생성자
     public Car(String name) {
+        this(name, new RandomNumberGenerator());
+    }
+
+    // 테스트용 생성자
+    public Car(String name, ITestRandomNumberGenerator randomNumberGenerator) {
         if(name == null || name.length() > 5) {
             throw new IllegalArgumentException("[ERROR] 자동차의 이름은 5자 이하만 가능합니다.");
         }
         this.name = name;
         this.position = 0;
+        this.randomNumberGenerator = randomNumberGenerator;
     }
 
     public String getName() { return name; }
