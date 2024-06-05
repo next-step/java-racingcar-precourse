3bd147c454346098573841008ab6932f63b54225
daolove0323 <daolove0323@gmail.com>
2024-06-05 14:48:35 +0900

    refactor: refactor Controller class

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,5 +1,6 @@
 package controller;
 
+import java.util.InputMismatchException;
 import model.Car;
 import model.GameModel;
 import view.View;
@@ -11,8 +12,8 @@ public class Controller {
 
     private static final int MAX_CAR_NAME_LENGTH = 5;
     private static Controller instance;
-    GameModel gameModel;
-    View view;
+    private GameModel gameModel;
+    private View view;
 
     private Controller() {
         gameModel = new GameModel();
@@ -26,56 +27,98 @@ public class Controller {
         return instance;
     }
 
-    private boolean areCarNameLengthsValid(String[] carNames) {
-        for (String carName : carNames) {
-            if (carName.length() > MAX_CAR_NAME_LENGTH) {
-                return false;
-            }
-        }
-        return true;
+    public void clear() {
+        gameModel = new GameModel();
+        view = new View();
     }
 
-    private boolean areCarNamesUnique(String[] carNames) {
+    public GameModel getGameModel() {
+        return gameModel;
+    }
+
+    private void validateCarNameLength(String[] carNames) throws IllegalArgumentException {
+        for (String carName : carNames) {
+            if (MAX_CAR_NAME_LENGTH < carName.length() || carName.isEmpty()) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void validateCarNameUniqueness(String[] carNames) throws IllegalStateException {
         HashSet<String> uniqueCarNames = new HashSet<>();
         for (String carName : carNames) {
             if (uniqueCarNames.contains(carName)) {
-                return false;
+                throw new IllegalStateException();
             }
             uniqueCarNames.add(carName);
         }
-        return true;
     }
 
-    private void getCarNames() {
-        boolean isAbnormal = false;
-        String[] carNames = view.requestCarNames().split(",");
+    private void validateCarNameCount(String[] carNames) throws NonCarException {
+        if (carNames.length == 0) {
+            throw new NonCarException();
+        }
+    }
+
+    public boolean areCarNamesValid(String[] carNames) {
+        boolean isValid = true;
         try {
-            if (!areCarNameLengthsValid(carNames)) {
+            validateCarNameLength(carNames);
+            validateCarNameUniqueness(carNames);
+            validateCarNameCount(carNames);
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] : 자동차 이름의 길이는 1 ~ 5 이어야 합니다.\n");
+            isValid = false;
+        } catch (IllegalStateException e) {
+            System.out.println("[ERROR] : 자동차 이름은 중복될 수 없습니다.\n");
+            isValid = false;
+        }
+        return isValid;
+    }
+
+    public boolean isRepeatCountValid(String repeatCount) {
+        boolean isValid = true;
+        try {
+            if (Integer.parseInt(repeatCount) < 0) {
                 throw new IllegalArgumentException();
             }
-            if (!areCarNamesUnique(carNames)) {
-                throw new IllegalStateException();
-            }
+        } catch (NumberFormatException e) {
+            System.out.println("[ERROR] : 정수 형태 입력이 아닙니다.\n");
+            isValid = false;
         } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] : 자동차 이름의 길이는 5보다 작거나 같습니다.");
-            System.out.println();
-            isAbnormal = true;
-        } catch (IllegalStateException e) {
-            System.out.println("[ERROR] : 자동차의 이름은 중복될 수 없습니다.");
-            System.out.println();
-            isAbnormal = true;
+            System.out.println("[ERROR] : 반복 횟수는 음이 아닌 정수이어야 합니다.\n");
+            isValid = false;
+        } catch (InputMismatchException e) {
+            System.out.println("[ERROR] : 반복할 횟수는 정수 타입이어야 합니다.\n");
+            isValid = false;
         }
-        if (isAbnormal) {
-            getCarNames();
-            return;
-        }
+        return isValid;
+    }
+
+    public void createCars(String[] carNames) {
         for (String carName : carNames) {
             gameModel.addCar(new Car(carName));
         }
     }
 
-    private void getRepeatCount() {
-        gameModel.setRepeatCount(view.requestRepeatCount());
+    public void setRepeatCount(int repeatCount) {
+        gameModel.setRepeatCount(repeatCount);
+    }
+
+    private String[] getCarNames() {
+        String[] carNames = view.requestCarNames().split(",");
+        if (!areCarNamesValid(carNames)) {
+            return getCarNames();
+        }
+        return carNames;
+    }
+
+    private int getRepeatCount() {
+        String repeatCount = view.requestRepeatCount();
+        if (!isRepeatCountValid(repeatCount)) {
+            return getRepeatCount();
+        }
+        return Integer.parseInt(repeatCount);
     }
 
     private void runOneCycle() {
@@ -106,7 +149,7 @@ public class Controller {
         return maxDist;
     }
 
-    private LinkedList<String> getWinners() {
+    public LinkedList<String> getWinners() {
         LinkedList<String> winners = new LinkedList<>();
         for (int i = 0; i < gameModel.getCarsCount(); ++i) {
             if (gameModel.getCar(i).getPos() == getMaxDistance()) {
@@ -117,9 +160,19 @@ public class Controller {
     }
 
     public void play() {
-        getCarNames();
-        getRepeatCount();
+        createCars(getCarNames());
+        setRepeatCount(getRepeatCount());
         run();
         view.printWinners(getWinners());
+    }
+}
+
+class NonCarException extends IllegalArgumentException {
+
+    public NonCarException() {
+    }
+
+    public NonCarException(String message) {
+        super(message);
     }
 }
