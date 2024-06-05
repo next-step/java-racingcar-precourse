837a7441e0e3287eedaf13d81540c252f52dd125
nove1080 <skwpqjq@gmail.com>
2024-06-06 01:47:33 +0900

    feat: ErrorMessage 중복 이름에 대한 메시지 추가

--- src/main/java/game/exception/constant/ErrorMessage.java
+++ src/main/java/game/exception/constant/ErrorMessage.java
@@ -4,8 +4,8 @@ import static game.config.constant.Rule.*;
 
 public enum ErrorMessage {
 
-    EXCEED_NAME_LENGTH("길이 %d를 초과하여 이름을 생성할 수 없습니다!".formatted(MAX_NAME_LENGTH));
-
+    EXCEED_NAME_LENGTH("길이 %d를 초과하여 이름을 생성할 수 없습니다!".formatted(MAX_NAME_LENGTH)),
+    DUPLICATE_NAME_FOUND("중복된 이름이 존재합니다!");
     private final String message;
 
     ErrorMessage(String message) {
