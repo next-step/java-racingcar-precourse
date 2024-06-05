d16b7e85cf01322857d3bab986e065ea96c1b967
nove1080 <skwpqjq@gmail.com>
2024-06-05 14:22:11 +0900

    feat: CarNameValidator 선언

---
+++ src/main/java/game/validator/CarNameValidator.java
@@ -0,0 +1,28 @@
+package game.validator;
+
+import java.util.StringTokenizer;
+
+public class CarNameValidator {
+
+    private CarNameValidator() {
+    }
+
+    public void validate(String input) {
+        validateDelimiter(input);
+        validateNameLength(input);
+        validateDuplication(input);
+    }
+
+    private void validateDelimiter(String input) {
+
+    }
+
+    private void validateNameLength(String input) {
+
+    }
+
+    private void validateDuplication(String input) {
+
+    }
+
+}
