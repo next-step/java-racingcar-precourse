b5e3af648ae76875cf6e96458dbc9d308e0b965a
nove1080 <skwpqjq@gmail.com>
2024-06-06 01:27:49 +0900

    feat: CarNameValidator 이름 길이 검증 메서드 추가

--- src/main/java/game/validator/CarNameValidator.java
+++ src/main/java/game/validator/CarNameValidator.java
@@ -1,5 +1,10 @@
 package game.validator;
 
+import static game.config.constant.Rule.*;
+
+import game.exception.constant.ErrorMessage;
+import java.util.Arrays;
+
 public class CarNameValidator {
 
     private static final CarNameValidator instance = new CarNameValidator();
@@ -17,7 +22,13 @@ public class CarNameValidator {
     }
 
     private void validateNameLength(String input) {
-
+        String[] names = input.split(NAME_DELIMITER);
+        Arrays.stream(names)
+            .filter(name -> name.length() > MAX_NAME_LENGTH)
+            .findAny()
+            .ifPresent(name -> {
+                throw new IllegalArgumentException(ErrorMessage.EXCEED_NAME_LENGTH.getMessage());
+            });
     }
 
     private void validateDuplication(String input) {
