8ad8f59c5a596bfcc31e14cb1bdf20252faf4efd
kangwlgns <kangji0615@gmail.com>
2024-06-05 15:27:18 +0900

    feat(RaceModelInterface): change a parameter's type of setNumberOfRounds method

--- src/main/java/model/RaceModelInterface.java
+++ src/main/java/model/RaceModelInterface.java
@@ -9,7 +9,7 @@ public interface RaceModelInterface {
 
     public int getNumberOfRounds();
 
-    public void setNumberOfRounds(String numberOfRounds);
+    public void setNumberOfRounds(int numberOfRounds);
 
     public ArrayList<CarServiceInterface> getCars();
 }
