9a84ddb8e03c49c9d792bebfeb4bb9f2484e6250
Lucerna00 <pjs3859811@naver.com>
2024-06-06 20:57:40 +0900

    feat(Output.java): add progress output

---
+++ src/main/java/view/Output.java
@@ -0,0 +1,12 @@
+package view;
+
+public class Output {
+
+    public static void printCarNamesProgress() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    }
+
+    public static void printNumberProgress() {
+        System.out.println("시도할 횟수는 몇회인가요?");
+    }
+}
