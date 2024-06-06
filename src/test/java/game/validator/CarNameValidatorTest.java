e77fd91ec8e0dd4a6fd682fba7e0c6166b574296
nove1080 <skwpqjq@gmail.com>
2024-06-06 15:45:05 +0900

    style: CarNameValidatorTest DisplayName 마침표 제거

--- src/test/java/game/validator/CarNameValidatorTest.java
+++ src/test/java/game/validator/CarNameValidatorTest.java
@@ -11,13 +11,13 @@ class CarNameValidatorTest {
 
     CarNameValidator carNameValidator = CarNameValidator.getInstance();
 
-    @ParameterizedTest(name = "{0}은 적절한 이름 입력입니다.")
+    @ParameterizedTest(name = "{0}은 적절한 이름 입력입니다")
     @ValueSource(strings = {"en.zo,silva,james","silva","_enzo,en zo,enzo "})
     void 적절한_이름_입력에_대해_예외를_던지지_않습니다(String input) {
         assertDoesNotThrow(() -> carNameValidator.validate(input));
     }
 
-    @ParameterizedTest(name = "{0}은 길이를 초과한 이름 입력입니다.")
+    @ParameterizedTest(name = "{0}은 길이를 초과한 이름 입력입니다")
     @ValueSource(strings = {"thiago,sil   ","en      ,zo"})
     void 부적절한_이름_길이는_예외를_발생시킵니다(String input) {
         IllegalArgumentException error = assertThrows(
@@ -26,7 +26,7 @@ class CarNameValidatorTest {
         assertThat(error.getMessage()).isEqualTo(ErrorMessage.EXCEED_NAME_LENGTH.getMessage());
     }
 
-    @ParameterizedTest(name = "{0}은 중복이 존재하는 이름 입력입니다.")
+    @ParameterizedTest(name = "{0}은 중복이 존재하는 이름 입력입니다")
     @ValueSource(strings = {"enzo,bruno,enzo","james,james,james"})
     void 중복이_존재하는_이름은_예외를_발생시킵니다(String input) {
         IllegalArgumentException error = assertThrows(
