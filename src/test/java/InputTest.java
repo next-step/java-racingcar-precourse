6a72ad5902c4fe21925055598071c9e0ed91046e
donghyuun <bomul0524@gmail.com>
2024-06-06 15:13:33 +0900

    feat: add input car names split&trim test method

--- src/test/java/InputTest.java
+++ src/test/java/InputTest.java
@@ -3,6 +3,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class InputTest {
@@ -17,15 +20,27 @@ public class InputTest {
 
     @Test
     @DisplayName("car names length validation")
-    void inputCarNamesValid() {
+    void inputCarNamesLengthValid() {
         // given
         String names = "honda, mizda  , genesis,kia";
         // when
-        String[] carNames = names.split(",");
+        String[] carNames = Arrays.stream(names.split(",")).map(String::trim).toArray(String[]::new);
         // then
         assertThrows(IllegalArgumentException.class, () -> {
             gameController.checkNameLength(carNames);
         });
+    }
+    @Test
+    @DisplayName("car names length validation")
+    void inputCarNamesSplitValid() {
+        // given
+        String names = "honda,mizda  , kia  ";
+        // when
+        String[] carNames = Arrays.stream(names.split(",")).map(String::trim).toArray(String[]::new);
+        // then
+        assertThat(carNames[0]).isEqualTo("honda");
+        assertThat(carNames[1]).isEqualTo("mizda");
+        assertThat(carNames[2]).isEqualTo("kia");
     }
 
     @Test
