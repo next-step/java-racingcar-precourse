b0485bd076234b0aaa470510b45bc16b796e7d2b
us4c0d3 <jwseok312@gmail.com>
2024-06-06 22:54:50 +0900

    feat($service): create findWinners method

--- src/main/java/service/GameService.java
+++ src/main/java/service/GameService.java
@@ -1,5 +1,6 @@
 package service;
 
+import java.util.ArrayList;
 import java.util.List;
 import model.Car;
 
@@ -14,6 +15,27 @@ public class GameService {
         for(Car car : cars) {
             car.move();
         }
+    }
+
+    public List<Car> findWinners(List<Car> cars) {
+        int winnerPosition = findMaxPosition(cars);
+        List<Car> winners = new ArrayList<>();
+        for(Car car : cars) {
+            if(car.getPosition() == winnerPosition) {
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
+
+    private int findMaxPosition(List<Car> cars) {
+        int max = -1;
+        for(Car car : cars) {
+            if(max < car.getPosition()) {
+                max = car.getPosition();
+            }
+        }
+        return max;
     }
 
 }
---
+++ src/test/java/service/GameServiceTest.java
@@ -0,0 +1,43 @@
+package service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Car;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import util.IRandomNumberGenerator;
+import util.TestRandomNumberGenerator;
+
+class GameServiceTest {
+    @Test
+    @DisplayName("findWinners test")
+    void findWinnersTest() throws Exception {
+        // given
+        GameService service = new GameService();
+        IRandomNumberGenerator winTestNumberGenerator = new TestRandomNumberGenerator(6);
+        IRandomNumberGenerator loseTestNumberGenerator = new TestRandomNumberGenerator(2);
+        Car win1 = new Car("win1", winTestNumberGenerator);
+        Car win2 = new Car("win2", winTestNumberGenerator);
+        Car lose = new Car("lose", loseTestNumberGenerator);
+
+        // input data
+        List<Car> input = new ArrayList<>();
+        input.add(win1);
+        input.add(lose);
+        input.add(win2);
+        service.moveCars(input);
+
+        List<Car> expected = new ArrayList<>();
+        expected.add(win1);
+        expected.add(win2);
+
+        // when
+        List<Car> result = service.findWinners(input);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+}
