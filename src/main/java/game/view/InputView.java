434e028ea7ae099e8f234f49462ee0c088f335cb
nove1080 <skwpqjq@gmail.com>
2024-06-06 12:34:32 +0900

    feat: InputView 자동차 이름 입력 메서드 추가

--- src/main/java/game/view/InputView.java
+++ src/main/java/game/view/InputView.java
@@ -1,12 +1,29 @@
 package game.view;
 
+import game.config.constant.Rule;
+import game.validator.CarNameValidator;
+import game.view.constant.InputMessage;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
-    private Scanner scanner = new Scanner(System.in);
+    private final Scanner scanner;
+    private final CarNameValidator carNameValidator;
 
-    private InputView() {
+    public InputView() {
+        scanner = new Scanner(System.in);
+        carNameValidator = CarNameValidator.getInstance();
+    }
+
+    public List<String> enterCarNames() {
+        System.out.println(InputMessage.CAR_NAME);
+
+        String input = scanner.next();
+        carNameValidator.validate(input);
+
+        return Arrays.asList(input.split(Rule.NAME_DELIMITER));
     }
 
 }
