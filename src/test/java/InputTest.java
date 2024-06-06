7c3051a86b07d5a6e9ca36f49002672a4d6a7a47
donghyuun <bomul0524@gmail.com>
2024-06-06 14:42:41 +0900

    feat: add InputTest class

--- src/test/java/InputTest.java
+++ src/test/java/InputTest.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class InputTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InputTest {
+
+    GameController gameController;
+
+    @BeforeEach
+    @DisplayName("initialization")
+    void setUp() {
+        gameController = new GameController();
+    }
+
+    @Test
+    @DisplayName("car names length validation")
+    void inputCarNamesValid() {
+        // given
+        String names = "honda, mizda  , genesis,kia";
+        // when
+        String[] carNames = names.split(",");
+        // then
+        assertThrows(IllegalArgumentException.class, () -> {
+            gameController.checkNameLength(carNames);
+        });
+    }
+
+    @Test
+    @DisplayName("car race try count validation")
+    void inputTryCountValid() {
+        // given
+        int tryCount;
+        // when
+        tryCount = 0;
+        // then
+        assertThrows(IllegalArgumentException.class, () -> {
+            gameController.checkTryCountValue(tryCount);
+        });
+    }
 }
