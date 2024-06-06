d35ddbca64a828bc8f0b54205c21c67b25575393
donghyuun <bomul0524@gmail.com>
2024-06-06 15:03:12 +0900

    refactor: refactoring GameContolelr class

--- src/main/java/GameController.java
+++ src/main/java/GameController.java
@@ -17,21 +17,17 @@ public class GameController {
             String names = scanner.nextLine();
             carNames = names.split(",");
 
-            for (int i = 0; i < carNames.length; i++) {
-                carNames[i] = carNames[i].trim();
-            }
+            for (int i = 0; i < carNames.length; i++) { carNames[i] = carNames[i].trim(); }
             try {
                 checkNameLength(carNames);// if wrong: throws IllegalArgumentException
-                break; // correct
+                return carNames; // correct
             } catch (IllegalArgumentException e) {
                 System.out.println("[ERROR] The name of the car must be within 5 characters. please type again");
             }
-
         }
-        return carNames;
     }
 
-    private void checkNameLength(String[] carNames) {
+    public void checkNameLength(String[] carNames) {
         for (String carName : carNames) {
             if(carName.length() > 5){
                 throw new IllegalArgumentException();
@@ -46,7 +42,7 @@ public class GameController {
                 System.out.println("Enter count for try");
                 tryCount = scanner.nextInt();
                 checkTryCountValue(tryCount);
-                break;
+                return tryCount;
             } catch (InputMismatchException e) {
                 System.out.println("wrong value type, please type again");
                 scanner.nextLine();  // make input buffer empty
@@ -54,19 +50,12 @@ public class GameController {
                 System.out.println("wrong value, value should be more than 0, please type again");
             }
         }
-        System.out.println("tryCount = " + tryCount);
-
-        return tryCount;
     }
 
-    private void checkTryCountValue(int tryCount) {
+    public void checkTryCountValue(int tryCount) {
         if (tryCount <= 0) {
             throw new IllegalArgumentException();
         }
     }
-
-    private void checkTryCountType(int tryCount) {
-    }
-
 
 }
