0d95f651e186fd925cfa5bdd8952ced4e3c61b42
Kim Sanghae <pelikan@pusan.ac.kr>
2024-06-07 04:53:30 +0900

    fix: debug blank strip stream

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,16 +1,18 @@
 package view;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static void printCarInputQuestion() {
+    private static void printCarInputQuestion() {
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public static void printIterationInputQuestion() {
+    private static void printIterationInputQuestion() {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 
@@ -26,9 +28,12 @@ public class InputView {
         try {
             List<String> splitedCarNames = List.of(scanner.nextLine()
                     .split(","));
+            splitedCarNames = splitedCarNames.stream()
+                    .map(String::strip)
+                    .toList();
             validateSplitedCarName(splitedCarNames);
             return splitedCarNames;
-        } catch (IllegalArgumentException e) {
+        } catch (IllegalArgumentException | NoSuchElementException e) {
             System.out.println("[ERROR] " + e.getMessage());
         }
         return null;
@@ -36,11 +41,20 @@ public class InputView {
 
     private static void validateSplitedCarName(List<String> splitCarNmae) {
         for (String s : splitCarNmae) {
-            validateCarName(s);
+            validateCarNameNull(s);
+            validateCarNameLength(s);
         }
     }
 
-    private static void validateCarName(String s) {
+    private static void validateCarNameNull(String s) {
+        if (s.isEmpty()) {
+            // System.out.println(s);
+            throw new IllegalArgumentException("공백은 허용 되지 않습니다.");
+        }
+    }
+
+
+    private static void validateCarNameLength(String s) {
         if (s.length() > 5) {
             throw new IllegalArgumentException("자동차의 이름은 5자를 초과할 수 없습니다.");
         }
