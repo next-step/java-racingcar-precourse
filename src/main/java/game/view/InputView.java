e724efb3fea44860a46e1a936f76265485baf8a6
nove1080 <skwpqjq@gmail.com>
2024-06-06 15:44:30 +0900

    feat: InputView 시도 횟수 입력 메서드 추가

--- src/main/java/game/view/InputView.java
+++ src/main/java/game/view/InputView.java
@@ -1,6 +1,7 @@
 package game.view;
 
 import game.config.constant.Rule;
+import game.validator.AttemptCountValidator;
 import game.validator.CarNameValidator;
 import game.view.constant.InputMessage;
 import java.util.Arrays;
@@ -11,10 +12,12 @@ public class InputView {
 
     private final Scanner scanner;
     private final CarNameValidator carNameValidator;
+    private final AttemptCountValidator attemptCountValidator;
 
     public InputView() {
         scanner = new Scanner(System.in);
         carNameValidator = CarNameValidator.getInstance();
+        attemptCountValidator = AttemptCountValidator.getInstance();
     }
 
     public List<String> enterCarNames() {
@@ -26,4 +29,17 @@ public class InputView {
         return Arrays.asList(input.split(Rule.NAME_DELIMITER));
     }
 
+    /**
+     * 시도 횟수를 입력 받습니다.
+     *
+     * @return 시도 횟수
+     */
+    public int enterAttemptCount() {
+        System.out.println(InputMessage.ATTEMPT_COUNT);
+
+        String input = scanner.next();
+        attemptCountValidator.validate(input);
+
+        return Integer.parseInt(input);
+    }
 }
