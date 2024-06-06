56693176b65bc914654745857b147427c5f366f4
donghyeon <thornappl2@gmail.com>
2024-06-07 01:44:00 +0900

    feat: OutputView 작성 - 사용자에게 결과를 출력해주는 OutputView 클래스 작성 - printRace, printWinners 함수 추가

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,17 @@
+package racingcar.view;
+
+import racingcar.model.Car;
+
+import java.util.List;
+
+public class OutputView {
+    public static void printRace(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+        }
+    }
+
+    public static void printWinners(List<String> winners) {
+        System.out.println("최종 우승자 : " + String.join(", ", winners));
+    }
+}
