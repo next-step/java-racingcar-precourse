e105eaa70ac5848e5f1589ab9f8587bba61dc74a
Lucerna00 <pjs3859811@naver.com>
2024-06-07 01:12:07 +0900

    feat(Game.java): add number input process to game view

--- src/main/java/view/Game.java
+++ src/main/java/view/Game.java
@@ -6,17 +6,33 @@ import controller.GameController;
 public class Game {
 
     private GameController gameController = new GameController();
+    private int number;
 
     public void start() {
-        carNamesProgress();
+        carNamesProcess();
+        numberProcess();
     }
 
-    public void carNamesProgress() {
+    public void carNamesProcess() {
         Output.printCarNamesProgress();
         while (true) {
             String[] carNames = Input.scanCarNames();
             try {
                 this.gameController.setCars(carNames);
+                break;
+            } catch (IllegalArgumentException e) {
+                Output.printException();
+            }
+        }
+    }
+
+    public void numberProcess() {
+        Output.printNumberProgress();
+        while (true) {
+            String strNumber = Input.scanNumber();
+            try {
+                Validation.checkNumber(strNumber);
+                this.number = Integer.parseInt(strNumber);
                 break;
             } catch (IllegalArgumentException e) {
                 Output.printException();
