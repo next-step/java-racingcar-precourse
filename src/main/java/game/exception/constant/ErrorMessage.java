8ecaee9755190cb6eaa628b1c674c53192411fda
nove1080 <skwpqjq@gmail.com>
2024-06-06 12:41:29 +0900

    feat: ErrorMessage [ERROR] 라벨 적용

--- src/main/java/game/exception/constant/ErrorMessage.java
+++ src/main/java/game/exception/constant/ErrorMessage.java
@@ -3,9 +3,9 @@ package game.exception.constant;
 import static game.config.constant.Rule.*;
 
 public enum ErrorMessage {
-
-    EXCEED_NAME_LENGTH("길이 %d를 초과하여 이름을 생성할 수 없습니다!".formatted(MAX_NAME_LENGTH)),
-    DUPLICATE_NAME_FOUND("중복된 이름이 존재합니다!");
+    
+    EXCEED_NAME_LENGTH("[ERROR] 길이 %d를 초과하여 이름을 생성할 수 없습니다!".formatted(MAX_NAME_LENGTH)),
+    DUPLICATE_NAME_FOUND("[ERROR] 중복된 이름이 존재합니다!");
     private final String message;
 
     ErrorMessage(String message) {
