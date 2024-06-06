d19dba67b27bece6a61405264267f30f1bdbdc75
us4c0d3 <jwseok312@gmail.com>
2024-06-06 22:59:45 +0900

    test($service): create test code for moveCars method

--- src/test/java/service/GameServiceTest.java
+++ src/test/java/service/GameServiceTest.java
@@ -13,6 +13,33 @@ import util.TestRandomNumberGenerator;
 
 class GameServiceTest {
     @Test
+    @DisplayName("moveCars test")
+    void moveCarsTest() throws Exception {
+        // given
+        GameService service = new GameService();
+        IRandomNumberGenerator moveTestNumberGenerator = new TestRandomNumberGenerator(6);
+        IRandomNumberGenerator notMoveTestNumberGenerator = new TestRandomNumberGenerator(2);
+        Car move1 = new Car("win1", moveTestNumberGenerator);
+        Car move2 = new Car("win2", moveTestNumberGenerator);
+        Car not1 = new Car("lose", notMoveTestNumberGenerator);
+
+        List<Car> resultCars = new ArrayList<>();
+        resultCars.add(move1);
+        resultCars.add(move2);
+        resultCars.add(not1);
+
+        // when
+        service.moveCars(resultCars);
+        service.moveCars(resultCars);
+
+        // then
+        assertThat(move1.getPosition()).isEqualTo(2);
+        assertThat(move2.getPosition()).isEqualTo(2);
+        assertThat(not1.getPosition()).isEqualTo(0);
+
+    }
+
+    @Test
     @DisplayName("findWinners test")
     void findWinnersTest() throws Exception {
         // given
