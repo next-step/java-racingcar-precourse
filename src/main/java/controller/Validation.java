7e6fbf0a3564fca7688ae2c589a7cb5a520019c6
Lucerna00 <pjs3859811@naver.com>
2024-06-06 18:18:20 +0900

    feat(Validation.java): add method to check if there are duplicate car names

--- src/main/java/controller/Validation.java
+++ src/main/java/controller/Validation.java
@@ -1,6 +1,8 @@
 package controller;
 
 import java.util.regex.Pattern;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Validation {
 
@@ -13,5 +15,16 @@ public class Validation {
 
     public static boolean isEnglish(String carName) {
         return Pattern.matches("^[a-zA-Z]*$", carName);
+    }
+
+    public static boolean isDuplication(String[] carNames) {
+        Set<String> set = new HashSet<>();
+
+        for (String element : carNames) {
+            if (!set.add(element)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
