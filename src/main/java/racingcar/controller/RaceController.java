cde41b9c9d4cd4b70184320b22334be40a87d9ba
donghyeon <thornappl2@gmail.com>
2024-06-07 01:48:33 +0900

    feat: RaceController 작성 - 사용자의 입력을 받아 모델을 업데이트하고, 결과를 뷰에 전달하는 RaceController 클래스 작성 - 기능 미구현

---
+++ src/main/java/racingcar/controller/RaceController.java
@@ -0,0 +1,12 @@
+package racingcar.controller;
+
+import racingcar.model.Car;
+import racingcar.model.Race;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+public class RaceController {
+    public void run(){
+        //기능 구현 예정
+    }
+}
