aa0e16c172143fff11274231764c87accbc6e8d3
Kim Sanghae <pelikan@pusan.ac.kr>
2024-06-07 17:41:15 +0900

    feat: Add GameDTO init test and getMovements test

--- src/test/java/model/GameDTOTest.java
+++ src/test/java/model/GameDTOTest.java
@@ -3,28 +3,40 @@ package model;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
 class GameDTOTest {
     @Test
-    void getCarsMovements() {
+    @DisplayName("GameDTO 생성 테스트")
+    public void testCreateGameDTO() throws Exception {
+        // given
+        List<CarDTO> cars = Arrays.asList(new CarDTO("car1"), new CarDTO("car2"), new CarDTO("car3"));
+        int iteration = 3;
+
+        // when
+        GameDTO gameDTO = new GameDTO(cars, iteration);
+
+        // then
+        assertThat(gameDTO.getIteration()).isEqualTo(iteration);
+        assertThat(gameDTO.getCarNames()).containsExactly("car1", "car2", "car3");
     }
 
     @Test
-    void getCarNames() {
-    }
+    @DisplayName("자동차 movemnets 테스트")
+    public void testCarMovements() throws Exception {
+        // given
+        List<CarDTO> cars = Arrays.asList(new CarDTO("car1"), new CarDTO("car2"), new CarDTO("car3"));
+        int iteration = 3;
+        GameDTO gameDTO = new GameDTO(cars, iteration);
 
-    @Test
-    void getIteration() {
-    }
+        // when
+        gameDTO.getCarsMovements();
 
-    @Test
-    void getRecords() {
-    }
-
-    @Test
-    void addMovements() {
+        // then
+        assertThat(gameDTO.getRecords()
+                .get("car1")).isBetween(0, 3);
     }
 }
