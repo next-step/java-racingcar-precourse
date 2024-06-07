9493f935986d2f268c870193c697809ec1906a6d
momnpa333 <momnpa333@gmail.com>
2024-06-07 20:06:47 +0900

    refeat(RacingCarRoundRepositoryImpl.java): 저장 로직 변경

--- src/main/java/domain/RacingCarRound.java
+++ src/main/java/domain/RacingCarRound.java
@@ -1,11 +1,12 @@
 package domain;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RacingCarRound {
 
     private final Integer round;
-    private final List<RacingCar> racingCars;
+    public final List<RacingCar> racingCars;
 
     private RacingCarRound(Integer round, List<RacingCar> racingCars) {
         this.round = round;
@@ -16,4 +17,14 @@ public class RacingCarRound {
         return new RacingCarRound(round, racingCars);
     }
 
+    public List<RacingCar> getRacingCars() {
+        return racingCars;
+    }
+
+    public RacingCarRound copy() {
+        List<RacingCar> copiedCars = racingCars.stream()
+            .map(RacingCar::copy)
+            .collect(Collectors.toList());
+        return new RacingCarRound(this.round, copiedCars);
+    }
 }
