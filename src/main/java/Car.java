76a7617d290741527109156d95645ee858fbbfb0
xkrwjdals <developjmt@gmail.com>
2024-06-07 23:22:04 +0900

    feat : Create CarDTO model named 'Car'
    - Car class has two variable
    - one is String type, name and int type distance
    - Method toString() of Car class is print distance * "-"

---
+++ src/main/java/Car.java
@@ -0,0 +1,34 @@
+public class Car {
+    private String name;
+    private int distance;
+
+    Car(String n, int d) {
+        this.name = n;
+        this.distance = d;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i < this.distance; i++) {
+            sb.append("-");
+        }
+        return sb.toString();
+    }
+}
