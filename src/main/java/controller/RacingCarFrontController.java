23da8b6a7e2565de20bf1d1bf0700669ef0426ef
momnpa333 <momnpa333@gmail.com>
2024-06-07 16:06:39 +0900

    feat(RacingCarFrontController.java): 함수형 인터페이스 제작

--- src/main/java/controller/RacingCarFrontController.java
+++ src/main/java/controller/RacingCarFrontController.java
@@ -1,6 +1,6 @@
 package controller;
 
-import controller.RacingCarRequest.CreateCarRequest;
+import controller.RacingCarRequest;
 import java.util.Scanner;
 
 public class RacingCarFrontController {
@@ -12,10 +12,11 @@ public class RacingCarFrontController {
     }
 
     private boolean CreateRacingCars(Scanner scanner) {
+        // TODO input view 구현
         System.out.println("자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
         try {
             String input = scanner.nextLine();
-            racingCarController.createRacingCar(new CreateCarRequest(input));
+            racingCarController.createRacingCar(new RacingCarRequest.CreateCarRequest(input));
             return true;
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
@@ -25,6 +26,7 @@ public class RacingCarFrontController {
     }
 
     private boolean ChooseRacingCount(Scanner scanner) {
+        // TODO input view 구현
         System.out.println("시도할 회수는 몇회인가요?");
         try {
             int input = scanner.nextInt();
@@ -37,12 +39,23 @@ public class RacingCarFrontController {
         }
     }
 
+    private void executeWithRetry(InputTask task, Scanner scanner) {
+        boolean success = false;
+        while (!success) {
+            success = task.execute(scanner);
+        }
+    }
+
     public void run() {
         Scanner scanner = new Scanner(System.in);
-        while (!CreateRacingCars(scanner)) {
-        }
-        while (!ChooseRacingCount(scanner)) {
-        }
+        executeWithRetry(this::CreateRacingCars, scanner);
+        executeWithRetry(this::ChooseRacingCount, scanner);
         scanner.close();
+    }
+
+    @FunctionalInterface
+    private interface InputTask {
+
+        boolean execute(Scanner scanner);
     }
 }
