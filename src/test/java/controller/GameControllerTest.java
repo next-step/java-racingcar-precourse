0625792bd30307c70e11f5ff01e38b9abb9e3939
Kim Sanghae <pelikan@pusan.ac.kr>
2024-06-07 17:39:36 +0900

    feat: Add GameController init test

--- src/test/java/controller/GameControllerTest.java
+++ src/test/java/controller/GameControllerTest.java
@@ -2,19 +2,40 @@ package controller;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import view.InputView;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class GameControllerTest {
 
     @Test
-    @DisplayName("우승자동차 선정 테스트")
-    public void testGetWinner() throws Exception{
+    @DisplayName("GameController 생성 테스트")
+    public void testCreateGameController() {
         // given
+        List<String> carNames = Arrays.asList("car1", "car2", "car3");
+        int gameIteration = 5;
 
         // when
+        GameController gameController = new GameController(carNames, gameIteration);
 
         // then
+        assertThat(gameController.getGameIteration()).isEqualTo(gameIteration);
+    }
+
+    @Test
+    @DisplayName("우승자 선정 시에 넣은 자동차 이름만 나오는지 테스트")
+    public void testGetWinner() {
+        // given
+        List<String> carNames = Arrays.asList("car1", "car2", "car3");
+        int gameIteration = 5;
+        GameController gameController = new GameController(carNames, gameIteration);
+
+        // when
+        List<String> winner = gameController.getWinner();
+
+        // then
+        assertThat(winner).allMatch(carNames::contains);
     }
 }
