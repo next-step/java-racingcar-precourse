63fe16fed6f1d17e429f60f04b6c476fc5eaf8b3
Happeeee <cmg118@naver.com>
2024-06-07 14:29:09 +0900

    feat: 시도 횟수 입력
    - 숫자가 아니면 예외 발생

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -15,9 +15,24 @@ public class InputView {
         return carNames.split(DELIMITER);
     }
 
+    public int inputRound() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        String round = scanner.nextLine();
+        validateNumber(round);
+        return Integer.parseInt(round);
+    }
+
     private void validateBlank(String carNames) {
         if (carNames == null || carNames.isBlank()) {
             throw new IllegalArgumentException("[ERROR] 자동차 이름 목륵은 null 이거나 빈 문자열일 수 없습니다.");
+        }
+    }
+
+    private void validateNumber(String round) {
+        for (char c : round.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                throw new IllegalArgumentException("[ERROR] 반복 횟수는 숫자만 입력할 수 있습니다.");
+            }
         }
     }
 }
