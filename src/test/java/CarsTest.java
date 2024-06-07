649cf799223bc2dd285bc66878543a9f670525b0
Lucerna00 <pjs3859811@naver.com>
2024-06-07 18:32:12 +0900

    test(CarsTest.java): add test case for winners

--- src/test/java/CarsTest.java
+++ src/test/java/CarsTest.java
@@ -27,4 +27,41 @@ public class CarsTest {
         assertThat(cars.getCars().get("woni")).isEqualTo("---");
         assertThat(cars.getCars().get("jun")).isEqualTo("-");
     }
+
+    @DisplayName("우승자 한 명")
+    @Test
+    public void carsTest2() {
+        // given
+        Cars cars = new Cars();
+        cars.setCars(new String[]{"pobi", "woni", "jun"});
+
+        // when
+        cars.addDistance("pobi");
+        cars.addDistance("pobi");
+        cars.addDistance("woni");
+        cars.addDistance("woni");
+        cars.addDistance("woni");
+        cars.addDistance("jun");
+
+        // then
+        assertThat(cars.getWinners()).isEqualTo(new String[]{"woni"});
+    }
+
+    @DisplayName("우승자 여러 명")
+    @Test
+    public void carsTest3() {
+        // given
+        Cars cars = new Cars();
+        cars.setCars(new String[]{"pobi", "woni", "jun"});
+
+        // when
+        cars.addDistance("pobi");
+        cars.addDistance("pobi");
+        cars.addDistance("woni");
+        cars.addDistance("woni");
+        cars.addDistance("jun");
+
+        // then
+        assertThat(cars.getWinners()).isEqualTo(new String[]{"pobi", "woni"});
+    }
 }
