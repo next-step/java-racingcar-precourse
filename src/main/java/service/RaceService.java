17e8e18ef495c32a35bc83848a1e7d200ad93f2b
kangwlgns <kangji0615@gmail.com>
2024-06-07 21:07:31 +0900

    feat(RaceService): modify the type of getCars method

--- src/main/java/service/RaceService.java
+++ src/main/java/service/RaceService.java
@@ -2,7 +2,6 @@ package service;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 import model.Race;
@@ -30,7 +29,7 @@ public class RaceService implements Subject, RaceServiceInterface {
     }
 
     @Override
-    public ArrayList<CarServiceInterface> getCars() {
+    public List<CarServiceInterface> getCars() {
         return race.getCars();
     }
 
