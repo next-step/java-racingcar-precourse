bf45d9fdecec942761fb69856e03788d1915f9ce
Lucerna00 <pjs3859811@naver.com>
2024-06-07 17:38:06 +0900

    test(Validation.test): add test cases for car names

---
+++ src/test/java/ValidationTest.java
@@ -0,0 +1,87 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import controller.Validation;
+
+public class ValidationTest {
+
+    @DisplayName("자동차 이름이 5자 이상인 경우 예외 발생")
+    @Test
+    public void carNamesTest1() {
+        // given
+        String[] carNames = {"pobi", "woniii", "jun"};
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> Validation.checkCarNames(carNames))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차 이름이 영문이 아닌 경우 예외 발생")
+    @Test
+    public void carNamesTest2() {
+        // given
+        String[] carNames = {"pobi", "woni2", "jun"};
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> Validation.checkCarNames(carNames))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차 이름이 공백인 경우 예외 발생")
+    @Test
+    public void carNamesTest3() {
+        // given
+        String[] carNames = {"pobi", "", "jun"};
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> Validation.checkCarNames(carNames))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차 이름에 띄어쓰기가 있는 경우 예외 발생")
+    @Test
+    public void carNamesTest4() {
+        // given
+        String[] carNames = {"pobi", "wo ni", "jun"};
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> Validation.checkCarNames(carNames))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차 이름이 중복되는 경우 예외 발생")
+    @Test
+    public void carNamesTest5() {
+        // given
+        String[] carNames = {"pobi", "pobi", "jun"};
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> Validation.checkCarNames(carNames))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("자동차 이름이 MAX 보다 많은 경우 예외 발생") // MAX = 5
+    @Test
+    public void carNamesTest6() {
+        // given
+        String[] carNames = {"pobi", "woni", "jun", "ryan", "frodo", "neo"};
+
+        // when
+
+        // then
+        assertThatThrownBy(() -> Validation.checkCarNames(carNames))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
