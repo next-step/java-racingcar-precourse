67c93d2467d658161040a797c81d40895fca7299
momnpa333 <momnpa333@gmail.com>
2024-06-07 16:07:10 +0900

    feat(RacingCarRepository.java): save 구현

--- src/main/java/repository/RacingCarRepository.java
+++ src/main/java/repository/RacingCarRepository.java
@@ -1,5 +1,9 @@
 package repository;
 
+import domain.RacingCar;
+import java.util.List;
+
 public interface RacingCarRepository {
 
+    List<RacingCar> save(RacingCar racingCar);
 }
--- src/main/java/repository/RacingCarRepositoryImpl.java
+++ src/main/java/repository/RacingCarRepositoryImpl.java
@@ -1,5 +1,15 @@
 package repository;
 
+import domain.RacingCar;
+import java.util.List;
+
 public class RacingCarRepositoryImpl implements RacingCarRepository {
 
+    List<RacingCar> racingCarMemory;
+
+    @Override
+    public List<RacingCar> save(RacingCar racingCar) {
+        racingCarMemory.add(racingCar);
+        return racingCarMemory;
+    }
 }
