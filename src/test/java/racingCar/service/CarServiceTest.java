e02cc89cbff536bec02280ed8b19a15e4a96efbc
5win <woozzoo7@gmail.com>
2024-06-07 18:39:44 +0900

    test(CarServiceTest.java): Add CarService test code - saveCars() test code

--- src/test/java/racingCar/service/CarServiceTest.java
+++ src/test/java/racingCar/service/CarServiceTest.java
@@ -1,4 +1,25 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingCar.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingCar.repository.CarMemoryRepository;
+
 class CarServiceTest {
-  
+    CarMemoryRepository carMemoryRepository = new CarMemoryRepository();
+    CarService carService = new CarService(carMemoryRepository);
+
+    @AfterEach
+    void afterEach() {
+        carMemoryRepository.clearCarDTOMap();
+    }
+
+    @Test
+    @DisplayName("입력에 대한 Car 객체 저장 테스트")
+    void saveCars() {
+        String inputNames = "Car1,car2,Car3";
+        carService.saveCars(inputNames);
+        Assertions.assertThat(carMemoryRepository.findAll().size()).isEqualTo(3);
+    }
 }
