a8747f0d6d910f590f17701cdb0951a739ddff40
xkrwjdals <developjmt@gmail.com>
2024-06-07 23:44:02 +0900

    feat : Create input class
    - input class has 3 methods
    - inputPlayers() returns String value that is name for racing cars
    - inputMatch() returns int value that is number of playing race
    - createCarArray(String str, Car[] cars) is void type and update distance and name of Car object array

---
+++ src/main/java/Input.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Input {
+    public String inputPlayer() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextLine();
+    }
+
+    public int inputMatch() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+
+    public void createCarArray(String str, Car[] cars) {
+        String[] names = str.split(",");
+        int n = names.length;
+        for (int i=0; i<n; i++) {
+            cars[i] = new Car(names[i], 0);
+        }
+    }
+}
