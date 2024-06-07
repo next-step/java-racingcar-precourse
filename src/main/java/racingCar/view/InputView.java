be500b00439dcf5d376e0b2b583e7578f1542a8d
5win <woozzoo7@gmail.com>
2024-06-07 17:06:04 +0900

    feat(InputView.java): Add inputTryNum() method

--- src/main/java/racingCar/view/InputView.java
+++ src/main/java/racingCar/view/InputView.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class InputView {
     private static final String INPUT_CAR_NAME_MSG = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    private static final String INPUT_TRY_NUM_MSG = "시도할 횟수는 몇회인가요?";
 
     /**
      * 자동차 이름을 입력 받는 메서드
@@ -27,5 +28,27 @@ public class InputView {
             }
         }
         return carNames;
+    }
+
+    /**
+     * 시도할 횟수를 입력 받는 메서드
+     * 잘못된 값 입력 시 다시 입력 받는다.
+     * @return 시도할 횟수
+     */
+    public static int inputTryNum() {
+        int tryNum;
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            try {
+                System.out.println(INPUT_TRY_NUM_MSG);
+                tryNum = scanner.nextInt();
+                InputValidator.validateInputTryNum(tryNum);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return tryNum;
     }
 }
