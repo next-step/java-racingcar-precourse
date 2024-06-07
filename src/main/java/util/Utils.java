9502fdee14e91dac4fe51a1a0f3ea4a1736f0e50
sunandrabbit <sunandrabbit@gamil.com>
2024-06-08 06:48:10 +0900

    feat: 0~9 사이의 수를 생성하는 기능 구현

---
+++ src/main/java/util/Utils.java
@@ -0,0 +1,11 @@
+package util;
+
+import java.util.Random;
+
+public class Utils {
+
+    public static int getRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(0, 10);
+    }
+}
