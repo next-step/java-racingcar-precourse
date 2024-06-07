05ee4c0e612eff33095d1710fb9cc5b7cc73dd41
momnpa333 <momnpa333@gmail.com>
2024-06-07 20:04:14 +0900

    feat(RacingConfig.java): config 수정

--- src/main/java/config/RacingCarConfig.java
+++ src/main/java/config/RacingCarConfig.java
@@ -1,16 +1,22 @@
 package config;
 
 import controller.RacingCarController;
+import controller.RacingCarFrontController;
 import domain.RacingCarService;
 import repository.RacingCarRepository;
 import repository.RacingCarRepositoryImpl;
 import repository.RacingCarRoundRepository;
 import repository.RacingCarRoundRepositoryImpl;
+import view.InputView;
+import view.OutputView;
 
 /**
  * RacingCarConfig Repository, Service, Controller를 생성하고 연결하는 클래스
  */
 public class RacingCarConfig {
+
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
 
     private static final RacingCarRepository racingCarRepository = new RacingCarRepositoryImpl();
     private static final RacingCarRoundRepository racingCarRoundRepository = new RacingCarRoundRepositoryImpl();
@@ -20,9 +26,11 @@ public class RacingCarConfig {
 
     private static final RacingCarController racingCarController = new RacingCarController(
         racingCarService);
+    private static final RacingCarFrontController racingCarFrontController = new RacingCarFrontController(
+        racingCarController, outputView, inputView);
 
-    public static RacingCarController getRacingCarController() {
-        return racingCarController;
+    public static RacingCarFrontController getRacingCarFrontController() {
+        return racingCarFrontController;
     }
 
 }
