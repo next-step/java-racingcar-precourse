ed0972144596dc596bda9defd490099bd76c1528
momnpa333 <momnpa333@gmail.com>
2024-06-07 16:07:59 +0900

    feat(RacingCarService.java): service내 레이싱카 생성 로직 추가

--- src/main/java/domain/RacingCarService.java
+++ src/main/java/domain/RacingCarService.java
@@ -1,5 +1,6 @@
 package domain;
 
+import domain.RacingCarCommand;
 import repository.RacingCarRepository;
 
 public class RacingCarService {
@@ -9,4 +10,10 @@ public class RacingCarService {
     public RacingCarService(RacingCarRepository racingCarRepository) {
         this.racingCarRepository = racingCarRepository;
     }
+
+    public void createRacingCar(RacingCarCommand.Create create) {
+        RacingCar.create(create).forEach(racingCarRepository::save);
+    }
+
+    
 }
