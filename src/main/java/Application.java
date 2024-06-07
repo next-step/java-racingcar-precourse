a0e41c1dd66ed45f9ca324659f458f99c9968b2e
us4c0d3 <jwseok312@gmail.com>
2024-06-08 01:26:45 +0900

    docs($main): add description about entry point of the application with javadoc

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,6 +4,11 @@ import view.GameView;
 
 public class Application {
 
+    /**
+     * 게임의 entry point
+     *
+     * @param args input argument(없음)
+     */
     public static void main(String[] args) {
         GameService service = new GameService();
         GameView view = new GameView();
