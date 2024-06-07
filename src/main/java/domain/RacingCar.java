08068362d8cb4d2e9f49ccdf77189f1665b2da8a
momnpa333 <momnpa333@gmail.com>
2024-06-07 18:03:49 +0900

    feat(RacingCar.java): 비즈니스 메서드 구현

--- src/main/java/domain/RacingCar.java
+++ src/main/java/domain/RacingCar.java
@@ -13,11 +13,29 @@ public class RacingCar {
         this.position = 0L;
     }
 
+    /**
+     * 생성 메서드
+     */
     public static List<RacingCar> create(RacingCarCommand.Create create) {
         return create.name().stream()
             .map(RacingCar::new)
             .toList();
     }
 
+    /**
+     * 비즈니스 메서드
+     */
+    public void move() {
+        if (isForward()) {
+            position++;
+        }
+    }
+
+    /**
+     * racing car가 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
+     */
+    private boolean isForward() {
+        return Math.floor(Math.random() * 10) >= 4;
+    }
 
 }
