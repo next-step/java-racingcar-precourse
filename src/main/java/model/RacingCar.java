7c9dde8344632c3791881d2b4c4d2f0eb2c05739
minju26 <mjalswn26@gmail.com>
2024-06-08 02:21:13 +0900

    feat: Implement logic for car racing

--- src/main/java/model/RacingCar.java
+++ src/main/java/model/RacingCar.java
@@ -16,4 +16,8 @@ public class RacingCar {
     public int getPosition() {
         return position;
     }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
 }
--- src/main/java/service/RacingGameService.java
+++ src/main/java/service/RacingGameService.java
@@ -1,12 +1,48 @@
 package service;
 
 import model.RacingCar;
+import util.RandomNumberGenerator;
+import view.CarRacingView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RacingGameService {
+
+    private final CarRacingView carRacingView = new CarRacingView();
+    private static final String RESULT_MSG = "실행 결과";
+
     public void playRacingGame(ArrayList<RacingCar> racingCars, int attempt){
-        System.out.println(racingCars);
-        System.out.println(attempt);
+        System.out.println(RESULT_MSG);
+        System.out.println();
+
+        for(int i = 0; i < attempt; i++){
+            moveCar(racingCars);
+            carRacingView.displayRacingCars(racingCars);
+        }
+        carRacingView.displayWinners(winners(racingCars));
+    }
+
+    private void moveCar(ArrayList<RacingCar> racingCars){
+        for (RacingCar car : racingCars) {
+            int newPosition = car.getPosition() + RandomNumberGenerator.generateRandNum();
+            car.setPosition(newPosition);
+        }
+    }
+
+    private String[] winners(ArrayList<RacingCar> racingCars) {
+
+        int maxPosition = racingCars.stream()
+                .mapToInt(RacingCar::getPosition)
+                .max()
+                .orElse(0);
+
+        List<String> winnerList = new ArrayList<>();
+        for (RacingCar car : racingCars) {
+            if (car.getPosition() == maxPosition) {
+                winnerList.add(car.getName());
+            }
+        }
+        return winnerList.toArray(new String[0]);
     }
 }
