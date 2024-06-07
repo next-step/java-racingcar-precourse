26b4a1818c8c7d35e00563cffba0a2eb5a51763d
Happeeee <cmg118@naver.com>
2024-06-07 15:09:53 +0900

    feat: 잘못된 값 입력시 오류 메시지 출력

--- src/main/java/controller/RaceController.java
+++ src/main/java/controller/RaceController.java
@@ -4,13 +4,16 @@ import java.util.function.Supplier;
 import model.Cars;
 import model.Round;
 import view.InputView;
+import view.OutputView;
 
 public class RaceController {
 
     private final InputView inputView;
+    private final OutputView outputView;
 
-    public RaceController(InputView inputView) {
+    public RaceController(InputView inputView, OutputView outputView) {
         this.inputView = inputView;
+        this.outputView = outputView;
     }
 
     public void run() {
@@ -23,7 +26,7 @@ public class RaceController {
             try {
                 return supplier.get();
             } catch (IllegalArgumentException e) {
-                
+                outputView.printErrorMessage(e.getMessage());
             }
         }
     }
---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,8 @@
+package view;
+
+public class OutputView {
+
+    public void printErrorMessage(String message) {
+        System.out.println(message);
+    }
+}
