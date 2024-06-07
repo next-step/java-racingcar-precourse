80c5056214b9615b9b137963df3a1efed832fe83
momnpa333 <momnpa333@gmail.com>
2024-06-07 18:15:52 +0900

    feat(RacingCarService.java): playRacing, getGameResult 제작

--- src/main/java/domain/RacingCarService.java
+++ src/main/java/domain/RacingCarService.java
@@ -1,19 +1,41 @@
 package domain;
 
-import domain.RacingCarCommand;
+import domain.RacingCarModel;
+import java.util.List;
+import java.util.stream.IntStream;
 import repository.RacingCarRepository;
+import repository.RacingCarRoundRepository;
 
 public class RacingCarService {
 
     private final RacingCarRepository racingCarRepository;
+    private final RacingCarRoundRepository racingCarRoundRepository;
 
-    public RacingCarService(RacingCarRepository racingCarRepository) {
+    public RacingCarService(RacingCarRepository racingCarRepository,
+        RacingCarRoundRepository racingCarRoundRepository) {
         this.racingCarRepository = racingCarRepository;
+        this.racingCarRoundRepository = racingCarRoundRepository;
     }
 
     public void createRacingCar(RacingCarCommand.Create create) {
         RacingCar.create(create).forEach(racingCarRepository::save);
     }
 
-    
+    /**
+     * round 수 만큼 RacingCar를 이동시킨다. round가 끝날때 마다 round별 RacingCar를 저장한다.
+     */
+    public void playRacing(int racingRound) {
+        List<RacingCar> racingCars = racingCarRepository.findAll();
+        IntStream.range(0, racingRound)
+            .forEach(round -> {
+                racingCars.forEach(RacingCar::move);
+                racingCarRoundRepository.save(RacingCarRound.create(round, racingCars));
+            });
+    }
+
+    public RacingCarModel.GameResult getGameResult() {
+        List<RacingCarRound> racingCarRounds = racingCarRoundRepository.findAll();
+        return RacingCarModel.GameResult.from(racingCarRounds);
+    }
+
 }
