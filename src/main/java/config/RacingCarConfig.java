5ec7a6567ee4fe4b1d00c6d487c01cba5582a6e7
momnpa333 <momnpa333@gmail.com>
2024-06-07 18:04:17 +0900

    feat(RacingCarConfig.java): RacingCarRoundRepository 의존성 추가

--- src/main/java/config/RacingCarConfig.java
+++ src/main/java/config/RacingCarConfig.java
@@ -4,6 +4,8 @@ import controller.RacingCarController;
 import domain.RacingCarService;
 import repository.RacingCarRepository;
 import repository.RacingCarRepositoryImpl;
+import repository.RacingCarRoundRepository;
+import repository.RacingCarRoundRepositoryImpl;
 
 /**
  * RacingCarConfig Repository, Service, Controller를 생성하고 연결하는 클래스
@@ -11,9 +13,10 @@ import repository.RacingCarRepositoryImpl;
 public class RacingCarConfig {
 
     private static final RacingCarRepository racingCarRepository = new RacingCarRepositoryImpl();
+    private static final RacingCarRoundRepository racingCarRoundRepository = new RacingCarRoundRepositoryImpl();
 
     private static final RacingCarService racingCarService = new RacingCarService(
-        racingCarRepository);
+        racingCarRepository, racingCarRoundRepository);
 
     private static final RacingCarController racingCarController = new RacingCarController(
         racingCarService);
