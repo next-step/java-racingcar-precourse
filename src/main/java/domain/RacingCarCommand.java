292c3790975f845d1eefad97ce388099ec9ba7ce
momnpa333 <momnpa333@gmail.com>
2024-06-07 16:05:31 +0900

    feat(RacingCarCommand.java): Command create 인자 수정

--- src/main/java/domain/RacingCarCommand.java
+++ src/main/java/domain/RacingCarCommand.java
@@ -1,17 +1,26 @@
 package domain;
 
+import java.util.List;
+import util.Validator;
+
 /**
  * command에서 유효성 검사를 수행합니다.
  */
 public class RacingCarCommand {
 
-    public record Create(String name) {
+    public record Create(List<String> name) {
 
-        public static RacingCarCommand.Create from(String name) {
+        public static RacingCarCommand.Create from(List<String> names) {
             // 유효한 이름인지 검사합니다.
-            RacingCarValidator.validateName(name);
+            return new RacingCarCommand.Create(names);
+        }
+    }
 
-            return new RacingCarCommand.Create(name);
+    public record Racing(int round) {
+
+        public static RacingCarCommand.Racing from(int round) {
+
+            return new RacingCarCommand.Racing(round);
         }
     }
 
