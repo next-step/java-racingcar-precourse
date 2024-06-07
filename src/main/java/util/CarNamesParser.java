d1eb56f369318609f45c715006d3a6d69d091393
us4c0d3 <jwseok312@gmail.com>
2024-06-07 19:29:11 +0900

    feat($readme): add exception control for checking name duplicate

--- src/main/java/util/CarNamesParser.java
+++ src/main/java/util/CarNamesParser.java
@@ -1,7 +1,9 @@
 package util;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CarNamesParser {
 
@@ -12,6 +14,15 @@ public class CarNamesParser {
      * @return 쉼표(,)를 기준으로 문자열을 분리하여 각각 저장한 문자열 리스트
      */
     public static List<String> parse(String input) {
-        return Arrays.asList(input.split(","));
+        List<String> names = Arrays.asList(input.split(","));
+        Set<String> nameSet = new HashSet<>();
+
+        for(String name : names) {
+            if(!nameSet.add(name)) {
+                throw new IllegalArgumentException("[ERROR] 중복된 이름이 있습니다 : " + name);
+            }
+        }
+
+        return names;
     }
 }
