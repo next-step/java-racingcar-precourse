149bc4a5207ddbe45f3bad6f1160d52ff8abd0da
htkim <htkim85@hyundai-autoever.com>
2024-06-07 14:35:10 +0900

    feat(Application): main 함수 구현

--- src/main/java/camp/nextstep/edu/Application.java
+++ src/main/java/camp/nextstep/edu/Application.java
@@ -1,6 +1,13 @@
 package camp.nextstep.edu;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        UI ui = new UI(scanner);
+        ui.inputCarNames();
+        ui.inputRepeatCount();
+        ui.run();
     }
 }
