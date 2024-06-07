ba3a37a82d7916bafc5b75289af6d4762fb0348d
5win <woozzoo7@gmail.com>
2024-06-07 20:04:40 +0900

    test(CarServiceTest.java): Add test code of getWinner() method

--- src/test/java/racingCar/service/CarServiceTest.java
+++ src/test/java/racingCar/service/CarServiceTest.java
@@ -4,7 +4,10 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingCar.domain.CarDTO;
 import racingCar.repository.CarMemoryRepository;
+
+import java.util.List;
 
 class CarServiceTest {
     CarMemoryRepository carMemoryRepository = new CarMemoryRepository();
@@ -21,5 +24,16 @@ class CarServiceTest {
         String inputNames = "Car1,car2,Car3";
         carService.saveCars(inputNames);
         Assertions.assertThat(carMemoryRepository.findAll().size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("최종 우승자 리스트 반환 테스트")
+    void getWinner() {
+        String inputNames = "car1,car2,car3";
+        carService.saveCars(inputNames);
+        List<CarDTO> carDTOList = carMemoryRepository.findAll();
+        carDTOList.get(0).increasePos();
+        carDTOList.get(1).increasePos();
+        Assertions.assertThat(carService.getWinner().size()).isEqualTo(2);
     }
 }
