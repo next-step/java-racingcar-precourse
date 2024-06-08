ce08246d933aab10ed280a170d9c1d1edbd955ff
배민수 <duecost@gmail.com>
2024-06-08 23:25:12 +0900

    test: 자동차 전진 기능 (move 메소드) 테스트

--- src/test/java/CarModelTest.java
+++ src/test/java/CarModelTest.java
@@ -11,4 +11,14 @@ public class CarModelTest {
         int moveCount = car.getMoveCount();
         assertThat(carName.equals("test_car") && moveCount == 0).isTrue();
     }
+
+    @Test
+    public void moveTest() {
+        Car car = new Car();
+
+        for (int i = 1; i <= 10; i++) {
+            car.move();
+            assertThat(car.getMoveCount()).isLessThanOrEqualTo(i);
+        }
+    }
 }
