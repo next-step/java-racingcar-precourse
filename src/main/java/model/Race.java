e185756d02f0e1ec8c9b23140e0ec0dde36eace1
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-06-08 20:15:59 +0900

    feat:레이스 결과 판단 기능

--- src/main/java/model/Race.java
+++ src/main/java/model/Race.java
@@ -21,4 +21,13 @@ public class Race {
         return judgeWinners();
     }
 
+    public List<Car> judgeWinners() {
+        int maxPosition = cars.stream()
+                .mapToInt(Car::getPosition)
+                .max()
+                .orElse(0);
+        return cars.stream()
+                .filter(car -> car.getPosition() == maxPosition)
+                .toList();
+    }
 }
