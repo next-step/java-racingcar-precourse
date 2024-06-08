ece020bebcea27644f22697abc9074ed491e4c1f
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-06-08 20:23:39 +0900

    refactor: racecontroller 제거 - raceController제거 후 application 으로 이동

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,12 +1,19 @@
 import controller.RaceInputController;
 import java.util.List;
 import model.Car;
+import model.Race;
+import view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-        RaceInputController inputController = new RaceInputController();
+        final RaceInputController inputController = new RaceInputController();
+        List<String> carNames = inputController.getUserInputForCarNames();
+        int rounds = inputController.getUserInputForRaceCount();
 
-        final int amount = inputController.getUserInputForRaceCount();
-        final List<Car> cars = lottoService.purchaseLottos(amount);
+        Race race = new Race(carNames, rounds);
+        List<Car> result = race.start();
+
+        OutputView.printRaceStatus(result);
+        OutputView.printWinners(result);
     }
 }
