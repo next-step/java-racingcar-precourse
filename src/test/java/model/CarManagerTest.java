08bba8dc14b3e23ecf37c96de12a61521fbb1048
mac <20j.code@gmail.com>
2024-06-08 22:36:52 +0900

    test : getRaceLog() 테스트 추가
    자동차 이름의 key를 반환하는지

--- src/test/java/model/CarManagerTest.java
+++ src/test/java/model/CarManagerTest.java
@@ -2,6 +2,7 @@ package model;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -27,5 +28,31 @@ class CarManagerTest {
         //then
         assertThrows(IllegalArgumentException.class,()->{cm.addCar(nocar);},"중복 이름 예외 던지기");
         assertDoesNotThrow(()->cm.addCar(car2),"중복 아닐 시 통과");
+    }
+
+    @Test
+    void getRaceLog() {
+        //given
+        String car1 = "hah";
+        String car2 = "ooo";
+        String car3 = "ijij";
+        Map<String,Integer> log;
+
+        cm.addCar(car1);
+        cm.addCar(car2);
+        cm.addCar(car3);
+
+        //when
+        cm.race();
+        log = cm.getRaceLog();
+
+        //then
+        assertNotNull(log.get(car1));
+        assertNotNull(log.get(car3));
+
+        assertTrue(log.get(car1)>=0);
+
+
+
     }
 }
