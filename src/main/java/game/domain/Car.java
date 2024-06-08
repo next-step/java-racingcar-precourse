9a7e9f343d690ba569416c6b99d070b3feebd638
nove1080 <skwpqjq@gmail.com>
2024-06-08 18:53:04 +0900

    feat: Car equalsAndHashcode, toString 오버라이딩

--- src/main/java/game/domain/Car.java
+++ src/main/java/game/domain/Car.java
@@ -51,4 +51,28 @@ public class Car {
         return randomValue >= Rule.MOVEMENT_THRESHOLD;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return position == car.position && Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+            "name='" + name + '\'' +
+            ", position=" + position +
+            '}';
+    }
 }
