9cdbedb0b798e4cb299d800d825903d20991a690
배민수 <duecost@gmail.com>
2024-06-08 22:25:05 +0900

    feat: 컨트롤러를 통해 게임을 시작, 진행, 종료하는 메인 클래스와 메인 함수 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,9 @@
 public class Application {
 
     public static void main(String[] args) {
-
+        GameController gameController = new GameController();
+        gameController.gameSetting();
+        gameController.gamePlay();
+        gameController.gameDone();
     }
 }
