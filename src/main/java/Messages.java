76657b2422eef630286e7d830c5faf568372017b
yoong-saks <wjdghtjd06@gmail.com>
2024-06-09 02:26:12 +0900

    refactor : 예외처리를 출력하기위한 메시지 추가

--- src/main/java/Messages.java
+++ src/main/java/Messages.java
@@ -3,4 +3,7 @@ public class Messages {
 
     public static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     public static final String INPUT_REPEAT_NUMBER = "시도할 회수는 몇회인가요?";
+
+    public static final String CAR_NAME_EMPTY_ERR = "자동차 이름은 비어있을 수 없습니다.";
+    public static final String CAR_NAME_DUPLICATE_ERR = "자동차 이름은 중복될 수 없습니다.";
 }
