7014f3bc67f39d39520dd1202e9dfc4b4891740f
nove1080 <skwpqjq@gmail.com>
2024-06-08 23:02:44 +0900

    test: CarsTest Cars.getMaxPosition() 에 대한 테스트 추가

--- src/test/java/game/domain/CarsTest.java
+++ src/test/java/game/domain/CarsTest.java
@@ -11,6 +11,25 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 class CarsTest {
 
+    @ParameterizedTest(name = "{0}의 최대 위치는 {1} 입니다")
+    @MethodSource("getCarAndMaxPosition")
+    void getMaxPosition(Cars cars, int maxPosition) {
+        assertThat(cars.getMaxPosition()).isEqualTo(maxPosition);
+    }
+
+    private static Stream<Arguments> getCarAndMaxPosition() {
+        int maxPosition = 5;
+
+        List<Car> carList = new ArrayList<>();
+        carList.add(new Car("enzo", maxPosition));
+        carList.add(new Car("gusto", 3));
+        carList.add(new Car("james", 1));
+
+        return Stream.of(
+            Arguments.arguments(new Cars(carList), maxPosition)
+        );
+    }
+
     @ParameterizedTest(name = "{0}의 경주 결과 승자는 {1}입니다")
     @MethodSource("getCarListAndWinner")
     void findWinner(Cars cars, Cars winners) {
