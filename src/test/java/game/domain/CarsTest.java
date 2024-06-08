16f3337f1c76ace892f359292379491da8739996
nove1080 <skwpqjq@gmail.com>
2024-06-08 19:08:54 +0900

    test: CarsTest findWinner()에 대한 테스트 추가

---
+++ src/test/java/game/domain/CarsTest.java
@@ -0,0 +1,35 @@
+package game.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class CarsTest {
+
+    @ParameterizedTest(name = "{0}의 경주 결과 승자는 {1}입니다")
+    @MethodSource("getCarList")
+    void findWinner(Cars cars, Cars winners) {
+        assertThat(cars.findWinners()).isEqualTo(winners);
+    }
+
+    private static Stream<Arguments> getCarList() {
+        List<Car> carList = new ArrayList<>();
+        carList.add(new Car("enzo", 5));
+        carList.add(new Car("gusto", 5));
+        carList.add(new Car("james", 1));
+
+        List<Car> winners = new ArrayList<>();
+        winners.add(new Car("enzo", 5));
+        winners.add(new Car("gusto", 5));
+
+        return Stream.of(
+            Arguments.arguments(new Cars(carList), new Cars(winners))
+        );
+    }
+
+}
