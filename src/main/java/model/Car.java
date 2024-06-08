e73fcf9b54e2baccb3c01bf8ee1a58029d17ddff
JinSeoHyun <15roa1121@gmail.com>
2024-06-08 13:56:43 +0900

    자동차 모델 구현 중, car 클래스 구현 - 일반차, 슈퍼카 추가를 고려해서 구현

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,33 @@
+package model;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public abstract class Car implements Transportation {
+    private final String carname;
+    private int distance;
+    private int distanceUnit;
+
+    Car(String carname,DistanceUnit distanceUnit) {
+        this.carname = carname;
+        this.distance = 0;
+        this.distanceUnit = distanceUnit.ordinal();
+
+    }
+    public Map<String, Integer> run(){
+        if(useRandomValues() >=4){
+            distance += this.distanceUnit;
+        }
+        Map<String, Integer> result = new HashMap<>();
+        result.put(this.carname, this.distance); // 키(carname)와 값(distance)을 맵에 추가
+        return result;
+    }
+
+    public int useRandomValues() {
+        Random random = new Random();
+        int randomInt = random.nextInt(10);
+        return randomInt;
+    }
+
+}
