42a2dc4f536650235786d24d1c39ed267d467afc
Shsin9797 <aaa8141@naver.com>
2024-06-08 22:49:00 +0900

    style(Car) : Car class 의 엔터키 처리

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,21 +1,28 @@
+import java.util.Random;
+
 public class Car {
     private String name;
-
     private int state =0;
 
+    public void move() {
+
+        Random r = new Random();
+        int randNum= r.nextInt(0,9);
+
+        if (randNum >= 4) {
+            setState(getState() + 1);
+        }
+    }
 
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name= name;
     }
-
     public int getState() {
         return state;
     }
-
     public void setState(int newState) {
         this.state = newState;
     }
