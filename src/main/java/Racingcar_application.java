1204f68e587047e312a18e7ae13be40e5a0b29a2
백정근 <godflower8@naver.com>
2024-06-09 01:54:22 +0900

    feat : 6. 우승자 식별, 7. 우승자 출력

--- src/main/java/Racingcar_application.java
+++ src/main/java/Racingcar_application.java
@@ -6,10 +6,12 @@ import java.util.*;
 public class Racingcar_application {
 
     public static void main(String[] args) throws IOException {
+        // 1. 자동차 이름 입력
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분");
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine(), ",");
 
+        // 2. 시도할 횟수 입력
         System.out.println("시도할 횟수는 몇 회인가요?");
         Scanner sc = new Scanner(System.in);
         int cnt = sc.nextInt();
@@ -20,10 +22,38 @@ public class Racingcar_application {
         }
 
         for(int i=0; i<cnt; i++) {
+            // 4. 0-9 사이 무작위 값 생성
+            // 5. 무작위 값이 4이상인 경우, 전진
             raceMap = startRacing(raceMap);
+            // 3. 횟수별로 자동차 이름 출력
             printRacing(raceMap);
         }
 
+        // 6. 우승자 식별
+        String winner = getWinner(raceMap);
+        // 7. 우승자 출력
+        System.out.println("최종 우승자 : " + winner);
+    }
+
+    private static String getWinner(Map<String, String> raceMap) {
+        int winRace = 0;
+        int raceCnt = 0;
+        String winner = "";
+
+        for(String s : raceMap.keySet()){
+            raceCnt = raceMap.get(s).length();
+            if(winRace <= raceCnt) winRace = raceCnt;
+        }
+
+        for(String s : raceMap.keySet()){
+            if (raceMap.get(s).length() == winRace){
+                winner = winner + ", " + s;
+            }
+        }
+        if (winner.startsWith(", ")){
+            winner = winner.replaceFirst(", ", "");
+        }
+        return winner;
     }
 
     private static Map<String, String> startRacing(Map<String, String> raceMap) {
