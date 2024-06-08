2fbf23b0ea93055b29e4681a9f566e393c7693fb
mac <20j.code@gmail.com>
2024-06-08 18:09:20 +0900

    chore : getName() 추가

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -38,6 +38,10 @@ public class Car {
         return position;
     }
 
+    public String getName(){
+        return name;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
