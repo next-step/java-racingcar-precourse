8e3088c952b5139ac2728be5841c15b8122a488d
mac <20j.code@gmail.com>
2024-06-08 22:40:18 +0900

    feat : getRandomValue 추가
    0~9 사이의 랜덤한 값을 반환

--- src/main/java/model/RandomMaker.java
+++ src/main/java/model/RandomMaker.java
@@ -1,8 +1,15 @@
 package model;
 
-public class RandomMaker {
+import java.util.Random;
 
+public class RandomMaker {
+    Random random = new Random();
+
+    /**
+     * 랜덤한 값을 생성합니다.
+     * @return 0 <= return <= 9
+     */
     public int getRandomValue() {
-        return 0;
+        return random.nextInt(0,10);
     }
 }
