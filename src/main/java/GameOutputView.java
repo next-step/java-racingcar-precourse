7da6aea76d326173ac52c01c2b7e59f7c03a2e27
배민수 <duecost@gmail.com>
2024-06-08 22:22:02 +0900

    feat: 자동차 객체 배열과 우승 전진 횟수를 받아서, 우승자들을 출력하는 함수 구현

--- src/main/java/GameOutputView.java
+++ src/main/java/GameOutputView.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameOutputView {
     public void printPlayResult(Car[] carList) {
         for (Car car : carList) {
@@ -13,5 +16,21 @@ public class GameOutputView {
             System.out.println(carName + " : " + moveCountSymbol);
         }
         System.out.println();
+    }
+
+    public void printWinner(Car[] carList, int winnerMoveCount) {
+        System.out.print("최종 우승자 : ");
+
+        List<String> winners = new ArrayList<String>();
+
+        for (Car car : carList) {
+            int carMoveCount = car.getMoveCount();
+
+            if (carMoveCount == winnerMoveCount) {
+                winners.add(car.getCarName());
+            }
+        }
+
+        System.out.println(String.join(", ", winners));
     }
 }
