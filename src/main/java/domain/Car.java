485409e8253c9bfa58a817b1a567ab1ccb6597d4
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-06-08 15:03:36 +0900

    feat: 입력 메세지 출력

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -5,14 +5,15 @@ public class Car {
     private int position;
 
     public Car(String carName) {
-        // 차 이름 검증
+        // 차 이름 검증 필요
         this.carName = carName;
         this.position = 0;
     }
 
-    public void moveCar(){
+    public void moveCar() {
         int randomNumber = (int) (Math.random() * 10);
-        if (randomNumber>=4)
+        if (randomNumber >= 4) {
             position++;
+        }
     }
 }
