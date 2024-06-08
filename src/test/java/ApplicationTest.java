15edf523080389f581f820c85d6f36e6150d387b
dlwnstn0524 <ljs052450@gmail.com>
2024-06-09 00:06:01 +0900

    feature: Test Libarary Injection and Test Code Create

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class ApplicationTest {
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class ApplicationTest {
+    @Test
+    void 게임진행(){
+        // given
+        run();
+        // when
+
+        // then
+
+    }
+
+    @Test
+    void 모킹_테스트(){
+        List<String> mock = Mockito.mock(List.class);
+        when(mock.get(0)).thenReturn("Hello, Mockito!");
+
+        assertEquals("Hello, Mockito!", mock.get(0));
+
+        verify(mock, times(1)).get(0);
+    }
+
 }
