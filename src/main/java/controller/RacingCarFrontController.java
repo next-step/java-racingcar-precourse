6ee9df9896774de368ef493a2db34708fc5d14f0
momnpa333 <momnpa333@gmail.com>
2024-06-08 15:52:51 +0900

    docs(RacingCarFrontController.java): 주석 추가

--- src/main/java/controller/RacingCarFrontController.java
+++ src/main/java/controller/RacingCarFrontController.java
@@ -8,12 +8,16 @@ public class RacingCarFrontController {
     private final RacingCarController racingCarController;
     private final OutputView outputView;
 
+
     public RacingCarFrontController(RacingCarController racingCarController,
         OutputView outputView) {
         this.racingCarController = racingCarController;
         this.outputView = outputView;
     }
 
+    /**
+     * 애플리케이션 실행 메서드. 입력 뷰를 생성하고  입력에 따라 레이싱 카 생성 및 게임 실행을 반복적으로 시도합니다.
+     */
     public void run() {
         try (InputView inputView = new InputView()) {
             executeWithRetry(this::createRacingCars, inputView);
@@ -21,6 +25,9 @@ public class RacingCarFrontController {
         }
     }
 
+    /**
+     * 레이싱 카를 생성하는 메서드. 입력 뷰에서 입력을 받아 컨트롤러에 전달합니다.
+     */
     private boolean createRacingCars(InputView inputView) {
         try {
             String input = inputView.enterCarNames();
@@ -32,6 +39,9 @@ public class RacingCarFrontController {
         }
     }
 
+    /**
+     * 레이싱 게임을 실행하는 메서드. 입력 뷰에서 round를 입력 받아 컨트롤러에 전달합니다.
+     */
     private boolean playRacing(InputView inputView) {
         try {
             String input = inputView.enterTryCount();
@@ -45,6 +55,9 @@ public class RacingCarFrontController {
         }
     }
 
+    /**
+     * 입력 작업을 반복적으로 시도하는 메서드.
+     */
     private void executeWithRetry(InputTask task, InputView inputView) {
         boolean success = false;
         while (!success) {
@@ -52,7 +65,9 @@ public class RacingCarFrontController {
         }
     }
 
-
+    /**
+     * 입력 작업을 위한 함수형 인터페이스.
+     */
     @FunctionalInterface
     private interface InputTask {
 
--- src/main/java/domain/RacingCar.java
+++ src/main/java/domain/RacingCar.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class RacingCar {
 
     private final String name;
-
     private int position;
 
     private RacingCar(String name) {
@@ -47,7 +46,7 @@ public class RacingCar {
     /**
      * racing car가 전진하는 조건은 0에서 9 사이에서 무작위 값을 구한 후 무작위 값이 4 이상일 경우이다.
      */
-    private boolean isForward() {
+    protected boolean isForward() {
         return Math.floor(Math.random() * 10) >= 4;
     }
 
