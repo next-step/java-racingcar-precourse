df881c0b29d3d798a740ce5625f79e9ab68fd6f6
배민수 <duecost@gmail.com>
2024-06-08 15:19:38 +0900

    docs(수정): README.md 수정

---
+++ src/main/java/Car.java
@@ -0,0 +1,17 @@
+public class Car {
+
+    private String carName;
+    private int moveCount;
+
+    public Car() {
+        carName = "default";
+        moveCount = 0;
+    }
+
+    public Car(String carName) {
+        this.carName = carName;
+        moveCount = 0;
+    }
+
+    public int
+}
