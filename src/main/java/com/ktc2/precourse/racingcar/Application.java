ee9529192f8b679fd0a72ace54fc431f523ef0b0
lja3723 <lja3723@gmail.com>
2024-06-08 16:25:14 +0900

    feat: add main entry 프로그램의 주 진입점 클래스 추가

---
+++ src/main/java/com/ktc2/precourse/racingcar/Application.java
@@ -0,0 +1,18 @@
+package com.ktc2.precourse.racingcar;
+
+import com.ktc2.precourse.racingcar.controller.RacingCarGameController;
+
+/**
+ * Application Main Entry class
+ */
+public class Application {
+
+    /**
+     * Application Main Entry
+     *
+     * @param args main entry arguments
+     */
+    public static void main(String[] args) {
+        RacingCarGameController.play();
+    }
+}
