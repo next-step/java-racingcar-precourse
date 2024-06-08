bfd7acec46b74b89e58e6b209d412ae3ce98858b
nove1080 <skwpqjq@gmail.com>
2024-06-08 14:03:12 +0900

    feat: Cars 구현

---
+++ src/main/java/game/domain/Cars.java
@@ -0,0 +1,13 @@
+package game.domain;
+
+import java.util.List;
+
+public class Cars {
+
+    private List<Car> carList;
+
+    public Cars(List<Car> carList) {
+        this.carList = carList;
+    }
+
+}
