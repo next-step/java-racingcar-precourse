b60fde107aab10a1c1594c2384803b99086e7134
배민수 <duecost@gmail.com>
2024-06-08 20:11:51 +0900

    feat: 사용자로부터 입력받은 자동차 이름들에 대한 검증 함수 구현

--- src/main/java/GameInputView.java
+++ src/main/java/GameInputView.java
@@ -4,6 +4,15 @@ import java.util.Scanner;
 
 public class GameInputView {
 
+    private boolean validatorInputCarNameList(String[] carNames) {
+        for (String car : carNames) {
+            if (car.length() > 5) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
     public List<String> inputCarNameList() {
         Scanner scanner = new Scanner(System.in);
@@ -11,6 +20,15 @@ public class GameInputView {
 
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] carNames = scanner.nextLine().split(",");
+
+        boolean isValidCarNames = validatorInputCarNameList(carNames);
+
+        while (!isValidCarNames) {
+            System.out.println("자동차 이름을 쉼표로 구분해주시고, 각 이름의 길이는 5자 이하로 작성해주세요.");
+            carNames = scanner.nextLine().split(",");
+            isValidCarNames = validatorInputCarNameList(carNames);
+        }
+
         scanner.close();
 
         for (String car : carNames) {
