adb3131f3d9e866714b1cc3e504463287caaef61
Shsin9797 <aaa8141@naver.com>
2024-06-09 02:20:54 +0900

    feat(Game) :  사용자로부터 시도 회수 입력받고 이상치 체크하는 기능 구현

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,8 +5,9 @@ public class Game {
 
     public List<String> getCarName() {
         List<String> carNames;
+        Scanner sc = new Scanner(System.in);
+
         while (true) {
-            Scanner sc = new Scanner(System.in);
             System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
             carNames = List.of(sc.nextLine().split(","));
             if (CheckInput.checkCarNames(carNames)) {
@@ -15,6 +16,21 @@ public class Game {
             }
         }
         return carNames;
+    }
+
+    public int getTryNum() {
+        int tryNum;
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("시도할 회수는 몇회인가요?");
+            try {
+                tryNum = sc.nextInt();
+                break;
+            } catch (IllegalArgumentException e) {
+                CheckInput.isWrongValue();
+            }
+        }
+        return tryNum;
     }
 
 
