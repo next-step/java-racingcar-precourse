7f5ec8daf09834840b7b5cf8e44b4746d2c714a0
Yoo KyeongMi <yookyungmi02@gmail.com>
2024-06-08 17:10:23 +0900

    feat: 사용자 입력 기능 추가

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,11 +1,11 @@
 package view;
 
-public class GameView {
-    public void printCarNameInput() {
+public class InputView {
+    public final static void printCarNameInput() {
         System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
     }
 
-    public void printCountInput() {
+    public final static void printCountInput() {
         System.out.println("시도할 회수는 몇회인가요?");
     }
 }
