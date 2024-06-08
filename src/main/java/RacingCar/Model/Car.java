eaed9b2d5009c1c3190d6709e28234db3238f936
Gahyeon Lee <best11gh@naeileun.dev>
2024-06-08 18:19:52 +0900

    feat: car class 생성

---
+++ src/main/java/RacingCar/Model/Car.java
@@ -0,0 +1,28 @@
+package RacingCar.Model;
+
+public class Car {
+  private final String name;
+  private int position;
+
+  public Car(String name) {
+    if (name.length() > 5) {
+      throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
+    }
+    this.name = name;
+    this.position = 0;
+  }
+
+  public void move(int randomValue) {
+    if (randomValue >= 4) {
+      position++;
+    }
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getPosition() {
+    return position;
+  }
+}
