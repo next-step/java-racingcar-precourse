0e3b4854cd4bc82106a832f356796f914913f550
Woojae Jeong <woo6388@naver.com>
2024-06-09 22:44:10 +0900

    Style : Modify annotation
    numberOfMove 변수는 현재 차가 얼마나 이동했는지를 담는 변수가 의미상 맞음.
    tryIncreaseNumberOfMove의 함수에 주석을 닮.

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 public class Car {
     private String carName; //자동차 이름
-    private int numberOfMove; //지금까지 얼마나 이동했는지 담는 변수
+    private int numberOfMove; // 얼마나 이동했는지 담는 변수
     private static final Random random = new Random();
 
     public Car(String carName){
@@ -20,7 +20,7 @@ public class Car {
     public int getNumberOfMove() {
         return numberOfMove;
     }
-    public void tryIncreaseNumberOfMove() {
+    public void tryIncreaseNumberOfMove() { //랜덤값을 구해서 랜덤값이 4이상인경우 움직이는 함수
         int ramdomValue = random.nextInt(10);
         if (ramdomValue >= 4) {
             numberOfMove++;
