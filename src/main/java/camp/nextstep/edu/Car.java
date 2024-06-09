52233d1d296a6203dbe8cf45b785a74430ae8854
Jeongyeon Kim <wjddus9132@gmail.com>
2024-06-09 09:54:23 +0900

    feat(Car): 자동차 모델 구현
    - 자동차 정보를 담은 Car 클래스 생성

---
+++ src/main/java/camp/nextstep/edu/Car.java
@@ -0,0 +1,20 @@
+package camp.nextstep.edu;
+
+public class Car {
+
+    private String name;
+    private int location;
+
+    public Car(String name) {
+        this.name = name;
+        this.location = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getLocation() {
+        return location;
+    }
+}
