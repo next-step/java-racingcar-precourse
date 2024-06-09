d9ccda104ff1156db1f46a942e1d56e1e409057d
Kangboom <jkl6147@naver.com>
2024-06-09 10:45:56 +0900

    feat(CarList): add carList, setCarList(), getCarList()

---
+++ src/main/java/model/CarList.java
@@ -0,0 +1,25 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CarList {
+    List<Car> carList;
+
+    public CarList(){
+        carList = new ArrayList<>();
+    }
+    public void setCarList(String carNames){
+        String[] inputList = carNames.split(",");
+
+        for(String name : inputList) {
+            Car car = new Car(name.trim(), 0);
+            carList.add(car);
+        }
+    }
+    public List<Car> getCarList(){
+        return carList;
+    }
+
+}
