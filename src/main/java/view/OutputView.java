035988126ac5a3049cffe36796f0bc61c8b215d8
young-in <hardworkingini@gmail.com>
2024-06-10 06:10:54 +0900

    feat (Output.java) : 각 라운드 별, 자동차들의 이름, 전진한 거리를 출력하기 위한 view 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,11 +1,16 @@
 package view;
 
+import domain.RacingCar;
+import domain.RacingCars;
+
 public class OutputView {
 
     private static final String INPUT_CAR_NAMES = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_TRY_COUNT = "시도할 회수는 몇회인가요?";
     private static final String NEW_LINE = "\n";
     private static final String EXECUTION_RESULT = "실행 결과";
+    private static final String COLON_SIGN = " : ";
+    private static final String MOVE_SIGN = "-";
 
     public void printRequestCarNames(){
         System.out.println(INPUT_CAR_NAMES);
@@ -17,5 +22,16 @@ public class OutputView {
 
     public void printExecutionResult(){
         System.out.println(NEW_LINE + EXECUTION_RESULT);
+    }
+
+    public void printRoundByExecutionResults(final RacingCars cars){
+        StringBuilder resultByRound = new StringBuilder();
+        for (RacingCar car : cars.getCars()) {
+            resultByRound.append(car.getRacingCarName())
+                    .append(COLON_SIGN)
+                    .append(MOVE_SIGN.repeat(car.getDistance()))
+                    .append(NEW_LINE);
+        }
+        System.out.println(resultByRound);
     }
 }
