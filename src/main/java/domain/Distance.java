874b8e3cb72b625f7f256d2d1029365688daac33
young-in <hardworkingini@gmail.com>
2024-06-10 05:32:04 +0900

    feat(Distance.java) : racingCar의 위치를 바꾸기 위한 record class 구현

---
+++ src/main/java/domain/Distance.java
@@ -0,0 +1,9 @@
+package domain;
+
+public record Distance(int location) {
+    private static final int MOVE_VALUE = 1;
+
+    public Distance increase(){
+        return new Distance(location + MOVE_VALUE);
+    }
+}
