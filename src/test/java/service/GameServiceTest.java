76a2c2317200f79d9c01f7a898c018b10528e3e1
bokyeong <wntjdals0412@gmail.com>
2024-06-09 20:49:09 +0900

    Test: 우승 자동차 출력 기능 테스트

--- src/test/java/service/GameServiceTest.java
+++ src/test/java/service/GameServiceTest.java
@@ -4,15 +4,17 @@ import domain.Car;
 import domain.CarRepository;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameServiceTest {
     CarRepository carRepository = CarRepository.getInstance();
-    private static final GameService gameService = GameService.getInstance() ;
+    private static final GameService gameService = GameService.getInstance();
+
 
     //각 테스트가 끝나면 repository가 초기화된다.
     @AfterEach
@@ -21,6 +23,7 @@ class GameServiceTest {
     }
 
     @Test
+    @DisplayName("자동차 생성 테스트")
     void createCars() {
         //given
         String[] names = {"abc","bob","tom"};
@@ -36,9 +39,29 @@ class GameServiceTest {
     @Test
     void moveCars() {
 
+
     }
     @Test
-    void findWinner() {
+    @DisplayName("최종 우승 자동차 출력 테스트")
+    void findWinner() throws NoSuchFieldException, IllegalAccessException {
+        //given
+        String[] names = {"abc","bob","tom"};
+        Integer[] positions = {1,4,4};
+        for(int i=0;i<3;i++){
+            Car car = new Car(names[i]);
+            setPosition(car,positions[i]);
+            carRepository.save(car);
+        }
+        String[] expected = {"bob","tom"};
+
+        assertArrayEquals(expected,gameService.findWinner().toArray());
+    }
+
+    public static void setPosition(Car car, int position) throws NoSuchFieldException, IllegalArgumentException, IllegalAccessException {
+        Field field = car.getClass().getDeclaredField("position");
+        //position 필드 set 가능하게 함
+        field.setAccessible(true);
+        field.setInt(car,position);
     }
 
 
