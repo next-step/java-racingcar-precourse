a2ccad0a2ae34c0669e2773081691be1c478e337
dong_yxxn <g0326kdy@gmail.com>
2024-06-10 01:07:18 +0900

    refactor(test): test function of get winner list

--- src/test/java/service/RacingServiceTest.java
+++ src/test/java/service/RacingServiceTest.java
@@ -2,6 +2,8 @@ package service;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import model.RacingCarDTO;
 import org.assertj.core.api.Assertions;
@@ -53,5 +55,27 @@ class RacingServiceTest{
             assertThat(expectedForwards.get(car.getName()))
                         .isEqualTo(car.getForward());
         }
+    }
+
+    @Test
+    @DisplayName("우승자 명단 반환")
+    public void getWinnerList(){
+        //given
+        RacingCarDTO car1 = new RacingCarDTO("car1");
+        RacingCarDTO car2 = new RacingCarDTO("car2");
+        RacingCarDTO car3 = new RacingCarDTO("car3");
+        car1.setForward(1);
+        car2.setForward(3);
+        car3.setForward(3);
+
+        ArrayList<String> expected = new ArrayList<>(List.of("car2", "car3"));
+
+        //when
+        rs.getCarRepository().getAllCars().add(car1);
+        rs.getCarRepository().getAllCars().add(car2);
+        rs.getCarRepository().getAllCars().add(car3);
+
+        //then
+        assertThat(expected).isEqualTo(rs.getWinnerArrList());
     }
 }
