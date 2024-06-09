7226059d50bb2b0eb06a3d27120823366867dde6
Shsin9797 <aaa8141@naver.com>
2024-06-09 20:27:35 +0900

    feat(Game) : 입력 받은 자동차 개수 체크하는 기능 추가

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -14,13 +14,13 @@ public class Game {
             try {
                 System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
                 carNames = List.of(sc.next().split(","));
+                CheckInput.checkCarCount(carNames);
                 CheckInput.checkCarNames(carNames);
                 return carNames;
             } catch (IllegalArgumentException | IllegalStateException e) {
                 System.out.println(e.getMessage());
             }
         }
-
     }
 
     public int getTryNum() {
