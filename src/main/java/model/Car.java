88555b0f4320ea2d40ce8ec661dea8947ba51316
Woojae Jeong <woo6388@naver.com>
2024-06-09 18:56:50 +0900

    "Refactor: Modify Car Class"

--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -1,33 +1,30 @@
 package model;
 
+import java.util.Random;
+
 public class Car {
     private String carName; //자동차 이름
-    private boolean move; //전진할지 멈출지 담는 변수
     private int numberOfMove; //지금까지 얼마나 이동했는지 담는 변수
+    private static final Random random = new Random();
 
     public Car(String carName){
         this.carName = carName;
-        move = false;
         numberOfMove = 0;
     }
 
     public int getNumberOfMove() {
         return numberOfMove;
     }
-
-    public void increaseNumberOfMove() {
-        numberOfMove++;
+    public void tryIncreaseNumberOfMove() {
+        int ramdomValue = random.nextInt(10);
+        if (ramdomValue >= 4) {
+            numberOfMove++;
+        }
     }
+
 
     public String getCarName() {
         return carName;
     }
 
-    public boolean isMove() {
-        return move;
-    }
-
-    public void setMove(boolean move) {
-        this.move = move;
-    }
 }
