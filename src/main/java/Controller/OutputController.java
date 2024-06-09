125f7a56d8941005d92e0a89e3fa4e1e4b771115
mac <20j.code@gmail.com>
2024-06-09 20:43:14 +0900

    feat : printRaceWinner 추가

--- src/main/java/Controller/OutputController.java
+++ src/main/java/Controller/OutputController.java
@@ -16,6 +16,13 @@ public class OutputController {
     }
 
     public void printRaceWinner(Set<String> fastCarName) {
+        String printString = "최종 우승자 : ";
+
+        for(String s : fastCarName){
+            printString += s+", ";
+        }
+        System.out.println(printString.substring(0,printString.length()-2));
+
     }
 
     private String positionMaker(int count){
