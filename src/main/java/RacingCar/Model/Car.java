b75d370c953c215e1d07579c877e901d311b0ef9
Gahyeon Lee <best11gh@naeileun.dev>
2024-06-09 19:01:32 +0900

    feat: Race class

--- src/main/java/RacingCar/Model/Car.java
+++ src/main/java/RacingCar/Model/Car.java
@@ -1,13 +1,11 @@
 package RacingCar.Model;
 
 public class Car {
+
   private final String name;
   private int position;
 
   public Car(String name) {
-    if (name.length() > 5) {
-      throw new IllegalArgumentException("자동차 이름은 5자 이하만 가능합니다.");
-    }
     this.name = name;
     this.position = 0;
   }
