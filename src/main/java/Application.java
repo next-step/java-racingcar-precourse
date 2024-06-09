99a5f15ad8fc0d0fe8dc1c64eecba4716a7bdecb
Kangboom <jkl6147@naver.com>
2024-06-09 10:43:46 +0900

    feat(Application): add racing.run()

---
+++ src/main/java/Application.java
@@ -0,0 +1,8 @@
+import controller.RacingController;
+
+public class Application {
+    public static void main(String[] args) {
+        RacingController racing = new RacingController();
+        racing.run();
+    }
+}
