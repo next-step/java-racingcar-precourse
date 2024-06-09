1f1977e493e93666442f3018e032f21fd658b8b0
dlwnstn0524 <ljs052450@gmail.com>
2024-06-09 22:19:07 +0900

    feature: Add Test Code

--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,4 +1,8 @@
+import Controller.RacingController;
+import Utils.RandomUtil;
+import Views.InputView;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 import java.util.List;
@@ -10,11 +14,19 @@ public class ApplicationTest {
     @Test
     void 게임진행(){
         // given
-        run();
+        RacingController rc = RacingController.getInstance();
+        rc.run();
+        InputView mockInputView = mock(InputView.class);
+        RandomUtil mockRandomUtil = mock(RandomUtil.class);
+
+
         // when
+        when(mockInputView.requestName()).thenReturn("leo,neo,geo");
+        when(mockInputView.requestTime()).thenReturn("5");
+        when(mockRandomUtil.getRandomArray(3, 0, 9)).thenReturn(new int[]{0,0,4});
 
         // then
-
+        assertEquals();
     }
 
     @Test
