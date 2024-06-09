be82f48eef86d4bfeecb35317676100ca67a2bfe
JunHyeongChoi <ajfeh22@gmail.com>
2024-06-09 21:15:04 +0900

    feat: 0부터 9까지의 무작위 수 생성

---
+++ src/main/java/util/RandomNumberGenerator.java
@@ -0,0 +1,13 @@
+package util;
+
+import java.util.Random;
+
+public class RandomNumberGenerator {
+
+    private final Random random = new Random();
+
+    public int generateRandomNumber() {
+        return random.nextInt(10);
+    }
+
+}
