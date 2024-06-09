fad2c977dbd117b22db24d478c69595888f8cdc6
dong_yxxn <g0326kdy@gmail.com>
2024-06-10 00:58:11 +0900

    refactor(service): add method for get winner list

--- src/main/java/service/RacingService.java
+++ src/main/java/service/RacingService.java
@@ -1,5 +1,7 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
 import model.RacingCarDTO;
 import repository.CarRepository;
 import repository.CarRepositoryImpl;
@@ -28,9 +30,30 @@ public class RacingService {
             }
         }
     }
+    public ArrayList<String> getWinnerArrList(){
+        ArrayList<String> winnerArrList;
+        ArrayList<RacingCarDTO> carList = carRepository.getAllCars();
+        int maxForward = getMaxForward(carList);
+        winnerArrList = getCollect(carList, maxForward);
+
+        return winnerArrList;
+    }
     protected void validateName(String name) {
         if (name == null || name.length() > 5) {
             throw new IllegalArgumentException("[ERROR] 이름은 5글자를 초과할 수 없습니다.");
         }
+    }
+    private ArrayList<String> getCollect(ArrayList<RacingCarDTO> carList, int maxForward) {
+        return carList.stream()
+            .filter(car -> car.getForward() == maxForward)
+            .map(RacingCarDTO::getName)
+            .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    private int getMaxForward(ArrayList<RacingCarDTO> carList) {
+        return carList.stream()
+            .mapToInt(RacingCarDTO::getForward)
+            .max()
+            .orElse(0);
     }
 }
