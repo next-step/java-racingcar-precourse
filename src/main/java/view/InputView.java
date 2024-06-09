6c6c83f572325fadb9bba73e110f7ce4ad07035d
young-in <hardworkingini@gmail.com>
2024-06-10 05:10:09 +0900

    feat(OutputView.java) : 경주용 자동차들의 이름 입력을 위한 안내사항 출력 view 작성

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -12,7 +12,6 @@ public class InputView {
     }
 
     public String[] inputCarNames(){
-        System.out.print("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String carNames = scanner.nextLine();
         return SplitingCarName.splitCarName(carNames);
     }
