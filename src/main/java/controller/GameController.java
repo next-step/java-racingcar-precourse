814597475c6bbf7287d0249da0daa128f53590f9
bokyeong <wntjdals0412@gmail.com>
2024-06-09 19:52:48 +0900

    Feat: 자동차 전진 기능

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -5,9 +5,10 @@ import utills.Validator;
 import view.InputView;
 import view.OutputView;
 
+import java.util.Map;
+
 public class GameController {
     //TODO
-    // 3.게임 진행
     // 4.승자 탐색
     private final InputView inputView = new InputView();
     private final OutputView outputView = new OutputView();
@@ -15,11 +16,24 @@ public class GameController {
     private static String[] carsName;
     private static int count;
 
-    public void playGame(){
+    public void startGame(){
         //TODO
         getUserInput();
         gameService.createCars(carsName);
+        playGame();
+    }
 
+    /**
+     * 게임을 진행하는 메서드 (사용자가 입력한 횟수만큼 진행)
+     */
+    private void playGame(){
+        Map<String,Integer> result;
+        outputView.outputText();
+        //count 횟수만큼 움직이고 출력
+        for (int i = 0; i<count; i++){
+            result = gameService.moveCars(carsName);
+            outputView.printResult(result);
+        }
     }
 
 
@@ -47,7 +61,7 @@ public class GameController {
                 String count = inputView.getCount();
                 Validator.validCheckCount(count);
                 return Integer.parseInt(count);
-            } catch(IllegalArgumentException e){
+            } catch(IllegalArgumentException e) {
                 outputView.printError(e.getMessage());
             }
         }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,7 +1,28 @@
 package view;
 
+import java.util.Map;
+
 public class OutputView {
+
+    /**
+     * 에러가 발생했을시 [ERROR]로 시작하는 에러메시지 출력하는 메서드
+     *
+     * @param err 에러메시지
+     */
     public void printError(String err){
         System.out.println("[ERROR]" + err);
     }
+
+    public void outputText(){
+        System.out.println("실행 결과");
+    }
+
+    public void printResult(Map<String,Integer> cars){
+        for(String name: cars.keySet()){
+            System.out.println(name+ " : " + "-".repeat(Math.max(0, cars.get(name))));
+        }
+        System.out.println();
+    }
+
+    //TODO 승자 출력하기
 }
