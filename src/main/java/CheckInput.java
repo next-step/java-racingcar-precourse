9ad5a3a6fa717a0c50b036574c0b47688068197a
Shsin9797 <aaa8141@naver.com>
2024-06-09 20:26:31 +0900

    feat(CheckInput) : 자동차 입력값이 없을때 예외처리 추가

--- src/main/java/CheckInput.java
+++ src/main/java/CheckInput.java
@@ -3,36 +3,32 @@ import java.util.IllegalFormatException;
 import java.util.List;
 
 public class CheckInput {
-    public static boolean checkCarNames(List<String> carNames) {
-        if(!checkCarNameDiffer(carNames)){
-            return false;
+
+    public static void checkCarCount(List<String> carNames) {
+        if (carNames.size() < 2) {
+            throw new IllegalArgumentException("[ERROR] 자동차를 2대 이상 적어야 경주가 시작됩니다.");
         }
+    }
+
+    public static void checkCarNames(List<String> carNames) {
+        checkCarNameDiffer(carNames);
         for (String carName : carNames) {
             int carLength = carName.length();
-            if(!checkCarNameLength(carName)){
-                return false;
-            };
+            checkCarNameLength(carName);
         }
-        return true;
     }
 
-    public static boolean checkCarNameDiffer(List<String> carNames) {
-        boolean check = true;
+    public static void checkCarNameDiffer(List<String> carNames) {
         HashSet<String> setNames = new HashSet<>(carNames);
         if (setNames.size() != carNames.size()) {
-            check=false;
             throw new IllegalStateException("[ERROR] 자동차의 이름은 모두 다르게 입력해주세요.");
         }
-        return check;
     }
 
-    public static boolean checkCarNameLength(String name) {
-        boolean result = true;
-        if ((name.length() < 1) || (name.length() > 5)) {
-            result=  false;
+    public static void checkCarNameLength(String name) {
+        if ((name.isEmpty()) || (name.length() > 5)) {
             throw new IllegalArgumentException("[ERROR] 차 이름의 길이는 1글자~5글자 사이로 입력해주세요.");
         }
-        return result;
     }
 
     public static void isWrongIntValue(int tryNum) {
