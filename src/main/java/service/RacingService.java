ce6c2136600abeae0c83caf40f05418572aff239
Kangboom <jkl6147@naver.com>
2024-06-09 10:48:31 +0900

    feat(RacingService): add getRandomNum(), race(), getMaxDistance(), getWinner()

---
+++ src/main/java/service/RacingService.java
@@ -0,0 +1,47 @@
+package service;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import model.Car;
+import model.CarList;
+
+public class RacingService {
+    private Random random ;
+    public RacingService(){
+        random = new Random();
+    }
+    private int getRandomNum(){
+        return random.nextInt(10);
+    }
+
+    public void race(CarList carList){
+        // 랜덤 값 나와서 전진하는 로직
+        for(Car car : carList.getCarList()) {
+            int randNum = getRandomNum();
+            if(randNum >= 4){
+                car.move();
+            }
+        }
+    }
+
+    public int getMaxDistance(CarList carList){
+        // 우승자 로직
+        int maxDistance = 0;
+        for( Car car : carList.getCarList()){
+            if(maxDistance < car.getDistance()){
+                maxDistance = car.getDistance();
+            }
+        }
+        return maxDistance;
+    }
+
+    public List<String> getWinner(int maxDistance, CarList carList){
+        List<String> winner = new ArrayList<>();
+        for( Car car : carList.getCarList()){
+            if(car.getDistance() == maxDistance){
+                winner.add(car.getName());
+            }
+        }
+        return winner;
+    }
+}
