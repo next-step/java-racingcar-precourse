66f6743c9f808e5635524b8de83a783d2dbcae3c
bolee058 <bolee058@gmail.com>
2024-06-09 21:49:15 +0900

    feat: 자동차 경주 게임 기능 구현

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,25 @@
+package model;
+
+public class Car {
+
+    private String name;
+    private int position = 0;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(int randomNumber) {
+        if (randomNumber >= 4){
+            position++;
+        }
+    }
+}
