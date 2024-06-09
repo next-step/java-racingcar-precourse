048a499d07c18e32d31ecfeedc6d2767efc682a0
Woojae Jeong <woo6388@naver.com>
2024-06-09 13:19:45 +0900

    Refactory: Deleting Result class and Modifying car classes.

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -11,8 +11,9 @@ public class Controller {
     public void play() { //자동차 경주 게임 전반의 흐름을 처리
         carName = EntryCarName.getCarName(); //사용자에게 입력받은 자동차 이름 저장
         moveCnt = EntryMoveCnt.getMoveCnt(); //사용자에게 입력받은 시도 횟수 저장
-        carList=makeCar(carName);
-        DecisionMove.decide(carList);
+        carList=makeCar(carName); //자동차 객체 생성
+        DecisionMove.decide(carList); //자동차들을 전진할지 멈출지 판단
+
     }
 
     public Vector<Car> makeCar(Vector<String> carName) { //car 객체를 생성하는 기능
--- src/main/java/model/Car.java
+++ src/main/java/model/Car.java
@@ -3,10 +3,20 @@ package model;
 public class Car {
     private String carName; //자동차 이름
     private boolean move; //전진할지 멈출지 담는 변수
+    private int numberOfMove; //지금까지 얼마나 이동했는지 담는 변수
 
     public Car(String carName){
         this.carName = carName;
         move = false;
+        numberOfMove = 0;
+    }
+
+    public int getNumberOfMove() {
+        return numberOfMove;
+    }
+
+    public void setNumberOfMove(int numberOfMove) {
+        this.numberOfMove = numberOfMove;
     }
 
     public String getCarName() {
