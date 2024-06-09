c9eaedaa54869e1cea93f06a11931d1a963e9580
yjhannn <114985804+yjhannn@users.noreply.github.com>
2024-06-09 17:39:01 +0900

    add : CarRacingApplication 추가

---
+++ src/main/java/CarRacingApplication.java
@@ -0,0 +1,9 @@
+import controller.CarRaceController;
+
+public class CarRacingApplication {
+    public static void main(String[] args) {
+        CarRaceController carRaceController = new CarRaceController();
+        carRaceController.run();
+    }
+
+}
