af63ec2f901bda0879805c5770794c2cbe6746b5
doyooon <ian661087@gmail.com>
2024-06-10 05:05:09 +0900

    feat InputView : 이동 횟수 입력 받기

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -14,6 +14,21 @@ public class InputView {
         this.carNames = scanner.nextLine().split("\\s*,\\s*");
     }
 
+    public void inputIteration() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("시도할 회수는 몇회인가요?");
+        try { // 횟수 입력 검사
+            this.iteration = scanner.nextInt();
+            if (this.iteration < 1) {
+                throw new IllegalArgumentException();
+            }
+        } catch (InputMismatchException e) { // 숫자가 아닌 값을 입력 받은 경우
+            System.out.println("[ERROR] : 시도할 회수는 자연수만 입력 가능합니다. 다시 입력 해주세요.");
+        } catch (IllegalArgumentException e) { // 0 또는 음의 정수를 입력 받은 경우
+            System.out.println("[ERROR] : 시도할 회수는 자연수만 입력 가능합니다. 다시 입력 해주세요.");
+        }
+    }
+
     public String[] getCarNames() {
         return this.carNames;
     }
