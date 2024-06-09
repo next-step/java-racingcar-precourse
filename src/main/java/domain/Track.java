83f885396bc5ddfa0dda68e3932d7295026af683
sanghee0820 <lsh277604@gmail.com>
2024-06-09 13:15:00 +0900

    feat: Track클래스 추가

---
+++ src/main/java/domain/Track.java
@@ -0,0 +1,12 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Track {
+    private final List<Car> cars;
+
+    public Track() {
+        this.cars = new ArrayList<>();
+    }
+}
