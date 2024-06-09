81507f0fc913633c280ca09c2366719aa2efbc9b
Awhn <ahnshwan@outlook.com>
2024-06-09 16:50:57 +0900

    feat : Race class 모델 클래스 Race 작성

---
+++ src/main/java/model/Race.java
@@ -0,0 +1,52 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Race {
+    private final List<Car> cars;
+    private final int iterNum;
+    private final Random random;
+
+    public Race(List<String> names, int iterNum) {
+        this.iterNum = iterNum;
+        this.random = new Random();
+        this.cars = new ArrayList<>();
+        for(String name : names) {
+            this.cars.add(new Car(name));
+        }
+    }
+
+    public void init() {
+        for (int i = 0; i < iterNum; i++) {
+            round();
+        }
+    }
+
+    private void round() {
+        for (Car car : cars) {
+            int randNum = random.nextInt(10);
+            if (randNum >= 4) {
+                car.moveFoward();
+            }
+        }
+    }
+
+
+    public List<Car> checkWinner() {
+        int maxPosition = 0;
+        List<Car> winners = new ArrayList<>();
+        for (Car car : cars) {
+            if (car.getPosition() > maxPosition) {
+                maxPosition = car.getPosition();
+            }
+        }
+        for (Car car : cars) {
+            if (car.getPosition() == maxPosition) {
+                winners.add(car);
+            }
+        }
+        return winners;
+    }
+}
