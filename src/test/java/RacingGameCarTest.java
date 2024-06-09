6dc5c5d687cc55b0b8910858f65dd27a27d4fa70
amm0124 <amm012450@gmail.com>
2024-06-09 16:20:48 +0900

    FEAT : RacingGameCarTest toString Test 추가

--- src/test/java/RacingGameCarTest.java
+++ src/test/java/RacingGameCarTest.java
@@ -1,13 +1,20 @@
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import org.junit.jupiter.api.Test;
 import Model.RacingGameCar;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RacingGameCarTest {
 
     @Test
     public void testForwardingCar() {
-        RacingGameCar car = new RacingGameCar("TestCar", 0);
+        RacingGameCar car = new RacingGameCar("TestCar", 3);
         car.forwardingCar();
-        assertEquals(1, car.getForward());
+        assertEquals(4, car.getForward());
+    }
+
+    @Test
+    public void testToString() {
+        RacingGameCar car = new RacingGameCar("TestCar", 5);
+        assertEquals("TestCar : -----", car.toString());
     }
 }
