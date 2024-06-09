44f830af5fa6170cb27a3825675d2891b00cece7
Long9725 <develop9725@gmail.com>
2024-06-09 12:21:22 +0900

    <FEAT> RacerController setUpRacer 메소드 구현

---
+++ src/main/java/controller/RacerController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import entity.Racer;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class RacerController {
+    private List<Racer> raceList;
+
+    public RacerController() {
+        this.raceList = new ArrayList<>();
+    }
+
+    public void setUpRacer(String nameString) {
+        validateName(nameString);
+
+        List<Racer> newRacerSet = Arrays.stream(nameString.split(","))
+                .map(Racer::new)
+                .toList();
+
+        raceList.clear();
+        raceList.addAll(newRacerSet);
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException(Racer.VALIDATE_NAME_ERROR_MESSAGE);
+        }
+    }
+}
