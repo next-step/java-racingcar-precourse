0fb1fc2a24924417ff74fca4852e669b190e79d3
young-in <hardworkingini@gmail.com>
2024-06-10 05:54:09 +0900

    feat (Application.java) : racing game 시작 구현

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,9 @@
+import controller.RacingGameController;
+
 public class Application {
+    public static void main(String[] args) {
+        RacingGameController racingGameController = new RacingGameController();
+        racingGameController.startGame();
+    }
 }
+
