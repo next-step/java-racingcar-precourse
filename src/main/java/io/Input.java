2156ab3f8874276d2ebc21d8e0a92680ee348b96
박민규 <pmq0101@naver.com>
2024-06-10 00:50:01 +0900

    feat(io): Input/Output 추가

--- src/main/java/io/Input.java
+++ src/main/java/io/Input.java
@@ -6,10 +6,16 @@ public class Input {
     private Scanner scanner = new Scanner(System.in);
 
     public String getCarName(){
-
+        System.out.println("경주할 자동차 이름을 입력하세요. (이름은 쉼표(,) 기준으로 구분)");
+        return scanner.next();
     }
 
     public String getRaceCount(){
+        System.out.println("시도할 횟수는 몇회인가요?");
+        return scanner.next();
+    }
 
+    public String deleteSpace(String raceInput){
+        return raceInput.replace(" ", "");
     }
 }
