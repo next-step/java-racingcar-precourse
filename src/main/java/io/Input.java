b241c4e9e5556b96d9eda2b5bff94f86756694c9
박민규 <pmq0101@naver.com>
2024-06-09 14:54:25 +0900

    feat(io): default Setting

---
+++ src/main/java/io/Input.java
@@ -0,0 +1,15 @@
+package io;
+
+import java.util.Scanner;
+
+public class Input {
+    private Scanner scanner = new Scanner(System.in);
+
+    public String getCarName(){
+
+    }
+
+    public String getRaceCount(){
+
+    }
+}
---
+++ src/main/java/io/Output.java
@@ -0,0 +1,12 @@
+package io;
+
+public class Output {
+    public void printRace(){
+
+    }
+
+    public void printWinner(){
+
+    }
+}
+
