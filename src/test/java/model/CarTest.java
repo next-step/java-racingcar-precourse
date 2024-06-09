6a8f92caa15f7accaa0f5022046bd8a066c2261c
cussle <cussle@kakao.com>
2024-06-10 05:00:12 +0900

    test: 주요 로직에 대한 단위 테스트 구현
    - Car 클래스의 이름 유효성, 위치 초기화 및 전진 기능 테스트 추가

--- src/test/java/model/CarTest.java
+++ src/test/java/model/CarTest.java
@@ -14,14 +14,25 @@ public class CarTest {
     }
 
     @Test
+    public void carNameShouldNotBeEmpty() {
+        IllegalArgumentException thrown = assertThrows(
+            IllegalArgumentException.class,
+            () -> new Car("")
+        );
+        assertEquals("[ERROR] 자동차 이름은 1자 이상 5자 이하만 가능합니다.", thrown.getMessage());
+    }
+
+    @Test
     public void carShouldStartAtPositionZero() {
         Car car = new Car("test");
+        car.initializePosition();
         assertEquals(0, car.getPosition());
     }
 
     @Test
     public void carShouldMove() {
         Car car = new Car("test");
+        car.initializePosition();
         car.move();
         assertEquals(1, car.getPosition());
     }
