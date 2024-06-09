23c6bd18a032cf2aedac68a3f41227b70d30bd3d
nove1080 <skwpqjq@gmail.com>
2024-06-10 00:35:19 +0900

    feat: Rule final 키워드 추가

--- src/main/java/game/config/constant/Rule.java
+++ src/main/java/game/config/constant/Rule.java
@@ -5,10 +5,10 @@ public class Rule {
     private Rule() {
     }
 
-    public static String NAME_DELIMITER = ",";
-    public static int MAX_NAME_LENGTH = 5;
-    public static int MIN_NAME_LENGTH = 1;
-    public static int MAX_RANDOM_VALUE = 9;
-    public static int MOVEMENT_THRESHOLD = 4;
-    public static String CAR_POSITION_MAKER = "-";
+    public static final String NAME_DELIMITER = ",";
+    public static final int MAX_NAME_LENGTH = 5;
+    public static final int MIN_NAME_LENGTH = 1;
+    public static final int MAX_RANDOM_VALUE = 9;
+    public static final int MOVEMENT_THRESHOLD = 4;
+    public static final String CAR_POSITION_MAKER = "-";
 }
