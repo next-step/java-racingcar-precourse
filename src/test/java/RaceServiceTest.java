bfa42bb504b6a0d82fbf2b48f081c23abb33969d
kangwlgns <kangji0615@gmail.com>
2024-06-09 22:37:06 +0900

    test(RaceServiceTest): add getWinnersTest

--- src/test/java/RaceServiceTest.java
+++ src/test/java/RaceServiceTest.java
@@ -9,6 +9,8 @@ import service.RaceServiceInterface;
 
 // 구체적 로직을 갖는 RaceService에 대한 테스트.
 // 단순한 getter, setter에 대한 테스트는 생략하였습니다.
+// service와 model을 분리하였으므로 model에 대한 테스트 없이 service에 대한 테스트만 진행합니다.
+// 또한 RaceService의 로직에서 car model의 모든 기능을 사용하므로 car model에 대한 테스트도 생략합니다.
 public class RaceServiceTest {
 
     // addCars를 통해 삽입이 잘 됐는지 확인
@@ -82,7 +84,8 @@ public class RaceServiceTest {
         String round = "a";
 
         // 잘못된 경우에 대해 테스트 (parse 문제)
-        Assertions.assertThatCode(() -> raceService.prepareRace(round)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatCode(() -> raceService.prepareRace(round))
+            .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
@@ -93,7 +96,28 @@ public class RaceServiceTest {
         String round = "101";
 
         // 잘못된 경우에 대해 테스트 (범위 문제)
-        Assertions.assertThatCode(() -> raceService.prepareRace(round)).isInstanceOf(IllegalArgumentException.class);
+        Assertions.assertThatCode(() -> raceService.prepareRace(round))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void startRaceTest() {
+        // 객체와 삽입할 배열을 준비
+        Race race = new Race();
+        RaceServiceInterface raceService = new RaceService(race, race);
+        String[] carsName = {"jih", "hyu", "abc", "xyz", "aaa", "bbb", "ccc", "ddd", "eee", "fff",
+            "ggg", "hhh"};
+
+        // race 준비 및 시작
+        raceService.addCars(carsName);
+        // 충분히 많은 시행에서 모두의 진행도가 동일하다면 어딘가 로직이 잘못된 것
+        raceService.prepareRace("100");
+        raceService.startRace();
+
+        // 모두의 진행도가 동일한지 확인
+        boolean isAllEqual = raceService.getCars().stream()
+            .allMatch(car -> raceService.getCars().get(0).getProgress() == car.getProgress());
+        Assertions.assertThat(isAllEqual).isFalse();
     }
 
     @Test
@@ -114,15 +138,15 @@ public class RaceServiceTest {
         // 승자 중 한명의 진행도를 받아옴.
         int winnerProgress = winners.get(0).getProgress();
         // 우선 승자들끼리 진행도가 모두 같은지 확인하는 변수 생성
-        boolean isWinnersProgressAllEqual = winners.stream().allMatch(car -> car.getProgress() == winnerProgress);
+        boolean isWinnersProgressAllEqual = winners.stream()
+            .allMatch(car -> car.getProgress() == winnerProgress);
         // getWinners로 받아온 승자의 진행도가 실제로 가장 높은지 확인하는 변수 생성
-        boolean isWinnerProgressTheHighest = raceService.getCars().stream().allMatch(car -> car.getProgress() <= winnerProgress);
+        boolean isWinnerProgressTheHighest = raceService.getCars().stream()
+            .allMatch(car -> car.getProgress() <= winnerProgress);
 
         // 승자들끼리의 진행도를 확인
         Assertions.assertThat(isWinnersProgressAllEqual).isTrue();
         // getWinners로 받아온 승자가 진짜 승자인지 확인
         Assertions.assertThat(isWinnerProgressTheHighest).isTrue();
     }
-
-
 }
