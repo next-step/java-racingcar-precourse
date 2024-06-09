adc65125070255b53762cbd6553ccc98fe44c839
Yeonwoo JI <95765163+speciling@users.noreply.github.com>
2024-06-10 04:48:04 +0900

    feat: 결과 출력 기능 구현

---
+++ src/main/java/view/MoveView.java
@@ -0,0 +1,16 @@
+package view;
+
+import java.util.Map;
+
+public class MoveView {
+
+    public static void printResultMessage() {
+        System.out.println("\n실행 결과");
+    }
+
+    public static void printResult(Map<String, Integer> positions) {
+        for (Map.Entry<String, Integer> entry : positions.entrySet()) {
+            System.out.println(entry.getKey() + " : " + "-".repeat(entry.getValue()));
+        }
+    }
+}
