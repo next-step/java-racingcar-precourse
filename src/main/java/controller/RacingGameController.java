0b0ad514aad6b4c67b9f7756ff8bf5eb1ec4223d
young-in <hardworkingini@gmail.com>
2024-06-10 06:02:02 +0900

    feat (RacingCameController.java) : racing game을 위한 racing car 생성을 위한 함수 생성

--- src/main/java/controller/RacingGameController.java
+++ src/main/java/controller/RacingGameController.java
@@ -1,5 +1,7 @@
 package controller;
 
+import domain.RacingCars;
+import util.CreatingCar;
 import view.InputView;
 import view.OutputView;
 
@@ -7,5 +9,11 @@ public class RacingGameController {
     private InputView inputView = new InputView();
     private OutputView outputView = new OutputView();
     public void startGame(){
+        RacingCars racingCar = createRacingCar();
+    }
+    private RacingCars createRacingCar(){
+        outputView.printRequestCarNames();
+        String[] carNames = inputView.inputCarNames();
+        return CreatingCar.createCars(carNames);
     }
 }
