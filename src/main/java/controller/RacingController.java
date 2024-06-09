44efb732a0769989cb09455d97d397ac72112548
dong_yxxn <g0326kdy@gmail.com>
2024-06-10 00:12:00 +0900

    feat(controller): implements RacingController class - add memeber variables(Scanner sc, RacingService rs, RacingView rv, int n) - add inputCarNames method

---
+++ src/main/java/controller/RacingController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.util.Scanner;
+import service.RacingService;
+import view.RacingView;
+
+public class RacingController {
+    private final Scanner sc;
+    private final RacingService rs;
+    private final RacingView rv;
+    private int n;
+    public RacingController(){
+        sc = new Scanner(System.in);
+        rs = new RacingService();
+        rv = new RacingView();
+    }
+    public void inputCarNames(){
+        while(true) {
+            rv.showInputCarNamesMsg();
+            String nameStr = sc.nextLine();
+            try {
+                rs.enrollCars(nameStr);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
