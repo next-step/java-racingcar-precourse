41a28b874acd1a93d91b39cb3096027ade191f0a
dong_yxxn <g0326kdy@gmail.com>
2024-06-10 00:51:01 +0900

    refactor(test): check forward adding

--- src/test/java/service/RacingServiceTest.java
+++ src/test/java/service/RacingServiceTest.java
@@ -2,11 +2,14 @@ package service;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.Map;
+import model.RacingCarDTO;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import repository.CarRepository;
+import utility.Fortuna;
 
 class RacingServiceTest{
     RacingService rs = new RacingService();
@@ -28,5 +31,27 @@ class RacingServiceTest{
 
         // Then
         assertThat(exception.getMessage()).isEqualTo(expectedErrorMessage);
+    }
+
+    @Test
+    @DisplayName("RandomInt가 4 미만이면 전진 못함, 4 이상이면 전진 가능")
+    public void cannotGoForward(){
+        //given
+        rs.enrollCars("car1, car2");
+        Map<String, Integer> expectedForwards = Map.of(
+            "car1", 1,
+            "car2", 0
+        );
+        //when
+        for(RacingCarDTO car : rs.getCarRepository().getAllCars()){
+            if(car.getName().equals("car1")){
+                car.setForward(car.getForward() + 1);
+            }
+        }
+        //then
+        for (RacingCarDTO car : rs.getCarRepository().getAllCars()) {
+            assertThat(expectedForwards.get(car.getName()))
+                        .isEqualTo(car.getForward());
+        }
     }
 }
