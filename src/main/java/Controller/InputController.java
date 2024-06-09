033ba49ce620ac2e9ad4bba7a97b81f33e2c8dd8
mac <20j.code@gmail.com>
2024-06-09 20:07:14 +0900

    feat : inputRaceCount 추가
    자동차 경주 횟수 입력 추가

--- src/main/java/Controller/InputController.java
+++ src/main/java/Controller/InputController.java
@@ -15,7 +15,7 @@ public class InputController {
 
         while (true) {
             try {
-                return readSc();
+                return readCarName();
 
             } catch(IllegalArgumentException e){
                 System.out.println(e.getMessage());
@@ -24,25 +24,40 @@ public class InputController {
         }
     }
     public int inputRaceCount() {
-
+        while (true){
+            try {
+                return readRaceCount();
+            } catch(IllegalArgumentException e){
+                System.out.println(e.getMessage());
+            }
+        }
     }
-    private Set<String> readSc(){
+    private Set<String> readCarName(){
 
         Set<String> carNames = new HashSet<>();
 
         System.out.println("경주할 자동차 이름을 입력하세요."
-            + "(이름은 쉼표(,) 기준으로 구분");
+            + "(이름은 쉼표(,) 기준으로 구분)");
 
-        while (sc.hasNext(",")) {
-            String s = sc.next(",");
+        for(String s : nameSpliter(sc.next())) {
             nameLengthCheck(s);
-            nameDuplicateCheck(carNames,s);
+            nameDuplicateCheck(carNames, s);
             carNames.add(s);
         }
 
         return carNames;
 
     }
+    private int readRaceCount(){
+        System.out.println("시도할 횟수는 몇회인가요?");
+        int count=-1;
+        if(sc.hasNext()){
+            count = sc.nextInt();
+        }
+        countCheck(count);
+        return count;
+    }
+
     private void nameLengthCheck(String s){
         if(s.length()>5){
             throw new IllegalArgumentException("[ERROR] : 자동차 이름은 5자 이하입니다.");
@@ -55,6 +70,16 @@ public class InputController {
                 throw new IllegalArgumentException("[ERROR] : 자동차 이름이 중복됩니다.");
             }
         }
+    }
+
+    private void countCheck(int count){
+        if(count<=0){
+            throw new IllegalArgumentException("[ERROR] : 경주 횟수는 1번 이상입니다.");
+        }
+    }
+
+    private String[] nameSpliter(String names){
+        return names.split(",");
     }
 
 
