a228930d00f87453bdebad097b38a3f24e569ce5
sanghee0820 <lsh277604@gmail.com>
2024-06-09 16:24:46 +0900

    feat: 차량 이동 로직 추가

--- src/main/java/domain/Track.java
+++ src/main/java/domain/Track.java
@@ -13,4 +13,16 @@ public class Track {
     public void addCar(String carName){
         cars.add(new Car(carName));
     }
+    public void step(List<Integer> seeds){
+        validateSeedSize(seeds);
+        for(int i = 0; i < seeds.size(); i++){
+            cars.get(i).move(seeds.get(i));
+        }
+    }
+
+    private void validateSeedSize(List<Integer> seeds){
+        if(seeds.size() != cars.size()){
+            throw new IllegalArgumentException("[ERROR]: Seed 개수가 차량 개수와 일치하지 않습니다");
+        }
+    }
 }
