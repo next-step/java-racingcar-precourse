f54e8f4e65b8182f2c103a4b417a49ed0fc24311
young-in <hardworkingini@gmail.com>
2024-06-10 06:24:16 +0900

    feat (RacingGameController.java) : racing car 이동한 거리를 출력하는 View 연결

--- src/main/java/controller/RacingGameController.java
+++ src/main/java/controller/RacingGameController.java
@@ -3,15 +3,19 @@ package controller;
 import domain.RacingCars;
 import domain.TryialNum;
 import util.CreatingCar;
+import util.RandomGeneratingMove;
 import view.InputView;
 import view.OutputView;
 
 public class RacingGameController {
     private InputView inputView = new InputView();
     private OutputView outputView = new OutputView();
+    private final RandomGeneratingMove randomGeneratingMove = new RandomGeneratingMove();
     public void startGame(){
         RacingCars racingCars = createRacingCars();
         TryialNum tryialNum = getTryCount();
+
+        racing(racingCars, tryialNum);
     }
 
     private RacingCars createRacingCars(){
@@ -25,4 +29,14 @@ public class RacingGameController {
         String inputTryCount = inputView.inputTryCount();
         return new TryialNum(inputTryCount);
     }
+
+    private void racing(RacingCars cars, TryialNum tryialNum) {
+        outputView.printExecutionResult();
+
+        for (int i = 0; i < tryialNum.getTrialNum(); i++) {
+            cars.moveAll(randomGeneratingMove);
+            outputView.printRoundByExecutionResults(cars);
+        }
+    }
+
 }
