456f43126de89e90c4faaf8e384819f9393c2c91
Ink-By <inkinkb@gmail.com>
2024-06-10 04:41:26 +0900

    feat: 메인 실행문

---
+++ src/main/java/Application.java
@@ -0,0 +1,8 @@
+import controller.GameController;
+
+public class Application {
+    public static void main(String[] args) {
+        GameController controller = new GameController();
+        controller.playGame();
+    }
+}
