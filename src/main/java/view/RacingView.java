b9804d448054f0d59e12c4216803e9894d8d9213
dong_yxxn <g0326kdy@gmail.com>
2024-06-10 00:19:10 +0900

    refactor(view): add showing methods of input number of try

--- src/main/java/view/RacingView.java
+++ src/main/java/view/RacingView.java
@@ -4,5 +4,11 @@ public class RacingView {
     public void showInputCarNamesMsg(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
     }
+    public void showInputNumOfTry(){
+        System.out.println("시도할 회수는 몇회인가요?");
+    }
+    public void errorMsgInputNumOfTry(){
+        System.out.println("[ERROR]유효한 숫자를 입력해주세요.");
+    }
 
 }
