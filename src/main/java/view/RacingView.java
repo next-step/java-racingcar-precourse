49bfaed040285fc2ff0200763cffe7f432a159d1
dong_yxxn <g0326kdy@gmail.com>
2024-06-10 00:38:16 +0900

    refactor(view): add methods to showing some Racing messages - add RacingStartMsg, showRacing methods

--- src/main/java/view/RacingView.java
+++ src/main/java/view/RacingView.java
@@ -1,5 +1,7 @@
 package view;
 
+import repository.CarRepository;
+
 public class RacingView {
     public void showInputCarNamesMsg(){
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
@@ -9,6 +11,12 @@ public class RacingView {
     }
     public void errorMsgInputNumOfTry(){
         System.out.println("[ERROR]유효한 숫자를 입력해주세요.");
+    }
+    public void showRacingStartMsg(){
+        System.out.println("실행 결과");
+    }
+    public void showRacing(CarRepository carRepository){
+        System.out.println(carRepository.toString());
     }
 
 }
