4ed6468e396d8318cf9cc375efc06d0cadbe4165
Shsin9797 <aaa8141@naver.com>
2024-06-09 19:38:09 +0900

    fix(CheckInput) : String 값을 받아서 int 인지 확인하는 구문 주석처리

--- src/main/java/CheckInput.java
+++ src/main/java/CheckInput.java
@@ -1,3 +1,4 @@
+import java.util.IllegalFormatException;
 import java.util.List;
 
 public class CheckInput {
@@ -12,9 +13,21 @@ public class CheckInput {
         return true;
     }
 
+   /* public static boolean checkInt(String intNum) {
+        try {
+            Integer.parseInt(intNum);
+            return true;
+        } catch (IllegalFormatException e) {
+            CheckInput.isWrongValue();
+            return false;
+        }
+    }*/
+
     public static void isWrongValue() {
         throw new IllegalArgumentException("[ERROR] 잘못된 값을 입력했습니다. 다시 입력하세요.");
 
     }
+
+
 
 }
