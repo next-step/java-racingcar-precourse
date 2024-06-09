740dc9a19f86b5b5d1e5fae450a42bdac2f80da8
nove1080 <skwpqjq@gmail.com>
2024-06-09 15:21:40 +0900

    docs: RandomNumberGenerator getNumber 주석 수정

--- src/main/java/game/util/RandomNumberGenerator.java
+++ src/main/java/game/util/RandomNumberGenerator.java
@@ -8,9 +8,9 @@ public class RandomNumberGenerator {
     }
 
     /**
-     * 파라미터로 전달받은 범위 안에서 무작위 양의 정수를 반환합니다.
-     * @param maxRange 최대값
-     * @return 양의 정수
+     * 파라미터로 전달받은 범위 안에서 무작위 음이 아닌 정수를 반환합니다.
+     * @param maxRange 난수의 최댓값
+     * @return 음이 아닌 정수
      */
     public static int getNumber(int maxRange) {
         Random random = new Random();
