a8a779369e039c78a183f9f71d11013bbd58a2de
nove1080 <skwpqjq@gmail.com>
2024-06-10 01:12:12 +0900

    test: CarNameValidatorTest 예외 검증 메서드 변경
    - assertThrows -> assertThatThrownBy

--- src/test/java/game/validator/CarNameValidatorTest.java
+++ src/test/java/game/validator/CarNameValidatorTest.java
@@ -1,8 +1,7 @@
 package game.validator;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import game.exception.constant.ErrorMessage;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -19,18 +18,16 @@ class CarNameValidatorTest {
     @ParameterizedTest(name = "{0}은 부적절한 길이의 이름이 존재합니다")
     @ValueSource(strings = {"thiago,sil   ", "en      ,zo", ",,,,", "        "})
     void 부적절한_이름_길이는_예외를_발생시킵니다(String input) {
-        IllegalArgumentException error = assertThrows(
-            IllegalArgumentException.class, () -> CarNameValidator.validate(input));
-
-        assertThat(error.getMessage()).isEqualTo(ErrorMessage.INVALID_NAME_LENGTH.getMessage());
+        assertThatThrownBy(() -> CarNameValidator.validate(input))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ErrorMessage.INVALID_NAME_LENGTH.getMessage());
     }
 
     @ParameterizedTest(name = "{0}은 중복이 존재하는 이름 입력입니다")
     @ValueSource(strings = {"enzo,bruno,enzo", "james,james,james"})
     void 중복이_존재하는_이름은_예외를_발생시킵니다(String input) {
-        IllegalArgumentException error = assertThrows(
-            IllegalArgumentException.class, () -> CarNameValidator.validate(input));
-
-        assertThat(error.getMessage()).isEqualTo(ErrorMessage.DUPLICATE_NAME_FOUND.getMessage());
+        assertThatThrownBy(() -> CarNameValidator.validate(input))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage(ErrorMessage.DUPLICATE_NAME_FOUND.getMessage());
     }
 }
