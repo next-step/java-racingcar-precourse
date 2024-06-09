106693057119ba9b20e9f12d9b7421468ed0ea86
young-in <hardworkingini@gmail.com>
2024-06-10 06:07:06 +0900

    feat (RacingCameController.java) : 게임 시작 시, 몇 번 이동할 지 선택하는 method 구현

--- src/main/java/controller/RacingGameController.java
+++ src/main/java/controller/RacingGameController.java
@@ -1,6 +1,7 @@
 package controller;
 
 import domain.RacingCars;
+import domain.TryialNum;
 import util.CreatingCar;
 import view.InputView;
 import view.OutputView;
@@ -10,10 +11,18 @@ public class RacingGameController {
     private OutputView outputView = new OutputView();
     public void startGame(){
         RacingCars racingCars = createRacingCars();
+        TryialNum tryialNum = getTryCount();
     }
+
     private RacingCars createRacingCars(){
         outputView.printRequestCarNames();
         String[] carNames = inputView.inputCarNames();
         return CreatingCar.createCars(carNames);
+    }
+
+    private TryialNum getTryCount(){
+        outputView.printRequestTryCount();
+        String inputTryCount = inputView.inputTryCount();
+        return new TryialNum(inputTryCount);
     }
 }
