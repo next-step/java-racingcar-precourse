39e30e7be3f15212a6502b6f1330b210e8727704
박민규 <pmq0101@naver.com>
2024-06-09 14:13:08 +0900

    feat(CarRepository): default setting

---
+++ src/main/java/CarRepository.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CarRepository {
+    private static final Map<String, Car> carList = new ConcurrentHashMap<>();
+
+    public void saveCar(){
+
+    }
+
+    public void findByName(){
+
+    }
+}
