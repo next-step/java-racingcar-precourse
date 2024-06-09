153df8dea56b93d0fd066c571b97033e6af2319a
amm0124 <amm012450@gmail.com>
2024-06-09 18:04:26 +0900

    FIX : 0~9.99999 값을 0~9 사이의 정수로 수정

--- src/main/java/Controller/ForwardLogic.java
+++ src/main/java/Controller/ForwardLogic.java
@@ -4,7 +4,7 @@ import java.util.Random;
 public class ForwardLogic {
     public boolean forwarding() {
         Random random = new Random();
-        double a = random.nextDouble() * 10; // 0부터 10 사이의 무작위 수 생성
-        return a >= 4;
+        int randomNumber = random.nextInt(10); // 0부터 9 사이의 무작위 정수 생성
+        return randomNumber >= 4;
     }
 }
