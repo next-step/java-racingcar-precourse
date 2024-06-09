bf31d21219aeabda432ee6c24b3bec7fb1fce642
young-in <hardworkingini@gmail.com>
2024-06-10 05:50:56 +0900

    feat (OutputView.java) : racing game controller 구현

---
+++ src/main/java/controller/RacingGameController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import view.InputView;
+import view.OutputView;
+
+public class RacingGameController {
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+    public void startGame(){
+    }
+}
