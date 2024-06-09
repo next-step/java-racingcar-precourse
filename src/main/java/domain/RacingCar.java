3c543dd09a8554ebd0fe79ee046864bf4030136b
young-in <hardworkingini@gmail.com>
2024-06-10 05:32:33 +0900

    feat(RacingCar.java) : racingCar의 위치 나타내기 위한 distance 변수 추가

--- src/main/java/domain/RacingCar.java
+++ src/main/java/domain/RacingCar.java
@@ -2,9 +2,11 @@ package domain;
 
 public class RacingCar {
     private RacingCarName racingCarName;
+    private Distance distance;
 
-    public RacingCar(final String name) {
+    public RacingCar(final String name, final int distance) {
         this.racingCarName = new RacingCarName(name);
+        this.distance = new Distance(distance);
     }
 
     public RacingCarName getRacingCarName() {
