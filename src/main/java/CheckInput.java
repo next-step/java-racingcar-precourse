73b26ff82be9168ff954bc89278cf7423d8e9978
Shsin9797 <aaa8141@naver.com>
2024-06-09 20:19:49 +0900

    fix(CheckInput) : 에러처리 수정 -> 에러처리가 되었을때 프로그램이 멈추지 않고 ERROR 메시지가 출력되고 다시 입력을 받도록 & 에러 발생 경우를 더 세분화함

--- src/main/java/CheckInput.java
+++ src/main/java/CheckInput.java
@@ -1,33 +1,43 @@
+import java.util.HashSet;
 import java.util.IllegalFormatException;
 import java.util.List;
 
 public class CheckInput {
     public static boolean checkCarNames(List<String> carNames) {
+        if(!checkCarNameDiffer(carNames)){
+            return false;
+        }
         for (String carName : carNames) {
             int carLength = carName.length();
-            if ((carLength < 1) || (carLength > 5)) {
-                CheckInput.isWrongValue();
+            if(!checkCarNameLength(carName)){
                 return false;
-            }
+            };
         }
         return true;
     }
 
-   /* public static boolean checkInt(String intNum) {
-        try {
-            Integer.parseInt(intNum);
-            return true;
-        } catch (IllegalFormatException e) {
-            CheckInput.isWrongValue();
-            return false;
+    public static boolean checkCarNameDiffer(List<String> carNames) {
+        boolean check = true;
+        HashSet<String> setNames = new HashSet<>(carNames);
+        if (setNames.size() != carNames.size()) {
+            check=false;
+            throw new IllegalStateException("[ERROR] 자동차의 이름은 모두 다르게 입력해주세요.");
         }
-    }*/
-
-    public static void isWrongValue() {
-        throw new IllegalArgumentException("[ERROR] 잘못된 값을 입력했습니다. 다시 입력하세요.");
-
+        return check;
     }
 
+    public static boolean checkCarNameLength(String name) {
+        boolean result = true;
+        if ((name.length() < 1) || (name.length() > 5)) {
+            result=  false;
+            throw new IllegalArgumentException("[ERROR] 차 이름의 길이는 1글자~5글자 사이로 입력해주세요.");
+        }
+        return result;
+    }
 
-
+    public static void isWrongIntValue(int tryNum) {
+        if (tryNum < 1) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 값을 입력했습니다. 1이상의 정수를 다시 입력하세요.");
+        }
+    }
 }
