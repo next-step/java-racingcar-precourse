e04ee931f516e673f677b6fc1591d8cf3de17d44
박민규 <pmq0101@naver.com>
2024-06-09 14:12:50 +0900

    feat(Race): default setting

---
+++ src/main/java/Race.java
@@ -0,0 +1,25 @@
+public class Race {
+    private int raceNum;
+
+    public void startRace(){
+        registCar();
+        int raceNum = getRaceCount();
+
+    }
+    public int getRaceCount(){
+
+    }
+    private String[] getCar(){
+
+    }
+    public void registCar(){
+        String[] carList = getCar();
+        for(String car: carList){
+
+        }
+    }
+
+
+
+
+}
