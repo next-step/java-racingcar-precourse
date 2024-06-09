10269f19023611e5ee144274e86b462d27a72653
Yeongseo Kim <dudtjakdl@gmail.com>
2024-06-09 22:34:06 +0900

    feat: GameInterface 입력 기능 구현

---
+++ src/main/java/app/view/GameInterface.java
@@ -0,0 +1,17 @@
+package app.view;
+
+import java.util.Scanner;
+
+public class GameInterface {
+    private Scanner scanner = new Scanner(System.in);
+
+    public String getRacingCarNames() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return scanner.nextLine();
+    }
+
+    public int getAttemptCount() {
+        System.out.println("시도할 회수는 몇회인가요?");
+        return scanner.nextInt();
+    }
+}
