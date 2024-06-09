9a4d967b8027aed009559886405a424ddb3a01fa
nove1080 <skwpqjq@gmail.com>
2024-06-10 01:08:57 +0900

    refactor: ErrorMessage 이름 길이 예외 메시지 변수명 수정
    - EXCEED_NAME_LENGTH -> INVALID_NAME_LENGTH

--- src/main/java/game/exception/constant/ErrorMessage.java
+++ src/main/java/game/exception/constant/ErrorMessage.java
@@ -4,7 +4,7 @@ import static game.config.constant.Rule.*;
 
 public enum ErrorMessage {
 
-    EXCEED_NAME_LENGTH("[ERROR] 길이 %d를 초과하여 이름을 생성할 수 없습니다!".formatted(MAX_NAME_LENGTH)),
+    INVALID_NAME_LENGTH("[ERROR] %d ~ %d 글자를 벗어난 이름이 존재합니다!".formatted(MIN_NAME_LENGTH, MAX_NAME_LENGTH)),
     DUPLICATE_NAME_FOUND("[ERROR] 중복된 이름이 존재합니다!"),
     INVALID_NUMBER_INPUT("[ERROR] 숫자를 입력해주세요!"),
     INVALID_ATTEMPT_COUNT_INPUT("[ERROR] 1 이상의 숫자를 입력해주세요!"),
