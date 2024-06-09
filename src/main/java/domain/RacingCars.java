ab9a3f8f24049b518ad44dcf570d24d4db8ece72
young-in <hardworkingini@gmail.com>
2024-06-10 04:57:41 +0900

    feat(RacingCars.java) : 경주용 자동차 2개 이상 생성을 위한 예외 생성

--- src/main/java/domain/RacingCars.java
+++ src/main/java/domain/RacingCars.java
@@ -5,10 +5,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class RacingCars {
+    private static final int MINIMUM_CAR_COUNT = 2;
     private final List<RacingCar> cars;
 
     public RacingCars(List<RacingCar> cars) {
         isDifferentCarName(cars);
+        minimumNumofCars(cars.size());
         this.cars = cars;
     }
 
@@ -21,6 +23,12 @@ public class RacingCars {
 
         if (carNameDuplicateSet.size() != carName.size()) {
             throw new IllegalArgumentException("자동차 이름을 중복으로 사용할 수 없습니다.");
+        }
+    }
+
+    private void minimumNumofCars(int size) {
+        if (size < MINIMUM_CAR_COUNT) {
+            throw new IllegalArgumentException("자동차 경주를 위해서는 최소 2대 이상의 자동차를 만들어야 합니다.");
         }
     }
 }
