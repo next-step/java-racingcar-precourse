9656540d43481b008b9ac475260662be22f46af1
Jeongyeon Kim <wjddus9132@gmail.com>
2024-06-09 10:46:56 +0900

    feat(Input): 시도 횟수 입력
    - Input 클래스에 시도 횟수 입력
    - 시도 횟수는 숫자만 입력 가능
    - 시도 횟수는 1회 이상 입력 가능

--- src/main/java/camp/nextstep/edu/Input.java
+++ src/main/java/camp/nextstep/edu/Input.java
@@ -39,4 +39,37 @@ public class Input {
             throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하로 입력하시오.");
         }
     }
+
+    public int inputMoveCount() {
+        System.out.println("시도할 횟수는 몇 회인가요?");
+        while (true) {
+            try {
+                String moveCount = scanner.nextLine();
+                return validateMoveCount(moveCount);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private int validateMoveCount(String count) {
+        int moveCount = checkNumberFormat(count);
+        checkMoveCount(moveCount);
+        return moveCount;
+    }
+
+
+    private int checkNumberFormat(String count) {
+        try {
+            return Integer.parseInt(count);
+        } catch (NumberFormatException e) {
+            throw new IllegalStateException("[ERROR] 시도 횟수는 정수 형태로 입력하시오.");
+        }
+    }
+
+    private void checkMoveCount(int moveCount) {
+        if (moveCount < 1) {
+            throw new IllegalArgumentException("[ERROR] 시도 횟수는 1회 이상 입력하시오.");
+        }
+    }
 }
