9d4b85548f3502276bc1f6b4ca46117666ddd025
wvssm <neoramiregi@gmail.com>
2024-06-10 12:10:57 +0900

    feat: 자동차 이름 유효성 검사 및 입력 받기

---
+++ src/main/java/racingcar/controller/Controller.java
@@ -0,0 +1,24 @@
+package racingcar.controller;
+
+import racingcar.model.Validator;
+import racingcar.view.InputView;
+
+public class Controller {
+    private final Validator validator = new Validator();
+    private final InputView inputView = new InputView();
+
+    private String getCarNamesUserInput() {
+        String input = inputView.requestCarNamesMessage();
+        return checkValidationCarNames(input);
+    }
+
+    private String checkValidationCarNames(String input) {
+        try {
+            validator.checkCarsName(input);
+            return input;
+        } catch (IllegalArgumentException e) {
+            System.out.println("[ERROR] " + e.getMessage());
+            return getCarNamesUserInput();
+        }
+    }
+}
