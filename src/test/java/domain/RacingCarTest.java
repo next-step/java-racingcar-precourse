6471c068be4abb78f787734a6dfced93ae177a01
young-in <hardworkingini@gmail.com>
2024-06-10 10:47:35 +0900

    feat (RacingCarTest.java) : 랜덤값이 4미만이면 정지, 4이상이면 1 이동하는게 맞는지 확인하는 test

--- src/test/java/domain/RacingCarTest.java
+++ src/test/java/domain/RacingCarTest.java
@@ -1,37 +1,25 @@
 package domain;
 
-import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class RacingCarTest {
-    @DisplayName("자동차 이름을 중복으로 사용할 수 없습니다.")
-    @Test
-    void isDifferentCarName() throws Exception{
-        //given
-        String racingCarName = "yoni";
-        RacingCar car = new RacingCar(racingCarName, 1);
+    @DisplayName("자동차는 전진 또는 멈출 수 있다.")
+    @ParameterizedTest
+    @CsvSource({"4,1", "5,1", "6,1",
+            "0,0", "1,0", "3,0"})
+    void carMoveOrStopByRandomNumber(int givenPower, int curLocation) {
+        // given
+        int startLocation = 0;
+        RacingCar car = new RacingCar("ini", startLocation);
 
-        List<RacingCar> cars = List.of(car, car);
+        // when
+        car.move(givenPower);
 
-        //when //then
-        Assertions.assertThatThrownBy(() -> new RacingCars(cars))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차 이름을 중복으로 사용할 수 없습니다.");
-    }
-
-    @DisplayName("자동차 경주를 위해서는 최소 2대 이상의 자동차를 만들어야 합니다.")
-    @Test
-    void minimumCountOfCars() throws Exception{
-        //given
-        String racingCarName = "ini";
-        RacingCar car = new RacingCar(racingCarName, 1);
-
-        List<RacingCar> cars = List.of(car);
-
-        //when //then
-        Assertions.assertThatThrownBy(() -> new RacingCars(cars))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("자동차 경주를 위해서는 최소 2대 이상의 자동차를 만들어야 합니다.");
+        // then
+        assertThat(car.getDistance()).isEqualTo(curLocation);
     }
 }
---
+++ src/test/java/domain/RacingCarsTest.java
@@ -0,0 +1,37 @@
+package domain;
+
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+public class RacingCarsTest {
+    @DisplayName("자동차 이름을 중복으로 사용할 수 없습니다.")
+    @Test
+    void isDifferentCarName() throws Exception{
+        //given
+        String racingCarName = "yoni";
+        RacingCar car = new RacingCar(racingCarName, 1);
+
+        List<RacingCar> cars = List.of(car, car);
+
+        //when //then
+        Assertions.assertThatThrownBy(() -> new RacingCars(cars))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 이름을 중복으로 사용할 수 없습니다.");
+    }
+
+    @DisplayName("자동차 경주를 위해서는 최소 2대 이상의 자동차를 만들어야 합니다.")
+    @Test
+    void minimumCountOfCars() throws Exception{
+        //given
+        String racingCarName = "ini";
+        RacingCar car = new RacingCar(racingCarName, 1);
+
+        List<RacingCar> cars = List.of(car);
+
+        //when //then
+        Assertions.assertThatThrownBy(() -> new RacingCars(cars))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("자동차 경주를 위해서는 최소 2대 이상의 자동차를 만들어야 합니다.");
+    }
+}
