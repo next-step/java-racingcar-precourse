5ed0d6c1c0daf1a91580e5efdecf11d1c0bc6153
wvssm <neoramiregi@gmail.com>
2024-06-10 12:14:00 +0900

    feat: 경주 진행(입력 처리, 경주 시작, 우승자 출력)

--- src/main/java/racingcar/controller/Controller.java
+++ src/main/java/racingcar/controller/Controller.java
@@ -4,13 +4,38 @@ import racingcar.model.Game;
 import racingcar.model.Validator;
 import racingcar.utils.Parser;
 import racingcar.view.InputView;
+import racingcar.view.OutputView;
 
 public class Controller {
     private final Validator validator = new Validator();
     private final InputView inputView = new InputView();
 
+    public void run(){
+        play(createGame());
+    }
+
+    private Game createGame(){
+        Game game = new Game();
+        registerRaceCar(game);
+        setTrial(game);
+        return game;
+    }
+
+    private void play(Game game){
+        OutputView.printHead();
+        while(!game.isGameEnd()){
+            game.play();
+            OutputView.printScore(game.getRaceCars());
+        }
+        OutputView.printWinners(game.getWinners());
+    }
+
     private void registerRaceCar(Game game){
         game.setRaceCars(Parser.parseCarName(getCarNamesUserInput()));
+    }
+
+    private void setTrial(Game game){
+        game.setTrial(Parser.parseTrialNumber(getTrialNumberUserInput()));
     }
 
     private String getCarNamesUserInput() {
