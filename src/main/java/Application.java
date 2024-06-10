4eb77e563c631a04ca3d9de51f59356eff737a4f
sanghee0820 <lsh277604@gmail.com>
2024-06-10 11:37:42 +0900

    feat: Application.main() 추가

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,10 +1,11 @@
 import controller.RacingGameController;
+import view.InputView;
 
 public class Application {
 
     public static void main(String[] args) {
         RacingGameController racingGameController = new RacingGameController();
-
+        racingGameController.carInit();
         racingGameController.runGame();
     }
 }
