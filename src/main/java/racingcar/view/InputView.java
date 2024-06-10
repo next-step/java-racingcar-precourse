fde0098b1493b525ea290a02083b68e3824abf8f
wvssm <neoramiregi@gmail.com>
2024-06-10 12:04:20 +0900

    feat: 자동차 이름 입력

---
+++ src/main/java/racingcar/view/InputView.java
@@ -0,0 +1,20 @@
+package racingcar.view;
+
+import java.util.Scanner;
+
+public class InputView {
+    private final Scanner scanner;
+
+    public InputView() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String requestCarNamesMessage() {
+        System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+        return getUserInput();
+    }
+
+    private String getUserInput() {
+        return scanner.nextLine();
+    }
+}
