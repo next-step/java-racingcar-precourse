ad58285d4021b2a06850e1dffac65ff3bf3dd116
wvssm <neoramiregi@gmail.com>
2024-06-10 11:45:31 +0900

    add: Game 모델 생성

---
+++ src/main/java/racingcar/model/Game.java
@@ -0,0 +1,19 @@
+package racingcar.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    private final List<Car> raceCars;
+    private static int trial;
+    private int trialNumber = 0;
+
+    public Game(){
+        Game.trial = 0;
+        raceCars = new ArrayList<>();
+    }
+
+    public List<Car> getRaceCars(){
+        return raceCars;
+    }
+}
