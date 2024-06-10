f4be75ce654461ceec1ba30d7d686be65311c907
wvssm <neoramiregi@gmail.com>
2024-06-10 11:42:46 +0900

    feat: 자동차 전진 기능 추가

---
+++ src/main/java/racingcar/model/Car.java
@@ -0,0 +1,26 @@
+package racingcar.model;
+
+public class Car {
+    private String name;
+    private int position;
+    private static final int FORWARD_CONDITION = 4;
+
+    public Car (String name){
+        this.name = name;
+        this.position = 0;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getPosition(){
+        return  position;
+    }
+
+    public void move(int condition){
+        if(condition>=FORWARD_CONDITION){
+            this.position += 1;
+        }
+    }
+}
