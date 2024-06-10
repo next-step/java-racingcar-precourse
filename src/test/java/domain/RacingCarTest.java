801b9e5747d474f7309186691d41fb8cf3653c72
young-in <hardworkingini@gmail.com>
2024-06-10 11:01:24 +0900

    feat (RacingCarTest.java) : 우승자 확인 test code 작성 완료

--- src/test/java/domain/RacingCarTest.java
+++ src/test/java/domain/RacingCarTest.java
@@ -1,9 +1,13 @@
 package domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -15,7 +19,7 @@ public class RacingCarTest {
     void carMoveOrStopByRandomNumber(int givenPower, int curLocation) {
         // given
         int startLocation = 0;
-        RacingCar car = new RacingCar("ini", startLocation);
+        RacingCar car = createCar("ini", startLocation);
 
         // when
         car.move(givenPower);
@@ -26,13 +30,13 @@ public class RacingCarTest {
 
     @DisplayName("같은 위치의 자동차가 존재하는지 확인할 수 있다.")
     @ParameterizedTest
-    @CsvSource({"2,2,true","2,1,false"})
+    @CsvSource({"2,2,true", "2,1,false"})
     void isSameDistanceOfCar(int myCarLocation,
                              int competeCarLocation,
-                             boolean sameDistanceCheck) throws Exception{
+                             boolean sameDistanceCheck) throws Exception {
         //given
-        RacingCar myCar = new RacingCar("ini", myCarLocation);
-        RacingCar competeCar = new RacingCar("yoni", competeCarLocation);
+        RacingCar myCar = createCar("ini", myCarLocation);
+        RacingCar competeCar = createCar("yoni", competeCarLocation);
 
         //when
         boolean sameDistance = myCar.isSameDistance(competeCar);
@@ -43,13 +47,13 @@ public class RacingCarTest {
 
     @DisplayName("전진한 자동차들을 비교할 수 있다.")
     @Test
-    void compareDistanceToCar() throws Exception{
+    void compareDistanceToCar() throws Exception {
 
         //given
         String myCarName = "ini";
-        String competeCarName= "yoni";
-        RacingCar myCar = new RacingCar(myCarName, 4);
-        RacingCar competeCar = new RacingCar(competeCarName, 2);
+        String competeCarName = "yoni";
+        RacingCar myCar = createCar(myCarName, 4);
+        RacingCar competeCar = createCar(competeCarName, 2);
 
         //when
         int compare = myCar.compareTo(competeCar);
@@ -58,4 +62,24 @@ public class RacingCarTest {
         assertThat(compare).isGreaterThan(0);
     }
 
+    @DisplayName("자동차 경주 게임의 우승자를 확인할 수 있다.")
+    @Test
+    void findWinners() throws Exception {
+        //given
+        RacingCar myCar = createCar("ini", 3);
+        RacingCar competeCar1 = createCar("yoni", 2);
+        RacingCar competeCar2 = createCar("yuni", 3);
+        RacingCars cars = new RacingCars(List.of(myCar, competeCar1, competeCar2));
+
+        //when
+        List<String> winners = cars.findWinners();
+
+        //then
+        Assertions.assertThat(winners).hasSize(2)
+                .containsExactly("ini", "yuni");
+    }
+
+    private static RacingCar createCar(String carName, int location) {
+        return new RacingCar(carName, location);
+    }
 }
