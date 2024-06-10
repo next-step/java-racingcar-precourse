a8cf0fddc16ef4126f61be8daa01492cafb2198d
wvssm <neoramiregi@gmail.com>
2024-06-10 12:08:06 +0900

    feat: 우승자 출력

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -17,6 +17,16 @@ public class OutputView {
         System.out.println();
     }
 
+    public static void printWinners(List<Car> winners){
+        StringBuilder winnerNames = new StringBuilder("최종 우승자 : ");
+        for(Car car : winners){
+            winnerNames.append(car.getName()).append(", ");
+        }
+        int length = winnerNames.length();
+        String result = winnerNames.substring(0, length - 2);
+        System.out.print(result);
+    }
+
     public static void printCarScore(Car car){
         System.out.print(car.getName()+" : ");
         printCarPosition(car);
